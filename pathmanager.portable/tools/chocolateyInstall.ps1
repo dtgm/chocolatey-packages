c0c47c02d8eff425aa2e891d7f1a2b6a1020ae21
dtgm1594 <dtgm1594@gmail.com>
2013-12-14 15:37:08 -0600

    pathmanager-1.1.1

---
+++ pathmanager.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'pathmanager'
+$url  = 'http://sourceforge.net/projects/pathmanager/files/Path%20Manager/PathManager-1.1.1/PathManager-1.1.1.exe/download'
+$fileType = 'exe'
+ 
+try {
+	$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+	$portableFile = Join-Path $scriptPath "$($packageName).$($fileType)"
+	Get-ChocolateyWebFile "$packageName" "$portableFile" "$url"
+	
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw
+}
