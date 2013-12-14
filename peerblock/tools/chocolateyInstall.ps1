e1205be4a89c73619742e61a4abef5cfb8d2f965
dtgm1594 <dtgm1594@gmail.com>
2013-12-14 00:21:46 -0600

    initial commit peerblock.1.1.0.518.nupkg

---
+++ peerblock/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'peerblock'
+$installerType = 'EXE'
+$url  = 'http://peerblock.googlecode.com/files/PeerBlock-Setup_v1.1_r518.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
