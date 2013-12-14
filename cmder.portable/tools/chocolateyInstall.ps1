bf5c81606638e3f90a3c31c2e70203d0cf8fa332
dtgm1594 <dtgm1594@gmail.com>
2013-12-14 16:21:27 -0600

    initial commit cmder-1.1.1

---
+++ cmder.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'cmder.portable'
+$url = 'https://github.com/bliker/cmder/releases/download/v1.1.1/cmder.7z'
+
+try { 
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  
+  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+  
+  $tempDir = "$env:TEMP\chocolatey\$($packageName)"
+  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+  $file = Join-Path $tempDir "$($packageName).7z"
+  Get-ChocolateyWebFile "$packageName" "$file" "$url"
+
+  Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$file`"" -Wait
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
---
+++ cmdermini.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'cmdermini.portable'
+$url = 'https://github.com/bliker/cmder/releases/download/v1.1.1/cmder_mini.zip'
+
+try { 
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
