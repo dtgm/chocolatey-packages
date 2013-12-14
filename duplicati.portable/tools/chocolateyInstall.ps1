d4d4a3797544e594d960161612fa3b8fdf2bd5de
dtgm1594 <dtgm1594@gmail.com>
2013-12-13 21:31:43 -0600

    initial commit

---
+++ duplicati.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'duplicati.portable'
+$url  = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
