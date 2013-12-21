121d45b41d7505e632f4bdda90fe808a31171d54
dtgm <dtgm1594@gmail.com>
2013-12-20 23:17:30 -0600

    initial commit

---
+++ thebrain.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url  = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '-q -overwrite'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
