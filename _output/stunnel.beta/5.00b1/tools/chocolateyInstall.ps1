acaaa62ccc7b73f450f60c57fc0b069897136a27
dtgm <dtgm1594@gmail.com>
2013-12-28 19:38:44 -0600

    initial commit

---
+++ _output/stunnel.beta/5.00b1/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'stunnel.beta'
+$installerType = 'EXE'
+$url  = 'https://www.stunnel.org/downloads/beta/stunnel-5.00b1-installer.exe'
+$silentArgs = '/S'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
---
+++ _output/stunnel/4.56/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'stunnel'
+$installerType = 'EXE'
+$url  = 'ftp://ftp.stunnel.org/stunnel/stunnel-4.56-installer.exe'
+$silentArgs = '/S'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
