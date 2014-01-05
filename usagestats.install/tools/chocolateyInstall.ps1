6bc5a249116276d98630d85505dc5a05832f964d
dtgm <dtgm1594@gmail.com>
2014-01-05 09:54:24 -0600

    initial up

---
+++ usagestats.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'usagestats'
+$installerType = 'msi'
+$silentArgs = '/qb'
+$url  = 'https://usagestats.codeplex.com/downloads/get/388694'
+
+Install-ChocolateyPackage -packageName "$packageName" -fileType "$installerType" -url "$url" -silentArgs "$silentArgs" -validExitCodes @(0)
---
+++ usagestats.install/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'usagestats'
+$msiPath = Join-Path $Env:TEMP ('chocolatey\' + $packageName + '\' + $packageName + 'Install.msi')
+$statements = '/x "' + $msiPath + '" /quiet'
+Start-ChocolateyProcessAsAdmin $statements "msiexec"
