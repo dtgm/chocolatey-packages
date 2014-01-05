b8525929f5cef2f519f7924ae6dc6bcc8e9889e6
dtgm <dtgm1594@gmail.com>
2014-01-05 10:45:07 -0600

    remove usagestats.install as dep

--- usagestats/tools/chocolateyInstall.ps1
+++ usagestats/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
-﻿#Install-VirtualPackage 'usagestats.install'
+﻿$packageName = 'usagestats'
+$installerType = 'msi'
+$silentArgs = '/qb'
+$url  = 'https://usagestats.codeplex.com/downloads/get/388694'
+
+Install-ChocolateyPackage -packageName "$packageName" -fileType "$installerType" -url "$url" -silentArgs "$silentArgs" -validExitCodes @(0)
