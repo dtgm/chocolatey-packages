bc705b7053d97f2f5ebc8a45580b031db746a222
dtgm <dtgm1594@gmail.com>
2014-01-06 18:39:03 -0600

    initial up

---
+++ wfn/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'wfn'
+$url  = 'http://wfn.codeplex.com/downloads/get/752943'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
---
+++ wfn/tools/chocolateyUninstall.ps1
@@ -0,0 +1,1 @@
+﻿UnInstall-ChocolateyZipPackage wfn 'wfnInstall.zip'
