f773da185f07abeb5ca180a33a557aa725c43a9a
Jamie Cook <jamie.cook@veitchlister.com.au>
2014-01-08 21:27:30 +1000

    upgrade to 1.1.3

--- cmdermini.portable/tools/chocolateyInstall.ps1
+++ cmdermini.portable/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'cmdermini.portable'
-$url = 'https://github.com/bliker/cmder/releases/download/v1.1.1/cmder_mini.zip'
+$url = 'https://github.com/bliker/cmder/releases/download/v1.1.3/cmder_mini.zip'
 
-try { 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
   Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
 
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+  throw
 }
