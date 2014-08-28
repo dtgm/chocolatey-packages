912d18a505e8ed3df262f03332912dc9a40cfee7
dtgm <dtgm1594@gmail.com>
2014-08-27 21:06:28 -0500

    Added cyberduck 4.5.2 generated _output content

---
+++ automatic/_output/cyberduck.install/4.5.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'cyberduck.install'
+$installerType = 'EXE'
+$url  = '{preupdate-url}'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
