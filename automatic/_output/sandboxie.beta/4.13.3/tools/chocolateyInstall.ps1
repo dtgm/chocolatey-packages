93a3275dd51d4a5c4122aad13fa3850b2f7eeaa1
dtgm <dtgm1594@gmail.com>
2014-08-28 01:37:47 -0500

    Updated _output files

---
+++ automatic/_output/sandboxie.beta/4.13.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'sandboxie.beta'
+$installerType = 'EXE'
+$url  = 'http://www.sandboxie.com/SandboxieInstall32-413-3.exe'
+$url64 = 'http://www.sandboxie.com/SandboxieInstall64-413-3.exe'
+$silentArgs = '/install /S /D=C:\Program Files\Sandboxie'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
