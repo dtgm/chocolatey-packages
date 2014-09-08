95dac299f9559bd0b9797252983a9cc5fb888451
dtgm <dtgm1594@gmail.com>
2014-09-08 01:27:11 -0500

    cmder uninstall scripts

---
+++ automatic/cmder/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿$binRoot = Get-BinRoot
+$installDir = Join-Path $binRoot "cmder"
+
+try { 
+	if (Test-Path $installDir){
+		try {
+			Chocolatey-Uninstall cmder.portable
+			Write-ChocolateySuccess "cmder.portable"
+		} catch {
+			Write-ChocolateyFailure "cmder.portable" "$($_.Exception.Message)"
+			throw
+		}
+	}
+	Write-ChocolateySuccess "cmder"
+} catch {
+  Write-ChocolateyFailure "cmder" "$($_.Exception.Message)"
+  throw 
+}
---
+++ automatic/cmdermini.portable/tools/chocolateyUninstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = '{{PackageName}}'
+$packageVersion = '{{PackageVersion}}'
+$binRoot = Get-BinRoot
+$installDir = Join-Path $binRoot "cmdermini"
+
+try { 
+	if (Test-Path $installDir){
+		Remove-Item -Recurse -Force $installDir
+	}
+	if (Test-Path -Path "$installDir") {
+		Remove-Item -Path -Force "$installDir"
+	}
+	Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
+
+if (Test-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\..\..\cmder.$packageVersion") {
+	Chocolatey-Uninstall cmder
+}
