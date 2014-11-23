5c06b91a1b537b33aecd72dbe32e7379487a3d27
dtgm <dtgm1594@gmail.com>
2014-11-23 01:05:36 -0600

    Update peerblock to automatic package

---
+++ automatic/_output/peerblock/1.2.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'peerblock'
+$installerType = 'exe'
+$url = 'https://peerblock.googlecode.com/files/PeerBlock-Setup_v1.2_r693.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ automatic/peerblock/tools/chocolateyUninstall.ps1
@@ -0,0 +1,19 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+	$fileType = 'exe'
+	$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+	$validExitCodes = @(0)
+
+	$processor = Get-WmiObject Win32_Processor
+	$is64bit = $processor.AddressWidth -eq 64
+	if ($is64bit) {
+		$unFile = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName*"}).UninstallString
+	} else {
+		$unFile = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName*"}).UninstallString
+	}
+	Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file -validExitCodes $validExitCodes
+	Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
