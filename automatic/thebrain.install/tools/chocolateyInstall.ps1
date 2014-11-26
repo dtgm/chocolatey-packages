27e58da027c5463b904f9045125c3a7f30b13568
dtgm <dtgm1594@gmail.com>
2014-11-26 16:06:06 -0600

    remove indentation of multi-line tags such as <description> and <releaseNotes> to prevent being parsed as Markdown per https://github.com/chocolatey/chocolatey/wiki/CreatePackages#package-description-and-release-notes

--- automatic/thebrain.install/tools/chocolateyInstall.ps1
+++ automatic/thebrain.install/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
+$installerType = 'exe'
 $url  = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '-q -overwrite'
+$validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
--- automatic/winsshd.install/tools/chocolateyInstall.ps1
+++ automatic/winsshd.install/tools/chocolateyInstall.ps1
@@ -3,4 +3,17 @@ $installerType = 'EXE'
 $url  = '{{DownloadUrl}}'
 $silentArgs = '-acceptEULA -startServer -startBssCtrl'
 
+Write-Verbose "You may pass native install arguments directly to Chocolatey with -ia -installArgs or -installArguments"
+Write-Verbose "			For example: cinst -installArgs '-installDir=''C:\foo bar\'' winsshd"
+Write-Verbose "			Note: Use two single quotes when double quotes are desired."
+Write-Verbose "BvSshServer-Inst -installDir=directory OR -defaultSite OR -site=site-name"
+Write-Verbose "                  [-force OR -abortOnWarning[=warning-list-or-mask]"
+Write-Verbose "                  [-acceptEULA] [-interactive] [-noRollback]"
+Write-Verbose "                  [-activationCode=activation-code-hex]"
+Write-Verbose "                  [-keypairs=keypairs-file]"
+Write-Verbose "                  [-settings=settings-file]"
+Write-Verbose "                  [-siteTypeSettings=fileName]"
+Write-Verbose "                  [-startService]"
+Write-Verbose "                  [-startBssCtrl]"
+
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
