978bec6f055c78073cff6dd96ce16f97e68adfc1
dtgm <dtgm1594@gmail.com>
2014-12-01 23:32:03 -0600

    update, uninstall added

---
+++ automatic/stunnel/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = '{{PackageName}}'
+$version = '{{PackageVersion}}'
+$url = '{{DownloadUrl}}'
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
+	Write-ChocolateySuccess $packageName	
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+	throw 
+}
