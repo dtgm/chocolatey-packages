532a0ea96ba12ef4ed6d70801936dce058f6ce83
dtgm <dtgm1594@gmail.com>
2014-12-02 02:07:56 -0600

    new packages

---
+++ automatic/_output/gsmartcontrol/0.8.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = 'gsmartcontrol'
+	$installerType = 'exe'
+	$url = 'http://hivelocity.dl.sourceforge.net/project/gsmartcontrol/0.8.7/gsmartcontrol-0.8.7.exe'
+	$silentArgs = '/S'
+	$validExitCodes = @(0)
+
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
+  Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
---
+++ automatic/_output/smartmontools/6.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = 'smartmontools'
+	$installerType = 'exe'
+	$url = 'http://softlayer-dal.dl.sourceforge.net/project/smartmontools/smartmontools/6.3/smartmontools-6.3-1.win32-setup.exe'
+	$silentArgs = '/S'
+	$validExitCodes = @(0)
+
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
+  Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
