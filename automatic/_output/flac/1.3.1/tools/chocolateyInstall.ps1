f5e747aa09fc8236c0f08577bf3f5be5a0148ea7
dtgm <dtgm1594@gmail.com>
2014-12-05 06:03:35 -0600

    new package, install/uninstall, icon

---
+++ automatic/_output/flac/1.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = 'flac'
+  $binRoot = Get-BinRoot
+  $instDir = Join-Path $binRoot $packageName
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = @('http://sourceforge.net/projects/flac/files/flac-win/flac-1.3.1-win/flac-1.3.1-win.zip/download', 'http://sourceforge.net/projects/flac/files/flac-win/flac-1.3.1-win/flac-1.3.1-win.zip/download')
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
+  Install-ChocolateyZipPackage "$packageName" "$url" "$instDir" "$url" 
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
---
+++ automatic/flac/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+  $binRoot = Get-BinRoot
+  $instDir = Join-Path $binRoot $packageName
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = {{DownloadUrlx64}}
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
+  Install-ChocolateyZipPackage "$packageName" "$url" "$instDir" "$url" 
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
