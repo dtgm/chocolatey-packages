f9c3d03b10a6e99f3fdc4c4af9d9b80ab43b0639
dtgm <dtgm1594@gmail.com>
2014-12-05 01:49:53 -0600

    new package

---
+++ automatic/_output/pdfedit/2014.0526.1531/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿try {
+	$packageName = 'pdfedit'
+	$installerType = 'msi'
+	$partialInstallArgs = '/quiet /qn /norestart TARGETDIR='
+	$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+	if ($is64bit) {$installPath = ${Env:ProgramFiles(x86)}} else {$installPath = $Env:ProgramFiles}
+	$installArgs = $($partialInstallArgs + '"' + $installPath + '"')
+	# due to limitations of Ketarin/chocopkgup when using repos like sf.net:
+	# 1. url is partly hardcoded and 2. DownloadUrlx64 is used to pass non-compatible nuget version number
+	$url = 'http://sourceforge.net/projects/pdfedit/files/pdfedit-win32/pdfedit-20140526_1531.msi/download'
+	$validExitCodes = @(0)
+	Install-ChocolateyPackage "$packageName" "$installerType" "$installArgs" "$url" -validExitCodes "$validExitCodes"
+  Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
