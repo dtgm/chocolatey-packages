56a7c818780aa44ea4b9faedb6bf7393723f0e10
dtgm <dtgm1594@gmail.com>
2014-12-04 21:18:01 -0600

    sf.net download url points to general url not specific mirror

--- automatic/_output/smartmontools/6.3.1/tools/chocolateyInstall.ps1
+++ automatic/_output/smartmontools/6.3.1/tools/chocolateyInstall.ps1
@@ -1,8 +1,11 @@
 ﻿try {
 	$packageName = 'smartmontools'
-	$installerType = 'exe'
-	$url = 'http://softlayer-dal.dl.sourceforge.net/project/smartmontools/smartmontools/6.3/smartmontools-6.3-1.win32-setup.exe'
+	$installerType = 'exe'	
 	$silentArgs = '/S'
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = '@('http://sourceforge.net/projects/smartmontools/files/smartmontools/6.3/smartmontools-6.3-1.win32-setup.exe/download', 'http://sourceforge.net/projects/smartmontools/files/smartmontools/6.3/smartmontools-6.3-1.win64-setup.exe/download')'
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
 	$validExitCodes = @(0)
 
 	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
