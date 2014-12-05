33596b879a9e599bf38bbfc1ec77897436e50ce8
dtgm <dtgm1594@gmail.com>
2014-12-04 21:18:01 -0600

    sf.net download url points to general url not specific mirror

--- automatic/_output/gsmartcontrol/0.8.7/tools/chocolateyInstall.ps1
+++ automatic/_output/gsmartcontrol/0.8.7/tools/chocolateyInstall.ps1
@@ -1,8 +1,11 @@
 ﻿try {
 	$packageName = 'gsmartcontrol'
 	$installerType = 'exe'
-	$url = 'http://hivelocity.dl.sourceforge.net/project/gsmartcontrol/0.8.7/gsmartcontrol-0.8.7.exe'
 	$silentArgs = '/S'
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = @('http://sourceforge.net/projects/gsmartcontrol/files/0.8.7/gsmartcontrol-0.8.7.exe/download', 'http://sourceforge.net/projects/gsmartcontrol/files/0.8.7/gsmartcontrol-0.8.7.exe/download')
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
 	$validExitCodes = @(0)
 
 	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
--- automatic/gsmartcontrol/tools/chocolateyInstall.ps1
+++ automatic/gsmartcontrol/tools/chocolateyInstall.ps1
@@ -1,8 +1,11 @@
 ﻿try {
 	$packageName = '{{PackageName}}'
 	$installerType = 'exe'
-	$url = '{{DownloadUrl}}'
 	$silentArgs = '/S'
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = {{DownloadUrlx64}}
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
 	$validExitCodes = @(0)
 
 	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
