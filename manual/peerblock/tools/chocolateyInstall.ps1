19f140ae0f8668941e311451020bd9cdd1622d1f
dtgm <dtgm1594@gmail.com>
2014-12-05 06:14:09 -0600

    deprecated, converted to automatic

--- manual/peerblock/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName = 'peerblock'
-$installerType = 'EXE'
-$url  = 'http://peerblock.googlecode.com/files/PeerBlock-Setup_v1.1_r518.exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
--- manual/stunnel/tools/chocolateyInstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$packageName = 'stunnel'
-$installerType = 'EXE'
-$url  = 'ftp://ftp.stunnel.org/stunnel/stunnel-4.56-installer.exe'
-$silentArgs = '/S'
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
