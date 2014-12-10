005f3edaf6ed760a8e4bc0a2f54ca0ee52478e33
dtgm <dtgm1594@gmail.com>
2014-12-10 07:34:45 -0600

    https://github.com/dtgm/chocolatey-packages/issues/4

--- automatic/_output/flac/1.3.1/tools/chocolateyInstall.ps1
+++ automatic/_output/flac/1.3.1/tools/chocolateyInstall.ps1
@@ -1,12 +1,17 @@
 ﻿try {
 	$packageName = 'flac'
-  $binRoot = Get-BinRoot
-  $instDir = Join-Path $binRoot $packageName
+	$packageVersion = '1.3.1'
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFolder = "$packageName-$packageVersion-win"
 	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
-	$urlArray = @('http://sourceforge.net/projects/flac/files/flac-win/flac-1.3.1-win/flac-1.3.1-win.zip/download', 'http://sourceforge.net/projects/flac/files/flac-win/flac-1.3.1-win/flac-1.3.1-win.zip/download')
-	$url = $urlArray[0]
-	$url64 = $urlArray[1]
-  Install-ChocolateyZipPackage "$packageName" "$url" "$instDir" "$url" 
+	$url = http://tcpdiag.dl.sourceforge.net/project/flac/flac-win/flac-1.3.1-win/flac-1.3.1-win.zip
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+	$osBitness = Get-ProcessorBits
+	if ($osBitness -eq 64) {
+		Remove-Item -Recurse (Join-Path "$unzipLoc" (Join-Path "$installFolder" 'bin32'))
+	} else {
+		Remove-Item -Recurse (Join-Path "$unzipLoc" (Join-Path "$installFolder" 'bin64'))
+	}
 } catch {
   Write-ChocolateyFailure $packageName $($_.Exception.Message)
   throw
--- automatic/flac/tools/chocolateyInstall.ps1
+++ automatic/flac/tools/chocolateyInstall.ps1
@@ -1,12 +1,17 @@
 ﻿try {
 	$packageName = '{{PackageName}}'
-  $binRoot = Get-BinRoot
-  $instDir = Join-Path $binRoot $packageName
+	$packageVersion = '{{PackageVersion}}'
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFolder = "$packageName-$packageVersion-win"
 	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
-	$urlArray = {{DownloadUrlx64}}
-	$url = $urlArray[0]
-	$url64 = $urlArray[1]
-  Install-ChocolateyZipPackage "$packageName" "$url" "$instDir" "$url" 
+	$url = {{DownloadUrl}}
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+	$osBitness = Get-ProcessorBits
+	if ($osBitness -eq 64) {
+		Remove-Item -Recurse (Join-Path "$unzipLoc" (Join-Path "$installFolder" 'bin32'))
+	} else {
+		Remove-Item -Recurse (Join-Path "$unzipLoc" (Join-Path "$installFolder" 'bin64'))
+	}
 } catch {
   Write-ChocolateyFailure $packageName $($_.Exception.Message)
   throw
