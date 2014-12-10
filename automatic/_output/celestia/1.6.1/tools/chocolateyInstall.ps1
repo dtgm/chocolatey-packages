6449e83d313ee0f94a6c4ed95d3f3d9d9d933ada
dtgm <dtgm1594@gmail.com>
2014-12-10 07:42:31 -0600

    sed -i 's/, //g' *.nuspec

--- automatic/_output/celestia/1.6.1/tools/chocolateyInstall.ps1
+++ automatic/_output/celestia/1.6.1/tools/chocolateyInstall.ps1
@@ -5,7 +5,6 @@
 	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
 	$urlArray = @('http://sourceforge.net/projects/celestia/files/Celestia-win32-bin/1.6.1/celestia-win32-1.6.1.exe/download', 'http://sourceforge.net/projects/celestia/files/Celestia-win32-bin/1.6.1/celestia-win32-1.6.1.exe/download')
 	$url = $urlArray[0]
-	$url64 = $urlArray[1]
 	$validExitCodes = @(0)
 	
 	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
--- automatic/_output/gsmartcontrol/0.8.7/tools/chocolateyInstall.ps1
+++ automatic/_output/gsmartcontrol/0.8.7/tools/chocolateyInstall.ps1
@@ -5,7 +5,6 @@
 	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
 	$urlArray = @('http://sourceforge.net/projects/gsmartcontrol/files/0.8.7/gsmartcontrol-0.8.7.exe/download', 'http://sourceforge.net/projects/gsmartcontrol/files/0.8.7/gsmartcontrol-0.8.7.exe/download')
 	$url = $urlArray[0]
-	$url64 = $urlArray[1]
 	$validExitCodes = @(0)
 
 	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
