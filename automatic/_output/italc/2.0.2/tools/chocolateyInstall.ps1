9b19eac8d2c6c9b9558f603a36a1bc06fda9d63d
dtgm <dtgm1594@gmail.com>
2014-12-10 10:35:59 -0600

    init, install/uninstall scripts, icon

---
+++ automatic/_output/italc/2.0.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = 'italc'
+	$version = '2.0.2'
+	$installerType = 'exe'
+	$silentArgs = '/S'
+	$url = 'https://github.com/iTALC/italc/releases/download/v2.0.2/italc-2.0.2-win32-setup.exe'
+	$url64 = 'https://github.com/iTALC/italc/releases/download/v2.0.2/italc-2.0.2-win64-setup.exe'
+	$validExitCodes = @(0)
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes "$validExitCodes"
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+	throw 
+}
---
+++ automatic/italc/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+	$version = '{{PackageVersion}}'
+	$installerType = 'exe'
+	$silentArgs = '/S'
+	$url = '{{DownloadUrl}}'
+	$url64 = '{{DownloadUrlx64}}'
+	$validExitCodes = @(0)
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes "$validExitCodes"
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+	throw 
+}
