cb977b291c47f0311eabce51000ddcc31d7ae76e
dtgm <dtgm1594@gmail.com>
2014-12-11 11:17:16 -0600

    init

---
+++ automatic/_output/mediainfo/0.7.71/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = 'mediainfo'
+	$installerType = 'exe'
+	# misuse 64-bit variable in auto pkg to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$url = 'http://sourceforge.net/projects/mediainfo/files/binary/mediainfo-gui/0.7.71/MediaInfo_GUI_0.7.71_Windows.exe/download'
+	$silentArgs = '/S'
+	$validExitCodes = @(0)
+	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes "$validExitCodes"
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
---
+++ automatic/_output/mediainfo/0.7.71/tools/chocolateyUninstall.ps1
@@ -0,0 +1,29 @@
+﻿try {
+	$packageName = 'mediainfo'
+	$fileType = 'exe'
+	$silentArgs = '/S'
+	$validExitCodes = @(0)
+	# find uninstaller
+	$unPathWow = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+	$unPath = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+	if (Test-Path $unPathWow\$packageName*) {
+		$unString = (Get-ItemProperty $unPathWow\$packageName* UninstallString).UninstallString
+	} elseif (Test-Path $unPath\$packageName*) {
+		$unString = (Get-ItemProperty $unPath\$packageName* UninstallString).UninstallString
+	} else {
+		Write-Warning "$packageName is not installed or installer may have changed it's install path."
+		Write-Warning "Please alert package maintainer."
+		throw
+	}
+	# if uninstall string contains a parameter, remove it
+	if ($unString | select-string -pattern /) {
+		$unProg = $unString | %{ $_.Split(' /')[0]; }
+	} else {
+		$unProg = $unString
+  }
+	Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unProg" -validExitCodes "$validExitCodes"
+  Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
