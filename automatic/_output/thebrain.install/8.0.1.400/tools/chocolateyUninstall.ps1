18da8fefa866762dd0281b48d91da1224a6f6adc
dtgm <dtgm1594@gmail.com>
2014-12-13 20:28:06 -0600

    only remove current version of associated meta-package

--- automatic/_output/thebrain.install/8.0.1.400/tools/chocolateyUninstall.ps1
+++ automatic/_output/thebrain.install/8.0.1.400/tools/chocolateyUninstall.ps1
@@ -1,22 +1,29 @@
 ﻿try {
 	$packageName = 'thebrain.install'
-	$packageMeta = 'thebrain'
+	$packageVersion = '8.0.1.400'
 	$fileType = 'exe'
 	$silentArgs = '-q'
 	$validExitCodes = @(0)
+	
+	# remove associated metapackage
+	$packageMeta = 'thebrain'
 	$chocoRoot = $Env:ChocolateyInstall
 	if ($chocoRoot -eq $null) {
 		$chocoRoot = "$Env:ProgramData\chocolatey"
 	}
 	$chocoLib = Join-Path $chocoRoot 'lib'
-	if (Test-Path $chocoLib\$packageMeta.[0-9]*) { 
-		Remove-Item "$chocoLib\$packageMeta.[0-9]*" -Force -Recurse -ErrorAction SilentlyContinue
-		Write-Debug "Deleted $chocoLib\$packageMeta.*"
+	if (Test-Path $chocoLib\$packageMeta.$packageVersion) { 
+		Remove-Item "$chocoLib\$packageMeta.$packageVersion" -Force -Recurse -ErrorAction SilentlyContinue
+		Write-Debug "Deleted $chocoLib\$packageMeta.$packageVersion"
 	}
+	
+	# find uninstaller
 	$unFile = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageMeta*"}).UninstallString
 	if ($unFile -eq $null) {
 		$unFile = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageMeta*"}).UninstallString
 	}
+	
+	# uninstall package
 	Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unFile" -validExitCodes "$validExitCodes"
 	Write-ChocolateySuccess $packageName
 } catch {
