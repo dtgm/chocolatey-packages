589fd905dbe91a949f8f5b47faaccf0f8ce1ef83
dtgm <dtgm1594@gmail.com>
2014-12-14 08:33:38 -0600

    init, install/uninstall scripts

---
+++ automatic/_output/ekeyfinder/0.1.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = 'ekeyfinder'
+	$packageVersion = '0.1.7'
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFolder = "$packageName-$packageVersion-win"
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$url = 'http://sourceforge.net/projects/ekeyfinder/files/v0.1%20Beta/EKeyfinder_v0.1beta7_win32.zip/download'
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
---
+++ automatic/ekeyfinder/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+	$packageVersion = '{{PackageVersion}}'
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFolder = "$packageName-$packageVersion-win"
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$url = '{{DownloadUrlx64}}'
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
