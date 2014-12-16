9b07fc0da268cb3876a16bbf333f765e2d94ee17
dtgm <dtgm1594@gmail.com>
2014-12-16 00:42:49 -0600

    new package

---
+++ automatic/_output/blat/3.2.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = "blat"
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = @('http://sourceforge.net/projects/blat/files/Blat%20Full%20Version/32%20bit%20versions/Win2000%20and%20newer/blat323_32.full.zip/download', 'http://sourceforge.net/projects/blat/files/Blat%20Full%20Version/64%20bit%20versions/blat323_64.full.zip/download')
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc" "$url64"
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
---
+++ automatic/blat/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = "{{PackageName}}"
+  $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+	$urlArray = {{DownloadUrlx64}}
+	$url = $urlArray[0]
+	$url64 = $urlArray[1]
+  Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc" "$url64"
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
