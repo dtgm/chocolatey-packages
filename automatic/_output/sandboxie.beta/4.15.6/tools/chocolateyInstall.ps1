0e423380e1823a2c8e9bdb84ddcea060b87c75e8
dtgm <dtgm1594@gmail.com>
2014-12-18 20:19:57 -0600

    updates

---
+++ automatic/_output/sandboxie.beta/4.15.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'sandboxie.beta'
+$packageId = 'sandboxie'
+$installerType = 'EXE'
+$url  = 'http://www.sandboxie.com/SandboxieInstall32-415-6.exe'
+$url64 = 'http://www.sandboxie.com/SandboxieInstall64-415-6.exe'
+$silentArgsInstall = '/install /S /D=C:\Program Files\Sandboxie'
+$silentArgsUpgrade = '/upgrade /S'
+$chocoRoot = $env:ChocolateyInstall
+if ($chocoRoot -eq $null) {
+	$chocoRoot = "$env:programdata\chocolatey"
+}
+try {
+	if (Test-Path $chocoRoot\lib\$packageName.install.*) { 
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgsUpgrade" "$url" "$url64"
+	} else {
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgsInstall" "$url" "$url64"
+	}
+	Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
