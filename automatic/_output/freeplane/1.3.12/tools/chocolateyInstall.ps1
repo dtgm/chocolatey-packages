e31666e8ec725631baa78bd580994211bdc58171
dtgm <dtgm1594@gmail.com>
2014-12-19 04:38:33 -0600

    auto package created from http://chocolatey.org/api/v2/package/FreePlane/1.2.23 freeplane.nuspec: added iconurl, summary freeplane.nuspec: updated releasenotes, description, tags added uninstall script cleaned install script, added proper silent parameters

---
+++ automatic/_output/freeplane/1.3.12/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'freeplane'
+$installerType = 'exe'
+# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+$url = 'http://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-1.3.12.exe/download'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)  #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ automatic/freeplane/tools/chocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+	$fileType = 'exe'
+	$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP'
+	$validExitCodes = @(0)
+	$unString = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName"}).UninstallString
+	Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes "$validExitCodes"
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
