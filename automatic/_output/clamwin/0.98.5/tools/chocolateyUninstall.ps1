117b536b8037906cb1d6bd7471fed55b13bc5ff0
dtgm <dtgm1594@gmail.com>
2014-12-20 01:00:51 -0600

    upgrade

---
+++ automatic/_output/clamwin/0.98.5/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'clamwin'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+try {
+	if ($is64bit) {
+		$unpath = "${Env:ProgramFiles(x86)}\ClamWin\unins000.exe"
+	} else {
+		$unpath = "$Env:ProgramFiles\ClamWin\unins000.exe"
+	}
+	Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
