b6b3c682ecb9697906a5c4331ddaf19680419268
dtgm <dtgm1594@gmail.com>
2014-12-31 00:58:54 -0600

    new packages

---
+++ automatic/_output/jstock/1.0.7.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'jstock'
+$installerType = 'exe'	
+$silentArgs = '/S'
+$url = 'http://github.com/yccheok/jstock/releases/download/release_1-0-7u/jstock-1.0.7u-setup.exe'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ automatic/_output/pulse/0.1.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'pulse'
+$packageVersion = '0.1.4'
+$unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://download.ind.ie/pulse/latest/pulse-windows-386-0.1.4.zip'
+$url64 = 'http://download.ind.ie/pulse/latest/pulse-windows-amd64-0.1.4.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc" "$url64"
