f4970f7d3d270ee3cc17bcde1c1e17c97ae0073c
dtgm <dtgm1594@gmail.com>
2014-12-31 05:56:28 -0600

    update per moderation comments

--- automatic/_output/pulse/0.1.3/tools/chocolateyInstall.ps1
+++ automatic/_output/pulse/0.1.3/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'pulse'
 $packageVersion = '0.1.3'
 $unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://download.ind.ie/pulse/latest/pulse-windows-386-0.1.3.zip'
-$url64 = 'http://download.ind.ie/pulse/latest/pulse-windows-amd64-0.1.3.zip'
+$url = 'http://download.ind.ie/pulse/0.1.3/pulse-windows-386-0.1.3.zip'
+$url64 = 'http://download.ind.ie/pulse/0.1.3/pulse-windows-amd64-0.1.3.zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc" "$url64"
