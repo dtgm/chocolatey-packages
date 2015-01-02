1cd830f27418c68d624e57f71a72c806bc1caf62
dtgm <dtgm1594@gmail.com>
2015-01-02 14:22:19 -0600

    new packages

---
+++ automatic/_output/dd/0.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = "dd"
+$url = "http://www.chrysocome.net/downloads/dd-0.5.zip"
+$unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
---
+++ automatic/_output/doxbox/6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'doxbox'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/t-d-k/doxbox/releases/download/v6.0-beta/InstallDoxBox_v60Beta.exe'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
