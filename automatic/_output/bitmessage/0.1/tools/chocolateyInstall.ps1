7d3a75511de4ab25631080fb597435ad8db35d09
dtgm <dtgm1594@gmail.com>
2015-01-12 00:01:21 -0600

    updates per moderation comments

---
+++ automatic/_output/bitmessage/0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿try {
+	$packageName = 'bitmessage'
+	$packageVersion = '0.4.4'
+	$url = 'https://bitmessage.org/download/windows/Bitmessage.exe'
+	$chocTempDir = Join-Path $Env:Temp "chocolatey"
+	$pkgTempDir = Join-Path $chocTempDir "$packageName"
+	$tempFile = Join-Path $pkgTempDir "$($packageName).exe"
+	$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	if (![System.IO.Directory]::Exists($pkgTempDir)) {[System.IO.Directory]::CreateDirectory($pkgTempDir)}
+	Get-ChocolateyWebFile "$packageName" "$tempFile" "$url"
+	Copy-Item "$tempFile" "$installDir"
+	$exeFile = (Get-ChildItem "$installDir\bitmessage.exe" FullName).FullName
+	Remove-BinFile "$packageName" "$exeFile"
+	Generate-BinFile "$packageName" "$exeFile"
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
--- automatic/_output/bitmessage/0.4.4/tools/chocolateyInstall.ps1
+++ automatic/_output/bitmessage/0.4.4/tools/chocolateyInstall.ps1
@@ -2,16 +2,10 @@
 	$packageName = 'bitmessage'
 	$packageVersion = '0.4.4'
 	$url = 'https://bitmessage.org/download/windows/Bitmessage.exe'
-	$chocTempDir = Join-Path $Env:Temp "chocolatey"
-	$pkgTempDir = Join-Path $chocTempDir "$packageName"
-	$tempFile = Join-Path $pkgTempDir "$($packageName).exe"
 	$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-	if (![System.IO.Directory]::Exists($pkgTempDir)) {[System.IO.Directory]::CreateDirectory($pkgTempDir)}
-	Get-ChocolateyWebFile "$packageName" "$tempFile" "$url"
-	Copy-Item "$tempFile" "$installDir"
-	$exeFile = (Get-ChildItem "$installDir\bitmessage.exe" FullName).FullName
-	Remove-BinFile "$packageName" "$exeFile"
-	Generate-BinFile "$packageName" "$exeFile"
+	$installFile = Join-Path $installDir "$($packageName).exe"
+	Get-ChocolateyWebFile "$packageName" "$installFile" "$url"
+	Install-ChocolateyPath "$nodePath" 'Machine'  # Machine will assert admin control
 	Write-ChocolateySuccess "$packageName"
 } catch {
 	Write-ChocolateyFailure $packageName $($_.Exception.Message)
