d30341c78f7082a53111605bb5f3818cc96c72c0
dtgm <dtgm1594@gmail.com>
2015-01-25 04:42:07 -0600

    nuspec descriptions markdown

--- automatic/_output/peercoin/0.4.0/tools/chocolateyInstall.ps1
+++ automatic/_output/peercoin/0.4.0/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'peercoin'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = 'http://iweb.dl.sourceforge.net/project/ppcoin/0.4.0/ppcoin-0.4.0-win32-setup.exe'
+$url = 'http://hivelocity.dl.sourceforge.net/project/ppcoin/0.4.0/ppcoin-0.4.0-win32-setup.exe'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
--- automatic/mooncoin/tools/chocolateyInstall.ps1
+++ automatic/mooncoin/tools/chocolateyInstall.ps1
@@ -1,6 +1,17 @@
 $packageName = '{{PackageName}}'
-$installerType = 'exe'
-$silentArgs = '/S'
+$packageVersion = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$url64 = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -url64bit "$url64" -specificFolder "" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
+try {
+	Set-Content -Path ("$toolsDir\$packageName-$packageVersion.exe.gui") -Value $nul
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
