9b448821a9563e59d43961f1892adb995b9dee76
dtgm <dtgm1594@gmail.com>
2015-01-28 08:35:12 -0600

    new package

---
+++ automatic/_output/sre/1.3.1.12/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = 'sre'
+	$packageVersion = '1.3.1.12'
+	$url = 'http://sourceforge.net/projects/sre/files/SRE%20%201.3.1.12/Small%20Registry%20Editor1.3.1.12.exe/download'
+	$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFile = Join-Path $toolsDir "sre.exe"
+	Get-ChocolateyWebFile "$packageName" "$installFile" "$url"
+	Set-Content -Path ("$toolsDir\sre.exe.gui") -Value $nul
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
---
+++ automatic/sre/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+	$packageName = '{{PackageName}}'
+	$packageVersion = '{{PackageVersion}}'
+	$url = '{{DownloadUrlx64}}'
+	$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$installFile = Join-Path $toolsDir "sre.exe"
+	Get-ChocolateyWebFile "$packageName" "$installFile" "$url"
+	Set-Content -Path ("$toolsDir\sre.exe.gui") -Value $nul
+	Write-ChocolateySuccess "$packageName"
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw
+}
