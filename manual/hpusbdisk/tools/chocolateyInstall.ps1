d937e79fea06cfc2e676672f55f27ef2c55bc45a
dtgm <dtgm1594@gmail.com>
2015-01-28 03:15:19 -0600

    ^ cleaned up description - removed redundant uninstall + include exe with package + add copyright + perform sha1 checksum before installing

---
+++ manual/hpusbdisk/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+	$packageName = 'hpusbdisk'
+	$checksum = '280298bcc4f6d2547babf36d8126cdef44fe4298'
+	$checksumType = 'sha1'
+  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $nodePath = Join-Path $scriptPath 'HPUSBDisk.exe'
+  Get-CheckSumValid -file $nodePath -checkSum $checksum -checksumType $checksumType
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
