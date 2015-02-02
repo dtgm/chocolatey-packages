30f0e92e2d40edd5cc7df14516eb792e15a1bd52
dtgm <dtgm1594@gmail.com>
2015-02-02 12:42:21 -0600

    package handover

---
+++ automatic/_output/stellarium/0.13.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'stellarium'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+$urlArray = @('http://sourceforge.net/projects/stellarium/files/Stellarium-win32/0.13.2/stellarium-0.13.2-win32.exe/download', 'http://sourceforge.net/projects/stellarium/files/Stellarium-win64/0.13.2/stellarium-0.13.2-win64.exe/download')
+$url = $urlArray[0]
+$url64 = $urlArray[1]
+$validExitCodes = @(0)
+#$checksum = 'x'
+#$checksumType = 'sha1'
+#$checksum64 = 'x'
+#$checksumType64 = 'sha1'
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
---
+++ automatic/stellarium/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+# hack to overcome limitations of Ketarin/chocopkgup when using repos like sf.net
+$urlArray = {{DownloadUrlx64}}
+$url = $urlArray[0]
+$url64 = $urlArray[1]
+$validExitCodes = @(0)
+#$checksum = '{{Checksum}}'
+#$checksumType = 'sha1'
+#$checksum64 = '{{Checksumx64}}'
+#$checksumType64 = 'sha1'
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
