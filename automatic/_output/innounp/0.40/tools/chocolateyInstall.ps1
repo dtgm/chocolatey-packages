a5862acfb91e49c2dac3c15ea106d03cc4f862ff
dtgm <dtgm1594@gmail.com>
2015-02-03 03:40:57 -0600

    new package

--- automatic/_output/innounp/0.40/tools/chocolateyInstall.ps1
+++ automatic/_output/innounp/0.40/tools/chocolateyInstall.ps1
@@ -1,4 +1,21 @@
-﻿$packageName = "innounp"
-$url = "http://sourceforge.net/projects/innounp/files/innounp/innounp 0.40/innounp040.rar/download"
-$unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+﻿try {
+	$packageName = "innounp"
+	$url = "http://sourceforge.net/projects/innounp/files/innounp/innounp 0.40/innounp040.rar/download"
+	$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$rarFile = "$toolsDir\innounp.rar"
+  Get-ChocolateyWebFile "$packageName" "$rarFile" "$url"
+	if (Test-Path "${Env:ProgramFiles(x86)}\7-zip") {
+		$cmd7z = "${Env:ProgramFiles(x86)}\7-zip\7z.exe"
+	}	elseif (Test-Path "$Env:ProgramFiles\7-zip") {
+		$cmd7z = "$Env:ProgramFiles\7-zip\7z.exe"
+	} else {
+		Write-Warning "7-zip is not installed.  Please install 7-zip."
+		throw
+	}
+	Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$toolsDir" -ArgumentList "e $rarFile"
+	Remove-Item "$rarFile"
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
--- automatic/innounp/tools/chocolateyInstall.ps1
+++ automatic/innounp/tools/chocolateyInstall.ps1
@@ -1,4 +1,21 @@
-﻿$packageName = "{{PackageName}}"
-$url = "{{DownloadUrlx64}}"
-$unzipLoc = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$unzipLoc"
+﻿try {
+	$packageName = "{{PackageName}}"
+	$url = "{{DownloadUrlx64}}"
+	$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+	$rarFile = "$toolsDir\innounp.rar"
+  Get-ChocolateyWebFile "$packageName" "$rarFile" "$url"
+	if (Test-Path "${Env:ProgramFiles(x86)}\7-zip") {
+		$cmd7z = "${Env:ProgramFiles(x86)}\7-zip\7z.exe"
+	}	elseif (Test-Path "$Env:ProgramFiles\7-zip") {
+		$cmd7z = "$Env:ProgramFiles\7-zip\7z.exe"
+	} else {
+		Write-Warning "7-zip is not installed.  Please install 7-zip."
+		throw
+	}
+	Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "$toolsDir" -ArgumentList "e $rarFile"
+	Remove-Item "$rarFile"
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
