1adbe2d81cdf6a5019469b3f54bed15ae5637445
dtgm <dtgm1594@gmail.com>
2015-02-02 23:28:42 -0600

    warning; unable to install previous version

--- automatic/fsum/tools/chocolateyInstall.ps1
+++ automatic/fsum/tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 ﻿$packageName = '{{PackageName}}'
 $packageVersion = '{{PackageVersion}}'
+# no version-specific installers available, only latest
 $url = '{{DownloadUrl}}'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir"
