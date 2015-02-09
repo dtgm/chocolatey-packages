cf76648953afa2352dafa0b097aa40d6f4499a29
dtgm <dtgm1594@gmail.com>
2015-02-09 01:13:38 -0600

    package handover: blink https://github.com/Moredread/chocolatey-packages/pull/1
    + added uninstall script
    + added icons/blink.png
    + additional nuspec content
    ^ updated package
    ^ created automatic package

---
+++ automatic/_output/blink/0.9.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'blink'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://blink.sipthor.net/download.phtml?download&os=nt'
+$validExitCodes = @(0)
+$checksum = 'c49076d7f1d2c8d4a2875845ba4c54e3'
+$checksumType = 'md5'
+$checksumx64 = 'c49076d7f1d2c8d4a2875845ba4c54e3'
+$checksumTypex64 = $checksumType
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksumx64" -checksumType64 "$checksumTypex64"
---
+++ automatic/blink/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = '{{DownloadUrl}}'
+$validExitCodes = @(0)
+$checksum = '{{Checksum}}'
+$checksumType = 'md5'
+$checksumx64 = '{{Checksumx64}}'
+$checksumTypex64 = $checksumType
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksumx64" -checksumType64 "$checksumTypex64"
