26f9e3883865101559cbc8950bf51e9533975049
dtgm <dtgm1594@gmail.com>
2015-02-13 02:58:11 -0600

    update to point to correct dependency version of vcredist2005

---
+++ automatic/_output/linkshellextension/3.8.6.01/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'linkshellextension'
+$installerType = 'exe'
+$url = 'http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_win32.exe'
+$url64 = 'http://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_X64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+Install-ChocolateyPackage -packageName "$packageName" -fileType "$installerType" -url "$url" -url64bit "$url64" -silentArgs "$silentArgs" -validExitCodes $validExitCodes
