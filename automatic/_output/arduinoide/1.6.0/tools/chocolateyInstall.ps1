db146d44b77fe5a2b04c16e3b5f09ba227d007f6
dtgm <dtgm1594@gmail.com>
2015-02-13 12:58:33 -0600

    update

---
+++ automatic/_output/arduinoide/1.6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'arduinoide'
+$installerType = 'EXE'
+$url = 'http://arduino.cc/download_handler.php?f=/arduino-1.6.0-windows.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
