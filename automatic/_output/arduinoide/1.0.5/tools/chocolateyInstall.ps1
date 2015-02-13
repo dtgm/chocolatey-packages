b0d8cecc4654a5f52614b191a01847cc045022c2
dtgm <dtgm1594@gmail.com>
2015-02-13 01:26:29 -0600

    package handover: arduindoide https://github.com/adgellida/chocolateyautomaticpackages/pull/10

---
+++ automatic/_output/arduinoide/1.0.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'arduinoide'
+$installerType = 'EXE'
+#$url = 'http://arduino.googlecode.com/files/arduino-{version}-windows.exe'
+$url = 'http://arduino.googlecode.com/files/arduino-1.0.5-windows.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ automatic/arduinoide/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
