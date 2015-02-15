190b4f177edb9c4851913a07c8ddf83eabcef267
dtgm <dtgm1594@gmail.com>
2015-02-14 23:19:33 -0600

    - file *.gui; now created by install script - install.ps1; Write-ChocolateySuccess/Failure deprecated ^ nuspec; xmlns:xsi tag, formatting, explicitly source packaged files

--- manual/hpusbdisk/tools/chocolateyInstall.ps1
+++ manual/hpusbdisk/tools/chocolateyInstall.ps1
@@ -2,11 +2,10 @@
 	$packageName = 'hpusbdisk'
 	$checksum = '280298bcc4f6d2547babf36d8126cdef44fe4298'
 	$checksumType = 'sha1'
-  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  $nodePath = Join-Path $scriptPath 'HPUSBDisk.exe'
+  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $nodePath = Join-Path $toolsDir 'HPUSBDisk.exe'
   Get-CheckSumValid -file $nodePath -checkSum $checksum -checksumType $checksumType
-  Write-ChocolateySuccess "$packageName"
+  Set-Content -Path ("$toolsDir\$packageName.exe.gui") -Value $nul
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+  throw $_.Exception 
 }
