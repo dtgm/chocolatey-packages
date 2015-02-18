0739ecbbc57e13d14ec75c5d55ae4db186d1748b
dtgm <dtgm1594@gmail.com>
2015-02-17 21:31:47 -0600

    2.2.0.1706 is not same version as https://chocolatey.org/packages/ilspy/2.2.0.1706

---
+++ automatic/_output/ilspy/2.2.0.1707/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+$packageName = 'ilspy'
+$url = 'https://github.com/icsharpcode/ILSpy/releases/download/2.2/ILSpy_2.2.0.1706_Binaries.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum = 'a8479d89ffb32908a5da5517dc9d8c5d71acad4e'
+$checksumType = 'sha1'
+Set-Content -Path ("$toolsDir\$packageName.exe.gui") -Value $nul
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -Url64bit "" -Checksum "$checksum" -ChecksumType "$checksumType"
