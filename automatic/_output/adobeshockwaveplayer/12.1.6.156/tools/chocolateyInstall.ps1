7a4a138184dc3e95ae0e9fe839b76e6db74b90fe
dtgm <dtgm1594@gmail.com>
2015-02-18 00:16:39 -0600

    correct mistyped URLs

--- automatic/_output/adobeshockwaveplayer/12.1.6.156/tools/chocolateyInstall.ps1
+++ automatic/_output/adobeshockwaveplayer/12.1.6.156/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'adobeshockwaveplayer'
 $installerType = 'exe'
-$url = 'http://www.filehippo.com//download/file/828023e75e9ac71e82c9fa6ea3dcd3f988a8069997f3d9519f8aa28441f26b39/'
+$url = 'http://www.filehippo.com/download/file/828023e75e9ac71e82c9fa6ea3dcd3f988a8069997f3d9519f8aa28441f26b39/'
 $silentArgs = '/S /NCRC'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
