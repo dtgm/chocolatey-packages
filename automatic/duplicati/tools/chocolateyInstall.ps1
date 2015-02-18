bdde84fadcbc1e8fb428bb2d8893971218e66fb2
dtgm <dtgm1594@gmail.com>
2015-02-18 00:15:41 -0600

    move to automatic package

---
+++ automatic/duplicati/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
+$url  = '{{DownloadUrl}}'
+#$url  = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.msi'
+$url64  = '{{DownloadUrlx64}}'
+#$url64  = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0) 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
--- manual/duplicati/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'duplicati'
-$installerType = 'MSI'
-$url  = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.msi'
-$url64  = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.x64.msi'
-$silentArgs = '/quiet'
-$validExitCodes = @(0) 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
