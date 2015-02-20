4e1405fc606a99a1ab578b42dc8c75134fcab818
dtgm <dtgm1594@gmail.com>
2015-02-19 22:26:33 -0600

    fix download URI to static
    URI is bounces between
    http://fs41.filehippo.com/8820
    http://fs40.filehippo.com/4249
    
    hash remains the same between the two servers.  I'm not sure why 8820 and 4249 differ... I'm guessing they are part of the hash, but I can't see why they would want their path structure to differ or why they don't just use the file's MD5 as the hash... #smarterthanme

--- automatic/_output/adobeshockwaveplayer/12.1.6.156/tools/chocolateyInstall.ps1
+++ automatic/_output/adobeshockwaveplayer/12.1.6.156/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'adobeshockwaveplayer'
 $installerType = 'exe'
-$url = 'http://www.filehippo.com/download/file/828023e75e9ac71e82c9fa6ea3dcd3f988a8069997f3d9519f8aa28441f26b39/'
+$url = 'http://fs41.filehippo.com/8820/7b65b86a87844ae9a2e9a09187d2759c/Shockwave_Installer_Full.exe'
 $silentArgs = '/S /NCRC'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
