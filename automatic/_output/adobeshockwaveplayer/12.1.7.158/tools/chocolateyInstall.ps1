e466a8f901ff90239fb8b0530035c1d1c2c518fd
dtgm <dtgm1594@gmail.com>
2015-03-01 16:59:11 -0600

    dynamically retrieve download URL from FileHippo.com https://github.com/dtgm/chocolatey-packages/issues/9

--- automatic/_output/adobeshockwaveplayer/12.1.7.158/tools/chocolateyInstall.ps1
+++ automatic/_output/adobeshockwaveplayer/12.1.7.158/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'adobeshockwaveplayer'
 $installerType = 'exe'
-$checksum = 'c0b66e96963fcec3b3fac370a39bf73a7cc1a5fd'
+$checksum = '7bd7f120eb16aa218c55b5e2b22a63746bdfe9fe'
 $checksumType = 'sha1'
 $silentArgs = '/S /NCRC'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
@@ -9,7 +9,7 @@ $ahkFile = "$pwdPath\shockwave.ahk"
 $fileHippoName = "shockwave"
 
 Import-Module "$($pwdPath)\Get-FilenameFromRegex.ps1"
-$url1 = Get-FilenameFromRegex 'http://www.filehippo.com/download_shockwave/history/' 'download_shockwave/([\d]+)/">Shockwave 12.1.6.156<' 'http://www.filehippo.com/download_shockwave/$1/'
+$url1 = Get-FilenameFromRegex 'http://www.filehippo.com/download_shockwave/history/' 'download_shockwave/([\d]+)/">Shockwave 12.1.7.157<' 'http://www.filehippo.com/download_shockwave/$1/'
 Write-Host "Found URL which contains the download URL 1: $url1"
 $url2 = Get-FilenameFromRegex "$url1" 'download_shockwave/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_shockwave/download/$1/'
 Write-Host "Found URL which contains the download URL 2: $url2"
