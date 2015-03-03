05465c87324128809edb0da8c4db044f3e51f394
dtgm <dtgm1594@gmail.com>
2015-03-03 00:23:02 -0600

    [bug] these packages should fail to uninstall on 32-bit systems why? hkcu has no wow6432node path fix: only resolve hkcu to one location regardless of bit addressing

--- automatic/bitcoin.install/tools/chocolateyUninstall.ps1
+++ automatic/bitcoin.install/tools/chocolateyUninstall.ps1
@@ -1,18 +1,11 @@
+$packageName = '{{PackageName}}'
+$packageSearch = 'Bitcoin Core'
+$fileType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$unPath = "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$unString=(Get-ItemProperty "$unpath\$packageSearch*" UninstallString).UninstallString)
 try {
-  $packageName = '{{PackageName}}'
-  $packageSearch = 'Bitcoin Core'
-  $fileType = 'exe'
-  $silentArgs = '/S'
-  $validExitCodes = @(0)
-  $unPath = "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-  $unPathx86 = "HKCU:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-  $osBitness = Get-ProcessorBits
-  if ($osBitness -eq 64) {
-    if ($unString=(Get-ItemProperty "$unpath\$packageSearch*" UninstallString).UninstallString) {}
-      else { $unString=(Get-ItemProperty "$unPathx86\$packageSearch*" UninstallString).UninstallString }
-    } else {
-    $unString = (Get-ItemProperty "$unPath\$packageSearch*" UninstallString).UninstallString
-  }
   Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes $validExitCodes
 } catch {
   throw $_.Exception
--- automatic/riecoin/tools/chocolateyUninstall.ps1
+++ automatic/riecoin/tools/chocolateyUninstall.ps1
@@ -1,17 +1,10 @@
+$packageName = '{{PackageName}}'
+$fileType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$unPath = "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$unString=(Get-ItemProperty "$unpath\$packageName*" UninstallString).UninstallString)
 try {
-  $packageName = '{{PackageName}}'
-  $fileType = 'exe'
-  $silentArgs = '/S'
-  $validExitCodes = @(0)
-  $unPath = "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-  $unPathx86 = "HKCU:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-  $osBitness = Get-ProcessorBits
-  if ($osBitness -eq 64) {
-    if ($unString=(Get-ItemProperty "$unpath\$packageName*" UninstallString).UninstallString) {}
-      else { $unString=(Get-ItemProperty "$unPathx86\$packageName*" UninstallString).UninstallString }
-    } else {
-    $unString = (Get-ItemProperty "$unPath\$packageName*" UninstallString).UninstallString
-  }
   Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes $validExitCodes
 } catch {
   throw $_.Exception
