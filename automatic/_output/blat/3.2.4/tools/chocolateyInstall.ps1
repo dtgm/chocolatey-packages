36872e5464e21f9cb22a6f3d28efa35c27558c55
dtgm <dtgm1594@gmail.com>
2015-03-04 12:47:15 -0600

    update

--- automatic/_output/blat/3.2.4/tools/chocolateyInstall.ps1
+++ automatic/_output/blat/3.2.4/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$packageName = "{{PackageName}}"
-$urlArray = {{DownloadUrlx64}}
+$packageName = "blat"
+$urlArray = @('http://sourceforge.net/projects/blat/files/Blat%20Full%20Version/32%20bit%20versions/Win2000%20and%20newer/blat324_32.full.zip/download', 'http://sourceforge.net/projects/blat/files/Blat%20Full%20Version/64%20bit%20versions/blat324_64.full.zip/download')
 $url = $urlArray[0]
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = $urlArray[1]
-$checksum = '{{Checksum}}'
+$checksum = '527140583f0c5df1c35a8a58d45273d6a34c0d47'
 $checksumType = 'sha1'
-$checksum64 = '{{Checksumx64}}'
+$checksum64 = '12c2f2005d91f216935c5fcbf6e9ee5d3d6a5b1c'
 $checksumType64 = 'sha1'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
