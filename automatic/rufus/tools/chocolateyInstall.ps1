e42fb000f5c5bb98014dded95f1ce6bb5ccac01d
dtgm <dtgm1594@gmail.com>
2015-03-09 20:37:02 -0500

    omit 64-bit info

--- automatic/rufus/tools/chocolateyInstall.ps1
+++ automatic/rufus/tools/chocolateyInstall.ps1
@@ -3,20 +3,14 @@ $packageVersion = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha1'
-$url64 = "$url"
-$checksum64 = "$checksum"
-$checksumType64 = "$checksumType"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
 try {
   Get-ChocolateyWebFile -PackageName "$packageName" `
                         -FileFullPath "$installFile" `
                         -Url "$url" `
-                        -Url64bit "$url64" `
                         -Checksum "$checksum" `
                         -ChecksumType "$checksumType" `
-                        -Checksum64 "$checksum64" `
-                        -ChecksumType64 "$checksumType64"
   Set-Content -Path ("$installFile.gui") -Value $null
 
 } catch {
