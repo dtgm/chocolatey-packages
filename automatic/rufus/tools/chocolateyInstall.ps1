dca962e3e4e0f3d3d997aa4fd4995b917e3266b3
dtgm <dtgm1594@gmail.com>
2015-03-09 20:36:04 -0500

    64-bit same as 32-bit

--- automatic/rufus/tools/chocolateyInstall.ps1
+++ automatic/rufus/tools/chocolateyInstall.ps1
@@ -1,13 +1,22 @@
+$packageName = '{{PackageName}}'
+$packageVersion = '{{PackageVersion}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
 try {
-  $packageName = '{{PackageName}}'
-  $packageVersion = '{{PackageVersion}}'
-  $url = '{{DownloadUrl}}'
-  $checksum = '{{Checksum}}'
-  $checksumType = 'sha1'
-  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $installFile = Join-Path $toolsDir "$($packageName).exe"
-
-  Get-ChocolateyWebFile "$packageName" "$installFile" "$url" -checksum "$checksum" -checksumType "$checksumType"
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Url64bit "$url64" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType" `
+                        -Checksum64 "$checksum64" `
+                        -ChecksumType64 "$checksumType64"
   Set-Content -Path ("$installFile.gui") -Value $null
 
 } catch {
