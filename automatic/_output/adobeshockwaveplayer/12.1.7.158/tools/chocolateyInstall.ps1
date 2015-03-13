038eef92124906a1c979f42565121651e5b92aa4
dtgm <dtgm1594@gmail.com>
2015-03-13 05:18:15 -0500

    fix regex

--- automatic/_output/adobeshockwaveplayer/12.1.7.158/tools/chocolateyInstall.ps1
+++ automatic/_output/adobeshockwaveplayer/12.1.7.158/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $ahkFile = "$pwdPath\shockwave.ahk"
 $fileHippoName = "shockwave"
 
 Import-Module "$($pwdPath)\Get-FilenameFromRegex.ps1"
-$url1 = Get-FilenameFromRegex 'http://www.filehippo.com/download_shockwave/history/' 'download_shockwave/([\d]+)/">Shockwave 12.1.7.157<' 'http://www.filehippo.com/download_shockwave/$1/'
+$url1 = Get-FilenameFromRegex 'http://www.filehippo.com/download_shockwave/history/' 'download_shockwave/([\d]+)/"[^\d]+\s+12.1.7.157<' 'http://www.filehippo.com/download_shockwave/$1/'
 Write-Host "Found URL which contains the download URL 1: $url1"
 $url2 = Get-FilenameFromRegex "$url1" 'download_shockwave/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_shockwave/download/$1/'
 Write-Host "Found URL which contains the download URL 2: $url2"
