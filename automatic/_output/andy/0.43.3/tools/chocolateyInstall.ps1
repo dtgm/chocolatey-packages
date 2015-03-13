32d33614212bd5694cac7a1956a09d7cbb010f7a
dtgm <dtgm1594@gmail.com>
2015-03-13 05:19:19 -0500

    update

--- automatic/_output/andy/0.43.3/tools/chocolateyInstall.ps1
+++ automatic/_output/andy/0.43.3/tools/chocolateyInstall.ps1
@@ -1,8 +1,15 @@
-﻿$packageName = 'andy'
+$packageName = 'andy'
 $installerType = 'exe'
 $url = 'http://d1ubedgu4eka0j.cloudfront.net/ANDY/installer/v43/Andy_v43_3.exe'
 $silentArgs = '/S'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$validExitCodes = @(0)
 $checksum = '6c60b5a1915fe44593b47f61fee1c547'
 $checksumType = 'md5'
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
