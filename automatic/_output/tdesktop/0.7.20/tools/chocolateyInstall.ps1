a08c5d729622b001dd08ab144be8e14ad31a95ef
dtgm <dtgm1594@gmail.com>
2015-03-17 00:20:29 -0500

    update

---
+++ automatic/_output/tdesktop/0.7.20/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+$packageName = 'tdesktop'
+$installerType = 'exe'
+$url = 'https://updates.tdesktop.com/tsetup/tsetup.0.7.20.exe'
+$checksum = 'cc8a446d1c765c9e5903c307a02e87185a619fd0'
+$checksumType = 'sha1'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
