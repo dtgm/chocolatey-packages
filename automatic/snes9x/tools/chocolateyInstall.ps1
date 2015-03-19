215084964d29016ce0a256e3b00730131889c654
dtgm <dtgm1594@gmail.com>
2015-03-19 18:20:30 -0500

    + attempt start-menu checking https://github.com/dtgm/chocolatey-packages/issues/14

--- automatic/snes9x/tools/chocolateyInstall.ps1
+++ automatic/snes9x/tools/chocolateyInstall.ps1
@@ -7,4 +7,26 @@ $checksum64 = '{{Checksumx64}}'
 $checksumType64 = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -Url64bit "$url64" -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+try {
+  $addStartMenuItem = (
+  Get-Item $MyInvocation.MyCommand.Definition
+  ).Directory.GetFiles("Install-ChocolateyStartMenuLink.ps1")[0]
+  if (!(Get-Command "Install-ChocolateyStartMenuLink" -ErrorAction SilentlyContinue)) {
+    Write-Debug @"
+Using bundled Install-ChocolateyStartMenuLink @ $($addStartMenuItem.FullName)
+"@
+    . $addStartMenuItem.FullName
+  }
+Get-ChildItem $toolsDir\*.exe `
+  | ForEach-Object { Install-ChocolateyStartMenuLink -TargetFilePath "$($_.FullName)" }
+} catch {
+  throw $_.Exception
+}
