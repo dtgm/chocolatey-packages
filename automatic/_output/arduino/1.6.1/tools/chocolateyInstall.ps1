7263e00902d87105d27f17d738e69a309a1d0b94
dtgm <dtgm1594@gmail.com>
2015-03-19 18:42:31 -0500

    init https://chocolatey.org/packages/arduino deprecated https://chocolatey.org/packages/arduinoide why? coordinate naming: https://packages.debian.org/stable/arduino

---
+++ automatic/_output/arduino/1.6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'arduino'
+$installerType = 'EXE'
+$url = 'http://arduino.cc/download_handler.php?f=/arduino-1.6.1-windows.exe'
+$checksum = 'c52adad57147705ce96e83c362ebce5af60812c9'
+$checksumType = 'sha1'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/arduino/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
