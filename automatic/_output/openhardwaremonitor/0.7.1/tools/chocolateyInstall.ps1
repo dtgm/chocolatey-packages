d824ee9094ea27a810f14452783a4efb8025da5d
dtgm <dtgm1594@gmail.com>
2015-03-21 10:00:42 -0500

    + converted to automatic package + completed nuspec metadata - removed chocolatey dependency - removed legacy install scripts - removed unnecessary uninstall script - removed app pinning (these custom configurations should be implemented some other way)

--- automatic/_output/openhardwaremonitor/0.7.1/tools/chocolateyInstall.ps1
+++ automatic/_output/openhardwaremonitor/0.7.1/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$packageName = '{{PackageName}}'
+$packageName = 'openhardwaremonitor'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'http://openhardwaremonitor.org/files/openhardwaremonitor-v0.7.1-beta.zip'
+$checksum = '5446869281883c102f4180cf87385a124eadf87d'
 $checksumType = 'sha1'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
