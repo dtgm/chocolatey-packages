33a8e921d185a352879bd222c59b22375e02cb96
dtgm <dtgm1594@gmail.com>
2015-03-20 21:11:12 -0500

    ^ converted to automatic package + added uninstall script + added checksum to install script + complete missing nuspec fields + add svg icon from repo

---
+++ automatic/_output/deluge/1.3.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'deluge'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.deluge-torrent.org/windows/deluge-1.3.11-win32-setup.exe'
+$checksum = 'a7e2f12f0117474d703e69faee3e143c432f1f1e'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
