9e2650b3a9d4052ee50560352166e3cfbd63c9cf
dtgm <dtgm1594@gmail.com>
2015-03-23 06:23:58 -0500

    init

---
+++ automatic/ht/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null
+} catch {
+  throw $_.Exception
+}
