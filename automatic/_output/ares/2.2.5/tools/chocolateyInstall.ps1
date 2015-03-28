5af65c5b172d9a861c8ac79b77a2df47f5a7a360
dtgm <dtgm1594@gmail.com>
2015-03-28 06:06:51 -0500

    package handover: ares
    https://github.com/adgellida/chocolateyautomaticpackages/pull/13

---
+++ automatic/_output/ares/2.2.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ares'
+$installerType = 'EXE'
+#$url = 'http://heanet.dl.sourceforge.net/project/aresgalaxy/aresgalaxy/{anchor1}.exe'
+$url = 'http://heanet.dl.sourceforge.net/project/aresgalaxy/aresgalaxy/AresRegular225_072413/aresregular225_installer.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ automatic/_output/ares/2.2.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'ares'
+$installerType = 'EXE'
+$url = 'http://heanet.dl.sourceforge.net/project/aresgalaxy/aresgalaxy/AresRegular226_112313/aresregular226_installer.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
