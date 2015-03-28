877571c82886813b9c7e55c1490b88f05aab3ff4
dtgm <dtgm1594@gmail.com>
2015-03-28 11:43:41 -0500

    package handover: sumo
    https://github.com/adgellida/chocolateyautomaticpackages/pull/13

---
+++ automatic/_output/sumo/3.13.5.259/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'sumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
+$checksum = 'ac39d5c40aa7bff32e134052fa1cce326c13d2f1'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
