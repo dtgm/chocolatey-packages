be61079b3ecc5abcb81f7e1bf04cf154b70982b8
dtgm <dtgm1594@gmail.com>
2015-04-04 17:14:07 -0500

    nirsoft utils - http://www.nirsoft.net/utils/

---
+++ automatic/texniccenter/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'TeXnicCenter'
+$installerType = 'EXE'
+$url = 'http://sourceforge.net/projects/texniccenter/files/TeXnicCenter/2%20Beta%202/TXCSetup_2Beta2_Win32.exe/download'
+$url64 = 'http://sourceforge.net/projects/texniccenter/files/TeXnicCenter/2%20Beta%202/TXCSetup_2Beta2_x64.exe/download'
+$silentArgs = '/SP- /SILENT /NORESTART' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT"
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ automatic/texniccenter/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'TeXnicCenter'
+$installerType = 'EXE'
+$silentArgs = '/SP- /SILENT /NORESTAR'
+$filePath = $env:programfiles + '\TeXnicCenter\unins000.exe'
+
+Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$filePath"
