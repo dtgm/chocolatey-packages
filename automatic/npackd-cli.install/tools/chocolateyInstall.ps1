3a41a89dcf16aee60a3713cfac0bed6de432451e
dtgm <dtgm1594@gmail.com>
2015-04-06 08:08:39 -0500

    init: cmake*, bacula, kodos, regextester, hmne, gdevelop, 0install*, npackd-cli*, npackd*

---
+++ automatic/npackd-cli.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = '{{PackageName}}'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/regextester/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = '{{PackageName}}'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+$installFile = Join-Path $toolsDir "RegExTester.exe"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
