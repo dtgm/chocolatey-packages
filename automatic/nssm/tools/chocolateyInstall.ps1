d3f9b3c77f631d7b008199013c4a940b746fa860
dtgm <dtgm1594@gmail.com>
2015-04-06 08:11:56 -0500

    + created auto script ^ co-maintain

---
+++ automatic/nssm/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = '{{PackageName}}'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+if (Get-ProcessorBits 32) {
+    $ignoreFolder = 'win64'
+} else {
+   $ignoreFolder = 'win32'
+}
+Set-Content -Path ("$toolsDir\nssm-{{PackageVersion}}\$ignoreFolder\nssm.exe.ignore") `
+            -Value $null
