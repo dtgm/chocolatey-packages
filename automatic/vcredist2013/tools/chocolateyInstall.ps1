57ac1108ef64e3831be00718d70d3faa5ada9b6b
dtgm <dtgm1594@gmail.com>
2015-04-08 20:33:16 -0500

    use initial data from source to fix
    https://github.com/dtgm/chocolatey-packages/commit/af66cda7e0e41e7ceb7cc1b749fb69ab74d702b0

---
+++ automatic/vcredist2013/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿Install-ChocolateyPackage -packageName $packageName -fileType 'exe' -silentArgs '/Q' `
+                    -url 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe' `
+                    -url64bit 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe'
+
+
