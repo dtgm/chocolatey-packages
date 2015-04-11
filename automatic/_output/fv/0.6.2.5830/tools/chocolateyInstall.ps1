79cc73f3172990c3124ec424511410ae01615b8c
dtgm <dtgm1594@gmail.com>
2015-04-11 01:39:10 -0500

    update

--- automatic/_output/fv/0.6.2.5830/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = 'fv'
-$installerType = 'msi'
-$installArgs = '/quiet /qn /norestart'
-# due to limitations of Ketarin/chocopkgup when using repos like sf.net:
-$url = 'http://sourceforge.net/projects/fileverifier/files/fileverifier/0.6.3.5830/fv-0.6.3.5830W.msi/download'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$installArgs" "$url" -validExitCodes $validExitCodes
