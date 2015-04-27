816588ffb62fb2e7da435f8283c544f4e91dffcc
dtgm <dtgm1594@gmail.com>
2015-04-27 17:30:44 -0500

    http://chocolatey.org/api/v2/package/monodevelop/5.0.0.1

---
+++ automatic/_output/monodevelop/5.0.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = "monodevelop"
+$url = "http://download.xamarin.com/studio/Windows/XamarinStudio-5.0.0.878-0.msi"
+$kind = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $name $kind $silent $url
