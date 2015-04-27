04287079445dad9f458eb782c0a01dcf5b0c29dd
dtgm <dtgm1594@gmail.com>
2015-04-27 16:44:08 -0500

    https://github.com/AnthonyMastrean/chocolateypackages/tree/master/packages/gtksharp

---
+++ automatic/_output/gtksharp/2.12.25/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "gtksharp"
+$kind = "MSI"
+$silent = "/Q"
+$url = "http://download.xamarin.com/GTKforWindows/Windows/gtk-sharp-2.12.25.msi"
+
+Install-ChocolateyPackage $id $kind $silent $url
