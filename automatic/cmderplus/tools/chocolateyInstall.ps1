b2884eaf89be95fbe0ffcffe9d9dbc6f40293eff
dtgm <dtgm1594@gmail.com>
2015-04-27 17:59:01 -0500

    init

---
+++ automatic/cmderplus/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/firefox-nightly/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes
