488fd981d47fc4e8607ccf2a6e34a1dc7ac5f7ad
dtgm <dtgm1594@gmail.com>
2015-04-27 20:07:55 -0500

    init

---
+++ automatic/_output/easytag/2.3.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'easytag'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.gnome.org/binaries/win32/easytag/2.3/easytag-2.3.6-setup.exe'
+$checksum = '1ad81c53581eca51b3470bb5a8e8b23a889a4fa8'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
