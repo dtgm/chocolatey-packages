21e007764d26a29b8c51ef2aaef69f088d63b863
dtgm <dtgm1594@gmail.com>
2015-04-30 13:26:10 -0500

    init

---
+++ automatic/lili.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "LinuxLive USB Creator {{PackageVersion}}" |
+               Join-Path -ChildPath "LiLi USB Creator.exe.gui"
+Set-Content -Path "$installFile" `
+            -Value $null
---
+++ automatic/megui/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+Set-Content -Path "$installFile.gui" `
+            -Value $null
