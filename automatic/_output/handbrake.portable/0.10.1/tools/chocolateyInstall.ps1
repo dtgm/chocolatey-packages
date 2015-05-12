7498ab5fa7d377600180159c49ee30ade31d3bce
dtgm <dtgm1594@gmail.com>
2015-05-12 15:52:42 -0500

    package maintainer handover from https://github.com/AnthonyMastrean/chocolateypackages/tree/master/packages
    + converted into automatic packages
    + expanded nuspec content

---
+++ automatic/_output/handbrake.portable/0.10.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$name  = "handbrake.portable"
+$url   = "https://handbrake.fr/rotation.php?file=HandBrake-0.10.1-i686-Win_CLI.zip"
+$url64 = "https://handbrake.fr/rotation.php?file=HandBrake-0.10.1-x86_64-Win_CLI.zip"
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage -PackageName $name -Url $url -Url64 $url64 -UnzipLocation $content
---
+++ automatic/handbrake.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$url64 = '{{DownloadUrlx64}}'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
