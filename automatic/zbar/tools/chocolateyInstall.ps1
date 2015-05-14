5eeb48f46ac5f951fa56a711e4f90c1c5ffe06c3
dtgm <dtgm1594@gmail.com>
2015-05-14 14:25:01 -0500

    init

---
+++ automatic/zbar/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
+
+$regObj = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                           -ErrorAction:SilentlyContinue `
+          | Where-Object { $_.DisplayName -like "ZBar Bar Code Reader" }
+$addPath = $regObj.InstallLocation.Replace('"','')
+Install-ChocolateyPath (Join-Path $addPath 'bin')
---
+++ automatic/zbar/tools/chocolateyUninstall.ps1
@@ -0,0 +1,24 @@
+$packageName = '{{PackageName}}'
+$packageSearch = "ZBar Bar Code Reader"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unPath = Join-Path $toolsPath 'Uninstall-ChocolateyPath.psm1'
+try {
+  $regObj = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                      'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                      'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                            -ErrorAction:SilentlyContinue `
+  | Where-Object   { $_.DisplayName -like "$packageSearch" }
+  
+  $regObj | ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                                 -FileType "$installerType" `
+                                                 -SilentArgs "$($silentArgs)" `
+                                                 -File "$($_.UninstallString)" `
+                                                 -ValidExitCodes $validExitCodes }
+  Import-Module $unPath
+  $regObj | ForEach-Object { Uninstall-ChocolateyPath (Join-Path $_.InstallLocation.Replace('"','') 'bin') }
+} catch {
+  throw $_.Exception
+}
