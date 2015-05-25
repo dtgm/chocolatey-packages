f1736454ec41679e89724d73c75d6bf3d65af649
dtgm <dtgm1594@gmail.com>
2015-05-25 10:40:12 -0500

    init

---
+++ automatic/advor/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = Join-Path $toolsDir
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "AdvOR" `
+               | Join-Path -ChildPath "AdvOR.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/hashdeep/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
