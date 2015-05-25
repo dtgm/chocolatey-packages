d0ec52c310f8d73b4316fa1a47ce1569fe993571
dtgm <dtgm1594@gmail.com>
2015-05-24 22:33:01 -0500

    WinSCP converted to meta-package; winscp points to winscp.install and winscp.portable
    + checksums for all installers except translation files
    + created uninstaller for all
    ^ auto-support for ALL available translation files

---
+++ automatic/_output/winscp.portable/5.7.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'winscp.portable'
+$url = 'http://sourceforge.net/projects/winscp/files/WinSCP/5.7.3/winscp573.zip/download'
+$checksum = 'cdebe75eee2ef4c291d8afc745cd7899612645f7'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "WinSCP.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
