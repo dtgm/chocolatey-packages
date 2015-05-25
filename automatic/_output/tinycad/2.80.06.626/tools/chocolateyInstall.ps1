9ff8835fae9f5057de4b1b80f927406c9f6bb681
dtgm <dtgm1594@gmail.com>
2015-05-25 12:35:21 -0500

    init

---
+++ automatic/_output/tinycad/2.80.06.626/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'tinycad'
+$installerType = 'EXE'
+$url = 'http://netcologne.dl.sourceforge.net/project/tinycad/Official Release/2.80.06/TinyCAD_2.80.06_626_Production_Release.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ automatic/tinycad/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = '{{DownloadUrlx64}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "{{PackageName}}.jar"
+$ahkFile = Join-Path "$toolsDir" 'chocolateyInstall.ahk'
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
