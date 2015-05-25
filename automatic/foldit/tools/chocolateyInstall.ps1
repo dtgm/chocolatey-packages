961611e0ddbe908a6ad55b728083bd35fd3d2e1b
dtgm <dtgm1594@gmail.com>
2015-05-25 02:35:33 -0500

    init

---
+++ automatic/foldit/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+$binRoot = Get-BinRoot
+$installPath = Join-Path $binRoot "Foldit"
+$silentArgsBase = '/S /D='
+$silentArgs = $($silentArgsBase + '"' + $installPath + '"')
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
