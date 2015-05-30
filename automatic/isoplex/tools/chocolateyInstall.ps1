366732a10564c70e0006002cfbb9447a6f62a61c
dtgm <dtgm1594@gmail.com>
2015-05-30 16:35:04 -0500

    package handover: isoplex
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/isoplex/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/exenoui /quiet /qn /norestart'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
