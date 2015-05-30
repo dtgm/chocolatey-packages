093db950dabf1d252d8cff52e63f27fa0bcb984a
dtgm <dtgm1594@gmail.com>
2015-05-30 16:29:29 -0500

    package handover: adblockplusie
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/adblockplusie/1.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'adblockplusie'
+$installerType = 'EXE'
+$url = 'https://downloads.adblockplus.org/adblockplusie-1.1.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
