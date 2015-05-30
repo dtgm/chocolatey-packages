f85204be7e0a0a293c27ead5e2b8ae44a035ea42
dtgm <dtgm1594@gmail.com>
2015-05-30 16:31:59 -0500

    package handover: gamedownloader
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/gamedownloader/3.9/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'gamedownloader'
+$installerType = 'EXE'
+$url = 'http://netcologne.dl.sourceforge.net/project/dev-fire-gd/Game Downloader 3.9/Game Downloader 3.9 Setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
