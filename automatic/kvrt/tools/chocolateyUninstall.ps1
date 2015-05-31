24ccd84c693fd4e9934bcaab2e2cb60840c0c185
dtgm <dtgm1594@gmail.com>
2015-05-31 00:00:35 -0500

    package handover: kvrt
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/kvrt/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = '{{PackageName}}'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$batchStart = Join-Path $toolsPath "{{PackageName}}.bat"
+Uninstall-BinFile "$packageName" "$batchStart"
