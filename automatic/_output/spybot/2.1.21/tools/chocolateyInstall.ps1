999fae4ca76b995743d663581a99120ace13eb1a
dtgm <dtgm1594@gmail.com>
2015-05-31 13:01:21 -0500

    package handover: spybot
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/spybot/2.1.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'spybot'
+$installerType = 'EXE'
+#$url = 'http://spybotupdates.com/files/spybotsd-2.1.20-SR1.exe'
+#$url = 'http://spybotupdate.biz/files/spybotsd-2.1.20-SR1.exe'
+$url = 'http://spybotupdate.biz/files/spybotsd-2.1.21-SR2.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ automatic/_output/spybot/2.4.40/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'spybot'
+$installerType = 'EXE'
+$url = 'http://spybotupdates.com/files/spybot-2.4.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
