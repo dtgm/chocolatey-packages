9703f01c5dbd6270b0aba94f1b0f6c7701842073
dtgm <dtgm1594@gmail.com>
2015-05-31 12:20:33 -0500

    package handover: superantispyware
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/superantispyware/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+# ver 6.0.1194 fails on 8.1-64
+#$silentArgs = '/SILENT'
+$silentArgs = ''
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
