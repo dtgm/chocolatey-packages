1ac6dbef5edbbe7b42a05accc7bdc488ed5fca1f
dtgm <dtgm1594@gmail.com>
2015-05-30 20:21:53 -0500

    package handover: dumo
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/dumo/1.1.0.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'dumo'
+$installerType = 'EXE'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+
+#Necessary by now
+Remove-Item "$Home\Desktop\Clean computer - KCleaner.URL"
---
+++ automatic/_output/dumo/2.0.0.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '4f2cef82c52a338948e72aaf3ff2b5dba9fc75c0'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
