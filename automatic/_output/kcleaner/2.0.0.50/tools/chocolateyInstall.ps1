9ae904be9c1a2a5a34b5715dbb2304897b8e0dfb
dtgm <dtgm1594@gmail.com>
2015-06-02 16:28:56 -0500

    package handover: kcleaner
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/kcleaner/2.0.0.50/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'kcleaner'
+$installerType = 'EXE'
+#$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
