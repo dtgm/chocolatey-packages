9854c329638fbb820a1a5b4d477cec93f9cf7176
dtgm <dtgm1594@gmail.com>
2015-06-02 16:28:56 -0500

    package handover: kcleaner
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/kcleaner/2.4.0.56/tools/chocolateyInstall.ps1
@@ -0,0 +1,52 @@
+﻿$packageName = 'kcleaner'
+$version = '2.4.0.56'
+$exeToVersioning_x86 = "$Env:ProgramFiles\KC Softwares\KCleaner\KCleaner.exe"
+$exeToVersioning_x64 = "${Env:ProgramFiles(x86)}\KC Softwares\KCleaner\KCleaner.exe"
+$installerType = 'EXE'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$mantainer = 'tonigellida'
+
+try {
+
+	$processor = Get-WmiObject Win32_Processor
+	$is64bit = $processor.AddressWidth -eq 64
+	
+	if ($is64bit) {
+	
+ 		if (Test-Path $exeToVersioning_x64) {
+			$installedVersion = (Get-Command $exeToVersioning_x64).FileVersionInfo.FileVersion
+		}
+
+	} else {
+  
+		if (Test-Path $exeToVersioning_x86) {
+			$installedVersion = (Get-Command $exeToVersioning_x86).FileVersionInfo.FileVersion
+		}	
+		
+	}
+	
+	if ($installedVersion -gt $version) {
+		Write-Host "Your $packageName $installedVersion is higher than the $version proporcionated by chocolatey repo."
+		Write-Host "Please wait or contact the mantainer $mantainer to update this package."
+		Write-Host "When the package is updated try another time. Thanks."
+		
+	}elseif ($installedVersion -eq $version) {
+		Write-Host "$packageName $version is already installed."
+		
+	} else {
+
+        # Download and install the program
+
+		Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+		
+		}
+	
+	Write-ChocolateySuccess $packageName
+	
+} catch {
+
+		Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+		throw 
+}
---
+++ automatic/_output/kcleaner/2.5.0.59/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = 'ed8add5b704404bad79b81fea5938d351159720b'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
