e9472956442444d66127eb49f9bc09246baaea17
dtgm <dtgm1594@gmail.com>
2015-06-06 17:06:16 -0500

    init

---
+++ automatic/multipar.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
