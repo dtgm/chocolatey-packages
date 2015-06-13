aff1cd3d06c2cdd2ad6048c3c3c4a9a8abed86c2
dtgm <dtgm1594@gmail.com>
2015-06-12 19:15:29 -0500

    package handover: atraci
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/atraci/0.6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'atraci'
+$installerType = 'EXE'
+$url = 'https://raw.githubusercontent.com/Atraci/Atraci/gh-pages/releases/0.6.0/win/Atraci.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
