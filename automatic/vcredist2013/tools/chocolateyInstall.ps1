621fe68a1d6c2983f8c093df7cad59e1970d9422
dtgm <dtgm1594@gmail.com>
2015-06-16 00:55:40 -0500

    Fixed nuspec; checksum installer; add uninstaller

--- automatic/vcredist2013/tools/chocolateyInstall.ps1
+++ automatic/vcredist2013/tools/chocolateyInstall.ps1
@@ -1,5 +1,21 @@
-﻿Install-ChocolateyPackage -packageName $packageName -fileType 'exe' -silentArgs '/Q' `
-                    -url 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe' `
-                    -url64bit 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe'
+﻿$packageName = 'vcredist2013'
+$installerType = 'exe'
+$url = 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe'
+$checksum = 'b8fab0bb7f62a24ddfe77b19cd9a1451abd7b847'
+$checksumType = 'sha1'
+$url64 = 'http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe'
+$checksum64 = '8bf41ba9eef02d30635a10433817dbb6886da5a2'
+$checksumType64 = 'sha1'
+$silentArgs = '/Q'
+$validExitCodes = @(0,3010)  # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
