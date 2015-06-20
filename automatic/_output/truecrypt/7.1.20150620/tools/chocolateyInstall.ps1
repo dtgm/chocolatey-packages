84689f4693f0c266a4bef2b2d1a21c26e2f22799
dtgm <dtgm1594@gmail.com>
2015-06-20 01:14:10 -0500

    update security of package (checksum verification)
    published to public chocolatey feed at https://chocolatey.org/packages/truecrypt/7.1.20140621
    full changes documented in nuspec under releaseNotes

---
+++ automatic/_output/truecrypt/7.1.20150620/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'truecrypt'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.truecrypt.ch/current/TrueCrypt%20Setup%207.1a.exe'
+$checksum = '7689d038c76bd1df695d295c026961e50e4a62ea'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/truecrypt/7.1.20150620/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'truecrypt'
+$regSearch = 'TrueCrypt'
+$installerType = 'exe'
+$silentArgs = '/u'
+$validExitCodes = @(0)
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$regSearch" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString)" `
+                                               -ValidExitCodes $validExitCodes }
