e4a6c9625009e29d1be89b7980c228bf4c156cdb
dtgm <dtgm1594@gmail.com>
2015-06-20 01:11:24 -0500

    init package on which to make changes
    package as published at https://chocolatey.org/packages/truecrypt/7.1.20140621
    built by https://chocolatey.org/profiles/yoshimov

---
+++ automatic/_output/truecrypt/7.1.20140621/tools/chocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  $unpath = "${Env:ProgramFiles}\TrueCrypt\TrueCrypt Setup.exe"
+  # installer, will assert administrative rights
+  Uninstall-ChocolateyPackage 'truecrypt' 'EXE' '/u' "$unpath" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'truecrypt'
+} catch {
+  Write-ChocolateyFailure 'truecrypt' "$($_.Exception.Message)"
+  throw 
+}
