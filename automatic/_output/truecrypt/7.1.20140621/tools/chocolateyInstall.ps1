c43b10c11ac1fba4edaaac824b3e77386fbf6d22
dtgm <dtgm1594@gmail.com>
2015-06-20 01:11:24 -0500

    init package on which to make changes
    package as published at https://chocolatey.org/packages/truecrypt/7.1.20140621
    built by https://chocolatey.org/profiles/yoshimov

---
+++ automatic/_output/truecrypt/7.1.20140621/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿#try {
+  $downUrl = "https://download.truecrypt.ch/current/TrueCrypt%20Setup%207.1a.exe"
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'truecrypt' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+#  Write-ChocolateySuccess 'truecrypt'
+#} catch {
+#  Write-ChocolateyFailure 'truecrypt' "$($_.Exception.Message)"
+#  throw 
+#}
