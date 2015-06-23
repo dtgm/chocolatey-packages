8a16f37e63ff667161d5f9394c7c0d097786ed40
dtgm <dtgm1594@gmail.com>
2015-06-23 04:21:34 -0500

    new package added to feed: https://chocolatey.org/packages/

---
+++ automatic/fossil/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿Write-Verbose "This package is a metapackage. Metapackages are a chocolatey feature not yet completed. To retain future compatibility this package does not uninstall the dependent package it points to as designated with *.install or *.portable."
+if (Test-Path -PathType Container $env:ChocolateyInstall\lib\{{PackageName}}.portable) {
+  Write-Warning "Uninstall not complete, please also run the command: cuninst fossil.portable -y"
+}
