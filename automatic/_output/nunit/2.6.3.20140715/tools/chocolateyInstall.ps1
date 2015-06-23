2e5eae6655733375a814d5a7f72e1c6be7bf1332
dtgm <dtgm1594@gmail.com>
2015-06-23 09:04:17 -0500

    init https://chocolatey.org/packages/nunit/2.6.3.20140715

---
+++ automatic/_output/nunit/2.6.3.20140715/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'nunit.install'
+$installerType = 'MSI'
+$url = 'http://launchpad.net/nunitv2/trunk/2.6.3/+download/NUnit-2.6.3.msi'
+$silentArgs = '/passive'
+$validExitCodes = @(0) 
+Install-ChocolateyPackage $packageName $installerType "$silentArgs" "$url" -validExitCodes $validExitCodes
