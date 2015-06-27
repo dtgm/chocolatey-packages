42ca3ce539b440459b9b98bcd84eaa24efc80b56
dtgm <dtgm1594@gmail.com>
2015-06-26 22:06:37 -0500

    package handover: ddu
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "Display Driver Uninstaller.exe"
+
+# 0.9.7: custom headers not supported in 1st tier api helper functions
+# thusly, require more setup to use deeper, 2nd tier helper functions
+# 0.9.10: revisit: https://github.com/chocolatey/choco/issues/332
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName)Install.zip"
+$referer = "http://www.wagnardmobile.com/DDU/ddudownload.htm"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+                  
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+# create sidecar file for shimgen
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
