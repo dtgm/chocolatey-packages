c359226a37986ef61caf9a34395e10a5fcf4e897
dtgm <dtgm1594@gmail.com>
2015-06-29 00:40:23 -0500

    clean

--- automatic/mongodb/tools/chocolateyInstall.ps1
+++ automatic/mongodb/tools/chocolateyInstall.ps1
@@ -1,42 +1,78 @@
 $packageName = '{{PackageName}}'
-$mongoVersion = '{{PackageVersion}}'
+$version = '{{PackageVersion}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$url64 = '{{DownloadUrlx64}}'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha1'
 
-$fileName = "mongodb-win32-i386-$mongoVersion"
-$osBitness = Get-ProcessorBits
-if ($osBitness -eq 64) {$fileName = "mongodb-win32-x86`_64-$mongoVersion"}
+$fileName = "mongodb-win32-i386-$version"
+if (Get-ProcessorBits 64) {$fileName = "mongodb-win32-x86`_64-$version"}
 $url = "http://downloads.mongodb.org/win32/$fileName.zip"
 
-$binRoot = "$env:systemdrive\"
-### Using an environment variable to to define the bin root until we implement YAML configuration ###
-if($env:ChocolateyBinRoot -ne $null){$binRoot = join-path $env:SystemDrive $env:ChocolateyBinRoot}
+$binRoot = Get-BinRoot
+$mongoPath = Join-Path $binRoot $packageName
+$mongoBin = Join-Path $mongoPath 'bin'
 
-$mongoDir = $(join-path $binRoot $packageName)
-$mongod = join-path $mongoDir 'bin\mongod.exe'
 
-if(test-path $mongod){
+
+
+$mongoDaemon = Join-Path $mongoBin 'mongod.exe'
+if (Test-Path $mongoDaemon){
   Start-ChocolateyProcessAsAdmin "& $mongod --remove"
-  remove-item $mongoDir -recurse -force
-  }
+  Remove-Item $mongoPath -Recurse -Force
+}
 
-Install-ChocolateyZipPackage $packageName $url $binRoot
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$binRoot" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+                             
+if ((Get-ChildItem $installDir).Count -eq 1 ) {
+  Move-Item -Path (Join-Path (Get-ChildItem $installDir).FullName "\`*") -Destination "$installDir"
+}
+$unzipFile = 'mongodb-win32-*-{{PackageVersion}}'
+$unzipPath = (Get-ChildItem -Directory (Join-Path $binRoot $unzipFile)).FullName
+Rename-Item -Path  $unzipPath `
+            -NewName $packageName `
+            -Force
 
-move-item $(join-path $binRoot $fileName) $mongoDir -force
+Install-ChocolateyPath "$installDir" 'User'
 
-$dataDir = $(join-path $mongoDir 'data')
-if(!$(test-path $dataDir)){mkdir $dataDir}
+move-item $(Join-Path $binRoot $fileName) $mongoPath -force
 
-$logsDir = $(join-path $mongoDir 'logs')
-if(!$(test-path $logsDir)){mkdir $logsDir}
+$dataDir = Join-Path $mongoPath 'data'
+if (!$(Test-Path $dataDir)) {
+  mkdir $dataDir
+}
+$logsDir = Join-Path $mongoPath 'logs'
+if (!$(Test-Path $logsDir)){mkdir $logsDir}
+if (!$(Test-Path $tempDir)){mkdir $tempDir}
+New-Item -Path $tempDir -Force -ItemType Directory | Out-Null
 
-$binDir = join-path $env:chocolateyinstall 'bin'
-$batchFileName = Join-Path $binDir 'mongo.bat'
-$executable = join-path $mongoDir 'bin\mongo.exe'
+# create batch files
+$chocoBinPath = Join-Path $env:ChocolateyInstall 'bin'
+$exeFile = Join-Path $mongoPath 'bin\mongo.exe'
+$batchFile = Join-Path $chocoBinPath 'mongo.bat'
 "@echo off
-$executable %*" | Out-File $batchFileName -encoding ASCII
-
-$batchFileName = Join-Path $binDir 'MongoRotateLogs.bat'
+$exeFile %*" | Out-File $batchFile -encoding ASCII
+$batchFile = Join-Path $chocoBinPath 'mongoRotateLogs.bat'
 "@echo off
-$executable --eval `'db.runCommand(`"logRotate`")`' mongohost:27017/admin" | Out-File $batchFileName -encoding ASCII
+$exeFile --eval `'db.runCommand(`"logRotate`")`' mongohost:27017/admin" | Out-File $batchFile -encoding ASCII
 
-# Install and start mongodb as a Windows service
-Start-ChocolateyProcessAsAdmin "& $mongod --quiet --bind_ip 127.0.0.1 --logpath $(join-path $logsDir 'MongoDB.log') --logappend --dbpath $dataDir --directoryperdb --install; net start `"MongoDB`""
+# install and start mongodb as a Windows service
+$installArgs = '& $mongoDaemon --quiet --bind_ip 127.0.0.1 --logpath $(Join-Path $logsDir 'MongoDB.log') --logappend --dbpath $dataDir --directoryperdb --install; net start `"MongoDB`"'
+Start-ChocolateyProcessAsAdmin -Statements "$installArgs"
+
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "$packageName" |
+               Join-Path -ChildPath "$($packageName).exe.gui"
+
+               Start-ChocolateyProcessAsAdmin -Statements "$installArgs" `
+                             -ExeToRun "cmd.exe" `
+                             -ValidExitCodes $validExitCodes
