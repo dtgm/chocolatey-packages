768f48a27bf623a2ab8eb78bebd62499de772104
dtgm <dtgm1594@gmail.com>
2015-06-29 06:53:14 -0500

    make corrections that script missed
    https://github.com/dtgm/chocolatey-packages/issues/18
    grep -Poz '^###.*? Community\n\* \[M.*?\n[^*]' [a-z0-9]*/*.nuspec

--- automatic/_output/fossil/1.33/tools/chocolateyUninstall.ps1
+++ automatic/_output/fossil/1.33/tools/chocolateyUninstall.ps1
@@ -1,4 +1,18 @@
-﻿Write-Verbose "This package is a metapackage. Metapackages are a chocolatey feature not yet completed. To retain future compatibility this package does not uninstall the dependent package it points to as designated with *.install or *.portable."
-if (Test-Path -PathType Container $env:ChocolateyInstall\lib\fossil.portable) {
-  Write-Warning "Uninstall not complete, please also run the command: cuninst fossil.portable -y"
+$warningPreference = "Continue"
+$chocoLib = Join-Path $env:ChocolateyInstall "lib"
+if (Test-Path -PathType Container (Join-Path $chocoLib 'fossil.*')) {
+  Write-Warning "Uninstall NOT complete."
+  Write-Host 
+@"
+  This package is a metapackage; a chocolatey feature not yet fully implemented.
+  To retain future compatibility this package does not uninstall the dependent
+  package it points to as designated with *.install or *.portable.`n
+"@
+  Write-Warning "To finish removing the program installed by package fossil, please also run the command:"
+  Write-Host " `n`tcuninst " -NoNewLine
+  $list = (Get-ChildItem -Directory $chocoLib\fossil.*).Name
+  foreach ($i in $list) {
+    Write-Host "$i " -NoNewLine
+  }
+  Write-Host "-y"
 }
