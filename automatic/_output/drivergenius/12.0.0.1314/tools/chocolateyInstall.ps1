d3495cd98a684a820340dc7b05dde07d5e733746
dtgm <dtgm1594@gmail.com>
2015-06-30 03:48:54 -0500

    package handover: drivergenius
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/drivergenius/12.0.0.1314/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'drivergenius'
+$installerType = 'EXE'
+#$url = 'http://www.driver-soft.com/products/DG_Setup.exe'
+$url = 'http://www.driver-soft.com/products/DG_Setup.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
