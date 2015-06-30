57dc0a88a439902958457f6ce277785566655885
dtgm <dtgm1594@gmail.com>
2015-06-30 02:05:47 -0500

    package handover: advancedsystemtweaker
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/advancedsystemtweaker/2.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'advancedsystemtweaker'
+$installerType = 'EXE'
+#$url = 'http://www.tweaking.com/files/setups/tweaking.com_advanced_system_tweaker_setup.exe'
+$url = 'http://www.tweaking.com/files/setups/tweaking.com_advanced_system_tweaker_setup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
