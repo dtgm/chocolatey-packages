6d2709e1d03f5691ad327b473a2a9a3a4927afec
dtgm <dtgm1594@gmail.com>
2015-06-30 02:05:47 -0500

    package handover: advancedsystemtweaker
    per: https://github.com/chocolatey/choco/wiki/PackageMantainerHandover
    previous location: https://github.com/adgellida/chocolateyautomaticpackages
    new location: https://github.com/dtgm/chocolatey-packages/tree/master/automatic

---
+++ automatic/_output/advancedsystemtweaker/2.0.0/tools/chocolateyUninstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'advancedsystemtweaker'
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\Tweaking.com\Advanced System Tweaker\uninstall.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles}\Tweaking.com\Advanced System Tweaker\uninstall.exe"
+  }
+  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
