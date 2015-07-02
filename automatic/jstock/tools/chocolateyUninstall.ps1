6207f45bf65e5b02b963904b2e0fcb5dba05537b
dtgm <dtgm1594@gmail.com>
2015-07-01 22:03:19 -0500

    remove unnecessary usage of Get-WmiObject
    https://github.com/dtgm/chocolatey-packages/issues/11

--- automatic/jstock/tools/chocolateyUninstall.ps1
+++ automatic/jstock/tools/chocolateyUninstall.ps1
@@ -1,23 +1,21 @@
-try {
-  $packageName = '{{PackageName}}'
-  $fileType = 'exe'
-  $silentArgs = '/S'
-  $validExitCodes = @(0)
-  $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-  $ahkFile = "$scriptPath\$packageName.ahk"
-  $ahkRun = "$Env:Temp\$(Get-Random).ahk"
-  Copy-Item $ahkFile $ahkRun -Force
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $unPath = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-  } else {
-    $unPath = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
-  }
-  Start-Process 'AutoHotKey' "$ahkRun"
-  $unFile = (Get-ItemProperty $unPath\$packageName* UninstallString).UninstallString
-  Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unFile" -validExitCodes $validExitCodes
-  Remove-Item "$ahkRun"
-} catch {
-  throw $_.Exception 
-}
+$packageName = '{{PackageName}}'
+$packageSearch = "JStock"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile = "$scriptPath\$packageName.ahk"
+$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+Copy-Item $ahkFile $ahkRun -Force
+Start-Process 'AutoHotKey' "$ahkRun"
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch*" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString)" `
+                                               -ValidExitCodes $validExitCodes }
+Remove-Item "$ahkRun" -Force
