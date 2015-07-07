85a9193f30bc03e1197e65d3d70fa68180c4c2b6
dtgm <dtgm1594@gmail.com>
2015-07-07 12:05:10 -0500

    updated 2015.07.06

---
+++ automatic/_output/crystaldiskmark/4.1.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'crystaldiskmark'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://iij.dl.sourceforge.jp/crystaldiskmark/63516/CrystalDiskMark4_1_0-en.exe'
+$checksum = 'da599411f8bb7e2d085a9aa43d3ab829'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/digiKam/4.11.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'digikam'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.kde.org/stable/digikam/digiKam-installer-4.11.0-win32.exe'
+$checksum = '92b0ca67f78650d0fca7eb8523c4a35c7621ad5c'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
