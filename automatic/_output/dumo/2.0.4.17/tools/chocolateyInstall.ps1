8d008a41dea5073035b53c394bad7dacf04ed92d
dtgm <dtgm1594@gmail.com>
2015-07-07 12:07:09 -0500

    updated 2015.07.04

---
+++ automatic/_output/dumo/2.0.4.17/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '455b4e822e82b705c93acf7b60a1a1e3e56dce3b'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/telegram.portable/0.8.38/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.0.8.38.zip'
+$checksum = '164879be19f58ed7a1fa096fe98119ea0f40e1a5'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
