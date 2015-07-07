c05aef67747f5a2f1022afc71b6ef59585053c51
dtgm <dtgm1594@gmail.com>
2015-07-07 14:40:35 -0500

    init: https://chocolatey.org/packages/Gpg4win/2.2.3
    No public repo: https://chocolatey.org/profiles/dmurawsky

---
+++ automatic/_output/gpg4win/2.2.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'Gpg4win'
+$installerType = 'EXE'
+$url = 'http://files.gpg4win.org/gpg4win-2.2.3.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+if (test-path "c:\Program Files (x86)\GNU\GnuPG") {
+	& 'c:\Program Files (x86)\GNU\GnuPG\gpg4win-uninstall.exe' /S
+}
+if (test-path "c:\Program Files\GNU\GnuPG"){
+	& 'c:\Program Files\GNU\GnuPG\gpg4win-uninstall.exe' /S
+}
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
