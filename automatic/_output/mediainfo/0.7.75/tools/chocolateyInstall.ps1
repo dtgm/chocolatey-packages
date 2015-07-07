628b68377462b7ea5c7b1910cdd6db07dea9460f
dtgm <dtgm1594@gmail.com>
2015-07-07 12:07:31 -0500

    updated 2015.07.03

---
+++ automatic/_output/mediainfo/0.7.75/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'mediainfo'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mediaarea.net/download/binary/mediainfo-gui/0.7.75/MediaInfo_GUI_0.7.75_Windows.exe'
+$checksum = '455bed0cf43f1f469733ff971733569f620d23cf'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
