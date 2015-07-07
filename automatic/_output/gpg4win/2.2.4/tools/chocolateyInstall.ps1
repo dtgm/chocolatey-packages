6d103a4dc516946964b43f8313777582cd32eb68
dtgm <dtgm1594@gmail.com>
2015-07-07 16:55:25 -0500

    init
    + converted to an automatic package
    + installer checksum; downloaded files are verified
    - no longer uninstalls existing in default install path
    + uninstall script (only uninstalls version installed by this package)
    + expanded nuspec elements
    + listed bundled software installed in nuspec <description>

---
+++ automatic/_output/gpg4win/2.2.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'gpg4win'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://files.gpg4win.org/gpg4win-2.2.4.exe'
+$checksum = '8ddcbf14eb6df11139f709320a71d197a83bf9e1'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
