d0c932429bc552ac332ca77176e8312a1c2989ec
dtgm <dtgm1594@gmail.com>
2015-07-07 19:22:40 -0500

    init

---
+++ automatic/_output/gpg4win-vanilla/2.2.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'gpg4win-vanilla'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://files.gpg4win.org/gpg4win-vanilla-2.2.4.exe'
+$checksum = '5a702c1b357be6427d96b772f673840e2762d222'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
