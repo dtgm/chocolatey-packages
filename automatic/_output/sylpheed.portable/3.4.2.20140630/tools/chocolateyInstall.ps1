b8b0c5e5da5b6f52071f502d2409d5e249fb7db6
dtgm <dtgm1594@gmail.com>
2015-07-08 09:00:16 -0500

    handover: sylpheed, sylpheed.portable
    https://chocolatey.org/packages/sylpheed/3.4.2.20140629
    https://chocolatey.org/packages/sylpheed.portable/3.4.2.20140630
    
    * no public repo; https://chocolatey.org/profiles/anscom
    * >1 year without updates (2014.06.30 - 2015.07.08)
    * incorrect meta-packages
    * no uninstaller
    * no checksum

---
+++ automatic/_output/sylpheed.portable/3.4.2.20140630/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'sylpheed.portable'
+$url = 'http://jaist.dl.sourceforge.jp/sylpheed/61368/sylpheed-3.4.2-win32.zip'
+$url64 = $url
+try {
+if ( test-path "$env:ProgramFiles(x86)" ) {
+mkdir "$env:programfiles(x86)\Sylpheed"
+$destdir = "$env:programfiles(x86)\Sylpheed"
+}
+else {
+mkdir "$env:programfiles\Sylpheed"
+$destdir = "$env:programfiles\Sylpheed"
+$startmenu = "$env:appdata\Microsoft\Windows\Start Menu\programs"
+}
+} catch {
+Write-ChocolateyFailure $packageName $($_.Exception.Message)
+throw 
+}
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$startmenu\Sylpheed.lnk")
+$Shortcut.TargetPath = "$destdir\Sylpheed-3.4.2\sylpheed.exe"
+$Shortcut.Save()
+Install-ChocolateyZipPackage "$packageName" "$url" "$destdir" "$url64"
