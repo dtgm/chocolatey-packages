e94c0001c07036d3fb63e53eb6aa5e7d710fb2f1
dtgm <dtgm1594@gmail.com>
2015-07-08 01:35:05 -0500

    handover; https://chocolatey.org/packages/freeSSHD/1.2.6
    no public repo; https://chocolatey.org/profiles/dmurawsky
    latest package created 2013.02.05
    latest package version 1.2.6
    latest program version 1.3.1

---
+++ automatic/_output/freesshd/1.2.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'freeSSHD'
+$installerType = 'EXE' 
+$url = 'http://www.freesshd.com/freeSSHd.exe'
+$url64 = $url
+$silentArgs = '/VERYSILENT /NOICON /SUPPRESSMSGBOXES'
+$validExitCodes = @(0) 
+
+# main helpers - these have error handling tucked into them already
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+#try { 
+  #Start-ChocolateyProcessAsAdmin 'firewall add portopening tcp 22 "SSH and sFTP server"' 'netsh'
+    
+  #------- ADDITIONAL SETUP -------#
+  # make sure to uncomment the error handling if you have additional setup to do
+
+  #$processor = Get-WmiObject Win32_Processor
+  #$is64bit = $processor.AddressWidth -eq 64
+
+  
+  # the following is all part of error handling
+  #Write-ChocolateySuccess "$packageName"
+#} catch {
+#  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+#  throw 
+#}
