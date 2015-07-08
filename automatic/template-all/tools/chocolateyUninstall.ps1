5053e321af6468bcd7a7402230c63899f67eba34
dtgm <dtgm1594@gmail.com>
2015-07-07 19:31:57 -0500

    + install4j

--- automatic/template-all/tools/chocolateyUninstall.ps1
+++ automatic/template-all/tools/chocolateyUninstall.ps1
@@ -68,6 +68,25 @@ Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentV
                                                -ValidExitCodes $validExitCodes }
 
 
+### EXE; install4j ###
+$packageName = '{{PackageName}}'
+$packageSearch = "$packageName"
+$installerType = 'exe'
+$silentArgs = '-q'
+$validExitCodes = @(0)
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch*" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString)" `
+                                               -ValidExitCodes $validExitCodes }
+
+
+
 if ( $true ) { "y" } else { "b" }
 
 
