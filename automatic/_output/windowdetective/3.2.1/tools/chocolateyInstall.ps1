2dc72a6c4ba657a41697748b74be025be1174e90
dtgm <dtgm1594@gmail.com>
2015-07-08 10:12:23 -0500

    init: windowdetective

---
+++ automatic/_output/windowdetective/3.2.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = 'windowdetective'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://sourceforge.net/projects/windowdetective/files/release-3.2.1/Window-Detective-3.2.1-setup.exe/download'
+$checksum = 'ecbc6c2036be4242b5bebd447f2d8e4cef2600b0'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
