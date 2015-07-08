a548bd756e4ef98eae8ec4f385f1e39d27dcdbef
dtgm <dtgm1594@gmail.com>
2015-07-08 16:00:33 -0500

    handover; https://chocolatey.org/packages/upx/3.09
    * >2 years since last update
    * current version == 3.91

---
+++ automatic/_output/upx/3.09/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'upx' 
+$installerType = 'EXE' 
+$url = 'http://upx.sourceforge.net/download/upx309d.zip'
+$url64 = $url 
+$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" 
+$validExitCodes = @(0) 
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
