1b11486c5405ea1195b72879c54297dd89daa25c
dtgm <dtgm1594@gmail.com>
2015-07-09 07:37:21 -0500

    init
    efa38b1ac1278e16220c561c9a0c273891b8c0bb

---
+++ automatic/_output/openra/2015.06.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'openra'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com//OpenRA/OpenRA/releases/download/release-20150614/OpenRA-release-20150614.exe'
+$checksum = '9d3ac49b55e4a5a2e44d77783c5b5ca4'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
