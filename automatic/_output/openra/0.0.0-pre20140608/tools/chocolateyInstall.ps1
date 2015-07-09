efa38b1ac1278e16220c561c9a0c273891b8c0bb
dtgm <dtgm1594@gmail.com>
2015-07-08 22:22:17 -0500

    handover: https://chocolatey.org/packages/openra/0.0.0-pre20140608
    * >1 year since last update
    * no stable release

---
+++ automatic/_output/openra/0.0.0-pre20140608/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "openra"
+$url = "https://github.com/OpenRA/OpenRA/releases/download/release-20140608/OpenRA-release-20140608.exe"
+$kind = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $id $kind $silent $url $url64
