86176c9da823e7dc4ba7f33c93a98d75ee7e5ac0
dtgm <dtgm1594@gmail.com>
2015-07-09 21:45:54 -0500

    disable checksum for thebrain due to error in ketarin
    https://github.com/dtgm/chocolatey-packages/issues/25

--- automatic/_output/thebrain.install/8.0.1.800/tools/chocolateyInstall.ps1
+++ automatic/_output/thebrain.install/8.0.1.800/tools/chocolateyInstall.ps1
@@ -1,11 +1,7 @@
 $packageName = 'thebrain.install'
 $installerType = 'exe'
 $url  = 'http://api.thebrain.com/?a=doDirectDownload&id=70'
-$checksum = '9beacdf78523806d5a8960e0d6604505a1df7678'
-$checksumType = 'sha1'
 $url64 = 'http://api.thebrain.com/?a=doDirectDownload&id=76'
-$checksum64 = '{checksumx64}'
-$checksumType64 = 'sha1'
 $silentArgs = '-q -overwrite'
 $validExitCodes = @(0)
 Install-ChocolateyPackage -PackageName "$packageName" `
@@ -13,8 +9,4 @@ Install-ChocolateyPackage -PackageName "$packageName" `
                           -SilentArgs "$silentArgs" `
                           -Url "$url" `
                           -Url64bit "$url64" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -Checksum64 "$checksum64" `
-                          -ChecksumType64 "$checksumType64"
+                          -ValidExitCodes $validExitCodes
--- automatic/_output/thebrain.install/8.0.1.800/tools/chocolateyUninstall.ps1
+++ automatic/_output/thebrain.install/8.0.1.800/tools/chocolateyUninstall.ps1
@@ -1,32 +1,15 @@
-try {
-  $packageName = 'thebrain.install'
-  $packageVersion = '8.0.1.800'
-  $packageId = 'thebrain'
-  $fileType = 'exe'
-  $silentArgs = '-q'
-  $validExitCodes = @(0)
-  # disable uninstalling associated meta packages via 'cuninst' due to moderation
-  ###
-  # remove associated metapackage
-  #$packageMeta = 'thebrain'
-  #$chocoRoot = $Env:ChocolateyInstall
-  #if ($chocoRoot -eq $null) {
-  #	$chocoRoot = "$Env:ProgramData\chocolatey"
-  #}
-  #$chocoLib = Join-Path $chocoRoot 'lib'
-  #if (Test-Path $chocoLib\$packageMeta.$packageVersion) { 
-  #	Remove-Item "$chocoLib\$packageMeta.$packageVersion" -Force -Recurse -ErrorAction SilentlyContinue
-  #	Write-Debug "Deleted $chocoLib\$packageMeta.$packageVersion"
-  #}
-  ###
-  # find uninstaller
-  $unFile = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageId*"}).UninstallString
-  if ($unFile -eq $null) {
-    $unFile = (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageId*"}).UninstallString
-  }
-  # uninstall package
-  Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unFile" -validExitCodes "$validExitCodes"
-} catch {
-    throw $_.Exception
-}
-
+$packageName = 'thebrain.install'
+$packageSearch = "TheBrain "
+$installerType = 'exe'
+$silentArgs = '-q'
+$validExitCodes = @(0)
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch*" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString)" `
+                                               -ValidExitCodes $validExitCodes }
