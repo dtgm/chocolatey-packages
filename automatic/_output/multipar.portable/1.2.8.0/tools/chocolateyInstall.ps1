192dff1eaeb71740788a398b52d2a5c8de8cc11f
dtgm <dtgm1594@gmail.com>
2015-07-16 20:11:59 -0500

    upgraded 2015.07.12

---
+++ automatic/_output/multipar.portable/1.2.8.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'multipar.portable'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1280.zip'
+$checksum = '65363F38F17DC4C5D3AD14FF2C52EE842F96D137'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/wirelessconnectioninfo/1.10/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'wirelessconnectioninfo'
+$url = 'http://www.nirsoft.net/utils/wirelessconnectioninfo.zip'
+$checksum = '77d7279e978681d1f7f042e43d9ce402b679850e'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
