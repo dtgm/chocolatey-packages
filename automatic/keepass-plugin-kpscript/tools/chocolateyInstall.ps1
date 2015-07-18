ec36ed76c745e289c85b27957630079a1ff2b136
dtgm <dtgm1594@gmail.com>
2015-07-18 15:47:24 -0500

    [FIX] BOM marker embedded within other text causes install to fail
    https://github.com/dtgm/chocolatey-packages/issues/31

--- automatic/keepass-plugin-kpscript/tools/chocolateyInstall.ps1
+++ automatic/keepass-plugin-kpscript/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ if ($psver -ge 3) {
 } else {
   function Get-ChildItemDir {Get-ChildItem $args}
 }
-﻿$packageName = '{{PackageName}}'
+$packageName = '{{PackageName}}'
 $packageSearch = 'KeePass Password Safe'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
