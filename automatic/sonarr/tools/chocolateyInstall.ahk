864488d9fb6a4f2d989c2833f34496ae543d5d3e
dtgm <dtgm1594@gmail.com>
2015-07-19 01:32:03 -0500

    new packages: winauth,initool,autocomplete,mediainfo-cli,sonarr,duplicatecommander

---
+++ automatic/sonarr/tools/chocolateyInstall.ahk
@@ -0,0 +1,28 @@
+; default environment
+DetectHiddenWindows, off
+SetControlDelay, 20
+
+; modified environment
+#NoEnv
+#NoTrayIcon
+DetectHiddenText, off
+
+; variables
+procName = Nzbdrone.master.exe
+winConsole = ahk_class ConsoleWindowClass ahk_exe NzbDrone.Console.exe
+
+; window handling
+Process, wait, %procName%, 15
+nPID = %ErrorLevel%
+if (nPID = 0) {
+  return
+}
+Loop {
+  WinWait, %winConsole%, , 1
+  WinMinimize, %winConsole%
+  Process, Exist, %procName%
+  nPID = %ErrorLevel%
+  if (nPID = 0) {
+    break
+  }
+}
---
+++ automatic/winauth/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "WinAuth.exe"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
