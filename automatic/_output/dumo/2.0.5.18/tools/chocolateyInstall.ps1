2b460170635f8760cab4cc0ca41c7ca66ea159ed
dtgm <dtgm1594@gmail.com>
2015-07-19 05:48:39 -0500

    upgraded 2015.07.19

---
+++ automatic/_output/dumo/2.0.5.18/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = 'ad7bd4a2a7bcb9a8919daa29923e35a30a9cac98'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
