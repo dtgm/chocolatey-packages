2023a130f12c388414e77501d5c31a21a673185f
dtgm <dtgm1594@gmail.com>
2015-07-19 08:10:14 -0500

    new package: datamash

---
+++ automatic/_output/datamash/1.0.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'datamash'
+$url = 'http://files.housegordon.org/datamash/bin/datamash-1.0.6-windows.zip'
+$checksum = '6874c749945acb84ac0fd9b759f382a7fe64dd7b'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
