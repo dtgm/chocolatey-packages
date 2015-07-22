5567e34b37056286843463f9a4991dc8fb6b5686
dtgm <dtgm1594@gmail.com>
2015-07-22 01:35:21 -0500

    new prerelease package: dolphin

---
+++ automatic/_output/dolphin/4.0.7068/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-7068-x64.7z'
+$checksum = '440d76057933f50e7ebcbbc7b219b3665d7530fc'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
