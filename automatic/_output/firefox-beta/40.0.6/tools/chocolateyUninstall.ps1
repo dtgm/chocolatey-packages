fcc4182b38504b6262939bd457b4080f0519122e
dtgm <dtgm1594@gmail.com>
2015-07-22 01:21:34 -0500

    upgraded 2015.07.21: retroshare, firefox-beta

--- automatic/_output/firefox-beta/40.0.6/tools/chocolateyUninstall.ps1
+++ automatic/_output/firefox-beta/40.0.6/tools/chocolateyUninstall.ps1
@@ -1,16 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$packageSearch = "RetroShare"
+$packageName = 'firefox-beta'
+$packageSearch = "Firefox Beta*"
 $installerType = 'exe'
-$silentArgs = '/S'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
 
 Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                           'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
                           'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
                  -ErrorAction:SilentlyContinue `
-| Where-Object   { $_.DisplayName -like "$packageSearch" } `
+| Where-Object   { $_.DisplayName -like $packageSearch } `
 | ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
                                                -FileType "$installerType" `
-                                               -SilentArgs "$($silentArgs)" `
+                                               -SilentArgs "$silentArgs" `
                                                -File "$($_.UninstallString)" `
                                                -ValidExitCodes $validExitCodes }
