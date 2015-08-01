2e6dbf9169e6b47cfa5ff4d01733413bb5e1f09f
dtgm <dtgm1594@gmail.com>
2015-08-01 06:28:35 -0500

    upgraded 2015.07.28: aida64-networkaudit, aida64-engineer, aida64-business, aida64-extreme, dolphin, kvrt, nirlauncher

---
+++ automatic/_output/aida64-networkaudit/5.30.3500/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'aida64-networkaudit'
+$url = 'http://download.aida64.com/aida64networkaudit530.zip'
+$checksum = '871671e4d9c3f4c4266b20c5c5ced679b073613c'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "aida64.exe"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
