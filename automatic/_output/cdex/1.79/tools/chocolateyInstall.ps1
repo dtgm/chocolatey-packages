b5f4816e139dd4fb9331b3188e18a906998a86a4
dtgm <dtgm1594@gmail.com>
2015-08-31 22:27:10 -0600

    [FIX] cdex URL updated
    https://github.com/dtgm/chocolatey-packages/issues/41

--- automatic/_output/cdex/1.79/tools/chocolateyInstall.ps1
+++ automatic/_output/cdex/1.79/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$packageName = '{{PackageName}}'
+$packageName = 'cdex'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'http://mirror.cdex.mu/CDex-1.79-win32.exe'
+$checksum = 'e1e718bb1f3bd67f5ada0f6e1eff34d8d9ade0b4'
 $checksumType = 'sha1'
 $validExitCodes = @(0)
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
