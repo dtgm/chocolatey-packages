cee579f9227a3247ea5f37d08b9844dee3fc15e4
dtgm <dtgm1594@gmail.com>
2015-09-29 13:53:26 -0600

    Updated on 2015-01-27: advancedsystemcarefree

---
+++ automatic/_output/advancedsystemcarefree/6.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'advancedsystemcarefree'
+$installerType = 'EXE'
+#$url = 'http://download.iobit.com/asc-setup.exe'
+$url = 'http://download.iobit.com/asc-setup.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
