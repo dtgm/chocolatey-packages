e19f45c77b7e1b2d8213494ecdafbb081ec147d4
dtgm <dtgm1594@gmail.com>
2015-09-30 08:15:56 -0600

    Updated checksum. Up-revved to alert security mod as I am not sure if checksum is original.

---
+++ automatic/_output/fontforge/2015.08.24.20150930/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'fontforge'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/fontforge/fontforge/releases/download/20150824/FontForge-2015-08-24-Windows.exe'
+$checksum = '4c2c26bc9fc31e9206a2a555aa28dc73'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
