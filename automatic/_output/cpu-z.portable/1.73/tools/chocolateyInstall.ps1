507447a14939c1c19e5a4c330cc99c978988712e
dtgm <dtgm1594@gmail.com>
2015-09-30 10:10:21 -0600

    Fixed scraping: https://github.com/dtgm/chocolatey-packages/issues/51

---
+++ automatic/_output/cpu-z.portable/1.73/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'cpu-z.portable'
+$url = 'http://download.cpuid.com/cpu-z/cpu-z_1.73-en.zip'
+$checksum = 'c2c2a3011bf555ac770c229d0bfc209feb293c77'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile32 = Join-Path $toolsDir "cpuz_x32.exe"
+$installFile64 = Join-Path $toolsDir "cpuz_x64.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile32.gui") `
+            -Value $null
+Set-Content -Path ("$installFile64.gui") `
+            -Value $null
