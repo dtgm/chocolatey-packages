8ade668ec248e254bed2a0964d2923e51e756dc8
dtgm <dtgm1594@gmail.com>
2015-09-30 12:56:44 -0600

    datacrow migrated to Fosshub from SF: https://github.com/dtgm/chocolatey-packages/issues/51

--- automatic/_output/datacrow/4.1.0/tools/chocolateyInstall.ps1
+++ automatic/_output/datacrow/4.1.0/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$packageName = '{{PackageName}}'
+$packageName = 'datacrow'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = '{{PackageGuid}}'
-$checksum = '{{Checksum}}'
+$url = 'http://www.fosshub.com/genLink/Data-Crow/datacrow_4_1_0_windows_installer.zip'
+$checksum = 'e1e6cf5f867de776786ba71d304a5af3809e90f9'
 $checksumType = 'sha1'
 $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -13,7 +13,7 @@ $install64Opts = "$toolsDir\setup64bit.xml"
 $chocoTempDir = Join-Path $Env:Temp "chocolatey"
 $tempDir = Join-Path $chocoTempDir "$packageName"
 if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-$zipFile = Join-Path $tempDir '{{DownloadUrlx64}}'
+$zipFile = Join-Path $tempDir 'datacrow_4_1_0_windows_installer.zip'
 
 try {
   # Combatibility - This function has not been merged
