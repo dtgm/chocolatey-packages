6e2e1c746e7811fe886dbba98b49ea026388a74f
dtgm <dtgm1594@gmail.com>
2015-09-30 15:56:49 -0600

    new package: bitmeteros

---
+++ automatic/_output/bitmeteros/0.7.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,40 @@
+$packageName = 'bitmeteros'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://codebox.org.uk/downloads/bitmeteros/windows'
+$checksum = 'f17bfeb01755465c0293746049c7c2f6e231d3ef'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+$chocoTempDir = Join-Path $Env:Temp "chocolatey"
+$pkgTempDir = Join-Path $chocoTempDir "$packageName"
+if (![System.IO.Directory]::Exists($pkgTempDir)) {[System.IO.Directory]::CreateDirectory($pkgTempDir)}
+$zipFile = Join-Path $pkgTempDir "$($packageName)Install.zip"
+$installFile = Join-Path $pkgTempDir "BitMeterOSInstaller.exe"
+
+Write-Debug "Downloading to $($zipFile)..."
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$zipFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Write-Debug "Extracting $($zipFile) to $($pkgTempDir)..."
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$pkgTempDir" `
+                    -SpecificFolder "" `
+                    -PackageName "$packageName"
+
+Write-Verbose "Starting AutoHotKey for assistance in silent and/or automated install"
+Write-Verbose "Actively using the computer while AutoHotKey is running may interfere with automation"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile = Join-Path $toolsDir "chocolateyInstall.ahk"
+$ahkExe = 'AutoHotKey'
+Start-Process $ahkExe $ahkFile
+
+Write-Debug "Installing $($installFile)..."
+Install-ChocolateyInstallPackage -PackageName "$packageName" `
+                                 -FileType "$installerType" `
+                                 -SilentArgs "$silentArgs" `
+                                 -File "$installFile" `
+                                 -ValidExitCodes $validExitCodes 
