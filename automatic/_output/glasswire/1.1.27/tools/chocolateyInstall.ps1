e17808d3a7bd812b6d64f3fff737368d916e6054
dtgm <dtgm1594@gmail.com>
2015-10-03 16:15:01 -0600

    Updated: https://github.com/dtgm/chocolatey-packages/issues/51

---
+++ automatic/_output/glasswire/1.1.27/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://www.glasswire.com/download/GlassWireSetup.exe'
+$checksum = 'DA45B7A6E555CED11BD527D706DE8D81'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
