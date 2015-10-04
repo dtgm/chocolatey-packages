41d90b89619f5a70c9fdcc264eeda32eb6a8765f
dtgm <dtgm1594@gmail.com>
2015-10-04 07:17:43 -0600

    Package handover: bacula
    Last updated 20140823
    Was never installable
    Previous maintainer: https://chocolatey.org/profiles/utrecht

---
+++ automatic/bacula/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+$packageName = '{{PackageName}}'
+$packageSearch = 'Bacula'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkFile = Join-Path $scriptPath "baculaUninstall.ahk"
+$ahkExe = 'AutoHotKey'
+$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+Copy-Item $ahkFile "$ahkRun" -Force
+Start-Process $ahkExe $ahkRun
+
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$silentArgs" `
+                                               -File "$($_.UninstallString)" `
+                                               -ValidExitCodes $validExitCodes }
+Remove-Item "$ahkRun" -Force
