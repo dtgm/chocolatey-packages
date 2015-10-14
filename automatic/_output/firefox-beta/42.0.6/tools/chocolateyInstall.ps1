1b2c4dc89527a7db2c114c12740f3fe6eabde4b0
dtgm <dtgm1594@gmail.com>
2015-10-14 04:33:10 -0600

    Updated on 2015-10-13: dolphin,firefox-beta,kvrt

---
+++ automatic/_output/firefox-beta/42.0.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$packageName = 'firefox-beta'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/42.0b6/win32/en-US/Firefox Setup 42.0b6.exe'
+$url64 = 'https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/42.0b6/win64/en-US/Firefox%20Setup%2042.0b6.exe'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes
