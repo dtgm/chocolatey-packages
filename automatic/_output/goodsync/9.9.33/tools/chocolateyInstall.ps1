66d8b4c5f363595eb23f16d9b63fb8633e3ba1db
dtgm <dtgm1594@gmail.com>
2015-10-23 23:03:10 -0600

    Updated on 2015-10-23: dolphin,evernote2onenote,goodsync,multipar,multipar.install,multipar.portable

---
+++ automatic/_output/goodsync/9.9.33/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'goodsync'
+$fileType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
+$checksum = 'bcd50a91efbfd6fc93b2efb86e8f0baa1a58ac11'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$fileType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
