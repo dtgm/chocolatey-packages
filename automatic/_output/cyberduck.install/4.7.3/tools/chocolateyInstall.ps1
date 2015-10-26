40064baf26286dcdf426c546bcf9827d8343353d
dtgm <dtgm1594@gmail.com>
2015-10-25 18:33:10 -0600

    Updated on 2015-10-25: cyberduck,cyberduck.install

---
+++ automatic/_output/cyberduck.install/4.7.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-4.7.3.exe'
+$checksum = 'ab48cc3ee1e98fcc75dcdd20f5bb7c0b0aae0336'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
