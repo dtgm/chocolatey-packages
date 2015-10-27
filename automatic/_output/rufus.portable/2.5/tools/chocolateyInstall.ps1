fccbb5977507cff6ade8151856b7980038e912bb
dtgm <dtgm1594@gmail.com>
2015-10-26 23:03:09 -0600

    Updated on 2015-10-26: nirlauncher,rufus,rufus.install,rufus.portable

---
+++ automatic/_output/rufus.portable/2.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'rufus.portable'
+$url = 'http://rufus.akeo.ie/downloads/rufus-2.5p.exe'
+$checksum = '33ea32d49411ce016a0a6c74565c1468fe517375'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "rufus.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null
+} catch {
+  throw $_.Exception
+}
