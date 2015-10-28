833051d2edda0392eab7862f686edcedc6cc0d87
dtgm <dtgm1594@gmail.com>
2015-10-28 06:03:27 -0600

    Updated on 2015-10-28: dolphin,kvrt,qttabbar,rammap

---
+++ automatic/_output/rammap/1.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+$packageName = 'rammap'
+$url = 'https://download.sysinternals.com/files/RAMMap.zip'
+$checksum = 'ed2f9cb5e54b59460ef0820e7ae8affbd8b8e3e5'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'RamMap'
+$regPath = Join-Path $regRoot $regPkg
+if (! Test-Path $regRoot) {New-Item -Path "$regRoot"}
+if (! Test-Path $regPath) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
