e77f700d102b8232c93195e0716a8c24120ff29f
dtgm <dtgm1594@gmail.com>
2015-10-28 16:03:09 -0600

    Updated on 2015-10-28: exiftool

---
+++ automatic/_output/exiftool/10.04/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.04.zip'
+$checksum = 'b20352c9b5ed5b9a4f050f0599d58217f216411a'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
