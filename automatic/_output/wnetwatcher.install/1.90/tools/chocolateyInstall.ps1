2f7042310c745a078d1232cd854a350357358f61
dtgm <dtgm1594@gmail.com>
2015-10-27 18:33:12 -0600

    Updated on 2015-10-27: adobeshockwaveplayer,wnetwatcher,wnetwatcher.install,wnetwatcher.portable

---
+++ automatic/_output/wnetwatcher.install/1.90/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'wnetwatcher.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher_setup.exe'
+$checksum = 'd5ec0e8b538539bc1867ac12396c560eddcb974c'
+$checksumType = 'sha1'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
