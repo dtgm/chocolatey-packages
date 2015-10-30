49c78a2b4bff33de91c87a7ea34adf065572287b
dtgm <dtgm1594@gmail.com>
2015-10-30 11:33:08 -0600

    Updated on 2015-10-30: dolphin,opensavefilesview

---
+++ automatic/_output/dolphin/4.0.8074/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8074-x64.7z'
+$checksum = '007d2e05610c018493b0631bcf82789303cee22d'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
