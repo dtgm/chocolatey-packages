5b61fa5c4bf762bba335d53c2e3a0fcd62d524cb
dtgm <dtgm1594@gmail.com>
2015-11-06 20:58:49 -0700

    Updated on 2015-11-02: mediainfo,mediainfo-cli,windowsrepair

---
+++ automatic/_output/mediainfo/0.7.79/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'mediainfo'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mediaarea.net/download/binary/mediainfo-gui/0.7.79/MediaInfo_GUI_0.7.79_Windows.exe'
+$checksum = '6fcf6010927209e181bf6aa8e4c0a0db84f4bb11'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
