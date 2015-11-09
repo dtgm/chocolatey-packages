73849a85b74512c2041d56a4dd1cdf97cfd91917
dtgm <dtgm1594@gmail.com>
2015-11-09 01:03:07 -0700

    Updated on 2015-11-08: csvfileview

---
+++ automatic/_output/csvfileview/2.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'csvfileview'
+$url = 'http://www.nirsoft.net/utils/csvfileview.zip'
+$checksum = '88116a9de3b65716b6279524a174e092c8491a9b'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
