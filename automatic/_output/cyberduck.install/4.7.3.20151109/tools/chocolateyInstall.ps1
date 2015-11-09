752bed8b7691bb664ccaf903ea331e3de4961c39
dtgm <dtgm1594@gmail.com>
2015-11-09 15:11:52 -0700

    FIX: https://github.com/dtgm/chocolatey-packages/issues/62

---
+++ automatic/_output/cyberduck.install/4.7.3.20151109/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-4.7.3.exe'
+$checksum = '2af6ef22de8799567a9fc3dbf89db977e591b743'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
