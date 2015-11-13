6e2484165a6f534d843233f40e9cab9735c3029e
dtgm <dtgm1594@gmail.com>
2015-11-13 01:03:09 -0700

    Updated on 2015-11-12: cmake,cmake.install,cmake.portable,thebrain,thebrain.install

---
+++ automatic/_output/cmake.portable/3.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'cmake.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://cmake.org//files/v3.4/cmake-3.4.0-win32-x86.zip'
+$checksum = '6b5096625cae7596e81566a00cab292f15adb9d7'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
