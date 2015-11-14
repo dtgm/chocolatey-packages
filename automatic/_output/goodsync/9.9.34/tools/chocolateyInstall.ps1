a74070db1ab64d807b87b776a6ad83ff542c1489
dtgm <dtgm1594@gmail.com>
2015-11-14 01:33:08 -0700

    Updated on 2015-11-13: goodsync

---
+++ automatic/_output/goodsync/9.9.34/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'goodsync'
+$fileType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
+$checksum = 'caf4fe82cfeb68b8343321d2b5fdd82493951f54'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$fileType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
