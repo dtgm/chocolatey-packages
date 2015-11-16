07a23a0269ccb118ee8f89e1fb22c4412a72a2dd
dtgm <dtgm1594@gmail.com>
2015-11-16 01:33:08 -0700

    Updated on 2015-11-15: ddu,dolphin

---
+++ automatic/_output/dolphin/4.0.8157/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8157-x64.7z'
+$checksum = '69b5e4d56e373c5db584b9be06883f053217d444'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
