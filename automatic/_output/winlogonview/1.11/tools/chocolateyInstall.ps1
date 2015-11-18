0d2b6865b2433f9040e8978b1958e4800fb39c7c
dtgm <dtgm1594@gmail.com>
2015-11-18 08:03:08 -0700

    Updated on 2015-11-18: kvrt,winlogonview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/winlogonview

---
+++ automatic/_output/winlogonview/1.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'winlogonview'
+$url = 'http://www.nirsoft.net/utils/winlogonview.zip'
+$checksum = '38d565d00cf528cb059563dcf7841b83e62be308'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
