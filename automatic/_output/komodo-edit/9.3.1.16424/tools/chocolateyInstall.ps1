45566fece0b4b833241a733ce82ef5c99ab66b1f
dtgm <dtgm1594@gmail.com>
2015-11-19 01:03:11 -0700

    Updated on 2015-11-18: dolphin,komodo-edit
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/komodo-edit

---
+++ automatic/_output/komodo-edit/9.3.1.16424/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'komodo-edit'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'http://downloads.activestate.com/Komodo/releases/9.3.1/Komodo-Edit-9.3.1-16424.msi'
+$checksum = '5d86161aa6f51e51a15b1089ed3cf794'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
