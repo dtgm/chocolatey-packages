f65ee1c06ec78f669c46b9e2b25e73256b17aec1
dtgm <dtgm1594@gmail.com>
2015-11-23 18:03:16 -0700

    Updated on 2015-11-23: dolphin,exifdataview,krita,kvrt,mariadb,nirlauncher
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/exifdataview
    https://chocolatey.org/packages/krita
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/mariadb
    https://chocolatey.org/packages/nirlauncher

---
+++ automatic/_output/exifdataview/1.05/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'exifdataview'
+$url = 'http://www.nirsoft.net/utils/exifdataview.zip'
+$checksum = '146bf58f20143cc8207033e6bdbd7f95f19ba24f'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
