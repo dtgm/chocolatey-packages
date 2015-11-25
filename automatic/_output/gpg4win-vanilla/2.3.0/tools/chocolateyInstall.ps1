dcd2ead714b5a7832b481afdb236f92b63236ad2
dtgm <dtgm1594@gmail.com>
2015-11-25 11:33:28 -0700

    Updated on 2015-11-25: adobeshockwaveplayer,dolphin,gpg4win,gpg4win-light,gpg4win-vanilla,kvrt,ums
    https://chocolatey.org/packages/adobeshockwaveplayer
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/gpg4win
    https://chocolatey.org/packages/gpg4win-light
    https://chocolatey.org/packages/gpg4win-vanilla
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/ums

---
+++ automatic/_output/gpg4win-vanilla/2.3.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'gpg4win-vanilla'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://files.gpg4win.org/gpg4win-vanilla-2.3.0.exe'
+$checksum = '5b73d46aba8a33eac9bcea46e003f55e2cda64fc'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
