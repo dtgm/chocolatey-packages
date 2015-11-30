56b303ad3b2773c0e23c6995678654c2d8725b6a
dtgm <dtgm1594@gmail.com>
2015-11-30 01:33:11 -0700

    Updated on 2015-11-29: ddu,dolphin,kcleaner
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kcleaner

---
+++ automatic/_output/kcleaner/2.6.3.65/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '11336cc5bdfa8fa14a26e02da38174e790347313'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
