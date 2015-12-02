17dc2bb31d7a8689e2a144685c06e37ac5149f8f
dtgm <dtgm1594@gmail.com>
2015-12-02 01:33:08 -0700

    Updated on 2015-12-01: stellarium,superantispyware
    https://chocolatey.org/packages/stellarium
    https://chocolatey.org/packages/superantispyware

---
+++ automatic/_output/stellarium/0.14.1/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+$packageName = 'stellarium'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+$unPath = "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$unPathx86 = "HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+try {
+  $osBitness = Get-ProcessorBits
+  if ($osBitness -eq 64) {
+    if ($unString=(Get-ItemProperty "$unpath\$packageName*" UninstallString).UninstallString) {}
+    else { $unString=(Get-ItemProperty "$unPathx86\$packageName*" UninstallString).UninstallString }
+  } else {
+    $unString = (Get-ItemProperty "$unPath\$packageName*" UninstallString).UninstallString
+  }
+  Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes $validExitCodes
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/superantispyware/6.0.1210/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$packageName = 'superantispyware'
+$installerType = 'exe'
+# ver 6.0.1194 fails on 8.1-64
+#$silentArgs = '/SILENT'
+$silentArgs = ''
+$url = 'http://cdn.superantispyware.com/SUPERAntiSpyware.exe'
+$checksum = 'e0cc8efe95e4443077bc8b5552deff02ca0d945c'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
