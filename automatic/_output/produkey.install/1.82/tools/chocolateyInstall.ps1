998dacff4e6f8192dc13fc85fc9f03fdc0bd19f4
dtgm <dtgm1594@gmail.com>
2015-12-01 18:03:08 -0700

    Updated on 2015-12-01: dolphin,monodevelop,produkey,produkey.install,produkey.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/monodevelop
    https://chocolatey.org/packages/produkey
    https://chocolatey.org/packages/produkey.install
    https://chocolatey.org/packages/produkey.portable

---
+++ automatic/_output/produkey.install/1.82/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'produkey.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/produkey_setup.exe'
+$checksum = '32640e9f2edf46430d16cc36b5e1b0a3b16b05ec'
+$checksumType = 'sha1'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
