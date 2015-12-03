f19454a6d020d10ff51a23b90ea5cc2122626d50
dtgm <dtgm1594@gmail.com>
2015-12-03 01:03:08 -0700

    Updated on 2015-12-02: cmake,cmake.install,cmake.portable,dolphin,wifiinfoview
    https://chocolatey.org/packages/cmake
    https://chocolatey.org/packages/cmake.install
    https://chocolatey.org/packages/cmake.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/wifiinfoview

---
+++ automatic/_output/cmake.portable/3.4.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'cmake.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://cmake.org//files/v3.4/cmake-3.4.1-win32-x86.zip'
+$checksum = '4894baeafc0368d6530bf2c6bfe4fc94056bd04a'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
