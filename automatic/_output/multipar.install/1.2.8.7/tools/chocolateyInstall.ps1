5c80954affa967745ff831442e375b6c10882dc2
dtgm <dtgm1594@gmail.com>
2015-12-06 01:33:11 -0700

    Updated on 2015-12-05: multipar,multipar.install,multipar.portable
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable

---
+++ automatic/_output/multipar.install/1.2.8.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar128_setup.exe'
+$checksum = '70CA32CFCB19FCC9230646F6FD524CEEB4E703C1'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
