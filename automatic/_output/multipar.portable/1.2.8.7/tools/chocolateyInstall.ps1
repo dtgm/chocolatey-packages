09f41c97a9b97f1463a6dda1728bb4170fd39477
dtgm <dtgm1594@gmail.com>
2015-12-06 01:33:11 -0700

    Updated on 2015-12-05: multipar,multipar.install,multipar.portable
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable

---
+++ automatic/_output/multipar.portable/1.2.8.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'multipar.portable'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar128.zip'
+$checksum = '4BA5F5ACA8EBE531702FBCB31EAF3276586FB043'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
