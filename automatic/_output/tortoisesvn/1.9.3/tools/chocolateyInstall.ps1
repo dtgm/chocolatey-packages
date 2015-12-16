988ef87cdf59101b4acbb1d0bb53e50157f12d62
dtgm <dtgm1594@gmail.com>
2015-12-15 18:06:17 -0700

    Updated on 2015-12-15: dolphin,kvrt,searchmyfiles,tortoisesvn,videocacheview.install
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/searchmyfiles
    https://chocolatey.org/packages/tortoisesvn
    https://chocolatey.org/packages/videocacheview.install

---
+++ automatic/_output/tortoisesvn/1.9.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'tortoisesvn'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart ADDLOCAL=ALL'
+$urlArray = @('https://sourceforge.net/projects/tortoisesvn/files/1.9.3/Application/TortoiseSVN-1.9.3.27038-win32-svn-1.9.3.msi', 'https://sourceforge.net/projects/tortoisesvn/files/1.9.3/Application/TortoiseSVN-1.9.3.27038-x64-svn-1.9.3.msi')
+$url = $urlArray[0]
+$checksum = '{checksum}'
+$checksumType = 'sha1'
+$url64 = $urlArray[1]
+$checksum64 = '{checksumx64}'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
