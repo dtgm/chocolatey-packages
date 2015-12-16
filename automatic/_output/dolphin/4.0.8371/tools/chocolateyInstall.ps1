066f8e9d8e49257aa0fd5559600f6f55ecebe589
dtgm <dtgm1594@gmail.com>
2015-12-16 01:33:20 -0700

    Updated on 2015-12-15: dolphin,gramps,tortoisesvn,windowsrepair
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/gramps
    https://chocolatey.org/packages/tortoisesvn
    https://chocolatey.org/packages/windowsrepair

---
+++ automatic/_output/dolphin/4.0.8371/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8371-x64.7z'
+$checksum = '4cc176f5ef47727756b749a654be267f37e9f536'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
