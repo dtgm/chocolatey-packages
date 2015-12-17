3551bd8b8f326fb227d0172f033be1558ad8900b
dtgm <dtgm1594@gmail.com>
2015-12-17 01:03:22 -0700

    Updated on 2015-12-16: dolphin,kvrt,linkshellextension
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/linkshellextension

---
+++ automatic/_output/dolphin/4.0.8375/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8375-x64.7z'
+$checksum = '1f71eb86147b666e640b98b6f35638f33ecdb7cc'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
