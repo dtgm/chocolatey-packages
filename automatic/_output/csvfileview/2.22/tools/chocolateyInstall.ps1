a3bb79a538bcef4c31d5d1c37157facce3db2079
dtgm <dtgm1594@gmail.com>
2015-12-17 18:33:18 -0700

    Updated on 2015-12-17: arduino,csvfileview,kvrt
    https://chocolatey.org/packages/arduino
    https://chocolatey.org/packages/csvfileview
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/csvfileview/2.22/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'csvfileview'
+$url = 'http://www.nirsoft.net/utils/csvfileview.zip'
+$checksum = '8cef9ba5736358654aa8c4726ef44512c4e5717b'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
