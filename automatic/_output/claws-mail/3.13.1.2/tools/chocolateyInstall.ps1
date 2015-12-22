50c89c12efa7af75381ddfb3ded7cad19d4d9196
dtgm <dtgm1594@gmail.com>
2015-12-22 01:03:07 -0700

    Updated on 2015-12-21: claws-mail,ddu,dispcalgui,dolphin,kvrt
    https://chocolatey.org/packages/claws-mail
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dispcalgui
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/claws-mail/3.13.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'claws-mail'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://www.claws-mail.org/win32/claws-mail-3.13.1-2.exe'
+$checksum = '9e01c6597e861a48e2f8a10fbfa56f1e80c1da3b'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
