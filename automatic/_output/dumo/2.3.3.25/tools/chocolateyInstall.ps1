1016087eaea9e0c3660fe89f2a25d9f9d460e19d
dtgm <dtgm1594@gmail.com>
2015-12-22 18:03:09 -0700

    Updated on 2015-12-22: adwcleaner,dolphin,dumo,exiftool,kvrt,love,love.install,love.portable,wnetwatcher,wnetwatcher.install,wnetwatcher.portable
    https://chocolatey.org/packages/adwcleaner
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/exiftool
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/love
    https://chocolatey.org/packages/love.install
    https://chocolatey.org/packages/love.portable
    https://chocolatey.org/packages/wnetwatcher
    https://chocolatey.org/packages/wnetwatcher.install
    https://chocolatey.org/packages/wnetwatcher.portable

---
+++ automatic/_output/dumo/2.3.3.25/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = 'e2d16089a7c0633f8e3058895a11f7560c849736'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/wnetwatcher.portable/1.91/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'wnetwatcher.portable'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher.zip'
+$checksum = '56210b2164fa440501e25a8f66926d7feb9bc939'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "wnetwatcher.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
