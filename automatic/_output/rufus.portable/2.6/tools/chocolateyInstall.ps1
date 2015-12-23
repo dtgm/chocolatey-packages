93f6a5008dc262f17e2d5c2d215d159e8608df84
dtgm <dtgm1594@gmail.com>
2015-12-23 01:33:07 -0700

    Updated on 2015-12-22: dolphin,kvrt,pwgen.portable,rufus,rufus.install,rufus.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/pwgen.portable
    https://chocolatey.org/packages/rufus
    https://chocolatey.org/packages/rufus.install
    https://chocolatey.org/packages/rufus.portable

---
+++ automatic/_output/rufus.portable/2.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'rufus.portable'
+$url = 'http://rufus.akeo.ie/downloads/rufus-2.6p.exe'
+$checksum = 'a1842404aa696c90cd626df13d84a799df713dbb'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "rufusp.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null
+} catch {
+  throw $_.Exception
+}
