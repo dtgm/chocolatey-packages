1822769d48ff995fd57c9793ee11357e66ae54a0
dtgm <dtgm1594@gmail.com>
2015-12-24 22:54:52 -0700

    FIX: tflash, turnflash mixup

--- automatic/_output/turnflash/1.00/tools/chocolateyInstall.ps1
+++ automatic/_output/turnflash/1.00/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$packageName = 'turnflash'
+$url = 'http://www.nirsoft.net/utils/tflash.zip'
+$checksum = '0f322ad8750128ab96f329915baac31ae047129a'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
