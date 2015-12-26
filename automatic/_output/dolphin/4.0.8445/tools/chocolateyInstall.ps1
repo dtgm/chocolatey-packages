642a7e8e472e561585fdcc8fad1eac733c28fa6f
dtgm <dtgm1594@gmail.com>
2015-12-26 01:03:09 -0700

    Updated on 2015-12-25: dolphin,goodsync,kvrt,sqlitebrowser
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/sqlitebrowser

---
+++ automatic/_output/dolphin/4.0.8445/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8445-x64.7z'
+$checksum = '797eff8f0f9cb7cadabd745f86370837f4fbf19f'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/goodsync/9.9.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'goodsync'
+$fileType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
+$checksum = '944ad16be1453450a24398e2bfb6e985b1accf73'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$fileType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
