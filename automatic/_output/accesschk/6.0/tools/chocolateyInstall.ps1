c9da82f48908664c53e1a8752f3982589c9c8f55
dtgm <dtgm1594@gmail.com>
2015-12-28 01:03:10 -0700

    Updated on 2015-12-28: accesschk,adrestore,autologon,autoruns,bginfo,cacheset,clockres,contig,coreinfo,ctrl2cap,desktops,disk2vhd,diskext,diskmon,diskview,efsdump,findlinks,handle,hex2dec,junction,ldmdump,listdlls,livekd,logonsessions,movefile,pendmoves,pipelist,portmon,procdump,psexec,psfile,psgetsid,psinfo,pskill,pslist,psloggedon,psloglist,pspasswd,psping,psservice,psshutdown,pssuspend,rammap,regdelnull,regjump,ru,sdelete,shareenum,shellrunas,sigcheck,streams,sync,tcpvcon,vmmap,volumeid,whois,winobj
    https://chocolatey.org/packages/accesschk
    https://chocolatey.org/packages/adrestore
    https://chocolatey.org/packages/autologon
    https://chocolatey.org/packages/autoruns
    https://chocolatey.org/packages/bginfo
    https://chocolatey.org/packages/cacheset
    https://chocolatey.org/packages/clockres
    https://chocolatey.org/packages/contig
    https://chocolatey.org/packages/coreinfo
    https://chocolatey.org/packages/ctrl2cap
    https://chocolatey.org/packages/desktops
    https://chocolatey.org/packages/disk2vhd
    https://chocolatey.org/packages/diskext
    https://chocolatey.org/packages/diskmon
    https://chocolatey.org/packages/diskview
    https://chocolatey.org/packages/efsdump
    https://chocolatey.org/packages/findlinks
    https://chocolatey.org/packages/handle
    https://chocolatey.org/packages/hex2dec
    https://chocolatey.org/packages/junction
    https://chocolatey.org/packages/ldmdump
    https://chocolatey.org/packages/listdlls
    https://chocolatey.org/packages/livekd
    https://chocolatey.org/packages/logonsessions
    https://chocolatey.org/packages/movefile
    https://chocolatey.org/packages/pendmoves
    https://chocolatey.org/packages/pipelist
    https://chocolatey.org/packages/portmon
    https://chocolatey.org/packages/procdump
    https://chocolatey.org/packages/psexec
    https://chocolatey.org/packages/psfile
    https://chocolatey.org/packages/psgetsid
    https://chocolatey.org/packages/psinfo
    https://chocolatey.org/packages/pskill
    https://chocolatey.org/packages/pslist
    https://chocolatey.org/packages/psloggedon
    https://chocolatey.org/packages/psloglist
    https://chocolatey.org/packages/pspasswd
    https://chocolatey.org/packages/psping
    https://chocolatey.org/packages/psservice
    https://chocolatey.org/packages/psshutdown
    https://chocolatey.org/packages/pssuspend
    https://chocolatey.org/packages/rammap
    https://chocolatey.org/packages/regdelnull
    https://chocolatey.org/packages/regjump
    https://chocolatey.org/packages/ru
    https://chocolatey.org/packages/sdelete
    https://chocolatey.org/packages/shareenum
    https://chocolatey.org/packages/shellrunas
    https://chocolatey.org/packages/sigcheck
    https://chocolatey.org/packages/streams
    https://chocolatey.org/packages/sync
    https://chocolatey.org/packages/tcpvcon
    https://chocolatey.org/packages/vmmap
    https://chocolatey.org/packages/volumeid
    https://chocolatey.org/packages/whois
    https://chocolatey.org/packages/winobj

--- automatic/_output/accesschk/6.0/tools/chocolateyInstall.ps1
+++ automatic/_output/accesschk/6.0/tools/chocolateyInstall.ps1
@@ -18,8 +18,8 @@ Write-Verbose "Accepting license..."
 $regRoot = 'HKCU:\Software\Sysinternals'
 $regPkg = 'AccessChk'
 $regPath = Join-Path $regRoot $regPkg
-if (! Test-Path $regRoot) {New-Item -Path "$regRoot"}
-if (! Test-Path $regPath) {New-Item -Path "$regRoot" -Name "$regPkg"}
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
 Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
 if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
   throw "Failed setting registry value."
