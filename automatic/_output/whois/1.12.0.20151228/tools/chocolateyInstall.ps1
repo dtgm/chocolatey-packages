b2f950a777f70dbd66e8af0b53783dfb8959f9ff
dtgm <dtgm1594@gmail.com>
2015-12-28 11:33:12 -0700

    Updated on 2015-12-28: bitcoin,dolphin,nsis,nsis.install,nsis.portable,portmon,whois
    https://chocolatey.org/packages/bitcoin
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/nsis
    https://chocolatey.org/packages/nsis.install
    https://chocolatey.org/packages/nsis.portable
    https://chocolatey.org/packages/portmon
    https://chocolatey.org/packages/whois

---
+++ automatic/_output/whois/1.12.0.20151228/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+$packageName = 'whois'
+$url = 'https://download.sysinternals.com/files/WhoIs.zip'
+$checksum = '8eb7ce6d4ceca65ca7c16eab8f34c035d6d6a423'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'Whois'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
