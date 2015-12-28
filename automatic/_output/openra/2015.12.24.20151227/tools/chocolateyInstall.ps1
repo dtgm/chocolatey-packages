c7f8059c857de385cf7e0ae4ffbd8124bab602fe
dtgm <dtgm1594@gmail.com>
2015-12-27 23:24:52 -0700

    FIX: https://github.com/dtgm/chocolatey-packages/issues/57
    https://chocolatey.org/packages/openra/2015.12.24.20151227

---
+++ automatic/_output/openra/2015.12.24.20151227/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'openra'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com//OpenRA/OpenRA/releases/download/release-20151224/OpenRA-release-20151224.exe'
+$checksum = '1239d48aa7889555aa65d1d1d5860d61'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
