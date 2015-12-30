10f3cc8998819a66d40ccffd9a2529f97a93c440
dtgm <dtgm1594@gmail.com>
2015-12-30 11:33:08 -0700

    Updated on 2015-12-30: kvrt,ums,zona
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/ums
    https://chocolatey.org/packages/zona

---
+++ automatic/_output/zona/1.0.6.8/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'zona'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://dl.appzona.org/ZonaSetup[KgI7].exe'
+$checksum = '{checksum}'
+$checksumType = 'sha1'
+$validExitCodes = @(0) #exit codes http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+$ahkExe = 'AutoHotKey'
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
