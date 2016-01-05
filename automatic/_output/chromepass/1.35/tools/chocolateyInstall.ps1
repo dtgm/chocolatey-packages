f2f2fd75a2299443fa5054723ac3ece2d1f2192d
dtgm <dtgm1594@gmail.com>
2016-01-04 18:33:13 -0700

    Updated 9 pkg(s) on 2016-01-04: chromepass,dar,ddu,dolphin,exiftool,nirlauncher,telegram,telegram.install,telegram.portable
    https://chocolatey.org/packages/chromepass
    https://chocolatey.org/packages/dar
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/exiftool
    https://chocolatey.org/packages/nirlauncher
    https://chocolatey.org/packages/telegram
    https://chocolatey.org/packages/telegram.install
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/chromepass/1.35/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+$packageName = 'chromepass'
+$url = 'http://www.nirsoft.net/toolsdownload/chromepass.zip'
+$checksum = '94132e0f4b2e90186b11da112fcd8e7003083de6'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://www.nirsoft.net/utils/$packageName.html"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/telegram.portable/0.9.17/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.0.9.17.zip'
+$checksum = 'be78816781a956a9a328a4113e80f869f2ea16ec'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
