fe7ef4c520598131b4b298513799f165b2a771e0
dtgm <dtgm1594@gmail.com>
2016-01-07 01:03:12 -0700

    Updated 2 pkg(s) on 2016-01-06: dolphin,outlookattachview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/outlookattachview

---
+++ automatic/_output/outlookattachview/2.87/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'outlookattachview'
+$url = 'http://www.nirsoft.net/utils/outlookattachview.zip'
+$checksum = '57c09e144dbaa250b082815038209fcf95f12817'
+$checksumType = 'sha1'
+$url64 = 'http://www.nirsoft.net/utils/outlookattachview-x64.zip'
+$checksum64 = '464371eb73c8223e6bda66ad2dd8bcc3c7593f7c'
+$checksumType64 = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
