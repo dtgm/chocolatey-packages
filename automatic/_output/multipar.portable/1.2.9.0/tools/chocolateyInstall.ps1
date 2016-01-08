0b2ff2a627cc122d5eaedca4a15930c58909d7cf
dtgm <dtgm1594@gmail.com>
2016-01-08 01:33:14 -0700

    Updated 5 pkg(s) on 2016-01-07: dolphin,multipar,multipar.install,multipar.portable,sumo
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable
    https://chocolatey.org/packages/sumo

---
+++ automatic/_output/multipar.portable/1.2.9.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'multipar.portable'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1290.zip'
+$checksum = '07607D055E4D9D10BF9C3914EB54674CE61816C5'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
