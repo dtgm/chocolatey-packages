0cda89209602fed7e6b7ffdb20d10fc907a33704
dtgm <dtgm1594@gmail.com>
2016-01-08 01:33:14 -0700

    Updated 5 pkg(s) on 2016-01-07: dolphin,multipar,multipar.install,multipar.portable,sumo
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable
    https://chocolatey.org/packages/sumo

---
+++ automatic/_output/multipar.install/1.2.9.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1290_setup.exe'
+$checksum = 'D62D7A9E59D25A5EA8DAB4A7C3F770916F3ADA6B'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/sumo/4.2.7.300/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'sumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
+$checksum = '525738769c317c0d741e914aec68259e6b3afc99'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
