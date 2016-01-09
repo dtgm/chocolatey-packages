9aeb9172d751474b549878bdc06b6c59c72f29dc
dtgm <dtgm1594@gmail.com>
2016-01-09 01:03:18 -0700

    Updated 3 pkg(s) on 2016-01-08: dolphin,jrt,stunnel
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/jrt
    https://chocolatey.org/packages/stunnel

---
+++ automatic/_output/jrt/8.0.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'jrt'
+$url = 'http://thisisudax.org/downloads/JRT.exe'
+$checksum = 'aee25fdd2cc9443fc5e4a7bf6790761d2a498448'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+# create an empty sidecar metadata file for closed-source shimgen.exe to indicate gui
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
