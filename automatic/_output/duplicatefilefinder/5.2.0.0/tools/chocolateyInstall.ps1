8dee905230e3f476036650d13a5025018c2a0ad6
dtgm <dtgm1594@gmail.com>
2016-01-13 18:03:16 -0700

    Updated 3 pkg(s) on 2016-01-13: dolphin,duplicatefilefinder,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/duplicatefilefinder
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/duplicatefilefinder/5.2.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'duplicatefilefinder'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /TASKS=desktopicon'
+$url = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
+$checksum = 'b58d2c6781d7dc462da19bbcae3bf1d4b0ff47f4'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
