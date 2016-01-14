dd97a660834b288a22684e439984bc15ce7f72b7
dtgm <dtgm1594@gmail.com>
2016-01-14 01:33:15 -0700

    Updated 3 pkg(s) on 2016-01-13: dolphin,ipnetinfo,ipnetinfo.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/ipnetinfo
    https://chocolatey.org/packages/ipnetinfo.portable

---
+++ automatic/_output/dolphin/4.0.8709/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-8709-x64.7z'
+$checksum = '2fea325e30e89f79da2428aa36c61db5514296d2'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
