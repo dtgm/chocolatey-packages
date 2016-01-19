11a1947668446f297373a403d8435718a73f44bd
dtgm <dtgm1594@gmail.com>
2016-01-19 13:10:51 -0700

    Packages that were never built due to uncalculated checksum
    https://github.com/dtgm/chocolatey-packages/issues/85

--- automatic/_output/kvrt/2015.12.30.0637/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$packageName = 'kvrt'
-$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$batchStart = Join-Path $toolsPath "kvrt.bat"
-Uninstall-BinFile "$packageName" "$batchStart"
--- automatic/_output/lili/2.9.3/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'lili'
-$installerType = 'exe'
-$url = 'http://download.tuxfamily.org/lilicreator/stable/LinuxLive USB Creator 2.9.3.exe'
-$checksum = '{checksum}'
-$checksumType = 'sha1'
-$silentArgs = '/S'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -Url "$url" -SilentArgs "$silentArgs" -ValidExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
