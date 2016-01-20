f775be04a83f62782ccf755f8e8beef76cacfc26
dtgm <dtgm1594@gmail.com>
2016-01-19 18:33:08 -0700

    Updated 4 pkg(s) on 2016-01-19: hashmyfiles,kvrt,webbrowserpassview.install,windowsrepair
    https://chocolatey.org/packages/hashmyfiles
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/webbrowserpassview.install
    https://chocolatey.org/packages/windowsrepair

---
+++ automatic/_output/kvrt/2016.01.19.0834/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '93c1819a0499abf2cc42540295d929cd86f445a1'
+$checksumType = 'sha1'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
