2548af43be54daebe1f5b7066aa1f3758a945a73
dtgm <dtgm1594@gmail.com>
2016-01-22 01:03:09 -0700

    Updated 1 pkg(s) on 2016-01-21: antidupl
    https://chocolatey.org/packages/antidupl

--- automatic/_output/antidupl/2.3.8/tools/chocolateyInstall.ps1
+++ automatic/_output/antidupl/2.3.8/tools/chocolateyInstall.ps1
@@ -1,19 +1,23 @@
-# generated vars
 $packageName = 'antidupl'
 $url = 'https://sourceforge.net/projects/antidupl/files/bin/AntiDupl.NET-2.3.8.exe'
 $checksum = '6cf70daf28b09b6ff353dbf9a28bfb27cd075698'
-
-# static vars
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileType = 'rar'
 
-# $Env:ChocolateyInstall\helpers\functions
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -Url64bit "" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$chocTempDir = Join-Path $env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion";}
+$file = Join-Path $tempDir "$($packageName)Install.$fileType"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$file" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Start-Process "7z" -ArgumentList "x -o`"$toolsDir`" -y `"$file`"" -Wait
 
 # create empty sidecar so shimgen creates shim for GUI rather than console
 $installFile = Join-Path -Path $toolsDir `
