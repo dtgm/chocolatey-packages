8d588a7719808124366d1bccff723d844ad4269d
dtgm <dtgm1594@gmail.com>
2016-01-22 11:33:09 -0700

    Updated 1 pkg(s) on 2015-05-28: duplicati.install
    https://chocolatey.org/packages/duplicati.install

--- automatic/_output/duplicati.install/1.3.4.20150408/tools/chocolateyInstall.ps1
+++ automatic/_output/duplicati.install/1.3.4.20150408/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $url = 'http://duplicati.googlecode.com/files/Duplicati 1.3.4.msi'
 $checksum = '39cb74afd45d83f29df9a3ef7881734f78a3e48c'
 $checksumType = 'sha1'
 $url64 = 'http://duplicati.googlecode.com/files/Duplicati%201.3.4.x64.msi'
-$checksum64 = '{checksumx64}'
+$checksum64 = '96756e6703261d1459f6d1d8d8053cada66cdb22'
 $checksumType64 = 'sha1'
 $validExitCodes = @(0)
 
