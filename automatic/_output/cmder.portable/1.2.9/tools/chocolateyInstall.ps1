c4993e529d52f5166ce034776d1283cf34955402
mixxorz <mixxorz@gmail.com>
2016-01-23 00:24:48 +0800

    Update cmder to 1.2.9
    * Fix cmdermini.portable install script
    * Fix keratin configuration for cmder

--- automatic/_output/cmder.portable/1.2.9/tools/chocolateyInstall.ps1
+++ automatic/_output/cmder.portable/1.2.9/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$packageName = 'cmder.portable'
+$url = 'https://github.com/bliker/cmder/releases/download/v1.2.9/cmder.7z'
+$checksum = 'D7F74429C51E375B684DD73E123BBFA4'
 $checksumType = 'md5'
 $binRoot = Get-BinRoot
-$installPath = Join-Path $binRoot "cmdermini"
+$installPath = Join-Path $binRoot "cmder"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
