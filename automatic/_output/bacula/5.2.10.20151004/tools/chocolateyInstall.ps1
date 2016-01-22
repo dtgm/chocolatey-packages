7c4ac1df1646770ca565aad3d9a3aa9783532746
dtgm <dtgm1594@gmail.com>
2016-01-22 01:03:08 -0700

    Updated 1 pkg(s) on 2015-10-04: bacula
    https://chocolatey.org/packages/bacula

--- automatic/_output/bacula/5.2.10.20151004/tools/chocolateyInstall.ps1
+++ automatic/_output/bacula/5.2.10.20151004/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'bacula'
 $installerType = 'exe'
 $silentArgs = '/S'
-$urlArray = @('https://sourceforge.net/projects/bacula/files/Win32_64/5.2.10/bacula-win32-5.2.10.exe/download', 'https://sourceforge.net/projects/bacula/files/Win32_64/5.2.10/bacula-win64-5.2.10.exe/download'
+$urlArray = @('https://sourceforge.net/projects/bacula/files/Win32_64/5.2.10/bacula-win32-5.2.10.exe/download', 'https://sourceforge.net/projects/bacula/files/Win32_64/5.2.10/bacula-win64-5.2.10.exe/download')
 $url = $urlArray[0]
 $checksum = 'a219763afca7ba224393c1e9c6cb9613349168f0'
 $checksumType = 'sha1'
