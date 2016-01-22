d391db2acf9268644d027c46ded381ed6532765d
dtgm <dtgm1594@gmail.com>
2016-01-22 11:33:10 -0700

    Updated 2 pkg(s) on 2016-01-22: cpu-z.portable,dogecoin.portable
    https://chocolatey.org/packages/cpu-z.portable
    https://chocolatey.org/packages/dogecoin.portable

--- automatic/_output/cpu-z.portable/1.74/tools/chocolateyInstall.ps1
+++ automatic/_output/cpu-z.portable/1.74/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'cpu-z.portable'
 $url = 'http://download.cpuid.com/cpu-z/cpu-z_1.74-en.zip'
-$checksum = '0b6336243edfd492e4252ba31930c945ed65e5b0'
+$checksum = '481012bb1c462f1074bb1b7a8ed3f34b7fd4f63c'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile32 = Join-Path $toolsDir "cpuz_x32.exe"
@@ -9,7 +9,6 @@ $installFile64 = Join-Path $toolsDir "cpuz_x64.exe"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
 
--- automatic/_output/dogecoin.portable/1.10.0/tools/chocolateyInstall.ps1
+++ automatic/_output/dogecoin.portable/1.10.0/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageName = 'dogecoin.portable'
 $url = 'https://github.com//dogecoin/dogecoin/releases/download/v1.10.0/dogecoin-1.10.0-win32.zip'
-$checksum = 'e79ab13254b19a6a7082cab7a94e33a7'
+$checksum = 'a7675b78f46f1f761fdbfd46d64958b5'
 $checksumType = 'md5'
 $url64 = 'https://github.com//dogecoin/dogecoin/releases/download/v1.10.0/dogecoin-1.10.0-win64.zip'
-$checksum64 = '7c23049bf8783d5dad13403069374419'
+$checksum64 = '418a204bee47e2d78ac95aaa65dfb53f'
 $checksumType64 = 'md5'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
