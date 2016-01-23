0b9400ab50cf9ad1da2150b7a1025c58ee89aab1
dtgm <dtgm1594@gmail.com>
2016-01-23 16:07:37 -0700

    Updated 1 pkg(s) on 2016-01-22: frost
    https://chocolatey.org/packages/frost

--- automatic/_output/frost/2011.03.05/tools/chocolateyInstall.ps1
+++ automatic/_output/frost/2011.03.05/tools/chocolateyInstall.ps1
@@ -3,10 +3,16 @@ $url = 'http://sourceforge.net/projects/jtcfrost/files/frost/2011-03-05/frost_20
 $checksum = 'caa34f3856d96e725ceb687efc0c507065151aa4'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzDir = Join-Path $toolsDir $packageName
+$batFile = Join-Path $unzDir "frost.bat"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
+
+(Get-Content $batFile) -replace '-jar frost.jar', '-jar %~dp0frost.jar' | Set-Content $batFile
+
+Install-BinFile -Name $packageName `
+                -Path $batFile
