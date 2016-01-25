ba759f9cb7dd22931874f296c0e430f0b04a7982
Maharishi Bhatia <Maharishi Bhatia>
2016-01-25 11:55:56 -0500

    adding the new release chocolatey package for todolist (7.0.12.1)

---
+++ manual/todolist/7.0.12.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'todolist'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://abstractspoon.pbworks.com/f/todolist_exe.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP'
+  url           = $url
+  
+
+  softwareName  = 'todolist*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
