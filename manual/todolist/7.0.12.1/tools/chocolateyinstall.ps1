7371d19f6a252de28a0597cb8821fe465352d2fe
dtgm <dtgm1594@gmail.com>
2016-02-02 22:59:39 -0700

    Unnecessary shimgens required for a package manager
    TDLUninstall.exe is handled by choco uninst todolist
    TDLUpdate.exe is handled by choco upgrade todolist

--- manual/todolist/7.0.12.1/tools/chocolateyinstall.ps1
+++ manual/todolist/7.0.12.1/tools/chocolateyinstall.ps1
@@ -1,38 +1,27 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$packageName = 'todolist'
+$url = 'http://abstractspoon.pbworks.com/f/todolist_exe.zip'
+$checksum = 'cfa8ea562520e933e6b9da511e6c474c288cd10b'
+$checksumType = 'sha1'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "ToDoList.exe"
+$ignoreFileUp = Join-Path $toolsDir "TDLUninstall.exe"
+$ignoreFileUn = Join-Path $toolsDir "TDLUpdate.exe"
 
-$packageName= 'todolist'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://abstractspoon.pbworks.com/f/todolist_exe.zip'
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
-  url           = $url
-  
-
-  softwareName  = 'todolist*'
-  checksum      = ''
-  checksumType  = 'md5'
-  checksum64    = ''
-  checksumType64= 'md5'
-}
-
-Install-ChocolateyZipPackage $packageName $url $toolsDir
-
-$files = get-childitem $installDir -include *.exe -recurse
-
-foreach ($file in $files) {
-   New-Item "$file.gui" -type file -force | Out-Null
-}
-
-
-
-
-
-
-
-
-
-
-
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
+Set-Content -Path ("$ignoreFileUp.ignore") `
+            -Value $null
+Set-Content -Path ("$ignoreFileUn.ignore") `
+            -Value $null
