6ccf19a62186aab8556306bd46d874b76fb03101
dtgm <dtgm1594@gmail.com>
2016-02-09 21:19:28 -0700

    Updated 1 pkg(s) on 2016-02-02: kis
    https://chocolatey.org/packages/kis

---
+++ automatic/_output/kis/16.0.1.445/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'kis'
+$packageSearch = "Kaspersky Internet Security"
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart REMOVE=ALL'
+$validExitCodes = @(0,3010)
+
+$reg = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                        -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like $packageSearch }
+$reg.PSChildName | ForEach-Object { $_ -match '{[\dA-F-]+}' }
+Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                            -FileType "$installerType" `
+                            -SilentArgs "$($Matches.Values) $silentArgs" `
+                            -ValidExitCodes $validExitCodes
