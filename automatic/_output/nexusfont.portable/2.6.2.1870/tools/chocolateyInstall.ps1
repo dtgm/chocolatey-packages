04a8bebbef335cf8c284e815cb61d33eea011b3b
dtgm <dtgm1594@gmail.com>
2016-02-11 20:38:07 -0700

    Updated 1 pkg(s) on 2016-01-19: nexusfont.portable
    https://chocolatey.org/packages/nexusfont.portable

--- automatic/_output/nexusfont.portable/2.6.2.1870/tools/chocolateyInstall.ps1
+++ automatic/_output/nexusfont.portable/2.6.2.1870/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'nexusfont.portable'
 $url = 'http://www.xiles.net/pds/nexusfont2.5.8.zip'
-$checksum = '279310de43bcf7a6caaf822595971b9015268976'
+$checksum = ''
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
