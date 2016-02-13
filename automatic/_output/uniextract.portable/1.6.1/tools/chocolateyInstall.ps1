ff05d9e1aad3eaa44d7c0ad216450f2682a52996
dtgm <dtgm1594@gmail.com>
2016-02-13 11:33:07 -0700

    Updated 1 pkg(s) on 2015-06-30: uniextract.portable
    https://chocolatey.org/packages/uniextract.portable

--- automatic/_output/uniextract.portable/1.6.1/tools/chocolateyInstall.ps1
+++ automatic/_output/uniextract.portable/1.6.1/tools/chocolateyInstall.ps1
@@ -3,9 +3,21 @@ $url = 'http://legroom.net///scripts/download.php?file=uniextract161_noinst'
 $checksum = 'e960091d203c84c6034c14a6146f8f4bb638b11a'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$fileType = 'rar'
+$chocTempDir = Join-Path $env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion";}
+$file = Join-Path $tempDir "$($packageName)Install.$fileType"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$file" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Start-Process "7z" -ArgumentList "x -o`"$toolsDir`" -y `"$file`"" -Wait
+
+$installFile = Join-Path $toolsDir "UniExtract.exe"
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
