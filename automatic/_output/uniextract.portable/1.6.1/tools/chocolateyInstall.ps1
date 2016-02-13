92907b630ded77ab160422b37cf37aa42c52b8c9
dtgm <dtgm1594@gmail.com>
2016-02-13 13:38:07 -0700

    Updated 1 pkg(s) on 2015-06-30: uniextract.portable
    https://chocolatey.org/packages/uniextract.portable

--- automatic/_output/uniextract.portable/1.6.1/tools/chocolateyInstall.ps1
+++ automatic/_output/uniextract.portable/1.6.1/tools/chocolateyInstall.ps1
@@ -1,13 +1,16 @@
 $packageName = 'uniextract.portable'
+$packageVersion = '1.6.1'
 $url = 'http://legroom.net///scripts/download.php?file=uniextract161_noinst'
 $checksum = 'e960091d203c84c6034c14a6146f8f4bb638b11a'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ignoreFiles = Join-Path $toolsDir "bin"
 $fileType = 'rar'
 $chocTempDir = Join-Path $env:Temp "chocolatey"
 $tempDir = Join-Path $chocTempDir "$packageName"
 if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
-if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion";}
+$tempDir = Join-Path $tempDir "$packageVersion"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
 $file = Join-Path $tempDir "$($packageName)Install.$fileType"
 
 Get-ChocolateyWebFile -PackageName "$packageName" `
@@ -21,3 +24,7 @@ Start-Process "7z" -ArgumentList "x -o`"$toolsDir`" -y `"$file`"" -Wait
 $installFile = Join-Path $toolsDir "UniExtract.exe"
 Set-Content -Path ("$installFile.gui") `
             -Value $null
+
+Get-ChildItem -Path $ignoreFiles -Recurse | 
+  Where { $_.Extension -eq '.exe' } |
+  % { New-Item $($_.FullName + '.ignore') -Force -ItemType file }
