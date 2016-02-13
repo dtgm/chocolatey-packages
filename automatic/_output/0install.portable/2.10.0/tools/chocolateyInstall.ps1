a4672ff7097ccdffee608e9cfa13aa9ab821fc40
dtgm <dtgm1594@gmail.com>
2016-02-13 13:38:07 -0700

    Updated 5 pkg(s) on 2016-02-13: 0install.portable,dolphin,folderchangesview,kvrt,uniextract.portable
    https://chocolatey.org/packages/0install.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/folderchangesview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/uniextract.portable

---
+++ automatic/_output/0install.portable/2.10.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = '0install.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://0install.de/files/zero-install.zip'
+$checksum = 'da86c8f823960756bf92a0324481e01ceff2cdaf'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
