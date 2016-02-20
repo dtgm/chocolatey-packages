0d0fbf340fe6182b6df32791d7f535b60b67d123
dtgm <dtgm1594@gmail.com>
2016-02-19 19:32:07 -0700

    Updated 1 pkg(s) on 2015-06-18: truecrypt
    https://chocolatey.org/packages/truecrypt

--- automatic/_output/truecrypt/7.1.20150620/tools/chocolateyUninstall.ps1
+++ automatic/_output/truecrypt/7.1.20150620/tools/chocolateyUninstall.ps1
@@ -3,13 +3,24 @@ $regSearch = 'TrueCrypt'
 $installerType = 'exe'
 $silentArgs = '/u'
 $validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkFile = Join-Path $scriptPath "chocolateyUninstall.ahk"
+$ahkExe = 'AutoHotKey'
+$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+Copy-Item $ahkFile "$ahkRun" -Force
+Start-Process $ahkExe $ahkRun
+
 Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                           'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
                           'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
                  -ErrorAction:SilentlyContinue `
-| Where-Object   { $_.DisplayName -like "$regSearch" } `
-| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+| Where-Object   { $_.DisplayName -like $regSearch } `
+| ForEach-Object { $_ -match '(?<=")[^"]+'
+                   Uninstall-ChocolateyPackage -PackageName "$packageName" `
                                                -FileType "$installerType" `
                                                -SilentArgs "$($silentArgs)" `
-                                               -File "$($_.UninstallString)" `
+                                               -File $matches[0] `
                                                -ValidExitCodes $validExitCodes }
+
+Remove-Item "$ahkRun" -Force -ErrorAction SilentlyContinue
