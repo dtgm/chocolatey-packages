2ab2032a097452f1d595bdc2e5e6a1c26cce0020
dtgm <dtgm1594@gmail.com>
2016-02-19 23:45:31 -0700

    FIX: qttabbar, uniextract.portable, zimbra-desktop
    https://github.com/dtgm/chocolatey-packages/issues/113

--- automatic/uniextract.portable/tools/chocolateyInstall.ps1
+++ automatic/uniextract.portable/tools/chocolateyInstall.ps1
@@ -3,9 +3,24 @@ $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$ignoreFiles = Join-Path $toolsDir "bin"
+$tempDir = Join-Path $env:Temp "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion";}
+$installArchive = Join-Path $tempDir "$($packageName)Install.rar"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installArchive" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Start-Process "7z" -ArgumentList "x -o`"$toolsDir`" -y `"$installArchive`"" -Wait
+
+$installFile = Join-Path $toolsDir "UniExtract.exe"
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
+
+Get-ChildItem -Path $ignoreFiles -Recurse | 
+  Where { $_.Extension -eq '.exe' } |
+  % { New-Item $($_.FullName + '.ignore') -Force -ItemType file }
