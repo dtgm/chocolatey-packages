1ef61d11726526214d0c2ba938bcf89550ae8cfa
dtgm <dtgm1594@gmail.com>
2016-02-19 23:39:21 -0700

    FIX: fontmatrix
    more robust registry algorithm
    https://github.com/dtgm/chocolatey-packages/issues/113

--- automatic/fontmatrix/tools/chocolateyInstall.ps1
+++ automatic/fontmatrix/tools/chocolateyInstall.ps1
@@ -6,9 +6,19 @@ $checksum = '{{Checksum}}'
 $checksumType = 'sha1'
 $validExitCodes = @(0)
 
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' `
-                 -Name "$Env:Temp\chocolatey\fontmatrix\fontmatrixInstall.exe" `
-                 -Value "^ WINXPSP3"
+$key = 'HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers'
+Write-Verbose "Verify registry key exists:`t`"$key`""
+$parts = $key -split '\\'
+foreach ($i in $parts) {
+    $path += $i + "\"
+    if (!(test-path $path)) {
+        New-Item -Path $path
+    }
+}
+
+Set-ItemProperty -Path $path `
+                 -Name "$env:Temp\fontmatrix\$env:PackageVersion\fontmatrixInstall.exe" `
+                 -Value "WINXPSP3"
 
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
@@ -18,8 +28,8 @@ Install-ChocolateyPackage -PackageName "$packageName" `
                           -Checksum "$checksum" `
                           -ChecksumType "$checksumType"
 
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers' `
-                    -Name "$Env:Temp\chocolatey\fontmatrix\fontmatrixInstall.exe"
+Remove-ItemProperty -Path $path `
+                    -Name "$env:Temp\fontmatrix\$env:PackageVersion\fontmatrixInstall.exe"
 
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $dllName = 'msvcr71.dll'
@@ -31,22 +41,20 @@ Get-ChecksumValid -File $dllFullName `
                   -Checksum $dllChecksum `
                   -ChecksumType $dllChecksumType
 
-$osBitness = Get-ProcessorBits
-if ($osBitness -eq 64) {
+Write-Verbose "Check for conflicts and install required DLL into system folder."
+if (Get-ProcessorBits 64) {
   $dstPath = Join-Path $Env:SystemRoot "SysWOW64"
 } else {
   $dstPath = Join-Path $Env:SystemRoot "System32"
 }
 $installPath = Join-Path $dstPath $dllFullName
-if (Test-Path $installPath) {
-  if (Get-ChecksumValid -File $installPath `
-                  -Checksum $dllChecksum `
-                  -ChecksumType $dllChecksumType) {
-    Write-Host "File already exists."
-  } else {
-    Write-Host "`'$installPath`' already exists."
-    Write-Host "Backing up existing file..."
-    Rename-Item $installPath $installPath.bak
-  }
+if (Get-ChecksumValid -File $installPath `
+                -Checksum $dllChecksum `
+                -ChecksumType $dllChecksumType) {
+  Write-Host "`'$installPath`' is already installed."
+} else {
+  Write-Host "`'$installPath`' already exists."
+  Write-Host "Backing up existing file to `'$installPath.bak`'"
+  Rename-Item $installPath $installPath.bak
+  Copy-Item $dllFullName $dstPath
 }
-Copy-Item $dllFullName $dstPath
--- automatic/fontmatrix/tools/uninstall.ahk
+++
@@ -1,6 +0,0 @@
-#NoEnv
-SetTitleMatchMode, 1  ;matches if title begins with string
-WinWait, FontMatrix ahk_class #32770, OK, 120
-WinActivate
-ControlClick, Button1, ahk_class #32770
-ExitApp
