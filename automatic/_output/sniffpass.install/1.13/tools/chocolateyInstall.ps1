79983e60abef4b9f8bf4c040a48019f4200e9a77
dtgm <dtgm1594@gmail.com>
2016-02-19 19:32:07 -0700

    Updated 1 pkg(s) on 2016-01-19: sniffpass.install
    https://chocolatey.org/packages/sniffpass.install

--- automatic/_output/sniffpass.install/1.13/tools/chocolateyInstall.ps1
+++ automatic/_output/sniffpass.install/1.13/tools/chocolateyInstall.ps1
@@ -6,10 +6,17 @@ $checksumType = 'sha1'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
+$options = @{
+  Headers = @{
+    Referer = 'http://www.nirsoft.net/utils/password_sniffer.html';
+  }
+}
+
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -Url "$url" `
                           -SilentArgs "$silentArgs" `
                           -ValidExitCodes $validExitCodes `
                           -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+                          -ChecksumType "$checksumType" `
+                          -Options $options
