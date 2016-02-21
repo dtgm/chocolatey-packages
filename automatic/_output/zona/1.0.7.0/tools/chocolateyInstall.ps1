0e31e073630dc35f0c0aa3cc5fc93a9f920e5e5a
dtgm <dtgm1594@gmail.com>
2016-02-21 12:38:10 -0700

    Updated 4 pkg(s) on 2016-02-21: dolphin,keepass-plugin-keecloud,nunit.portable,zona
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/keepass-plugin-keecloud
    https://chocolatey.org/packages/nunit.portable
    https://chocolatey.org/packages/zona

---
+++ automatic/_output/zona/1.0.7.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = 'zona'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://dl.appzona.org/ZonaSetup[r5Ia].exe'
+$checksum = '1a71127dc38d5d1dbb3d0675d1a046f18808f450'
+$checksumType = 'sha1'
+$validExitCodes = @(0) #exit codes http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+$ahkExe = 'AutoHotKey'
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
