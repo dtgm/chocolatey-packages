0e83c5f7f1b7a93b1ee38b0723a500dc34f8437f
dtgm <dtgm1594@gmail.com>
2016-02-21 06:38:10 -0700

    Updated 2 pkg(s) on 2016-02-21: dolphin,ums
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/ums

---
+++ automatic/_output/ums/6.1.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'ums'
+$installerType = 'exe'	
+$silentArgs = '/S'
+$url = 'http://sourceforge.net/projects/unimediaserver/files/Official%20Releases/Windows/UMS-6.1.0-Java7.exe/download'
+$checksum = '9eb7197df140ade2207429dabf9f5a49f0f88dad'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
