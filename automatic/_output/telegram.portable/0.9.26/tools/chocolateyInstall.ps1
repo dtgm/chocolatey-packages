b82516dbe691a8ff264e0fae69dcda17ecd8012d
dtgm <dtgm1594@gmail.com>
2016-02-22 06:38:08 -0700

    Updated 4 pkg(s) on 2016-02-22: dolphin,telegram,telegram.install,telegram.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/telegram
    https://chocolatey.org/packages/telegram.install
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/telegram.portable/0.9.26/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.0.9.26.zip'
+$checksum = '3d8a7377a500c397d5231bb23806714285543524'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
