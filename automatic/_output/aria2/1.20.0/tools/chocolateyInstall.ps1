c9d73b5bfd1a2d71224df1101b7846f882d99e3e
dtgm <dtgm1594@gmail.com>
2016-02-23 08:37:06 -0700

    FIX: #133

--- automatic/_output/aria2/1.20.0/tools/chocolateyInstall.ps1
+++ automatic/_output/aria2/1.20.0/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$packageName = 'aria2'
+$url = 'https://github.com/tatsuhiro-t/aria2/releases/download/release-1.20.0/aria2-1.20.0-win-32bit-build1.zip'
+$checksum = 'b4c8978760a0eae51914a8ed76a0f2ff'
 $checksumType = 'md5'
-$url64 = '{{DownloadUrlx64}}'
-$checksum64 = '{{Checksumx64}}'
+$url64 = 'https://github.com/tatsuhiro-t/aria2/releases/download/release-1.20.0/aria2-1.20.0-win-64bit-build1.zip'
+$checksum64 = 'e6e1ae54a26d95a57127c7acf00e4b15'
 $checksumType64 = 'md5'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
