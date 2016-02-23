35c9063d46ea9f16ecfaf0d6c47e0d3ee6e2aa49
dtgm <dtgm1594@gmail.com>
2016-02-23 08:44:25 -0700

    FIX: #134

--- automatic/_output/bulkrenameutility.portable/3.0.0.1/tools/chocolateyInstall.ps1
+++ automatic/_output/bulkrenameutility.portable/3.0.0.1/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$packageName = 'bulkrenameutility.portable'
+$url = 'http://www.bulkrenameutility.co.uk/Downloads/BRU_NoInstall.zip'
+$checksum = '5cf7bf6622d9738c399e40b53f6767c638a7d9c1'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 
