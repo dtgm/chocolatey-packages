37aaec981e9bb2b6b2e69be6080be83392633dcb
dtgm <dtgm1594@gmail.com>
2016-02-23 19:16:37 -0700

    updates and various fixes
    #113

--- automatic/npackd-cli.portable/tools/chocolateyInstall.ps1
+++ automatic/npackd-cli.portable/tools/chocolateyInstall.ps1
@@ -2,11 +2,10 @@ $packageName = '{{PackageName}}'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = '{{DownloadUrlx64}}'
 $checksum = '{{Checksum}}'
-$checksumType = 'sha1'
+$checksumType = 'md5'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
                              -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
+                             -ChecksumType "$checksumType"
