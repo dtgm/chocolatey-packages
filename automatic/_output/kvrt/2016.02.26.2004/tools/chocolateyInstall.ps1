66e9aa38f39f0cf32a744b60237a30994f21577e
dtgm <dtgm1594@gmail.com>
2016-02-26 22:33:07 -0700

    Updated 2 pkg(s) on 2016-02-26: kvrt,telegram.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/kvrt/2016.02.26.2004/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '65c76f0bbe91f1ee25f59c8995e5d6f0248db21c'
+$checksumType = 'sha1'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/telegram.portable/0.9.28/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.0.9.28.zip'
+$checksum = '0f3cb5fd854322e30f15c4296000efa4b3efc741'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
