9e0ffd3cce9eec99ef4dec98abd3c79799a3e7ad
dtgm <dtgm1594@gmail.com>
2016-02-28 12:38:09 -0700

    Updated 6 pkg(s) on 2016-02-28: awscli,bginfo,duplicatecommander,fbcacheview,kvrt,sumo
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/bginfo
    https://chocolatey.org/packages/duplicatecommander
    https://chocolatey.org/packages/fbcacheview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/sumo

---
+++ automatic/_output/bginfo/4.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+$packageName = 'bginfo'
+$url = 'https://download.sysinternals.com/files/BGInfo.zip'
+$checksum = '7595cf8bf089ff5d6690da6d6d81d74d5f09685b'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'BGInfo'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
