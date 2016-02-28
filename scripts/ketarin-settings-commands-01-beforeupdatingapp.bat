eb96a903a4d9ce628973ec139ffc8c18bc3282cd
dtgm <dtgm1594@gmail.com>
2016-02-27 18:47:36 -0700

    removed

--- scripts/ketarin-settings-commands-01-beforeupdatingapp.bat
+++
@@ -1,1 +0,0 @@
-chocopkgup {nopush} --packagename={appname} --version={version} --pg="{packageGuid}" --url="{preupdate-url}" --urlx64="{url64}" --packagepath="{file}" --checksum="{checksum}" --c64="{checksumx64}" --debug
