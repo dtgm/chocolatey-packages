3f6101580b566b2c5b30892b291bd7ded016969a
dtgm <dtgm1594@gmail.com>
2016-02-27 18:48:04 -0700

    extracted from https://github.com/dtgm/chocolatey-packages/blob/047728df9bdd86b80741d286ac53c72fb2af8c71/ketarin/settings.xml#L26
    Note: this file should match the contents of https://github.com/dtgm/chocolatey-packages/blob/master/ketarin/settings.xml#L26 between  `<string>System.String:` and `</string>`
    If a conflict arises, please use value as set in https://github.com/dtgm/chocolatey-packages/blob/master/ketarin/settings.xml#L26

---
+++ scripts/ketarin-settings-commands-01-beforeupdatingapp.bat
@@ -0,0 +1,1 @@
+chocopkgup {nopush} --packagename={appname} --version={version} --pg="{packageGuid}" --url="{preupdate-url}" --urlx64="{url64}" --packagepath="{file}" --checksum="{checksum}" --c64="{checksumx64}" --debug
