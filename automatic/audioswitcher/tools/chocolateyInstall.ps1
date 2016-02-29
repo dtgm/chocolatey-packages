e9ebef905403526de466bd74f885f0c2ba241972
dtgm <dtgm1594@gmail.com>
2016-02-28 18:40:35 -0700

    FIX: #141

--- automatic/audioswitcher/tools/chocolateyInstall.ps1
+++ automatic/audioswitcher/tools/chocolateyInstall.ps1
@@ -1,17 +1,16 @@
 $packageName = '{{PackageName}}'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
-$checksumType = 'sha1'
+$checksumType = 'md5'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
-try {
-  Get-ChocolateyWebFile -PackageName "$packageName" `
-                        -FileFullPath "$installFile" `
-                        -Url "$url" `
-                        -Checksum "$checksum" `
-                        -ChecksumType "$checksumType"
-  Set-Content -Path ("$installFile.gui") `
-              -Value $null
-} catch {
-  throw $_.Exception
-}
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
