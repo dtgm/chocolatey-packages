8e3f39d29a7417a08b60495ead1480a249e14d5b
dtgm <dtgm1594@gmail.com>
2016-03-02 10:33:07 -0700

    Updated 3 pkg(s) on 2016-03-02: keepass,keepass.install,keepass.portable
    https://chocolatey.org/packages/keepass
    https://chocolatey.org/packages/keepass.install
    https://chocolatey.org/packages/keepass.portable

---
+++ automatic/_output/keepass.portable/1.31/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿# generated vars
+$packageName = 'keepass.portable'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%201.x/1.31/KeePass-1.31.zip/download'
+$checksum = 'B99022C952222DE6E3F81A65A260186991F39B81'
+
+# static vars
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $Env:ChocolateyInstall\helpers\functions
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -Url64bit "" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "KeePass.exe.gui"
+Set-Content -Path $installFile `
+            -Value $null
