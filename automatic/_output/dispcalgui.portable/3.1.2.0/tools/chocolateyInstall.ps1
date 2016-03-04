dcbda6717c643a99fc70b623db5dc71089f9ecb8
dtgm <dtgm1594@gmail.com>
2016-03-03 22:33:08 -0700

    Updated 2 pkg(s) on 2016-03-03: awscli,dispcalgui.portable
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dispcalgui.portable

---
+++ automatic/_output/dispcalgui.portable/3.1.2.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dispcalgui.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://sourceforge.net/projects/dispcalgui/files/release/3.1.2.0/DisplayCAL-3.1.2.0-win32.zip/download'
+$checksum = '90686608657a8902ecf69369a9fe631d589e95b7'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
