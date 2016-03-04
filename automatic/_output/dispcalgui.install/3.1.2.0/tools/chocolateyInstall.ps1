12e79b7af98c50e9512d53be36a1306ca6c34ce0
dtgm <dtgm1594@gmail.com>
2016-03-03 18:38:10 -0700

    Updated 4 pkg(s) on 2016-03-03: cyberduck,cyberduck.install,dispcalgui,dispcalgui.install
    https://chocolatey.org/packages/cyberduck
    https://chocolatey.org/packages/cyberduck.install
    https://chocolatey.org/packages/dispcalgui
    https://chocolatey.org/packages/dispcalgui.install

---
+++ automatic/_output/dispcalgui.install/3.1.2.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dispcalgui.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/dispcalgui/files/release/3.1.2.0/DisplayCAL-3.1.2.0-Setup.exe/download'
+$checksum = '9cb7f12fae573336fc8bdcba0d580643a560be3b'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
