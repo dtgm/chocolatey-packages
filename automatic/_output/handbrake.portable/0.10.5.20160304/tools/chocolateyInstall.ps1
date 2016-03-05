e4d2426af1423dd441e7365f13f6c9d332553b77
dtgm <dtgm1594@gmail.com>
2016-03-04 18:38:10 -0700

    Updated 6 pkg(s) on 2016-03-04: cryptsync,dolphin,handbrake,handbrake.install,handbrake.portable,kvrt
    https://chocolatey.org/packages/cryptsync
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/handbrake
    https://chocolatey.org/packages/handbrake.install
    https://chocolatey.org/packages/handbrake.portable
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/handbrake.portable/0.10.5.20160304/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'handbrake.portable'
+$url = 'https://handbrake.fr/rotation.php?file=HandBrake-0.10.5-i686-Win_CLI.zip'
+$checksum = '7d66daf8a5b3208854e5a490ecefaf12f54e33ba'
+$checksumType = 'sha1'
+$url64 = 'https://handbrake.fr/rotation.php?file=HandBrake-0.10.5-x86_64-Win_CLI.zip'
+$checksum64 = 'cb9b25d289e8ff198cfc25065e0f659ac135d9b8'
+$checksumType64 = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
