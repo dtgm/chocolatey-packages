d28cc60951784b080133e14b01bcda2db423384e
dtgm <dtgm1594@gmail.com>
2016-03-04 18:38:10 -0700

    Updated 6 pkg(s) on 2016-03-04: cryptsync,dolphin,handbrake,handbrake.install,handbrake.portable,kvrt
    https://chocolatey.org/packages/cryptsync
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/handbrake
    https://chocolatey.org/packages/handbrake.install
    https://chocolatey.org/packages/handbrake.portable
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/cryptsync/1.2.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$packageName = 'cryptsync'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$urlArray = @('https://sourceforge.net/projects/cryptsync-sk/files/1.2.3/CryptSync-1.2.3.msi/download', 'https://sourceforge.net/projects/cryptsync-sk/files/1.2.3/CryptSync64-1.2.3.msi/download')
+$url = $urlArray[0]
+$checksum = '529e1c3246e61edc940cf67423a962617a561a6d'
+$checksumType = 'sha1'
+$url64 = $urlArray[1]
+$checksum64 = '{checksumx64}'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/handbrake.install/0.10.5.20160304/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'handbrake.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://handbrake.fr/rotation.php?file=HandBrake-0.10.5-i686-Win_GUI-1.exe'
+$checksum = '0acc3b72430c0420504eee9fb4b20466c0b6fc42'
+$checksumType = 'sha1'
+$url64 = 'https://handbrake.fr/rotation.php?file=HandBrake-0.10.5-x86_64-Win_GUI-1.exe'
+$checksum64 = '1cc2fff081e79ba7c4ce98bbe52919203d16d567'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
