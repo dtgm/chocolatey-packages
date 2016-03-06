9f230d6b85cf9de4297361974f45ff0a8dfc97e2
dtgm <dtgm1594@gmail.com>
2016-03-05 18:38:10 -0700

    Updated 4 pkg(s) on 2016-03-05: kvrt,nunit,nunit.install,nunit.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nunit
    https://chocolatey.org/packages/nunit.install
    https://chocolatey.org/packages/nunit.portable

---
+++ automatic/_output/nunit.install/3.2.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'nunit.install'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://github.com/nunit/nunit/releases/download/3.2.0/NUnit.3.2.0.msi'
+$checksum = '3fa87ed5a683e4455aeac4a4d75cea08'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/nunit.portable/3.2.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'nunit.portable'
+$url = 'https://github.com/nunit/nunit/releases/download/3.2.0/NUnit-3.2.0.zip'
+$checksum = '73026f43a5188f70c0d94c851fd4d5e2'
+$checksumType = 'md5'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
