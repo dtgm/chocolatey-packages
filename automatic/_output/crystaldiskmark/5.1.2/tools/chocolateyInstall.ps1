9c0bb4b7ee13e4c96f68015edd0bdb6402d3503e
dtgm <dtgm1594@gmail.com>
2016-03-06 04:33:07 -0700

    Updated 1 pkg(s) on 2016-03-06: crystaldiskmark
    https://chocolatey.org/packages/crystaldiskmark

---
+++ automatic/_output/crystaldiskmark/5.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'crystaldiskmark'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://jaist.dl.sourceforge.jp/crystaldiskmark/64531/CrystalDiskMark5_1_2-en.exe'
+$checksum = ''
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
