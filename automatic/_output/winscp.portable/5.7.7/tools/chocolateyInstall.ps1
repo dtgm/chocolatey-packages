d4c46ed07df694419193b545557e9ffc3810c705
dtgm <dtgm1594@gmail.com>
2016-03-06 06:38:08 -0700

    Updated 3 pkg(s) on 2016-03-06: winscp,winscp.install,winscp.portable
    https://chocolatey.org/packages/winscp
    https://chocolatey.org/packages/winscp.install
    https://chocolatey.org/packages/winscp.portable

---
+++ automatic/_output/winscp.portable/5.7.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'winscp.portable'
+$url = 'https://sourceforge.net/projects/winscp/files/WinSCP/5.7.7/winscp577.zip/download'
+$checksum = 'a27c828d65664d8575a944365f094b48bbb723be'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "WinSCP.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
