84349fcf575a42962657586bcf9301d155a93a4c
dtgm <dtgm1594@gmail.com>
2016-03-06 16:33:10 -0700

    Updated 1 pkg(s) on 2016-03-06: domainhostingview
    https://chocolatey.org/packages/domainhostingview

---
+++ automatic/_output/domainhostingview/1.72/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'domainhostingview'
+$url = 'http://www.nirsoft.net/utils/domainhostingview.zip'
+$checksum = ''
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
