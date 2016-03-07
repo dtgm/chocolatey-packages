294a66b2d222c0a204b5f7a084d19e104e581d22
dtgm <dtgm1594@gmail.com>
2016-03-07 04:33:07 -0700

    Updated 3 pkg(s) on 2016-03-07: electrum,electrum.install,electrum.portable
    https://chocolatey.org/packages/electrum
    https://chocolatey.org/packages/electrum.install
    https://chocolatey.org/packages/electrum.portable

---
+++ automatic/_output/electrum.portable/2.6.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$packageName = 'electrum.portable'
+$url = 'https://download.electrum.org/2.6.2/electrum-2.6.2-portable.exe'
+$checksum = ''
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "electrum.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  # create an empty sidecar metadata file for closed-source shimgen.exe to prevent blank black window
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null  
+} catch {
+  throw $_.Exception
+}
