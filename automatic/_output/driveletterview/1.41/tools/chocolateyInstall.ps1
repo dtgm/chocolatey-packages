983e4a6eb47b67668c0feef7281b67ad9497ad32
dtgm <dtgm1594@gmail.com>
2016-03-08 12:38:08 -0700

    Updated 2 pkg(s) on 2016-03-08: driveletterview,kvrt
    https://chocolatey.org/packages/driveletterview
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/driveletterview/1.41/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'driveletterview'
+$url = 'http://www.nirsoft.net/utils/driveletterview.zip'
+$checksum = ''
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
