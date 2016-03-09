6377749fed0fb15dab97120bd64cd6f678cf7739
dtgm <dtgm1594@gmail.com>
2016-03-09 06:38:07 -0700

    Updated 4 pkg(s) on 2016-03-09: devmanview,dolphin,jcpicker,kvrt
    https://chocolatey.org/packages/devmanview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/jcpicker
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/jcpicker/4.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'jcpicker'
+$url = 'http://annystudio.com/software/colorpicker/jcpicker.zip'
+$checksum = ''
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
