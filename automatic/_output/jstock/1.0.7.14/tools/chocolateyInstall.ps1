2f22b7fde026cc2ef48e3022f7bcdb883fc439fa
dtgm <dtgm1594@gmail.com>
2016-03-11 12:38:07 -0700

    Updated 2 pkg(s) on 2016-03-11: jstock,kvrt
    https://chocolatey.org/packages/jstock
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/jstock/1.0.7.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$packageName = 'jstock'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/yccheok/jstock/releases/download/release_1-0-7-14/jstock-1.0.7.14-setup.exe'
+$checksum = 'bbba6ea20297e95e1fa088a8f77facc0'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
