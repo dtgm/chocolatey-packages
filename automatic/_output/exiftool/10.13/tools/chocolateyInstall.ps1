e7b9e823db14903f1464246eb0135a46c66f452b
dtgm <dtgm1594@gmail.com>
2016-03-12 16:33:07 -0700

    Updated 1 pkg(s) on 2016-03-12: exiftool
    https://chocolatey.org/packages/exiftool

---
+++ automatic/_output/exiftool/10.13/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.13.zip'
+$checksum = '42fb0f8db9cb3adf35c684363019e4247f455d51'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
