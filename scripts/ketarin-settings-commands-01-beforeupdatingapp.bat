63f3ade20945a1386c3564bbea3f5412f3e153a2
dtgm <dtgm1594@gmail.com>
2016-03-13 13:11:56 -0600

    Legacy PreUpdateCommand

--- scripts/ketarin-settings-commands-01-beforeupdatingapp.bat
+++ scripts/ketarin-settings-commands-01-beforeupdatingapp.bat
@@ -1,1 +1,4 @@
+REM Requires
+REM   chocolateypackageupdater
+REM   ketarin
 chocopkgup {nopush} --packagename={appname} --version={version} --pg="{packageGuid}" --url="{preupdate-url}" --urlx64="{url64}" --packagepath="{file}" --checksum="{checksum}" --c64="{checksumx64}" --debug
