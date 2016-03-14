4fd4de018f5c00643fa48add89601c9c4b7e3d7e
dtgm <dtgm1594@gmail.com>
2016-03-14 07:33:49 -0600

    Prettify: spacing between vars and cmdlet
    $ for i in $(pcregrep -lM '^\s*\$(toolsDir|validExitCodes).*[^\n]\r\n(?=Install-)' */tools/chocolateyInstall.ps1); do echo sed -i 's!Install-.*!\r\n&!' $i; unix2dos $i; done

--- automatic/upx/tools/chocolateyInstall.ps1
+++ automatic/upx/tools/chocolateyInstall.ps1
@@ -3,6 +3,7 @@ $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
