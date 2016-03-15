981e973be05a1d94695d59bcc75960c91ce978ff
dtgm <dtgm1594@gmail.com>
2016-03-15 12:38:08 -0600

    Updated 3 pkg(s) on 2016-03-15: aria2,foldertimeupdate,mvc
    https://chocolatey.org/packages/aria2
    https://chocolatey.org/packages/foldertimeupdate
    https://chocolatey.org/packages/mvc

---
+++ automatic/_output/aria2/1.21.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'aria2'
+$url = 'https://github.com/tatsuhiro-t/aria2/releases/download/release-1.21.0/aria2-1.21.0-win-32bit-build1.zip'
+$checksum = 'b4c8978760a0eae51914a8ed76a0f2ff'
+$checksumType = 'md5'
+$url64 = 'https://github.com/tatsuhiro-t/aria2/releases/download/release-1.20.0/aria2-1.20.0-win-64bit-build1.zip'
+$checksum64 = 'e6e1ae54a26d95a57127c7acf00e4b15'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64 "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
