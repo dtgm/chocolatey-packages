c0fa3f70cc9e1c450d4da3d5e9954f6d0f3347f3
dtgm <dtgm1594@gmail.com>
2016-03-16 06:38:10 -0600

    Updated 3 pkg(s) on 2016-03-16: multipar,multipar.install,multipar.portable
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable

---
+++ automatic/_output/multipar.install/1.2.9.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1292_setup.exe'
+$checksum = '80166D87EE4EBAFF8D7AD026D0D4B7D2A1B00E11'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
