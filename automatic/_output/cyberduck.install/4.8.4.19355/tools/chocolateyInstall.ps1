4abe3bea61c559c2117b17775143bcff627cdb34
dtgm <dtgm1594@gmail.com>
2016-03-18 07:05:02 -0600

    Updated 3 pkg(s) on 2016-03-18: cyberduck,cyberduck.install,dolphin
    https://chocolatey.org/packages/cyberduck
    https://chocolatey.org/packages/cyberduck.install
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/cyberduck.install/4.8.4.19355/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-4.8.4.19355.exe'
+$checksum = '87e73ff568e38e11b6d49185811a5a6f15117ad7'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
