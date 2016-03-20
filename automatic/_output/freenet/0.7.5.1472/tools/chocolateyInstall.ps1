18cc06ba47cbe0755596f07de3ee03bda70547c0
dtgm <dtgm1594@gmail.com>
2016-03-19 19:05:01 -0600

    Updated 2 pkg(s) on 2016-03-19: freenet,kvrt
    https://chocolatey.org/packages/freenet
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/freenet/0.7.5.1472/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'freenet'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://downloads.freenetproject.org/alpha/installer/FreenetInstaller-1472.exe'
+$checksum = 'd5e88b9ff1a5f1f329ab3cf04460c97851427239'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
