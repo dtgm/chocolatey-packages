f2c3283fb1f2e69a302ed21c2708eeb7be8289f0
dtgm <dtgm1594@gmail.com>
2016-03-23 11:00:03 -0600

    Updated 2 pkg(s) on 2016-03-23: kvrt,nem
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nem

---
+++ automatic/_output/nem/0.6.74/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'nem'
+$installerType = 'exe'
+$silentArgs = '--mode unattended --unattendedmodeui none --debuglevel 0'
+$url = 'http://bob.nem.ninja/installer/nemwallet-0.6.74-windows-installer.exe'
+$checksum = '3c4185247303b8237a53da846df46fa3f08c5916'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
