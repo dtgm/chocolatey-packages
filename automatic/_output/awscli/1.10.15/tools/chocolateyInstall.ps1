d289b766fd1fe405ed3d692542ea9c9188ca0f6f
dtgm <dtgm1594@gmail.com>
2016-03-22 23:00:02 -0600

    Updated 1 pkg(s) on 2016-03-22: awscli
    https://chocolatey.org/packages/awscli

---
+++ automatic/_output/awscli/1.10.15/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32.msi'
+$checksum = '0cc180ef5aead6a8fee71b5e53fa323a3baef3e9'
+$checksumType = 'sha1'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64.msi'
+$checksum64 = '14ff74f780ee7baddeae1eee603a334e6cd40a98'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
