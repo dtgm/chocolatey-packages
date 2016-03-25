ae7eb92d73cdb23bbf68a24211e3e4bc11211d1e
dtgm <dtgm1594@gmail.com>
2016-03-24 19:05:03 -0600

    Updated 1 pkg(s) on 2016-03-24: cmake.portable
    https://chocolatey.org/packages/cmake.portable

---
+++ automatic/_output/cmake.portable/3.5.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+$packageName = 'cmake.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://cmake.org//files/v3.5/cmake-3.5.1-win32-x86.zip'
+$checksum = '2d63030fb9952a2f42a5b3abb3b753a3436981f2'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
