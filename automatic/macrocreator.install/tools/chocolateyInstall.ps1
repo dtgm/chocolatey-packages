9e6b13eea6fccc0cb6443d049bcf9b76e21d1808
dtgm <dtgm1594@gmail.com>
2016-03-26 21:25:12 -0600

    new meta-package: macrocreator

---
+++ automatic/macrocreator.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/macrocreator.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+$packageName = '{{PackageName}}'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'md5'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
