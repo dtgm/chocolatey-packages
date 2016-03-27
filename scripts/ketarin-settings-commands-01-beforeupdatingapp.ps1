b0bc1eef24f050b0b3942d47462b6846493aed7c
dtgm <dtgm1594@gmail.com>
2016-03-27 04:45:25 -0600

    Increase legibility: add comments, use line continuations

--- scripts/ketarin-settings-commands-01-beforeupdatingapp.ps1
+++ scripts/ketarin-settings-commands-01-beforeupdatingapp.ps1
@@ -1,10 +1,18 @@
-# requires:
+# Script version: 1.1.0.0
+#
+# Changelog
+#   add comments
+#   use line continuations
+#
+# Requires:
 #  chocolateypackageupdater
 #  >=ketarin-1.8.5
 
+# Package variables to be checked for content
 $vars = "version","url","url64","checksum","checksumx64","nopush"
 $hash = @{}
 
+# Build hash using $vars for $hash.keys and corresponding content, if any, to $hash.values
 foreach ($var in $vars) {
   if ($app.variables.ContainsKey($var)) {
     $hash[$var] = $app.variables.item($var).cachedcontent
@@ -14,6 +22,25 @@ foreach ($var in $vars) {
   Write-Host "`$hash[$var] is: $($hash[$var])"
 }
 
-Write-Host "Running: 'chocopkgup $($hash["nopush"]) --packagename=$app --version=`"$($hash["version"])`" --url=`"$($hash["url"])`" --urlx64=`"$($hash["url64"])`" --checksum=`"$($hash["checksum"])`" --c64=`"$($hash["checksumx64"])`" --debug'"
+Write-Host @"
+Running: 
+chocopkgup $($hash["nopush"]) ``
+           --packagename=$app ``
+           --version=`"$($hash["version"])`" ``
+           --url=`"$($hash["url"])`" ``
+           --urlx64=`"$($hash["url64"])`" ``
+           --checksum=`"$($hash["checksum"])`" ``
+           --c64=`"$($hash["checksumx64"])`" ``
+           --debug
+"@
 
-chocopkgup $hash["nopush"] --packagename=$app --version="$($hash["version"])" --url="$($hash["url"])" --urlx64="$($hash["url64"])" --checksum="$($hash["checksum"])" --c64="$($hash["checksumx64"])" --debug
+# Using corresponding hash values of keys from $vars, set parameters of 
+# https://chocolatey.org/packages/chocolateypackageupdater
+chocopkgup $hash["nopush"] `
+           --packagename=$app `
+           --version="$($hash["version"])" `
+           --url="$($hash["url"])" `
+           --urlx64="$($hash["url64"])" `
+           --checksum="$($hash["checksum"])" `
+           --c64="$($hash["checksumx64"])" `
+           --debug
