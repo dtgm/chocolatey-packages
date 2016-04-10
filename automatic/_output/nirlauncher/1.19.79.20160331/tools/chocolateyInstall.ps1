ee1b6acabf1ae14165c5f05faf73e8dbb38f5193
dtgm <dtgm1594@gmail.com>
2016-04-10 15:47:40 -0600

    Updated 3 pkg(s) on 2016-03-31: glasswire,gramps,nirlauncher
    https://chocolatey.org/packages/glasswire
    https://chocolatey.org/packages/gramps
    https://chocolatey.org/packages/nirlauncher

---
+++ automatic/_output/nirlauncher/1.19.79.20160331/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+$packageName = 'nirlauncher'
+$url = 'http://download.nirsoft.net/nirsoft_package_1.19.79.zip'
+$checksum = '7281147f98b54ea30c9bf52f10055d86b5878148'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$tempDir = Join-Path $env:Temp $packageName
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+$tempDir = Join-Path $tempDir $env:packageVersion
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://launcher.nirsoft.net/download.html"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
