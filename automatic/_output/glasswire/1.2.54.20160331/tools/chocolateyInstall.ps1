84ead7d26b3a971ba098072835090db3f1520e2c
dtgm <dtgm1594@gmail.com>
2016-04-10 15:47:40 -0600

    Updated 3 pkg(s) on 2016-03-31: glasswire,gramps,nirlauncher
    https://chocolatey.org/packages/glasswire
    https://chocolatey.org/packages/gramps
    https://chocolatey.org/packages/nirlauncher

---
+++ automatic/_output/glasswire/1.2.54.20160331/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = '220BFB7F6AC62C223DF69D88334D90AA'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
--- automatic/_output/gramps/4.2.2/tools/chocolateyInstall.ps1
+++ automatic/_output/gramps/4.2.2/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageName = 'gramps'
 $installerType = 'exe'
 $silentArgs = '/S'
-$urlArray = 
+$urlArray = @('https://sourceforge.net/projects/gramps/files/Stable/4.2.2/GrampsAIO-4.2.2-1_win32.exe', 'https://sourceforge.net/projects/gramps/files/Stable/4.2.2/GrampsAIO-4.2.2-1_win64.exe')
 $url = $urlArray[0]
-$checksum = '7fa654db965a5ce41eae0fcb4634c01326966626'
+$checksum = 'd085f174dcc76e5327382b730eb8b1f5a41c3d16'
 $checksumType = 'sha1'
 $url64 = $urlArray[1]
 $checksum64 = '9e6effd4b702350955c2ecc936b0f107833d8600'
