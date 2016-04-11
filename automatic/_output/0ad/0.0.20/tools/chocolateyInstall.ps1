3ce4090c30b16ce7482ec3794e9b605dbd7df4f6
dtgm <dtgm1594@gmail.com>
2016-04-10 21:56:49 -0600

    Updated 2 pkg(s) on 2016-04-10: 0ad,awscli
    https://chocolatey.org/packages/0ad
    https://chocolatey.org/packages/awscli

---
+++ automatic/_output/0ad/0.0.20/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = '0ad'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = '{url64}'
+$checksum = '{checksum}'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
