b466fdf5223e15472a14b8ca266e6a04ec73efb5
dtgm <dtgm1594@gmail.com>
2016-04-16 11:00:06 -0600

    Updated 1 pkg(s) on 2016-04-16: clamwin
    https://chocolatey.org/packages/clamwin

--- automatic/_output/clamwin/0.99.1/tools/chocolateyInstall.ps1
+++ automatic/_output/clamwin/0.99.1/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = 'clamwin'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url = 'http://sourceforge.net/projects/clamwin/files/clamwin/0.99.1/clamwin-0.99.1-setup.exe'
-$checksum = '{checksum}'
+$checksum = '0261faa23e363cae34525bcc33271c0dae2796b4'
 $checksumType = 'sha1'
 $validExitCodes = @(0)  # exit codes http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
