7f370b90e43df099129fccd8886ccb87774af7ce
dtgm <dtgm1594@gmail.com>
2016-04-18 17:00:02 -0600

    Updated 3 pkg(s) on 2016-04-18: kvrt,multibit-hd,nirlauncher
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/multibit-hd
    https://chocolatey.org/packages/nirlauncher

---
+++ automatic/_output/nirlauncher/1.19.82/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+$packageName = 'nirlauncher'
+$url = 'http://download.nirsoft.net/nirsoft_package_1.19.82.zip'
+$checksum = 'f2d4e25e640ffd165066a53c41d036d6dacecb64'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://launcher.nirsoft.net/download.html"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
