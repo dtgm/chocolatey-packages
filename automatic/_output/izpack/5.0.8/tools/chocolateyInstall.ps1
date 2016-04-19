0764ff4bc25876511afda714ddf6bb4582dc55c2
dtgm <dtgm1594@gmail.com>
2016-04-18 19:05:04 -0600

    Updated 2 pkg(s) on 2016-04-18: izpack,kvrt
    https://chocolatey.org/packages/izpack
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/izpack/5.0.8/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿# generated vars
+$packageName = 'izpack'
+$url = 'https://oss.sonatype.org/content/repositories/releases/org/codehaus/izpack/izpack-dist/5.0.8/izpack-dist-5.0.8.jar'
+$checksum = '4cfd51869a847c49e6c2ff5e6bb203de7594913d'
+
+# static vars
+$checksumType = 'sha1'
+$installerType = 'izpack'
+$validExitCodes = @(0)
+
+$tempDir = Join-Path $env:Temp "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+$tempDir = Join-Path $tempDir $env:packageVersion
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
+$installFile = Join-Path $tempDir "izpack-5.0.8-installer.jar"
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installXml = Join-Path $toolsDir "chocolateyInstall.xml"
+$installArgs = "java -jar $($installFile) $($installXml)"
+
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  Start-ChocolateyProcessAsAdmin -Statements "$installArgs" `
+                                 -ExeToRun "powershell" `
+                                 -ValidExitCodes $validExitCodes
+} catch {
+  throw
+}
