1b2960105874dc7bfd37c08af746134f453cf581
dtgm <dtgm1594@gmail.com>
2016-04-20 23:00:02 -0600

    Updated 7 pkg(s) on 2016-04-20: cmder.portable,cmdermini.portable,kvrt,linkshellextension,nsis,nsis.install,nsis.portable
    https://chocolatey.org/packages/cmder.portable
    https://chocolatey.org/packages/cmdermini.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/linkshellextension
    https://chocolatey.org/packages/nsis
    https://chocolatey.org/packages/nsis.install
    https://chocolatey.org/packages/nsis.portable

--- automatic/_output/nsis.portable/2.51/tools/chocolateyInstall.ps1
+++ automatic/_output/nsis.portable/2.51/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'nsis.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://sourceforge.net/projects/nsis/files/NSIS%203%20Pre-release/3.0rc1/nsis-3.0rc1.zip/download'
-$checksum = 'c3ef1623d20cd8c8c65fdbfb86fbd03faa6f0345'
+$url = 'https://sourceforge.net/projects/nsis/files/NSIS%202/2.51/nsis-2.51.zip/download'
+$checksum = '8366044a9e78878e48eeaaae6c2e203d99321cf5'
 $checksumType = 'sha1'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
