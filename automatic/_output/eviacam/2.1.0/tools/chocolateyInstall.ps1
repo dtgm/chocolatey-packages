980d5c2416137349593fa75614e7a30b567b96ca
dtgm <dtgm1594@gmail.com>
2016-04-21 16:38:28 -0600

    Updated 1 pkg(s) on 2016-04-21: eviacam
    https://chocolatey.org/packages/eviacam

---
+++ automatic/_output/eviacam/2.1.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'eviacam'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/eviacam/files/eviacam/2.1.0/eViacam-2.1.0.exe'
+$checksum = '36da89a2b68e0b8d53aa290fba6e67a0fd97e04a'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
