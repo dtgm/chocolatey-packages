ef31567e194f488e2487d6648a8601e27a4a4a52
dtgm <dtgm1594@gmail.com>
2016-04-22 11:00:02 -0600

    Updated 4 pkg(s) on 2016-04-22: cyberduck,cyberduck.install,kcleaner,kvrt
    https://chocolatey.org/packages/cyberduck
    https://chocolatey.org/packages/cyberduck.install
    https://chocolatey.org/packages/kcleaner
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/cyberduck.install/4.9.3.19798/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-4.9.3.19798.exe'
+$checksum = '8f272808a7dcd421655cc87d4b4dce2f44108837'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kcleaner/3.0.0.71/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '709441050a836e499b8aa6e7c08744fc22ecaf24'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
