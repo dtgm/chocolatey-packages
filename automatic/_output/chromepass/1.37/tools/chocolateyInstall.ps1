9b6c7316b73706f6b151ea375a2bc19882dc179b
dtgm <dtgm1594@gmail.com>
2016-04-24 11:00:02 -0600

    Updated 3 pkg(s) on 2016-04-24: chromepass,dolphin,kvrt
    https://chocolatey.org/packages/chromepass
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/chromepass/1.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+$packageName = 'chromepass'
+$url = 'http://www.nirsoft.net/toolsdownload/chromepass.zip'
+$checksum = '74f06cf730e57ec446da04f78c9f98e5e481b28c'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://www.nirsoft.net/utils/$packageName.html"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
