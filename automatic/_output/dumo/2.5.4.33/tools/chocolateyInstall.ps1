15fb124325f617a8de4386a6724c2ca9939554a6
dtgm <dtgm1594@gmail.com>
2016-04-30 07:05:02 -0600

    Updated 3 pkg(s) on 2016-04-30: dolphin,dumo,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dumo/2.5.4.33/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = 'a7d25e0d1f66d8c01abe7ef3b00e4cbd5da9ba02'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
