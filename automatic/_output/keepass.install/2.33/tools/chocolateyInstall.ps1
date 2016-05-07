bc19f7cf512caf082d972ae33c030b97bc331ac1
dtgm <dtgm1594@gmail.com>
2016-05-07 13:05:03 -0600

    Updated 2 pkg(s) on 2016-05-07: keepass.install,kvrt
    https://chocolatey.org/packages/keepass.install
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/keepass.install/2.33/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'keepass.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.33/KeePass-2.33-Setup.exe'
+$checksum = '55396FDED60E1A7080F28488A3D8821984738FA6'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
