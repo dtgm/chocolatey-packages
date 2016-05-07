8334ed537c11453900953ea3f1d69f368f6e6640
dtgm <dtgm1594@gmail.com>
2016-05-06 19:05:05 -0600

    Updated 2 pkg(s) on 2016-05-06: kcleaner,kvrt
    https://chocolatey.org/packages/kcleaner
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kcleaner/3.0.1.73/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '2928d402965cb20b62665d28fd1ea0ee15aacfd9'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
