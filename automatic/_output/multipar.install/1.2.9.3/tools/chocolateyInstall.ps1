0654825dca5cb2fe43d5e7e1e9660d915a28150d
dtgm <dtgm1594@gmail.com>
2016-05-08 19:05:03 -0600

    Updated 5 pkg(s) on 2016-05-08: dolphin,kvrt,multipar,multipar.install,multipar.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable

---
+++ automatic/_output/multipar.install/1.2.9.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1293_setup.exe'
+$checksum = '3FDDD0883B1D29D8DA546DF9F5A34059F07BC2E3'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/multipar.portable/1.2.9.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.portable'
+$url = 'http://multipar.eu//download/archive/dl.php?fn=MultiPar1293.zip'
+$checksum = '011069DAF0CA28FF4040956F8F33526F9058744F'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
