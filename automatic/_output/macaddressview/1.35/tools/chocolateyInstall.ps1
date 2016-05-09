2ced188aa28d9f49d835a4ccd49ca26b1b2a7478
dtgm <dtgm1594@gmail.com>
2016-05-09 07:05:04 -0600

    Updated 2 pkg(s) on 2016-05-09: kvrt,macaddressview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/macaddressview

---
+++ automatic/_output/macaddressview/1.35/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'macaddressview'
+$url = 'http://www.nirsoft.net/utils/macaddressview.zip'
+$checksum = 'bf214828a68860734199badeea1e60ec578ef564'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
