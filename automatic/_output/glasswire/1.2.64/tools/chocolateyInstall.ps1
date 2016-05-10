d54d313fc4000fea616ccde04f6880ce425d2b1b
dtgm <dtgm1594@gmail.com>
2016-05-10 07:05:07 -0600

    Updated 4 pkg(s) on 2016-05-10: adwcleaner,arduino,dolphin,glasswire
    https://chocolatey.org/packages/adwcleaner
    https://chocolatey.org/packages/arduino
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/glasswire

---
+++ automatic/_output/glasswire/1.2.64/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = 'DE5A323C8856F1799BA1049440915CD2'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
