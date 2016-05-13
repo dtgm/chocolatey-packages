a3cc508360e928c2f8a6b51d77cd3b4b8fbef90d
dtgm <dtgm1594@gmail.com>
2016-05-13 16:17:07 -0600

    faulty builds that were made in err

--- automatic/_output/jstock/1.0.7.22/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName = 'jstock'
-$installerType = 'exe'	
-$silentArgs = '/S'
-$url = 'http://github.com/yccheok/jstock/releases/download/release_1-0-7v/jstock-1.0.7v-setup.exe'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
--- automatic/_output/jstock/1.0.7.26/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'jstock'
-$installerType = 'exe'	
-$silentArgs = '/S'
-$url = 'http://github.com/yccheok/jstock/releases/download/release_1-0-7z/jstock-1.0.7z-setup.exe'
-$checksum = 'a50768d619a2d023474533d9db11b25f19928020'
-$checksumType = 'sha1'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage -PackageName "$packageName" -InstallerType "$installerType" -Url "$url" -SilentArgs "$silentArgs" -ValidExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
