ed23913776110fd426c8b5b43d5ccf1e4c0e29f0
dtgm <dtgm1594@gmail.com>
2016-05-18 17:00:02 -0600

    Updated 4 pkg(s) on 2016-05-18: dolphin,kcleaner,kvrt,wirelessnetview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kcleaner
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/wirelessnetview

---
+++ automatic/_output/kcleaner/3.0.2.74/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '34d5957d6a16912b55e2d8e4565b0e623a3b0af1'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2016.05.18.1613/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '075ec02a8a1addb67d7444dbe9bb2ea27b6919aa'
+$checksumType = 'sha1'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
