1f15317d90439cc0d85b8007301b228ae023a798
dtgm <dtgm1594@gmail.com>
2016-05-26 22:30:03 -0600

    Fix: #158
    deprecate winsshd, winsshd.install
    in favor of bitvise-ssh-server due to program name change

---
+++ automatic/_output/bitvise-ssh-server/6.47/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'bitvise-ssh-server'
+$softwareName = "Bitvise SSH Server*"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+$key | ForEach-Object {
+  Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                              -FileType "$installerType" `
+                              -SilentArgs "$($silentArgs)" `
+                              -File "& $($_.UninstallString)" `
+                              -ValidExitCodes $validExitCodes
+}
