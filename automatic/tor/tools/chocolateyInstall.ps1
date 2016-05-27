3b3c2528d560cb1fd2ee240582dd3e6be353943c
dtgm <dtgm1594@gmail.com>
2016-05-27 07:40:00 -0600

    new package: tor

--- automatic/tor/tools/chocolateyInstall.ps1
+++ automatic/tor/tools/chocolateyInstall.ps1
@@ -1,49 +1,11 @@
 $packageName = '{{PackageName}}'
-$urlArray = {{DownloadUrlx64}}
-$url = $urlArray[0]
+$url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
-$checksumType = 'sha1'
-$url64 = $urlArray[1]
-$checksum64 = '{{Checksumx64}}'
-$checksumType64 = 'sha1'
-$installerType = 'msi'
-$silentArgs = '/quiet /qn /norestart'
-$validExitCodes = @(0,3010)
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Write-Verbose "Downloading and installing program..."
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -Url64bit "$url64" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -Checksum64 "$checksum64" `
-                          -ChecksumType64 "$checksumType64"
-
-Write-Verbose "Querying registry for install location..."
-$packageSearch = "MariaDB *"
-$regKey = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                   'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                   'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                           -ErrorAction:SilentlyContinue `
-          | Where-Object {$_.DisplayName -like $packageSearch}
-
-Write-Verbose "Adding MariaDB bin directory to path environment variable..."
-$binPath = Join-Path $regKey.InstallLocation "bin"
-if ($binPath) {
-  Install-ChocolateyPath $binPath 'Machine'
-}
-
-Write-Verbose "Installing service..."
-Start-Process -FilePath "$binPath\mysqld" `
-              -ArgumentList '--install' `
-              -Wait `
-              -NoNewWindow
-
-Write-Verbose "Starting service..."
-Start-Process -FilePath "NET" `
-              -ArgumentList 'START MySQL' `
-              -Wait `
-              -NoNewWindow
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
