ca6c65577f19db99b51ba5b54f62d2eb9eea1ae6
dtgm <dtgm1594@gmail.com>
2016-06-01 17:00:01 -0600

    Updated 2 pkg(s) on 2016-06-01: nginx,viber
    https://chocolatey.org/packages/nginx
    https://chocolatey.org/packages/viber

---
+++ automatic/_output/viber/3.1.1.60/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'viber'
+$installerType = 'EXE'
+$url = 'http://download.cdn.viber.com/cdn/desktop/windows/ViberSetup.exe'
+$silentArgs = ''
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
