9e5c1b71a32c22207e4e4b94ba6ebcf6037c5181
dtgm <dtgm1594@gmail.com>
2016-06-01 17:02:51 -0600

    Fix #204 s/.net/.org/

--- automatic/_output/nginx/1.10.1/tools/chocolateyInstall.ps1
+++ automatic/_output/nginx/1.10.1/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'nginx'
-$url = 'http://www.nginx.net/download/nginx-1.10.1.zip'
+$url = 'https://www.nginx.org/download/nginx-1.10.1.zip'
 $checksum = 'c0a948de8cb59ad5e6998419a0048c1bc98a83b405064202ea61f00d61e03cba'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
