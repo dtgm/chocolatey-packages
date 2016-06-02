26e90e93f9165669be21cd019f8562363ba5bd73
dtgm <dtgm1594@gmail.com>
2016-06-02 01:05:02 -0600

    Updated 3 pkg(s) on 2016-06-02: systemninja,systemninja.install,systemninja.portable
    https://chocolatey.org/packages/systemninja
    https://chocolatey.org/packages/systemninja.install
    https://chocolatey.org/packages/systemninja.portable

---
+++ automatic/_output/systemninja.install/3.1.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'systemninja.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://singularlabs.com/download/10294/'
+$checksum = '566b740b0f7435040cc78947448f9e3e969b3bbefd9c4db12b45b10264cd1c4f'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
