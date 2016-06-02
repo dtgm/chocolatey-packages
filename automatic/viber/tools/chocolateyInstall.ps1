d942cb57c5c8a7456de53852ca545b88ba334070
dtgm <dtgm1594@gmail.com>
2016-06-01 19:23:07 -0600

    viber handover

--- automatic/viber/tools/chocolateyInstall.ps1
+++ automatic/viber/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$packageName = 'viber'
+﻿$packageName = '{{PackageName}}'
 $installerType = 'exe'
 $silentArgs = '/install /quiet /norestart'
-$url = 'http://download.cdn.viber.com/cdn/desktop/windows/ViberSetup.exe'
-$checksum = '225226700d596a1f5c2c5203020cc97954f9721fcf90c45bb6aa7156bbb2390e'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
