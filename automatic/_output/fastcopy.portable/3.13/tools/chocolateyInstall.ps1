191879f4f422b3e76f65f29fde43bd37fc229a58
dtgm <dtgm1594@gmail.com>
2016-06-03 17:00:02 -0600

    Updated 2 pkg(s) on 2016-06-03: fastcopy,fastcopy.portable
    https://chocolatey.org/packages/fastcopy
    https://chocolatey.org/packages/fastcopy.portable

--- automatic/_output/fastcopy.portable/3.13/tools/chocolateyInstall.ps1
+++ automatic/_output/fastcopy.portable/3.13/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageName = 'fastcopy.portable'
+$packageName = 'fastcopy.portable'
 $url = 'http://ftp.vector.co.jp/66/88/2323/FastCopy313.zip'
-$checksum = '{checksum}'
+$checksum = 'f1cd5ba25b772a45ebf4bde772b631a4989118490c190677c6c9c23bb5ec6515'
 $checksumType = 'sha256'
 $url64 = 'http://ftp.vector.co.jp/66/88/2323/FastCopy313_x64.zip'
-$checksum64 = '{checksumx64}'
+$checksum64 = 'b34ae52a3fc203194577431885852f4f1f86d2ce485beb106b1f0515819cdba5'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
