649dfa0489cb181b262c4e840bfc4e69d227e084
dtgm <dtgm1594@gmail.com>
2016-06-03 13:05:02 -0600

    Updated 2 pkg(s) on 2016-06-03: goodsync,kvrt
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/goodsync/9.9.48/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'goodsync'
+$fileType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
+$checksum = '1026b550526e5c5504ced3acfac73d8341f4ea93e3d820c55094128f1c793001'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$fileType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
