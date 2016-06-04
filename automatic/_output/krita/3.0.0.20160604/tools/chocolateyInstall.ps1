7db3147eee935fde36bde15a674558c0cd99e695
dtgm <dtgm1594@gmail.com>
2016-06-04 17:00:02 -0600

    Updated 1 pkg(s) on 2016-06-04: krita
    https://chocolatey.org/packages/krita

---
+++ automatic/_output/krita/3.0.0.20160604/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'krita'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://files.kde.org/krita/3/windows/krita-3.0-x86-setup.exe'
+$checksum = '2944a822d13de754dbb4e91d63edadb05af3dfa03d1524699553663174d3bc9e'
+$checksumType = 'sha256'
+$url64 = 'http://files.kde.org/krita/3/windows/krita-3.0-x64-setup.exe'
+$checksum64 = '914a40a1ff8aa08805d2cfdf32f9d46b793510bb97c1888887538aa7f82d7085'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
