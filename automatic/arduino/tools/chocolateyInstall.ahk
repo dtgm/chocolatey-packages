04e9fa1063f789d2cc2b00f98495c96ad3ee91cb
dtgm <dtgm1594@gmail.com>
2016-06-06 13:52:08 -0600

    attempt to fix arduino

--- automatic/arduino/tools/chocolateyInstall.ahk
+++ automatic/arduino/tools/chocolateyInstall.ahk
@@ -1,4 +1,5 @@
 #NoEnv
+#NoTrayIcon
 SendMode Input
 SetTitleMatchMode, 1   ;begins
 DetectHiddenText, off  ;toggle search hidden window text
@@ -6,7 +7,7 @@ DetectHiddenWindows, off  ;toggle detect hidden windows
 
 winTitle = Windows Security ahk_class #32770
 
-WinWait, %winTitle%, You should only install driver software from publishers you trust, 300
-ControlClick, &Install, %winTitle%
+WinWait, %winTitle%, , 300
+ControlClick, Button1, %winTitle%
 
 ExitApp
--- automatic/arduino/tools/chocolateyInstall.ps1
+++ automatic/arduino/tools/chocolateyInstall.ps1
@@ -1,15 +1,16 @@
 $packageName = '{{PackageName}}'
-$installerType = 'EXE'
+$installerType = 'exe'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
+
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $ahkExe = 'AutoHotKey'
-$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
-
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
 Start-Process $ahkExe $ahkFile
+
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
