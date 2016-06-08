e581284cf4b3d70f5883225e8cc3231f2e30b2d5
dtgm <dtgm1594@gmail.com>
2016-06-07 19:05:02 -0600

    Updated 1 pkg(s) on 2016-06-07: iconsext.portable
    https://chocolatey.org/packages/iconsext.portable

--- automatic/_output/iconsext.portable/1.47/tools/chocolateyInstall.ps1
+++ automatic/_output/iconsext.portable/1.47/tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
 $packageName = 'iconsext.portable'
 $url = 'http://www.nirsoft.net/utils/iconsext.zip'
-$checksum = '485dc440cec3a30d221c5401e8909607bd74a4eb'
-$checksumType = 'sha1'
+$checksum = '97582531dd0176e312f3a8b101e50d02224970aec5030f0e0ff2ac767f572981'
+$checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "iconsext.exe"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
 
