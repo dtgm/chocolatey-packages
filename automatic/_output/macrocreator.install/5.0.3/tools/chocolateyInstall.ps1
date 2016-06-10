74348c21382733ecb367aeeecc167c82ccb9aac4
dtgm <dtgm1594@gmail.com>
2016-06-10 01:05:02 -0600

    Updated 4 pkg(s) on 2016-06-09: kvrt,macrocreator,macrocreator.install,macrocreator.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/macrocreator
    https://chocolatey.org/packages/macrocreator.install
    https://chocolatey.org/packages/macrocreator.portable

---
+++ automatic/_output/macrocreator.install/5.0.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'macrocreator.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/Pulover/PuloversMacroCreator/releases/download/v5.0.3/MacroCreator-setup.exe'
+$checksum = '287f4a71eafd7f98b6669aa39ecf2261'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
