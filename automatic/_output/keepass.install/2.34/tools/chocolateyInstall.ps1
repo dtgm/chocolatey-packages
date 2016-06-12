8482b30a1aea0801098b2c61d7f2b24a76101aff
dtgm <dtgm1594@gmail.com>
2016-06-12 11:00:04 -0600

    Updated 3 pkg(s) on 2016-06-12: keepass,keepass.install,keepass.portable
    https://chocolatey.org/packages/keepass
    https://chocolatey.org/packages/keepass.install
    https://chocolatey.org/packages/keepass.portable

---
+++ automatic/_output/keepass.install/2.34/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'keepass.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.34/KeePass-2.34-Setup.exe'
+$checksum = '2505ABE932C20880996D10B328CFD046A6EEA366'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/keepass.portable/2.34/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿# generated vars
+$packageName = 'keepass.portable'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.34/KeePass-2.34.zip'
+$checksum = '27A790F7E554527D79E801F91081DC3D7655F25A'
+
+# static vars
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $Env:ChocolateyInstall\helpers\functions
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "KeePass.exe.gui"
+Set-Content -Path $installFile `
+            -Value $null
