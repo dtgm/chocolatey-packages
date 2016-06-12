f32058d0bf0b0e2a495ae67824129c6dda88b182
dtgm <dtgm1594@gmail.com>
2016-06-11 21:13:31 -0600

    remove Write-Verbose "...-InstallArguments..." which was causing to fail validation

--- automatic/bitvise-ssh-server/tools/chocolateyInstall.ps1
+++ automatic/bitvise-ssh-server/tools/chocolateyInstall.ps1
@@ -7,18 +7,6 @@ $silentArgs = ''
 $validExitCodes = @(0..63)
 
 $arguments = @{};
-Write-Verbose "You may pass native install arguments directly to Chocolatey with -installArguments"
-Write-Verbose "     Note: Use two single quotes when double quotes are desired."
-Write-Verbose "BvSshServer-Inst -installDir=directory OR -defaultSite OR -site=site-name"
-Write-Verbose "                  [-force OR -abortOnWarning[=warning-list-or-mask]"
-Write-Verbose "                  [-acceptEULA] [-interactive] [-noRollback]"
-Write-Verbose "                  [-renameExistingDir=existingDir]"
-Write-Verbose "                  [-activationCode=activation-code-hex]"
-Write-Verbose "                  [-keypairs=keypairs-file]"
-Write-Verbose "                  [-settings=settings-file]"
-Write-Verbose "                  [-siteTypeSettings=fileName]"
-Write-Verbose "                  [-startService]"
-Write-Verbose "                  [-startBssCtrl]"
 $packageParameters = $env:chocolateyPackageParameters;
 
 # Default values
