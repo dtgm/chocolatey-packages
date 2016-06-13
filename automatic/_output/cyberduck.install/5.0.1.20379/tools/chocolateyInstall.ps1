e969f3bb7f33c3f34cd2da561d361ae224e97854
dtgm <dtgm1594@gmail.com>
2016-06-13 07:05:04 -0600

    Updated 3 pkg(s) on 2016-06-13: cyberduck,cyberduck.install,kvrt
    https://chocolatey.org/packages/cyberduck
    https://chocolatey.org/packages/cyberduck.install
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/cyberduck.install/5.0.1.20379/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-5.0.1.20379.exe'
+$checksum = '0e1c06d04cba2cc75f277135d405d8df09054d7d6679dfac115aa239d6873ed7'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
