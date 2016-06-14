e6aea299c67c7530fa1236eba355788bb4a734bc
dtgm <dtgm1594@gmail.com>
2016-06-14 01:05:03 -0600

    Updated 2 pkg(s) on 2016-06-13: git-annex,kvrt
    https://chocolatey.org/packages/git-annex
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/git-annex/6.20160613/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = 'git-annex'
+$installerType = 'exe'
+$silentArgs = '/S /D=C:\Program Files\Git'
+$url = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
+$checksum = '841db1f017769e218636d90009043b72caff0806347f6dbec1cc345bae9df62e'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2016.06.14.0515/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '3bdd1c07c47fbf8d77c2d983dd726b90aa3067cf1806865d115c643365a4300c'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
