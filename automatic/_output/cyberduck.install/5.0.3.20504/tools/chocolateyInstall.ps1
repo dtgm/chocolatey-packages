ca41161b06330d531c3c6fbe3f1efd8f47f94923
dtgm <dtgm1594@gmail.com>
2016-06-17 13:05:04 -0600

    Updated 3 pkg(s) on 2016-06-17: cyberduck,cyberduck.install,dolphin
    https://chocolatey.org/packages/cyberduck
    https://chocolatey.org/packages/cyberduck.install
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/cyberduck.install/5.0.3.20504/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-5.0.3.20504.exe'
+$checksum = 'a6a1056f26d40675a377b3bb89e65e5e8426464f75ff3b6fe4ec0f6f1c2b1374'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/dolphin/4.0.9452/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-9452-x64.7z'
+$checksum = '30bd35f0449f1485d2ad280ef466ba6b3a551167a7c61a725208b27ea6f0ce27'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
