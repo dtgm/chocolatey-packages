29f6b6b83150e83b59d5615f93806cf6f26c7974
dtgm <dtgm1594@gmail.com>
2016-06-17 17:00:02 -0600

    Updated 2 pkg(s) on 2016-06-17: crystaldiskmark,kvrt
    https://chocolatey.org/packages/crystaldiskmark
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kvrt/2016.06.17.1926/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = 'cca84eb4f0d1999271afad863ec62de48cf710c468d30df5f19b010061135122'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
