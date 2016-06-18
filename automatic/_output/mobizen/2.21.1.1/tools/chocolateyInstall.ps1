a8b34937da7fe4c39952bb6dc24211a85bb99a29
dtgm <dtgm1594@gmail.com>
2016-06-18 17:00:02 -0600

    Updated 1 pkg(s) on 2016-06-18: mobizen
    https://chocolatey.org/packages/mobizen

---
+++ automatic/_output/mobizen/2.21.1.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'mobizen'
+$installerType = 'exe'
+$silentArgs = '/S /v/qn'
+$url = 'https://download.mobizen.com/download/mobizen.exe'
+$checksum = 'a80320a79823da215c34849d71cf719537d55ab7fd832f597ae9dfa73e8fa74e'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
