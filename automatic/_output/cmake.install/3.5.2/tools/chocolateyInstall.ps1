74187614744a58da3c00990551677294f1b8a22d
dtgm <dtgm1594@gmail.com>
2016-06-17 23:00:02 -0600

    Updated 2 pkg(s) on 2016-06-17: cmake,cmake.install
    https://chocolatey.org/packages/cmake
    https://chocolatey.org/packages/cmake.install

---
+++ automatic/_output/cmake.install/3.5.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'cmake.install'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'http://cmake.org/files/v3.5/cmake-3.5.2-win32-x86.msi'
+$checksum = '32a7d80fd69f37de7cbc03ce633d3d33cee16dd241145c0147ffc771306ac81e'
+$checksumType = 'sha256'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
