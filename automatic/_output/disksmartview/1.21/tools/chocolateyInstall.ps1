5042eaf1323e80986735390eeea89b0f98345143
dtgm <dtgm1594@gmail.com>
2016-06-19 01:05:02 -0600

    Updated 2 pkg(s) on 2016-06-18: disksmartview,dolphin
    https://chocolatey.org/packages/disksmartview
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/disksmartview/1.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'disksmartview'
+$url = 'http://www.nirsoft.net/utils/disksmartview.zip'
+$checksum = '8d10b7f4c8b60a39e33f28dc45782f734ce4af84bac33945251786c0c361f9dd'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/dolphin/4.0.9469/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-9469-x64.7z'
+$checksum = '5070995d528ae3394890ba605bed247f7bb504a091923576fc257c1c8eab5f71'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
