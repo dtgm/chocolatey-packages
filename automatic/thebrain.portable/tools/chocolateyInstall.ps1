c04230f4088d35d0bf0957463b916c3edbc77003
dtgm <dtgm1594@gmail.com>
2016-06-21 01:05:05 -0600

    new pkgs: copay, thebrain.portable

---
+++ automatic/thebrain.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$extractDir = Join-Path $toolsDir $packageName
+$installFile = Join-Path $extractDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
