5bec2ecd0b369169e6e626e251f239ef651efa21
dtgm <dtgm1594@gmail.com>
2016-06-21 13:05:02 -0600

    Updated 1 pkg(s) on 2016-06-21: copay
    https://chocolatey.org/packages/copay

---
+++ automatic/_output/copay/2.1.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'copay'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/bitpay/copay/releases/download/v2.1.0/Copay-win.exe'
+$checksum = '6ba88329695647376ac89020a45a81680b1eeb8836017ae93b942829a7c10aae'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
