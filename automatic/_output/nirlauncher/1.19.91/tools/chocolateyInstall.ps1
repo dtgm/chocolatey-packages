05e3f4f9662bcc8b545101d51135b94333ab155e
dtgm <dtgm1594@gmail.com>
2016-06-20 23:00:02 -0600

    Updated 6 pkg(s) on 2016-06-20: kvrt,nirlauncher,peazip,peazip.install,thebrain.portable,windowsrepair
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nirlauncher
    https://chocolatey.org/packages/peazip
    https://chocolatey.org/packages/peazip.install
    https://chocolatey.org/packages/thebrain.portable
    https://chocolatey.org/packages/windowsrepair

---
+++ automatic/_output/nirlauncher/1.19.91/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$packageName = 'nirlauncher'
+$url = 'http://download.nirsoft.net/nirsoft_package_1.19.91.zip'
+$checksum = '502e0d6bc4be341b399d90ce156b8a4241c7f2f1'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$chocoTempDir = Join-Path $Env:Temp "$packageName"
+if (![System.IO.Directory]::Exists($chocoTempDir)) {[System.IO.Directory]::CreateDirectory($chocoTempDir)}
+$zipFile = Join-Path $chocoTempDir "$($packageName).zip"
+
+$referer = "http://launcher.nirsoft.net/download.html"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
+
+Get-ChildItem -Filter "*.exe" -Recurse -Path "$toolsdir\*" -Exclude @("nircmd.exe", "nircmdc.exe") | %{ 
+    New-Item ($_.FullName + ".gui") -ErrorAction SilentlyContinue
+}
---
+++ automatic/_output/peazip.install/6.0.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'peazip.install'
+$urlArray = @('http://sourceforge.net/projects/peazip/files/6.0.3/peazip-6.0.3.WINDOWS.exe/download', 'http://sourceforge.net/projects/peazip/files/6.0.3/peazip-6.0.3.WIN64.exe/download')
+$url = $urlArray[0]
+$checksum = 'f695d6f85b74991995d23dc5a40b8a4c69e6b382243f1728a6e9d335eebfe42d'
+$checksumType = 'sha256'
+$url64 = $urlArray[1]
+$checksum64 = '04ea5a270bbb5d6fc0021837fbc0ad5f89354a807c55327ef130c3a730c83ce8'
+$checksumType64 = 'sha256'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
