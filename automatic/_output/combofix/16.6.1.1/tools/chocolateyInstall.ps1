40a205e1448b3dc4eed6ff95bb5ea40e6349ebf0
dtgm <dtgm1594@gmail.com>
2016-06-21 05:00:02 -0600

    Updated 2 pkg(s) on 2016-06-21: combofix,kvrt
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/combofix/16.6.1.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'combofix'
+$url = 'https://download.bleepingcomputer.com/sUBs/ComboFix.exe'
+$checksum = '5aeda94d6e8a35ae40aac6ef06a04eac4a52a2878226e9f3a04afde77960a029'
+$checksumType = 'sha256'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$installFile = Join-Path $toolsDir "ComboFix.exe"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Write-Verbose "Create an empty sidecar metadata file for shimgen.exe to designate executable as GUI."
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/kvrt/2016.06.21.0334/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '683926cdd5ce6f05306d0cb66f7821ab6eb7250d83ddcb6a8a4aed3bbd51f2a0'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
