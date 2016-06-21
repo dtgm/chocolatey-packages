2ac00162ef4bd99fa81b82c744be8756fa128b94
dtgm <dtgm1594@gmail.com>
2016-06-21 01:08:42 -0600

    handover: multiavchd
    updated URL and improved install/uninstall scripts

---
+++ manual/multiavchd/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'multiavchd'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://www.videohelp.com/download/multiAVCHD_4.1.exe'
+$checksum = 'bf357c9ff2012a6040438ea24349b82fafbfcacd368e99643bcc5d8198a6973e'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$installPath = Join-Path (Get-BinRoot) $packageName
+$silentArgs = $silentArgs + "/D=`"$installPath`""
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
+
+Install-ChocolateyPath $installPath 'User'
---
+++ manual/multiavchd/tools/chocolateyUninstall.ps1
@@ -0,0 +1,29 @@
+﻿$packageName = 'multiavchd'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$installPath = Join-Path (Get-BinRoot) $packageName
+$file = Join-Path $installPath "Uninstall.exe"
+
+if (Test-Path $file) {
+  Uninstall-ChocolateyPackage -PackageName $packageName `
+                              -FileType $installerType `
+                              -SilentArgs $silentArgs `
+                              -File $file `
+                              -ValidExitCodes $validExitCodes
+} else {
+  Write-Verbose "Not found: `"$file`""
+  Write-Host "$packageName has already been uninstalled."
+}
+
+if (Test-Path $installPath) {
+  Write-Verbose "Removing leftover files in `"$installPath`""
+  Remove-Item -Path $installPath -Recurse -Force
+}
+
+Write-Verbose "Removing from path..."
+$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$unPath = Join-Path $toolsPath 'Uninstall-ChocolateyPath.psm1'
+Import-Module $unPath
+Uninstall-ChocolateyPath $installPath 'User'
