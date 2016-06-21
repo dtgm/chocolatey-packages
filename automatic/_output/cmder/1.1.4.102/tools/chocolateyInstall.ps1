a84e1e59a117e0e0d4476b3f8fe98caa4b5076d7
dtgm <dtgm1594@gmail.com>
2016-06-21 17:00:02 -0600

    Updated 3 pkg(s) on 2016-06-21: cmder,dolphin,kvrt
    https://chocolatey.org/packages/cmder
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/cmder/1.1.4.102/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'cmder'
+$url = 'https://github.com/cmderdev/cmder/releases/download/v1.1.4.1/cmder.7z'
+$checksum = '06f8ed2542f3d49208ab73bffd71de78'
+$checksumType = 'md5'
+$binRoot = Get-BinRoot
+$installPath = Join-Path $binRoot 'cmder'
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -UnzipLocation $installPath `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType
+
+Install-ChocolateyPath $installPath 'user'
