28e8dc52c3ee6228c1286fb82a9a819f502530de
dtgm <dtgm1594@gmail.com>
2016-06-23 19:05:02 -0600

    Updated 3 pkg(s) on 2016-06-23: dolphin,firefox-nightly,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/4.0.9507/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-4.0-9507-x64.7z'
+$checksum = '4717807544396ad6ef2a2224364b522021b2cc8b6967dc2141f3d81be3d6f6e9'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
--- automatic/_output/firefox-nightly/50.0.1.2016062312/tools/chocolateyInstall.ps1
+++ automatic/_output/firefox-nightly/50.0.1.2016062312/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win32.installer.exe'
 $checksumType = 'sha256'
 $url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win64.installer.exe'
-$checksum64 = '{checksumx64}'
+$checksum64 = 'dbc94bd68bbd524e4ed8e3561874e92b469a760b62789d6e021e972d4ad7a984'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
