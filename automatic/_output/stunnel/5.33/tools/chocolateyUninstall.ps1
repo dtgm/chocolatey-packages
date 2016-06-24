da063f078492754b98e442630b9fe76ddad667ba
dtgm <dtgm1594@gmail.com>
2016-06-24 01:05:02 -0600

    Updated 1 pkg(s) on 2016-06-24: stunnel
    https://chocolatey.org/packages/stunnel

---
+++ automatic/_output/stunnel/5.33/tools/chocolateyUninstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'stunnel'
+$packageSearch = "$packageName*"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$key = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                        -ErrorAction:SilentlyContinue `
+     | Where-Object   { $_.DisplayName -like $packageSearch }
+
+$key | ForEach-Object { 
+         $unFile = $_.UninstallString -replace "^`"?([^`"]+).*","`$1"
+         Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                     -FileType "$installerType" `
+                                     -SilentArgs "$silentArgs" `
+                                     -File "$unFile" `
+                                     -ValidExitCodes $validExitCodes }
