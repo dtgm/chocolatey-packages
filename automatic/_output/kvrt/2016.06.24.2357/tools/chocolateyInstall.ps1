65b2a84a6c78fde6d8a20c3d3492452b460cfa50
dtgm <dtgm1594@gmail.com>
2016-06-24 23:00:08 -0600

    Updated 2 pkg(s) on 2016-06-24: firefox-nightly,kvrt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kvrt/2016.06.24.2357/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '22b95982c482bb158137b3db4a996b02c61a0f51cf83355bde1141abef205833'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
