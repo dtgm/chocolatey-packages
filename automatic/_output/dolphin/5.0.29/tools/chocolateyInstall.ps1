8c15d7abfec719affc5769a081ed5645a85292d0
dtgm <dtgm1594@gmail.com>
2016-06-25 13:05:02 -0600

    Updated 5 pkg(s) on 2016-06-25: dolphin,dumo,firefox-nightly,keepass-plugin-enhancedentryview,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/keepass-plugin-enhancedentryview
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.29/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-29-x64.7z'
+$checksum = '5e2a8900f3944cdbd4de21442f1b5cdb192bf011987c6378be5c22ef441f8787'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/firefox-nightly/50.0.1.2016062513/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win32.installer.exe'
+$checksum = '9fd912a5822d8f59bc25ea3eab822829c73bb505ca21c483dba2a57167ed8c0a'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win64.installer.exe'
+$checksum64 = '94b1de2e69faeff93073fbc37897d6f47e681ec44d853b89b64bc716ab6b9244'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
