7cb08d9a876936be4abf72ce1c0a0108ae8088f1
dtgm <dtgm1594@gmail.com>
2016-06-24 19:05:01 -0600

    Updated 3 pkg(s) on 2016-06-24: dolphin,goodsync,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/kvrt

--- automatic/_output/goodsync/9.9.49/tools/chocolateyInstall.ps1
+++ automatic/_output/goodsync/9.9.49/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $fileType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
 $url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
-$checksum = '{checksum}'
+$checksum = 'a9adc345b0381beee698348dd4d72d1e0497c1ddbf755ec4fc73e59c43e86767'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
