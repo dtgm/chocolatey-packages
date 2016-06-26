e73ec5b8028692cd25633b383165f375030748ac
dtgm <dtgm1594@gmail.com>
2016-06-26 17:00:02 -0600

    Updated 5 pkg(s) on 2016-06-26: firefox-nightly,kvrt,telegram,telegram.install,telegram.portable
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/telegram
    https://chocolatey.org/packages/telegram.install
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/telegram.portable/0.9.56/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.0.9.56.zip'
+$checksum = 'f7a18d60fd4f474cc9ab2ef5780425b4d66b7dd45a0b67cbf5027403da869c30'
+$checksumType = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
