a17cd5a09967a67c4a171a825e2d15beb2a8a80a
dtgm <dtgm1594@gmail.com>
2016-06-27 23:00:02 -0600

    Updated 4 pkg(s) on 2016-06-27: aida64-business,aida64-engineer,aida64-extreme,aida64-networkaudit
    https://chocolatey.org/packages/aida64-business
    https://chocolatey.org/packages/aida64-engineer
    https://chocolatey.org/packages/aida64-extreme
    https://chocolatey.org/packages/aida64-networkaudit

---
+++ automatic/_output/aida64-engineer/5.75.3900/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'aida64-engineer'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://download.aida64.com/aida64engineer575.exe'
+$checksum = 'c2ad8a03ff84a46583a1be25993dc5fae104e63116c74c4998f1550903f5396a'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/aida64-extreme/5.75.3900/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'aida64-extreme'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://download.aida64.com/aida64extreme575.exe'
+$checksum = 'ef9874d6d17c8566ae2482ab9d4fc8d2f7f594ea4b0cfe288d88bee26d35899e'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
