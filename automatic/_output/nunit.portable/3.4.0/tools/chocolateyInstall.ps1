924be4228a0e9f3a05c9124e29b1e53b617d9199
dtgm <dtgm1594@gmail.com>
2016-06-28 23:00:02 -0600

    Updated 6 pkg(s) on 2016-06-28: kvrt,nunit,nunit.install,nunit.portable,windowsrepair,wnetwatcher.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nunit
    https://chocolatey.org/packages/nunit.install
    https://chocolatey.org/packages/nunit.portable
    https://chocolatey.org/packages/windowsrepair
    https://chocolatey.org/packages/wnetwatcher.portable

---
+++ automatic/_output/nunit.portable/3.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'nunit.portable'
+$url = 'https://github.com/nunit/nunit/releases/download/3.4.0/NUnit-3.4.0.zip'
+$checksum = '8607f3859d390ee35214ed755de81a16'
+$checksumType = 'md5'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
