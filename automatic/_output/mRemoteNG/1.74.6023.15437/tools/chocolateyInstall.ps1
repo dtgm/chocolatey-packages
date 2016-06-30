7a04f1f60ed731baf92dfda15575b4e27fb1a632
dtgm <dtgm1594@gmail.com>
2016-06-29 19:05:04 -0600

    Updated 2 pkg(s) on 2016-06-29: kvrt,mRemoteNG
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/mRemoteNG

---
+++ automatic/_output/mRemoteNG/1.74.6023.15437/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'mRemoteNG'
+$installerType = 'exe'
+$url  = 'http://downloads.mremoteng.org/mRemoteNG-Installer-1.72.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -Url "$url" -SilentArgs "$silentArgs" -ValidExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
