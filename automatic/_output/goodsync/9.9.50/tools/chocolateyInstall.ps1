fe6e26685eb16cfaa70f671c55415f01134edb66
dtgm <dtgm1594@gmail.com>
2016-07-01 13:05:02 -0600

    Updated 4 pkg(s) on 2016-07-01: copay,goodsync,kvrt,mariadb
    https://chocolatey.org/packages/copay
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/mariadb

---
+++ automatic/_output/goodsync/9.9.50/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'goodsync'
+$fileType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://www.goodsync.com/download/GoodSync-Setup.msi'
+$checksum = '34b9ebebcc5f1455c307d5a62f4d605367ea9025765c23575cf74f60056648da'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$fileType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
