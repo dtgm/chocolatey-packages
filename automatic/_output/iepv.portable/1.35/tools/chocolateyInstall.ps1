c9605ee1ec2ffe49ad08d6d60677c875fca31a70
dtgm <dtgm1594@gmail.com>
2016-07-02 01:05:04 -0600

    Updated 3 pkg(s) on 2016-07-01: iepv.install,iepv.portable,kvrt
    https://chocolatey.org/packages/iepv.install
    https://chocolatey.org/packages/iepv.portable
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/iepv.portable/1.35/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$packageName = 'iepv.portable'
+$url = 'http://www.nirsoft.net/toolsdownload/iepv.zip'
+$checksum = 'ab05b4645a517b2fe36f5d65fe9b1558ff226d99afd69100680ad060833c7fbb'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "iepv.exe"
+
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://www.nirsoft.net/utils/"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
