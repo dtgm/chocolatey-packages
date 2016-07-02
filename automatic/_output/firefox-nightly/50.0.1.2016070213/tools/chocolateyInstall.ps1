f5b33150f2698f243d88b23bc9f6ea499dbf7dfc
dtgm <dtgm1594@gmail.com>
2016-07-02 11:00:26 -0600

    Updated 3 pkg(s) on 2016-07-02: firefox-nightly,kvrt,nxt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nxt

---
+++ automatic/_output/firefox-nightly/50.0.1.2016070213/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win32.installer.exe'
+$checksum = '8a20874ea47dd0b70a40159166892074c195bb507c1196c876d52bf44160e4f0'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win64.installer.exe'
+$checksum64 = 'a434e5027f28d60b3fbdef5c1059164f1d8deffa1275e57a38f9d801c6f71aa4'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/nxt/1.9.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'nxt'
+$url = 'https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-1.9.2.zip'
+$checksum = '4fe0520e5b4d0fe244dc9d8ab7943c1a107a7e4227ce4ae9f3236ae1dcc1a8ab'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
