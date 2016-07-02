3d8ff5ec624af98807f5722f7e3b8649f1ad0a55
dtgm <dtgm1594@gmail.com>
2016-07-02 05:00:21 -0600

    Updated 1 pkg(s) on 2016-07-01: speedfan
    https://chocolatey.org/packages/speedfan

--- automatic/_output/speedfan/4.52/tools/chocolateyInstall.ps1
+++ automatic/_output/speedfan/4.52/tools/chocolateyInstall.ps1
@@ -6,8 +6,7 @@ $checksum = 'e2ccb3c0d23f0d04ee8057f5ce3861eea952fb20694c1656c9805b1d4cd922ff'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
-$chocTempDir = Join-Path $Env:Temp "chocolatey"
-$tempDir = Join-Path $chocTempDir "$packageName"
+$tempDir = Join-Path $Env:Temp $packageName
 if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
 $installFile = Join-Path $tempDir "$($packageName)Install.$installerType"
 
