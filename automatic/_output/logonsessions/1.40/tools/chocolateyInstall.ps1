a1c97685a41e32bfc2d39b4504eb2736e5242d39
dtgm <dtgm1594@gmail.com>
2016-07-04 17:00:02 -0600

    Updated 6 pkg(s) on 2016-07-04: contig,kvrt,listdlls,logonsessions,meld,movefile
    https://chocolatey.org/packages/contig
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/listdlls
    https://chocolatey.org/packages/logonsessions
    https://chocolatey.org/packages/meld
    https://chocolatey.org/packages/movefile

---
+++ automatic/_output/logonsessions/1.40/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$packageName = 'logonsessions'
+$url = 'https://download.sysinternals.com/files/logonSessions.zip'
+$checksum = '116e748ceba543371cce0d3dffb4e13b9a91ee27ee28575aa34eda97ba97cf9c'
+$checksumType = 'sha256'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'LogonSessions'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
