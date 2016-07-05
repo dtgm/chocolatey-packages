6d7af595069d5c6df73e7d7ed2cdc1256b727a74
dtgm <dtgm1594@gmail.com>
2016-07-05 11:00:02 -0600

    Updated 5 pkg(s) on 2016-07-05: firefox-nightly,gpg4win,gpg4win-light,gpg4win-vanilla,kvrt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/gpg4win
    https://chocolatey.org/packages/gpg4win-light
    https://chocolatey.org/packages/gpg4win-vanilla
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/firefox-nightly/50.0.1.2016070512/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win32.installer.exe'
+$checksum = 'a894e4375fb3479ae1e4d32e7f79a3514a208597ddc30cf6c143f24164a29cca'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win64.installer.exe'
+$checksum64 = 'ae431e4b13ba4e850e608f3f465ac5ebff70de34950edfbaeba7270bff5ace1f'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/gpg4win-vanilla/2.3.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'gpg4win-vanilla'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://files.gpg4win.org/gpg4win-vanilla-2.3.2.exe'
+$checksum = '30863d2e1601334b9bb6fc370865d6568c8544e5'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
