dd9524b7fef07f02b95daaa2aa2951493b8fc715
dtgm <dtgm1594@gmail.com>
2016-07-05 23:00:02 -0600

    Updated 3 pkg(s) on 2016-07-05: kvrt,sonarr,stunnel
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/sonarr
    https://chocolatey.org/packages/stunnel

---
+++ automatic/_output/stunnel/5.34/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'stunnel'
+$url = 'https://www.stunnel.org/downloads/archive/5.x/stunnel-5.34-installer.exe'
+$checksum = 'abddf49a02e810bf618884f6ac8fde2c1e59bda73c65c4fd9a82b724524b4d9f'
+$checksumType = 'sha1'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
