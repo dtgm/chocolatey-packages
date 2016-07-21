a89633a741e9fc901ca3c63eb648fdaf547e6372
RB <RedBaron2@users.noreply.github.com>
2016-07-21 15:36:43 -0700

    Update chocolateyUninstall.ps1
    Spybot 2 changed the location in the registry to be a Hive name; This will find the correct hive name, and choose the correct uninstall file with Silent Argument.
    Should work except for the extremely rare chance they have a 32bit install on a 64bit system under hkcu

--- automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
+++ automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
@@ -1,16 +1,32 @@
 $packageName = 'spybot'
 $packageSearch = "Spybot - Search and Destroy*"
 $installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$silentArgs = '/SILENT'
 $validExitCodes = @(0)
+											  
+	$spybot_string = "\SOFTWARE" + $bitness +"\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1"
+	$hive = "hkcu"
+	$Spybot = $hive + ":" + $spybot_string
 
-Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-				                 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
-				                 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                 -ErrorAction:SilentlyContinue `
-| Where-Object   {$_.DisplayName -like $packageSearch} `
-| ForEach-Object {Uninstall-ChocolateyPackage -PackageName "$packageName" `
-								                              -FileType "$installerType" `
-								                              -SilentArgs "$($silentArgs)" `
-								                              -File "$($_.UninstallString)" `
-                                              -ValidExitCodes $validExitCodes}
+
+	if (Test-Path $Spybot) {
+	  $hk_level = "hkcu"
+	} else {
+	  $hk_level = "hklm"
+	}
+
+	$bitness = Get-ProcessorBits
+	@{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
+	
+	if (Test-Path $Spybot.QuietUninstallString) {
+	$spybot_uninstall_string = "QuietUninstallString"
+	$silentArgs = ""
+	} else {
+	$spybot_uninstall_string = "UninstallString"
+	}	
+
+	$file = (Get-ItemProperty -Path ( $hk_level + ":" +$spybot_string ) ).$spybot_uninstall_string
+
+return $file
+										  
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
