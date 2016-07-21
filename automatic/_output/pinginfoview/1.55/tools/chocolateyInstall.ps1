b94c863dabc574cb94f630c0edc2568316d90c2c
dtgm <dtgm1594@gmail.com>
2016-07-20 23:00:02 -0600

    Updated 6 pkg(s) on 2016-07-20: ares,deluge,networktrafficview,pinginfoview,smsniff,usbdeview
    https://chocolatey.org/packages/ares
    https://chocolatey.org/packages/deluge
    https://chocolatey.org/packages/networktrafficview
    https://chocolatey.org/packages/pinginfoview
    https://chocolatey.org/packages/smsniff
    https://chocolatey.org/packages/usbdeview

---
+++ automatic/_output/pinginfoview/1.55/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'pinginfoview'
+$url = 'http://www.nirsoft.net/utils/pinginfoview.zip'
+$checksum = '71807dd51fb91dc87aa4c6cb252a631285fd80f0c319c72e74a1e23dd965a0c6'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/usbdeview/2.61/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'usbdeview'
+$url = 'http://www.nirsoft.net/utils/usbdeview.zip'
+$checksum = '78a71ea0452bd115531f11c1bece6080982e226dc6592232a016d0a089c5a06b'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/usbdeview-x64.zip'
+$checksum64 = '4cd695b56f15816f8455e2672761b4f1b57e65b1452beccda3f8a3e70cec6584'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
