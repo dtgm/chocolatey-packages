05bac6fa5811c57ddb70bb72ad3969fba7119003
RB <RedBaron2@users.noreply.github.com>
2016-07-21 22:41:02 -0700

    Update chocolateyUninstall.ps1
    This should catch the possible Mis-installed versions of Spybot

--- automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
+++ automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
@@ -4,29 +4,35 @@ $installerType = 'exe'
 $silentArgs = '/SILENT'
 $validExitCodes = @(0)
 											  
-	$spybot_string = "\SOFTWARE" + $bitness +"\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1"
+	$spybot_string = "\SOFTWARE" + $bitty +"\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1"
 	$hive = "hkcu"
 	$Spybot = $hive + ":" + $spybot_string
 
 
-	if (Test-Path $Spybot) {
+	if ( (Test-Path $Spybot) -eq $true ) {
 	  $hk_level = "hkcu"
 	} else {
 	  $hk_level = "hklm"
 	}
 
 	$bitness = Get-ProcessorBits
-	@{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
+	$bitty = @{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
 	
 	if (Test-Path $Spybot.QuietUninstallString) {
-	$spybot_uninstall_string = "QuietUninstallString"
-	$silentArgs = ""
+	  $spybot_uninstall_string = "QuietUninstallString"
+	  $silentArgs = ""
 	} else {
-	$spybot_uninstall_string = "UninstallString"
-	}	
+	  $spybot_uninstall_string = "UninstallString"
+	}
 
-	$file = (Get-ItemProperty -Path ( $hk_level + ":" +$spybot_string ) ).$spybot_uninstall_string
+	if ( (Test-Path ( $hk_level + ":" +$spybot_string )) -eq $true ) {
+	  $spybot_key = ( $hk_level + ":" + $spybot_string )
+	} else  {
+	  $spybot_key = ( "hkcu:" + "\SOFTWARE" + $bitty +"\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1" )
+    	}
 
-return $file
+	$file = (Get-ItemProperty -Path ( $spybot_key ) ).$spybot_uninstall_string
+
+	return $file
 										  
 Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
