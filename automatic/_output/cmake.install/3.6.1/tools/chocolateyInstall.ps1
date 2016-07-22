fd52ec1e516167bef2ea55110345328f8a30390f
dtgm <dtgm1594@gmail.com>
2016-07-22 13:05:04 -0600

    Updated 5 pkg(s) on 2016-07-22: cmake,cmake.install,cmake.portable,dolphin,kvrt
    https://chocolatey.org/packages/cmake
    https://chocolatey.org/packages/cmake.install
    https://chocolatey.org/packages/cmake.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/cmake.install/3.6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'cmake.install'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'http://cmake.org/files/v3.6/cmake-3.6.1-win64-x64.msi'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/cmake.portable/3.6.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'cmake.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://cmake.org//files/v3.6/cmake-3.6.1-win32-x86.zip'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
