9048687f5536a56296d6a30354feedb2a45adaeb
dtgm <dtgm1594@gmail.com>
2016-07-22 23:00:02 -0600

    Updated 3 pkg(s) on 2016-07-22: cmake.install,firefox-nightly,kvrt
    https://chocolatey.org/packages/cmake.install
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

--- automatic/_output/cmake.install/3.6.1/tools/chocolateyInstall.ps1
+++ automatic/_output/cmake.install/3.6.1/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
 $url = 'http://cmake.org/files/v3.6/cmake-3.6.1-win64-x64.msi'
-$checksum = '{checksum}'
+$checksum = '155328d2bfaa8241ab309c05624ebb9664d08150e48250b6e7119ff733e5a65f'
 $checksumType = 'sha256'
 $validExitCodes = @(0,3010)
 
--- automatic/_output/firefox-nightly/50.0.1.2016072212/tools/chocolateyInstall.ps1
+++ automatic/_output/firefox-nightly/50.0.1.2016072212/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win32.installer.exe'
-$checksum = '{checksum}'
+$checksum = 'c902d844c08d1ce566fb2b9090accc16eaebbca641bdd023c55baf4c8daf1e74'
 $checksumType = 'sha256'
 $url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-50.0a1.en-US.win64.installer.exe'
-$checksum64 = '{checksumx64}'
+$checksum64 = '4d0c681f9b8348a9392c16f0418c877f6139817e9c6dd59b4dc28061bdc9c2f2'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
