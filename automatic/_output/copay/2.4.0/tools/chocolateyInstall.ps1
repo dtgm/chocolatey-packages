2a38e34ad80ad3ea9409ff8be287824e3d697a7b
dtgm <dtgm1594@gmail.com>
2016-07-28 18:38:22 -0600

    Updated 7 pkg(s) on 2016-07-28: copay,firefox-nightly,keepass-plugin-keeanywhere,kvrt,lyx,outlookattachview,unison
    https://chocolatey.org/packages/copay
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/keepass-plugin-keeanywhere
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/lyx
    https://chocolatey.org/packages/outlookattachview
    https://chocolatey.org/packages/unison

---
+++ automatic/_output/copay/2.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'copay'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/bitpay/copay/releases/download/v2.4.0/Copay-win.exe'
+$checksum = '228668a102ce05c3fad472bc087dec3cf43bb2b287f76cdcc1bf8c878d4ab221'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
---
+++ automatic/_output/lyx/2.2.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'lyx'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://ftp.lyx.org/pub/lyx/bin/2.2.1/LyX-221-Installer-2.exe'
+$checksum = '1a4f7d94d1d14b5bb04921fc1b7594d518a7bea4a1d16069038e19ed48ee1b1f'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
