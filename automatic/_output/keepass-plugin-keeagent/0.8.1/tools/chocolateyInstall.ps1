c435ced0d8dadbd5de257e67b9ca8070c115970b
dtgm <dtgm1594@gmail.com>
2016-07-29 10:58:50 -0600

    Updated 3 pkg(s) on 2016-07-29: firefox-nightly,keepass-plugin-keeagent,kvrt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/keepass-plugin-keeagent
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/keepass-plugin-keeagent/0.8.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,73 @@
+﻿# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+
+$packageName = 'keepass-plugin-keeagent'
+$typName = 'KeeAgent.plgx'
+$packageSearch = 'KeePass Password Safe'
+$url = 'http://lechnology.com//wp-content/uploads/2016/07/KeeAgent_v0.8.1.zip'
+$checksum = 'a2d0e0469b2c29237a03f1c6ea2d3328427e7c7cd8c37659581c1fa9f63ca1a2'
+$checksumType = 'sha256'
+
+try {
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*" `
+                           -and `
+                           $_.DisplayVersion -ge 2.19 `
+                           -and `
+                           $_.DisplayVersion -lt 3.0 } `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath
+if (! $installPath) {
+  Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "Searching $env:Path for unregistered install..."
+  $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+  if (! $installFullName) {
+    $installPath = [io.path]::GetDirectoryName($installFullName)
+  }
+}
+if (! $installPath) {
+  Write-Warning "$($packageSearch) not found."
+  throw
+}
+Write-Verbose "`t...found."
+
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+  $pluginPath = Join-Path $installPath "Plugins"
+  [System.IO.Directory]::CreateDirectory($pluginPath)
+}
+Write-Verbose "Downloading and extracting zip into tools dir."
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+# rename PLGX file so it is clear which plugins are managed via choco
+$typPlugin = Join-Path $toolsDir $typName
+$chocoPlugin = Join-Path $pluginPath "$($packageName).plgx"
+Move-Item -Path $typPlugin -Destination $chocoPlugin -Force -ErrorAction Stop
+if ( Get-Process -Name "KeePass" `
+                 -ErrorAction SilentlyContinue ) {
+  Write-Warning "$($packageSearch) is currently running. Plugin will be available at next restart of $($packageSearch)." 
+} else {
+  Write-Host "$($packageName) will be loaded the next time KeePass is started."
+  Write-Host "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Menu>Tools"
+}} catch {
+  throw $_.Exception
+}
