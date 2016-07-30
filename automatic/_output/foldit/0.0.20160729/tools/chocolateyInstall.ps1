aff7bded8d3642e7957063ffe233bfcb9676de27
dtgm <dtgm1594@gmail.com>
2016-07-29 19:05:02 -0600

    Updated 3 pkg(s) on 2016-07-29: autoruns,foldit,kvrt
    https://chocolatey.org/packages/autoruns
    https://chocolatey.org/packages/foldit
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/foldit/0.0.20160729/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'foldit'
+$installerType = 'exe'
+$url = 'http://fold.it/portal/files/app/Foldit-win_x86.exe'
+$checksum = 'd26e2a46130957eb3fc3cb0aaa7bcb5d007bc8527f157e15997d9e854e17b530'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$binRoot = Get-BinRoot
+$installPath = Join-Path $binRoot "Foldit"
+$silentArgsBase = '/S /D='
+$silentArgs = $($silentArgsBase + '"' + $installPath + '"')
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2016.07.29.2241/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '09aeb83e485100d6d54ce431a3cd150af0d35b28b173d8aa583283379caeb934'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
