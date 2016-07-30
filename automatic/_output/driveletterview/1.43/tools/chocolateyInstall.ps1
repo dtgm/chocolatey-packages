82a557f0e3fbe0ec1453b9d4ff4cf954edf64ef4
dtgm <dtgm1594@gmail.com>
2016-07-30 17:00:02 -0600

    Updated 6 pkg(s) on 2016-07-30: driveletterview,kvrt,whoistd.install,whoistd.portable,wnetwatcher,wnetwatcher.install
    https://chocolatey.org/packages/driveletterview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/whoistd.install
    https://chocolatey.org/packages/whoistd.portable
    https://chocolatey.org/packages/wnetwatcher
    https://chocolatey.org/packages/wnetwatcher.install

---
+++ automatic/_output/driveletterview/1.43/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'driveletterview'
+$url = 'http://www.nirsoft.net/utils/driveletterview.zip'
+$checksum = '91b4f8e4c0e5a0b3565bfc5d222c4ec37ce23229ce1291a3286a3ca0b9da8d65'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/wnetwatcher.install/1.99/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'wnetwatcher.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher_setup.exe'
+$checksum = '70416a95ce48d6f7a7d7301e33d88481a86b11b7de05c07d7f75fabff73b2a30'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
