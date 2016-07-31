3c24668e9790d19c1822a7afa281e5b6791c5335
dtgm <dtgm1594@gmail.com>
2016-07-31 07:05:02 -0600

    Updated 3 pkg(s) on 2016-07-31: dolphin,kvrt,stellarium
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/stellarium

---
+++ automatic/_output/dolphin/5.0.334/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-334-x64.7z'
+$checksum = 'f3f86c1d7eefc0a5ef2f26e279c812cda9af3b96d8ad17f5b58fb88598b97ad0'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/stellarium/0.15.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'stellarium'
+$installerType = 'exe'	
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$urlArray = @('http://sourceforge.net/projects/stellarium/files/Stellarium-win32/0.15.0/stellarium-0.15.0-win32.exe', 'http://sourceforge.net/projects/stellarium/files/Stellarium-win64/0.15.0/stellarium-0.15.0-win64.exe')
+$url = $urlArray[0]
+$checksum = '1b411ef369e949d21bee3dc741ccf16712e86bdd6392872fb633a23489383ba1'
+$checksumType = 'sha256'
+$url64 = $urlArray[1]
+$validExitCodes = @(0)
+$checksum64 = '099e32491aeecff08f326e00384f58be0780a06b547433b596265ddf953098e1'
+$checksumType64 = 'sha256'
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
