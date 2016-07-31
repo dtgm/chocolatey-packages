0c58aa6670ef21df1069e8b566d2d3d6481738ed
dtgm <dtgm1594@gmail.com>
2016-07-31 01:05:02 -0600

    Updated 2 pkg(s) on 2016-07-30: kvrt,moneyguru
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/moneyguru

---
+++ automatic/_output/moneyguru/2.10.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'moneyguru'
+$installerType = 'msi'
+$silentArgs = '/qn /norestart'
+$url = 'http://download.hardcoded.net/dupeguru_pe_win64_2.10.1.msi'
+$checksum = 'f3280c27d9d40cbe5906cf58792853076da85c57810b0b0dcc92cd85428f76e9'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
