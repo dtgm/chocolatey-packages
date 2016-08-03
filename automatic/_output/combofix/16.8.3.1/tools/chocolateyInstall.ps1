59441c7094fd6575e6312630ca725827608375fe
dtgm <dtgm1594@gmail.com>
2016-08-03 13:05:02 -0600

    Updated 3 pkg(s) on 2016-08-03: combofix,kvrt,sysmon
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/sysmon

---
+++ automatic/_output/combofix/16.8.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'combofix'
+$url = 'https://download.bleepingcomputer.com/sUBs/ComboFix.exe'
+$checksum = '3f4e19688016d37c9fda87220b93a1c1e8b6be99d3d7124f0fbaf8a3dc55d30f'
+$checksumType = 'sha256'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$installFile = Join-Path $toolsDir "ComboFix.exe"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Write-Verbose "Create an empty sidecar metadata file for shimgen.exe to designate executable as GUI."
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/sysmon/4.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'sysmon'
+$url = 'https://download.sysinternals.com/files/Sysmon.zip'
+$checksum = '29b0185c6d7eabefedcb40b2f79f6e70cebdc10df217042e75e9f23ff1f9168e'
+$checksumType = 'sha256'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
