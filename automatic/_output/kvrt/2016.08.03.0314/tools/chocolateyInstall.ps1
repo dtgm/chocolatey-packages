e2117a9a421fa29d2c29a44277c6d5981f02d520
dtgm <dtgm1594@gmail.com>
2016-08-03 01:05:02 -0600

    Updated 6 pkg(s) on 2016-08-02: dispcalgui,dispcalgui.install,dispcalgui.portable,electron,github,kvrt
    https://chocolatey.org/packages/dispcalgui
    https://chocolatey.org/packages/dispcalgui.install
    https://chocolatey.org/packages/dispcalgui.portable
    https://chocolatey.org/packages/electron
    https://chocolatey.org/packages/github
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kvrt/2016.08.03.0314/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '1a33f9d903dced38a15a894e6b64934a288c624b67d3574aebdc50ac2c23c14f'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
