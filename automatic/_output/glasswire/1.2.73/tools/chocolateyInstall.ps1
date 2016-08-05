ace8a983f5537e8c88eb08f4d107c78db8f1968f
dtgm <dtgm1594@gmail.com>
2016-08-05 07:05:02 -0600

    Updated 2 pkg(s) on 2016-08-05: dolphin,glasswire
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/glasswire

---
+++ automatic/_output/glasswire/1.2.73/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = 'F04225895C14CEE6974003771E48E6E6'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
