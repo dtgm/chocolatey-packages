e4ad1e826e21fbee29e2700e76df3a02aa5e67d8
dtgm <dtgm1594@gmail.com>
2016-08-06 01:05:02 -0600

    Updated 2 pkg(s) on 2016-08-05: kvrt,meld
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/meld

---
+++ automatic/_output/meld/3.16.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'meld'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://download.gnome.org/binaries/win32/meld/3.16/Meld-3.16.2-win32.msi'
+$checksum = ''
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
