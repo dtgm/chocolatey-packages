c1e0871dbf6d0613250ab45fb78569bc728d0db8
dtgm <dtgm1594@gmail.com>
2016-08-07 11:00:03 -0600

    Updated 3 pkg(s) on 2016-08-07: firefox-nightly,italc,kvrt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/italc
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/italc/3.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'italc'
+$version = '3.0.0'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/iTALC/italc/releases/download/v3.0.0/italc-3.0.0-win32-setup.exe'
+$checksum = 'cea4f5975f10c04d9b6971f25e744770'
+$checksumType = 'md5'
+$url64 = 'https://github.com/iTALC/italc/releases/download/v3.0.0/italc-3.0.0-win64-setup.exe'
+$checksum64 = '08f1c546e50ecc568989dfbae14b2d2e'
+$checksumType64 = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url "$url" -Url64bit "$url64" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
