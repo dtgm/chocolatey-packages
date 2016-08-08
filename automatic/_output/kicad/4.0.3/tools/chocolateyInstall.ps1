12c5aa25eee21a2b840e74f27f767a757fd7dccc
dtgm <dtgm1594@gmail.com>
2016-08-08 13:05:04 -0600

    Updated 2 pkg(s) on 2016-08-08: kicad,kvrt
    https://chocolatey.org/packages/kicad
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kicad/4.0.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'kicad'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://downloads.kicad-pcb.org/windows/stable/kicad-product-4.0.3-i686.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$url64 = 'http://downloads.kicad-pcb.org/windows/stable/kicad-product-4.0.3-x86_64.exe'
+$checksum64 = '{checksumx64}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
