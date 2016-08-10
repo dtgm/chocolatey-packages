4ef873b47cc90dfaa652705dba0857cf65b3cd0b
dtgm <dtgm1594@gmail.com>
2016-08-10 07:05:05 -0600

    Updated 5 pkg(s) on 2016-08-10: combofix,kvrt,wnetwatcher,wnetwatcher.install,wnetwatcher.portable
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/wnetwatcher
    https://chocolatey.org/packages/wnetwatcher.install
    https://chocolatey.org/packages/wnetwatcher.portable

---
+++ automatic/_output/wnetwatcher.portable/2.00/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'wnetwatcher.portable'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher.zip'
+$checksum = '97d654ef5709f15c7ce5c1242627636fd8b1fc6a8e81bd71166471203f6d283f'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "wnetwatcher.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
