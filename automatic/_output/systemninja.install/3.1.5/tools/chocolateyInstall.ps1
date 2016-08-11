fa016d64e4c338e0b835cd9f1e8bee3e288c7f7c
dtgm <dtgm1594@gmail.com>
2016-08-11 11:00:02 -0600

    Updated 5 pkg(s) on 2016-08-11: firefox-nightly,kvrt,systemninja,systemninja.install,systemninja.portable
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/systemninja
    https://chocolatey.org/packages/systemninja.install
    https://chocolatey.org/packages/systemninja.portable

---
+++ automatic/_output/systemninja.install/3.1.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'systemninja.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://singularlabs.com/download/10294/'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/systemninja.portable/3.1.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'systemninja.portable'
+$url = 'https://singularlabs.com/download/10298/'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
