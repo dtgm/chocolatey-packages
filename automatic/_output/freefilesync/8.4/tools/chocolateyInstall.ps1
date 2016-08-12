d0d0d028c847d95f79bfb1e310731d4606230f3c
dtgm <dtgm1594@gmail.com>
2016-08-11 23:00:02 -0600

    Updated 2 pkg(s) on 2016-08-11: freefilesync,kvrt
    https://chocolatey.org/packages/freefilesync
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/freefilesync/8.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'freefilesync'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.freefilesync.org/download/FreeFileSync_8.4_Windows_Setup.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
