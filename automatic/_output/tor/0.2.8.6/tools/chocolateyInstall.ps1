467c2a2e04ed4969703df45349bf4e6283c5d2ef
dtgm <dtgm1594@gmail.com>
2016-08-16 07:05:02 -0600

    Updated 3 pkg(s) on 2016-08-16: adwcleaner,kvrt,tor
    https://chocolatey.org/packages/adwcleaner
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/tor

---
+++ automatic/_output/tor/0.2.8.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'tor'
+$url = 'https://www.torproject.org/dist/torbrowser/6.0.4/tor-win32-0.2.8.6.zip'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
