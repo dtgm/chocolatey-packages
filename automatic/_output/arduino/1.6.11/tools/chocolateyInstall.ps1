f7a96a5e0fdbe3851b18b8bf1e2f1a3cd2f2d4e1
dtgm <dtgm1594@gmail.com>
2016-08-17 17:00:02 -0600

    Updated 3 pkg(s) on 2016-08-17: arduino,firefox-nightly,winrar
    https://chocolatey.org/packages/arduino
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/winrar

---
+++ automatic/_output/arduino/1.6.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'arduino'
+$installerType = 'exe'
+$url = 'http://arduino.cc/download.php?f=/arduino-1.6.11-windows.exe'
+$checksum = '8ff96a1c6ad95ca187670fff6094e4138fdc9c9edc3a88a73326f354d34ad48f'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
