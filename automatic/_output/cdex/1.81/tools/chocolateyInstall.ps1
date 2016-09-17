2cf75d5bf6793a38af34a852a3cecee5f92c9b96
Dag Wieers <dag@wieers.com>
2016-09-17 23:12:42 +0200

    Update link to package as original has disappeared
    The checksums are identical and the source is trustworthy.

--- automatic/_output/cdex/1.81/tools/chocolateyInstall.ps1
+++ automatic/_output/cdex/1.81/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 $packageName = 'cdex'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = 'http://mirror.cdex.mu/CDex-1.81-win32.exe'
+#$url = 'http://mirror.cdex.mu/CDex-1.81-win32.exe'
+$url = 'https://download.heise.de/software/9b13eed3373cebf4aca3029b694fec64/57ddb0b5/191936/cdex-1.81-win32.exe'
 $checksum = 'd1a26f4bc31c1dea7298ee3f62ee190b281bc0ed'
 $checksumType = 'sha1'
 $validExitCodes = @(0)
