c2e3f9d4ae63ed411d38e7ec35a3c0562ae01adf
RB <RedBaron2@users.noreply.github.com>
2016-10-15 23:56:29 -0700

    Update chocolateyUninstall.ps1
    This is a similar solution. It uses a chocolatey helper to find the uninstall string from the registry.

--- automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
+++ automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
@@ -1,38 +1,12 @@
 $packageName = 'spybot'
 $packageSearch = "Spybot - Search and Destroy*"
 $installerType = 'exe'
-$silentArgs = '/SILENT'
+#$silentArgs = '/SILENT'
 $validExitCodes = @(0)
-											  
-	$spybot_string = "\SOFTWARE" + $bitty +"\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1"
-	$hive = "hkcu"
-	$Spybot = $hive + ":" + $spybot_string
 
-
-	if ( (Test-Path $Spybot) -eq $true ) {
-	  $hk_level = "hkcu"
-	} else {
-	  $hk_level = "hklm"
-	}
-
-	$bitness = Get-ProcessorBits
-	$bitty = @{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
-	
-	if (Test-Path $Spybot.QuietUninstallString) {
-	  $spybot_uninstall_string = "QuietUninstallString"
-	  $silentArgs = ""
-	} else {
-	  $spybot_uninstall_string = "UninstallString"
-	}
-
-	if ( (Test-Path ( $hk_level + ":" +$spybot_string )) -eq $true ) {
-	  $spybot_key = ( $hk_level + ":" + $spybot_string )
-	} else  {
-	  $spybot_key = ( "hkcu:" + "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{B4092C6D-E886-4CB2-BA68-FE5A99D31DE7}_is1" )
-    	}
-
-	$file = (Get-ItemProperty -Path ( $spybot_key ) ).$spybot_uninstall_string
-
-	return $file
+   [array]$spybot = Get-UninstallRegistryKey -SoftwareName "Spybot*"
+ $file = $spybot.QuietUninstallString
+  if ( $null -eq $file ) { $file = $spybot.UninstallString }
+return $file
 										  
-Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file
