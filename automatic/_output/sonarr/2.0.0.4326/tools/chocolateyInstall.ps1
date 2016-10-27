743d411fa1723627155631e0c7b02541845d477a
dtgm <dtgm1594@gmail.com>
2016-10-27 02:52:45 -0500

    Updated 1 pkg(s) on 2015-07-09: sonarr
    https://chocolatey.org/packages/sonarr

---
+++ automatic/_output/sonarr/2.0.0.4326/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'sonarr'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://download.sonarr.tv/v2/master/latest/NzbDrone.master.exe'
+$checksum = 'd387ff44970bbda85946b023f062b64783d66e0edcad673c656e190f7da69b9f'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+$ahkExe = 'AutoHotKey'
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
