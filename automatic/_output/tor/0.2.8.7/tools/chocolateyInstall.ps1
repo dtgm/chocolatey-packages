3c5f05af517377eaeeeda288509a9818e897caf6
dtgm <dtgm1594@gmail.com>
2016-10-27 02:53:15 -0500

    Updated 5 pkg(s) on 2016-05-27: die,sketchup,sketchuppro.install,sketchupviewer,tor
    https://chocolatey.org/packages/die
    https://chocolatey.org/packages/sketchup
    https://chocolatey.org/packages/sketchuppro.install
    https://chocolatey.org/packages/sketchupviewer
    https://chocolatey.org/packages/tor

---
+++ automatic/_output/tor/0.2.8.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'tor'
+$url = 'https://www.torproject.org/dist/torbrowser/6.0.5/tor-win32-0.2.8.7.zip'
+$checksum = 'e7e30c9743d9c20852a375cd838f52b6aff4f3bbf4e5410a9a78ce7019af0dcc'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
