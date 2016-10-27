14d8a27ad63ec3f89fa547cfe04a187449ca07dc
dtgm <dtgm1594@gmail.com>
2016-10-27 02:52:45 -0500

    Updated 19 pkg(s) on 2015-07-08: autocomplete,biblioteq,claws-mail,electron,freesshd,iconsextract,initool,krita,krita-gemini,logrotate,mediainfo-cli,movieexplorer,openra,sylpheed,sylpheed.portable,upx,winauth,windowdetective,winspy
    https://chocolatey.org/packages/autocomplete
    https://chocolatey.org/packages/biblioteq
    https://chocolatey.org/packages/claws-mail
    https://chocolatey.org/packages/electron
    https://chocolatey.org/packages/freesshd
    https://chocolatey.org/packages/iconsextract
    https://chocolatey.org/packages/initool
    https://chocolatey.org/packages/krita
    https://chocolatey.org/packages/krita-gemini
    https://chocolatey.org/packages/logrotate
    https://chocolatey.org/packages/mediainfo-cli
    https://chocolatey.org/packages/movieexplorer
    https://chocolatey.org/packages/openra
    https://chocolatey.org/packages/sylpheed
    https://chocolatey.org/packages/sylpheed.portable
    https://chocolatey.org/packages/upx
    https://chocolatey.org/packages/winauth
    https://chocolatey.org/packages/windowdetective
    https://chocolatey.org/packages/winspy

---
+++ automatic/_output/openra/2016.10.19/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'openra'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com//OpenRA/OpenRA/releases/download/release-20161019/OpenRA-release-20161019.exe'
+$checksum = '4647670ac40582128f39a3e89e2ea106'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
