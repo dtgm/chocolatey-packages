34066e62be8b09af956d2d8e4d6bcedac014631b
dtgm <dtgm1594@gmail.com>
2016-10-27 02:52:42 -0500

    Updated 59 pkg(s) on 2015-07-02: accesschk,accessenum,adexplorer,adinsight,adrestore,autologon,autoruns,bginfo,cacheset,clockres,coreinfo,ctrl2cap,desktops,diskext,diskmon,diskview,du,eagle,efsdump,findlinks,handle,hex2dec,junction,ldmdump,listdlls,livekd,logonsessions,movefile,ntfsinfo,pendmoves,pipelist,portmon,procdump,psexec,psfile,psgetsid,psinfo,pskill,psloggedon,psloglist,pspasswd,psping,psservice,psshutdown,pssuspend,rammap,regdelnull,ru,shareenum,sigcheck,sync,sysmon,tcpvcon,twisted,vmmap,volumeid,whois,winobj,zoomit
    https://chocolatey.org/packages/accesschk
    https://chocolatey.org/packages/accessenum
    https://chocolatey.org/packages/adexplorer
    https://chocolatey.org/packages/adinsight
    https://chocolatey.org/packages/adrestore
    https://chocolatey.org/packages/autologon
    https://chocolatey.org/packages/autoruns
    https://chocolatey.org/packages/bginfo
    https://chocolatey.org/packages/cacheset
    https://chocolatey.org/packages/clockres
    https://chocolatey.org/packages/coreinfo
    https://chocolatey.org/packages/ctrl2cap
    https://chocolatey.org/packages/desktops
    https://chocolatey.org/packages/diskext
    https://chocolatey.org/packages/diskmon
    https://chocolatey.org/packages/diskview
    https://chocolatey.org/packages/du
    https://chocolatey.org/packages/eagle
    https://chocolatey.org/packages/efsdump
    https://chocolatey.org/packages/findlinks
    https://chocolatey.org/packages/handle
    https://chocolatey.org/packages/hex2dec
    https://chocolatey.org/packages/junction
    https://chocolatey.org/packages/ldmdump
    https://chocolatey.org/packages/listdlls
    https://chocolatey.org/packages/livekd
    https://chocolatey.org/packages/logonsessions
    https://chocolatey.org/packages/movefile
    https://chocolatey.org/packages/ntfsinfo
    https://chocolatey.org/packages/pendmoves
    https://chocolatey.org/packages/pipelist
    https://chocolatey.org/packages/portmon
    https://chocolatey.org/packages/procdump
    https://chocolatey.org/packages/psexec
    https://chocolatey.org/packages/psfile
    https://chocolatey.org/packages/psgetsid
    https://chocolatey.org/packages/psinfo
    https://chocolatey.org/packages/pskill
    https://chocolatey.org/packages/psloggedon
    https://chocolatey.org/packages/psloglist
    https://chocolatey.org/packages/pspasswd
    https://chocolatey.org/packages/psping
    https://chocolatey.org/packages/psservice
    https://chocolatey.org/packages/psshutdown
    https://chocolatey.org/packages/pssuspend
    https://chocolatey.org/packages/rammap
    https://chocolatey.org/packages/regdelnull
    https://chocolatey.org/packages/ru
    https://chocolatey.org/packages/shareenum
    https://chocolatey.org/packages/sigcheck
    https://chocolatey.org/packages/sync
    https://chocolatey.org/packages/sysmon
    https://chocolatey.org/packages/tcpvcon
    https://chocolatey.org/packages/twisted
    https://chocolatey.org/packages/vmmap
    https://chocolatey.org/packages/volumeid
    https://chocolatey.org/packages/whois
    https://chocolatey.org/packages/winobj
    https://chocolatey.org/packages/zoomit

---
+++ automatic/_output/sysmon/4.12/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'sysmon'
+$url = 'https://download.sysinternals.com/files/Sysmon.zip'
+$checksum = '9f59ff3fd628480a87cedeab7ecbf28804faee31b2d9d382a26912f132d5102c'
+$checksumType = 'sha256'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
