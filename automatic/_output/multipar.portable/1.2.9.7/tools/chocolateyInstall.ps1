f1a662ab3b943cd1789fd3e9231b44e451e04f97
dtgm <dtgm1594@gmail.com>
2016-10-27 02:52:29 -0500

    Updated 4 pkg(s) on 2015-06-04: moneyguru,multipar,multipar.install,multipar.portable
    https://chocolatey.org/packages/moneyguru
    https://chocolatey.org/packages/multipar
    https://chocolatey.org/packages/multipar.install
    https://chocolatey.org/packages/multipar.portable

---
+++ automatic/_output/multipar.portable/1.2.9.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'multipar.portable'
+$url = 'http://multipar.eu//download/MultiPar1297.zip'
+$checksum = '9FF71686292867F0E47A5A4FD88E9167AE5FF499'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "MultiPar.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
