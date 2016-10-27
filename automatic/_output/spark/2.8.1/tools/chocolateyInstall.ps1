ae78cc57ad9529cbace0a0afd71e280c9c0315f4
dtgm <dtgm1594@gmail.com>
2016-10-27 02:52:55 -0500

    Updated 1 pkg(s) on 2015-09-29: spark
    https://chocolatey.org/packages/spark

---
+++ automatic/_output/spark/2.8.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'spark'
+$installerType = 'exe'
+$silentArgs = '-q'
+$url = 'http://download.igniterealtime.org/spark/spark_2_8_1.exe'
+$checksum = ''
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
