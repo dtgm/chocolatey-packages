b14a107a8821ef085148c92bd2d2166309678cc3
dtgm <dtgm1594@gmail.com>
2016-10-27 02:53:13 -0500

    Updated 4 pkg(s) on 2016-05-13: apktool,manictime,manictime.install,manictime.portable
    https://chocolatey.org/packages/apktool
    https://chocolatey.org/packages/manictime
    https://chocolatey.org/packages/manictime.install
    https://chocolatey.org/packages/manictime.portable

---
+++ automatic/_output/manictime.install/3.6.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'manictime.install'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'http://cdn.manictime.com/setup/v3_6_7_0/ManicTimeSetup.msi'
+$checksum = 'ebe2238141a0f5ee1f9ce31d84d67a545d3c92bd419951ec484674642f5f5e7f'
+$checksumType = 'sha256'
+$validExitCodes = @(0, 3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/manictime.portable/3.6.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'manictime.portable'
+$url = 'http://cdn.manictime.com/setup/v3_6_7_0/ManicTimeUsb.zip'
+$checksum = 'dbd453cae6615362a74de5668fc7f2598660b6464e0e9c7964223974c1698057'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
