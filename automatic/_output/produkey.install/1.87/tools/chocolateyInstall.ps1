af75e65370bd865164d3dc652d6e9b57fa98167d
dtgm <dtgm1594@gmail.com>
2016-11-03 09:00:02 -0500

    Updated 9 pkg(s) on 2016-11-03: dolphin,kvrt,produkey,produkey.install,produkey.portable,sonarr,sumo,wfilecheck,windowsrepair
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/produkey
    https://chocolatey.org/packages/produkey.install
    https://chocolatey.org/packages/produkey.portable
    https://chocolatey.org/packages/sonarr
    https://chocolatey.org/packages/sumo
    https://chocolatey.org/packages/wfilecheck
    https://chocolatey.org/packages/windowsrepair

---
+++ automatic/_output/produkey.install/1.87/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'produkey.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/produkey_setup.exe'
+$checksum = '57ebff93b2a62992c6f09d68ce033d119362b1fc1a22ad0c23af355e94b0486a'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/sumo/5.0.6.338/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'sumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
+$checksum = 'ee1db28273aa8d07b1681f784c2179099cc4591f0410032eaacf37a1a5278a9e'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
