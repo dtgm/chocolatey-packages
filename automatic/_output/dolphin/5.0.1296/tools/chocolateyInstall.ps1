0992df9787d5a8ae061d57ba80c6146663a6a123
dtgm <dtgm1594@gmail.com>
2016-11-13 17:43:57 -0600

    Updated 6 pkg(s) on 2016-11-12: cmake,cmake.install,cmake.portable,dolphin,git-annex,kvrt
    https://chocolatey.org/packages/cmake
    https://chocolatey.org/packages/cmake.install
    https://chocolatey.org/packages/cmake.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/git-annex
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.1296/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1296-x64.7z'
+$checksum = '69d7d7659026b05067097b6704d4028022ed575aa9e37d3d61449dd42abf88d8'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/git-annex/6.20161110/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'git-annex'
+$installerType = 'exe'
+$silentArgs = '/S /D=C:\Program Files\Git'
+$url = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
+$checksum = '37bf9a064a4d9a259e8026919ed00121ec568820abc1da4356dd9928a2086086'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
