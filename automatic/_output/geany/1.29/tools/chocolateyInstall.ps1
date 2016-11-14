07c868d6852e0bfa6fd4c63e05f8cb2a9a3c3f72
dtgm <dtgm1594@gmail.com>
2016-11-14 14:16:43 -0600

    Updated 6 pkg(s) on 2016-11-13: combofix,dolphin,firefox-nightly,geany,kcleaner,kvrt
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/geany
    https://chocolatey.org/packages/kcleaner
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/geany/1.29/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'geany'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.geany.org/geany-1.29_setup.exe'
+$checksum = '75d7b99f56dceea13f65dd6f9898e2f883295f65100308288688a6886959e2a6'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kcleaner/3.2.1.83/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '2e09e4d5f081935d6c301120b5b895cefe415586a3c4484c0b6d0bc59e923274'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
