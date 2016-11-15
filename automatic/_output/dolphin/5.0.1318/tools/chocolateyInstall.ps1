bfb896c3a9cf7deedd26679a75c02b426805dc5e
dtgm <dtgm1594@gmail.com>
2016-11-15 08:16:36 -0600

    Updated 2 pkg(s) on 2016-11-15: dolphin,websitesniffer
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/websitesniffer

---
+++ automatic/_output/dolphin/5.0.1318/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1318-x64.7z'
+$checksum = '5e05e2772d2a1b6fb20b401e436c85782d860f507a4f0f5f647cc3c303747329'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
