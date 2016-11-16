302ef44dc28db29a42816fe9290f800eb680eab4
dtgm <dtgm1594@gmail.com>
2016-11-16 15:00:02 -0600

    Updated 3 pkg(s) on 2016-11-16: firefox-nightly,italc,kvrt
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/italc
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/firefox-nightly/53.0.1.2016111614/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-53.0a1.en-US.win32.installer.exe'
+$checksum = '1e5dc6e71e01d43f498e4aa39418c0705a0680c09de7f8332d77ace9b2c1f583'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-53.0a1.en-US.win64.installer.exe'
+$checksum64 = '6e41aadaf1ef35110a8c2f364de37282dfda710f498d8e3bd0623d5917798e09'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/italc/3.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'italc'
+$version = '3.0.1'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/iTALC/italc/releases/download/v3.0.1/italc-3.0.1-win32-setup.exe'
+$checksum = '2bd2b45e017187e4f34ea32db02ee66f'
+$checksumType = 'md5'
+$url64 = 'https://github.com/iTALC/italc/releases/download/v3.0.1/italc-3.0.1-win64-setup.exe'
+$checksum64 = 'd764b0e00e866fe6093f95c282054f9f'
+$checksumType64 = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url "$url" -Url64bit "$url64" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
