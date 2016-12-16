edfc3259556f7a6ec4496b43c98477f4af165925
dtgm <dtgm1594@gmail.com>
2016-12-16 00:36:20 -0600

    Closes #259
    * updated 1.3.1 to correct URL
    * fixed regex in ketarin

---
+++ automatic/_output/cmdermini/1.3.1.20161216/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'cmdermini'
+$url = 'https://github.com/cmderdev/cmder/releases/download/v1.3.1/cmder_mini.zip'
+$checksum = '090b010e3bd4a8174f15ed8bdef2a226'
+$checksumType = 'md5'
+$binRoot = Get-BinRoot
+$installPath = Join-Path $binRoot $packageName
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -UnzipLocation $installPath `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType
+
+Install-ChocolateyPath $installPath 'User'
