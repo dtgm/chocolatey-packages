5f1f2757f4d508aa42aee535f0cc5384ce1b2d63
dtgm <dtgm1594@gmail.com>
2016-12-17 09:00:02 -0600

    Updated 1 pkg(s) on 2016-12-17: cryptsync
    https://chocolatey.org/packages/cryptsync

---
+++ automatic/_output/cryptsync/1.2.7/tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'cryptsync'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$validExitCodes = @(0)
+
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageName*" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($_.PSChildName) $silentArgs" `
+                                               -ValidExitCodes $validExitCodes }
