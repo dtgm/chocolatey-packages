82fe2e88d98e3f665c5d9d4dff34c983f6711ba0
dtgm <dtgm1594@gmail.com>
2016-12-19 21:00:02 -0600

    Updated 2 pkg(s) on 2016-12-19: dolphin,exiftool
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/exiftool

---
+++ automatic/_output/exiftool/10.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.37.zip'
+$checksum = '55d594999016e9be61f633111022710b2c5eaf83'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
