c03a2b876d9e7050d7a8bbca1eef3d86bc07980c
dtgm <dtgm1594@gmail.com>
2016-12-21 03:00:05 -0600

    Updated 3 pkg(s) on 2016-12-20: ipnetinfo,ipnetinfo.install,ipnetinfo.portable
    https://chocolatey.org/packages/ipnetinfo
    https://chocolatey.org/packages/ipnetinfo.install
    https://chocolatey.org/packages/ipnetinfo.portable

---
+++ automatic/_output/ipnetinfo.install/1.74/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'ipnetinfo.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/ipnetinfo_setup.exe'
+$checksum = '0881b9029d767a839aa560378ee0b7798b2a7405d97baeba3d72bb1b482af42f'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/ipnetinfo.portable/1.74/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'ipnetinfo.portable'
+$url = 'http://www.nirsoft.net/utils/ipnetinfo.zip'
+$checksum = '75d9c7a347c8442ab45eb591ced5ff40b8aa49e7ecce4afafacd34d383cf45d2'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "ipnetinfo.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
