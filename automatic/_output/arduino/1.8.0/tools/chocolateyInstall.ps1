3f1a0369ae603e7edb73c0c9ac550de10b967721
dtgm <dtgm1594@gmail.com>
2016-12-22 15:00:01 -0600

    Updated 4 pkg(s) on 2016-12-22: arduino,dolphin,duplicatefilefinder,firefox-nightly
    https://chocolatey.org/packages/arduino
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/duplicatefilefinder
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/arduino/1.8.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'arduino'
+$installerType = 'exe'
+$url = 'http://arduino.cc/download.php?f=/arduino-1.8.0-windows.exe'
+$checksum = '7cbb3fab0cf9b4dbcc798e41ab9350f5802ec631679c6e84b3e85483ea57592e'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/firefox-nightly/53.0.1.2016122214/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-53.0a1.en-US.win32.installer.exe'
+$checksum = '2f4ba03d6ead1c6a8497eb88770caaecd28c4f35b6587bbc4d7750a8c16f4bdb'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-53.0a1.en-US.win64.installer.exe'
+$checksum64 = 'd648bf82a5ec2a6bcd914eedb4b17b3d116643e3710e3831091e4854b7f34fb6'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
