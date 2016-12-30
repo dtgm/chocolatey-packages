3c42e8f3c3240643f0617964834f0f8cb6cd3980
dtgm <dtgm1594@gmail.com>
2016-12-30 09:00:02 -0600

    Updated 4 pkg(s) on 2016-12-30: awscli,firefox-nightly,handbrake,handbrake.install
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/handbrake
    https://chocolatey.org/packages/handbrake.install

---
+++ automatic/_output/awscli/1.11.35/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.35.msi'
+$checksum = '9e1f067cbe1267eb20d3d9ce8cf969a0402b14a9fe7387b88ca90ae0ce9179fb'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.35.msi'
+$checksum64 = 'f81426323d8ecd482d301932760bae1accf0abe82ec75401272ff1057c60620e'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/handbrake.install/1.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'handbrake.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://handbrake.fr/rotation.php?file=HandBrake-1.0.1-i686-Win_GUI.exe'
+$checksum = 'a0dd0cb03dc16c3af208d566393f2d4e10b566c3'
+$checksumType = 'sha1'
+$url64 = 'https://handbrake.fr/rotation.php?file=HandBrake-1.0.1-x86_64-Win_GUI.exe'
+$checksum64 = '478b444e6707fb83a849fa4946becd01a6a03dcf'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
