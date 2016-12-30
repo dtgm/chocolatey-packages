c7403cccfa9b766e57836896b1a4f5bf50fa421b
dtgm <dtgm1594@gmail.com>
2016-12-30 15:00:02 -0600

    Updated 3 pkg(s) on 2016-12-30: dolphin,dumo,firefox-nightly
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/dumo/2.8.2.51/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '1ee120c37a5e4356ba9e37ced6e444283e51719de237edc9ef5811769e7f5451'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
