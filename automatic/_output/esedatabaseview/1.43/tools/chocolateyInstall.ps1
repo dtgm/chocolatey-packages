6cdab1692c9f6dd35cd545979e3773fb0e279c4f
dtgm <dtgm1594@gmail.com>
2016-12-31 15:00:02 -0600

    Updated 3 pkg(s) on 2016-12-31: dolphin,esedatabaseview,firefox-nightly
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/esedatabaseview
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/esedatabaseview/1.43/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'esedatabaseview'
+$url = 'http://www.nirsoft.net/utils/esedatabaseview.zip'
+$checksum = 'cad52b0ef1433bc8382eae7aeafe0f301925fb4fce0be9d1d9f7bc31335b2835'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
