b1b071034a464b7c08ce928a033bdcd1ea9a07d0
dtgm <dtgm1594@gmail.com>
2017-01-02 03:00:02 -0600

    Updated 1 pkg(s) on 2017-01-01: dolphin
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/dolphin/5.0.1632/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1632-x64.7z'
+$checksum = 'af85274af46ad210bdd557df6fb0b71f0c57587731d1522a0d93d3b70d10e97f'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
