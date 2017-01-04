55a6d9b1e2aebe5091861be93e5e5e796f04e2de
dtgm <dtgm1594@gmail.com>
2017-01-04 15:00:02 -0600

    Updated 6 pkg(s) on 2017-01-04: blink,dispcalgui,dispcalgui.install,dispcalgui.portable,dolphin,firefox-nightly
    https://chocolatey.org/packages/blink
    https://chocolatey.org/packages/dispcalgui
    https://chocolatey.org/packages/dispcalgui.install
    https://chocolatey.org/packages/dispcalgui.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/dispcalgui.install/3.2.3.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dispcalgui.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/dispcalgui/files/release/3.2.3.0/DisplayCAL-3.2.3.0-Setup.exe'
+$checksum = 'bb5835e98fb4ad0cca7f4b8d0ac2a0be5c3d6864177131d2a29dd5d15de71a4c'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
