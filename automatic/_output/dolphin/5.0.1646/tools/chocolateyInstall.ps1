bb8c8d1a74ca9e9ca1647d95c80e1d29af6d7a55
dtgm <dtgm1594@gmail.com>
2017-01-04 09:00:02 -0600

    Updated 5 pkg(s) on 2017-01-04: bitvise-ssh-server,combofix,ddu,dolphin,firefox-nightly
    https://chocolatey.org/packages/bitvise-ssh-server
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/dolphin/5.0.1646/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1646-x64.7z'
+$checksum = 'e7b380d774d4bba58d7769811350445af6c017fa3a628bb4fb501afadbe8c2cc'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
