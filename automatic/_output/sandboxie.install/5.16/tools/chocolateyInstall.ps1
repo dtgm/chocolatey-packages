031f56cab34525deae8da937f1f6fa9da65e5135
dtgm <dtgm1594@gmail.com>
2017-01-05 03:00:01 -0600

    Updated 2 pkg(s) on 2017-01-05: sandboxie,sandboxie.install
    https://chocolatey.org/packages/sandboxie
    https://chocolatey.org/packages/sandboxie.install

---
+++ automatic/_output/sandboxie.install/5.16/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$packageName = 'sandboxie.install'
+$packageSearch = 'sandboxie'
+$installerType = 'exe'
+$url = 'http://www.sandboxie.com/attic/SandboxieInstall32-516.exe'
+$checksum = '3e89a1cc3cf3432f56f692b52457f8a843438b81'
+$checksumType = 'sha1'
+$url64 = 'http://www.sandboxie.com/attic/SandboxieInstall64-516.exe'
+$checksum64 = '8ab0485a771083024e2a47608f7480ac9bcd07c0'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+$silentArgs = '/S'
+$reg = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                           -ErrorAction:SilentlyContinue `
+          | Where-Object   { $_.DisplayName -like "$packageSearch*" }
+if ($reg) { 
+  $silentArgs += ' /upgrade'
+} else {
+  $silentArgs += ' /install'
+}
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
