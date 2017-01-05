01db3ff373b58be5b9191926d89a8acd60280c90
dtgm <dtgm1594@gmail.com>
2017-01-04 21:00:02 -0600

    Updated 5 pkg(s) on 2017-01-04: awscli,dolphin,electrum,electrum.install,electrum.portable
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/electrum
    https://chocolatey.org/packages/electrum.install
    https://chocolatey.org/packages/electrum.portable

---
+++ automatic/_output/electrum.portable/2.7.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'electrum.portable'
+$url = 'https://download.electrum.org/2.7.14/electrum-2.7.14-portable.exe'
+$checksum = '9507af8597a40a115f3b12b466e129225a5a029d7e8093a4aa29bf540f58f620'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "electrum.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  # create an empty sidecar metadata file for closed-source shimgen.exe to prevent blank black window
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null  
+} catch {
+  throw $_.Exception
+}
