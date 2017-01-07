4f90e6aa4a6a8c6ca1fd4fccd9b9236ec375b438
dtgm <dtgm1594@gmail.com>
2017-01-06 19:25:07 -0600

    Updated 5 pkg(s) on 2017-01-06: dolphin,electrum,electrum.install,electrum.portable,firefox-nightly
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/electrum
    https://chocolatey.org/packages/electrum.install
    https://chocolatey.org/packages/electrum.portable
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/electrum.portable/2.7.15/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'electrum.portable'
+$url = 'https://download.electrum.org/2.7.15/electrum-2.7.15-portable.exe'
+$checksum = '6d750380471a38f4557f69df0bb558b90ead0ebb088cdcc4932f6001c505bafd'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "electrum.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  # create an empty sidecar metadata file for closed-source shimgen.exe to prevent blank black window
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null  
+} catch {
+  throw $_.Exception
+}
