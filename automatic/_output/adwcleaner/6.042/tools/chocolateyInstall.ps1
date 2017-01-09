282f0cfd2ec6d402b37aaf7fc9730b5daa311c0f
dtgm <dtgm1594@gmail.com>
2017-01-09 15:00:02 -0600

    Updated 6 pkg(s) on 2017-01-09: adwcleaner,arduino,electrum,electrum.install,electrum.portable,firefox-nightly
    https://chocolatey.org/packages/adwcleaner
    https://chocolatey.org/packages/arduino
    https://chocolatey.org/packages/electrum
    https://chocolatey.org/packages/electrum.install
    https://chocolatey.org/packages/electrum.portable
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/adwcleaner/6.042/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = 'adwcleaner'
+$checksum = 'e6febfa643fb29498c76d3d0561d61a5d5a74bf13b6cd41fcac92e97fda99ea3'
+$checksumType = 'sha256'
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$ahkFile = Join-Path $toolsDir 'chocolateyInstall.ahk'
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+Import-Module (Join-Path "$toolsDir" 'Get-HtmlFromRegex.psm1')
+
+$urlAppId = 'http://filehippo.com/download_adwcleaner/72653/'
+$search = 'download_adwcleaner/download/([\w\d]+)/'
+$replace = 'http://www.filehippo.com/en/download_adwcleaner/download/$1/'
+$urlTemp = Get-HtmlFromRegex $urlAppId $search $replace
+$search = '/download/file/([\w\d]+)/'
+$replace = 'http://www.filehippo.com/download/file/$1/'
+$url = Get-HtmlFromRegex $urlTemp $search $replace
+
+Get-ChocolateyWebFile -PackageName $packageName `
+                      -FileFullPath $installFile `
+                      -Url $url `
+                      -Checksum $checksum `
+                      -ChecksumType $checksumType
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
+
+Start-Process 'AutoHotKey' $ahkFile
+Start-Process $installFile
---
+++ automatic/_output/arduino/1.8.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'arduino'
+$installerType = 'exe'
+$url = 'http://arduino.cc/download.php?f=/arduino-1.8.1-windows.exe'
+$checksum = '05444829645b088eb9b79b516af4d57dda4a0f28259005feeb6a5bbd5f3062f7'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
