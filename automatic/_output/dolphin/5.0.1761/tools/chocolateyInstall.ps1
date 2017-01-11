c2e6ca51d8549def0628da9cf58a22354687f01e
dtgm <dtgm1594@gmail.com>
2017-01-11 15:00:02 -0600

    Updated 7 pkg(s) on 2017-01-11: bitvise-ssh-server,dolphin,electron,electrum,electrum.install,electrum.portable,firefox-nightly
    https://chocolatey.org/packages/bitvise-ssh-server
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/electron
    https://chocolatey.org/packages/electrum
    https://chocolatey.org/packages/electrum.install
    https://chocolatey.org/packages/electrum.portable
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/dolphin/5.0.1761/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1761-x64.7z'
+$checksum = '4984648ef5a734eeb2e534d7a842c2739c3bc8ec0f8c80c7efe083fe60b3e3fd'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/electrum.portable/2.7.17/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'electrum.portable'
+$url = 'https://download.electrum.org/2.7.17/electrum-2.7.17-portable.exe'
+$checksum = 'be216330605de73bb131dbc7ef5e8bf02bf7592fcb3f1573f0375dcdae3e0dbd'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "electrum.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+  # create an empty sidecar metadata file for closed-source shimgen.exe to prevent blank black window
+  Set-Content -Path ("$installFile.gui") `
+              -Value $null  
+} catch {
+  throw $_.Exception
+}
