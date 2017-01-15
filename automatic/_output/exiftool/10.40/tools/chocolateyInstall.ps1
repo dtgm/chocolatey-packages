9d19b15403466b89cdac488e43fdd1312c0c5fe1
dtgm <dtgm1594@gmail.com>
2017-01-14 21:00:02 -0600

    Updated 3 pkg(s) on 2017-01-14: bitvise-ssh-server,dolphin,exiftool
    https://chocolatey.org/packages/bitvise-ssh-server
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/exiftool

---
+++ automatic/_output/exiftool/10.40/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.40.zip'
+$checksum = '6bf1030494943349992fcb47536ab0d55b00afee'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
