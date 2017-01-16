46ccd4660e97466b133aa6eb3a7174144f704830
dtgm <dtgm1594@gmail.com>
2017-01-16 15:00:02 -0600

    Updated 2 pkg(s) on 2017-01-16: aria2,firefox-nightly
    https://chocolatey.org/packages/aria2
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/aria2/1.31.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'aria2'
+$url = 'https://github.com/aria2/aria2/releases/download/release-1.31.0/aria2-1.31.0-win-32bit-build1.zip'
+$checksum = 'b9ea9ddbc784d91599a58b6497493705'
+$checksumType = 'md5'
+$url64 = 'https://github.com/aria2/aria2/releases/download/release-1.31.0/aria2-1.31.0-win-64bit-build1.zip'
+$checksum64 = 'bc8aafd99bd5d04cad545600706defa4'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64 "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
