223d7dddaadc5c95cdf321c0c3a2367f98ff7d4f
dtgm <dtgm1594@gmail.com>
2017-01-17 09:00:02 -0600

    Updated 2 pkg(s) on 2017-01-17: awscli,dolphin
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/awscli/1.11.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.37.msi'
+$checksum = '8923e024d2cd517dcd8418ca7376102d2f692d694786123b1fb409de773d7a6d'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.37.msi'
+$checksum64 = 'db4f67c8d39c404542cf36c04a607d7a1925a037776c80d16b222b6011d609d3'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/dolphin/5.0.1880/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-1880-x64.7z'
+$checksum = 'ec9a7911908123adddfda039c042142e9fb2f931098b3962a41916c6a33c05a2'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
