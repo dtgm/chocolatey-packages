ccd8b84b0de33627c801e614090cbd62fcc8415c
dtgm <dtgm1594@gmail.com>
2017-01-22 00:27:36 -0600

    Updated 7 pkg(s) on 2017-01-21: manictime,myeventviewer,nk2edit.portable,outlookattachview,pan,projectlibre,projectlibre.portable
    https://chocolatey.org/packages/manictime
    https://chocolatey.org/packages/myeventviewer
    https://chocolatey.org/packages/nk2edit.portable
    https://chocolatey.org/packages/outlookattachview
    https://chocolatey.org/packages/pan
    https://chocolatey.org/packages/projectlibre
    https://chocolatey.org/packages/projectlibre.portable

---
+++ automatic/_output/myeventviewer/2.25/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'myeventviewer'
+$url = 'http://www.nirsoft.net/utils/myeventviewer.zip'
+$checksum = 'f3f9c2021185e6d4f5e89cf9b241eac67eb6e467db52cfe00370e40f07049313'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/myeventviewer-x64.zip'
+$checksum64 = 'd1e94d89f2831c76d46bb0b80e4248df82244b021950769bffab9cba0054ecfc'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/outlookattachview/2.97/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'outlookattachview'
+$url = 'http://www.nirsoft.net/utils/outlookattachview.zip'
+$checksum = '41cb42fce81f8c86fa8f05dd6b1a802c850e3d1437f8c5a3be884b1848f13d5f'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/outlookattachview-x64.zip'
+$checksum64 = 'c9ef1c009e54b5eca877d04029df1ad59b3ecd6abde65a3877bf2d44ef7eb9bf'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
