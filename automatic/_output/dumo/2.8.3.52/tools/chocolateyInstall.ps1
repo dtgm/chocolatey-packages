4e8b90b3832d907b8a85175d39eed22f6dfd0f3f
dtgm <dtgm1594@gmail.com>
2017-01-24 09:00:02 -0600

    Updated 7 pkg(s) on 2017-01-24: dnsdataview,dolphin,dumo,firefox-dev,firefox-nightly,kvrt,windowsrepair
    https://chocolatey.org/packages/dnsdataview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/firefox-dev
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/windowsrepair

---
+++ automatic/_output/dumo/2.8.3.52/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '44167199cedd36ccbedeafd5e99ab88cba2a0ae6f4a469a25834b64245e26cab'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/firefox-nightly/54.0.1.2017012317/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win32.installer.exe'
+$checksum = '83410c8b532a04438e3b0713c3b327cd994305551156df5f635003ffb139dfd1'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win64.installer.exe'
+$checksum64 = '41410da455e9282ed0d32ced039228ee1b790a5f4145bf636f4eea6077cbeb17'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
