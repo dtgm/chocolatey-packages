9244d0ad2cd8ec9e30ee015bf6b656afb38db7cb
dtgm <dtgm1594@gmail.com>
2017-01-27 03:00:05 -0600

    Updated 6 pkg(s) on 2017-01-26: awscli,dolphin,firefox-nightly,kvrt,searchmyfiles,sumo
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/searchmyfiles
    https://chocolatey.org/packages/sumo

---
+++ automatic/_output/awscli/1.11.43/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.43.msi'
+$checksum = 'a140e6049e8cd8258ad1abfea4de08f3d67ef6b594110c2ca46aa178dcf24d93'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.43.msi'
+$checksum64 = 'f13f63e3ee3f601c89bde1ee1c6f9282bc7ef2ffc1c66049574752fb33eef903'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/sumo/5.1.1.351/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'sumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
+$checksum = 'c934de09f0a067643ff76056894690e99513afbf745c3c67703cfba9dd9f1ccc'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "$($packageName)Install.ahk"
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
