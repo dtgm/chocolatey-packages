193d44249f50ccf38a96d57e68faf546cd6e261e
dtgm <dtgm1594@gmail.com>
2017-02-01 09:00:02 -0600

    Updated 5 pkg(s) on 2017-02-01: krita,kvrt,telegram,telegram.install,telegram.portable
    https://chocolatey.org/packages/krita
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/telegram
    https://chocolatey.org/packages/telegram.install
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/telegram.portable/1.0.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.1.0.6.zip'
+$checksum = '4d77c661147b686ade077bb392ecadbce7829bf01643e0748ddfe7164944bcef'
+$checksumType = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
