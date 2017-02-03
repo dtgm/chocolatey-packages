cc8b615a02d68853507817eaa7e587b8b76d11a1
dtgm <dtgm1594@gmail.com>
2017-02-02 21:00:02 -0600

    Updated 3 pkg(s) on 2017-02-02: colormania,dolphin,kvrt
    https://chocolatey.org/packages/colormania
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/colormania/6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'colormania'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.blacksunsoftware.com/downloads/ColorManiaSetup.exe'
+$checksum = '2d15001d6da29a33b1359371575b2eca45f3f8d90ef5e10a70e93681d2944d63'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
