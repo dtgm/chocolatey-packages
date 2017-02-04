4c4a48a475af05a92068a68d632f16b43670eaa0
dtgm <dtgm1594@gmail.com>
2017-02-03 21:00:02 -0600

    Updated 4 pkg(s) on 2017-02-03: dolphin,firefox-nightly,kcleaner,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kcleaner
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.2283/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2283-x64.7z'
+$checksum = 'f57389fbf9b2e6b4e4534dfd1bb43651d823abe7f6f2af9a14341c1422485562'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/kcleaner/3.2.4.87/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'kcleaner'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/kcleaner_lite.exe'
+$checksum = '7bd71ba7e3e97eb80531cae20f71d6be6dbb9ef00fb2b83327e493cc0484c023'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
