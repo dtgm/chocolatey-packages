0afd8ceccdbc3cb89a52c43e02077bb9ffc28ff1
dtgm <dtgm1594@gmail.com>
2017-02-04 21:00:02 -0600

    Updated 2 pkg(s) on 2017-02-04: countrytraceroute,kvrt
    https://chocolatey.org/packages/countrytraceroute
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/countrytraceroute/1.28/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'countrytraceroute'
+$url = 'http://www.nirsoft.net/utils/countrytraceroute.zip'
+$checksum = '1ccc01bd1992fa0c9da37c02a9b28665d325a57478714c7fdbb6e24a4f16ca1e'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
