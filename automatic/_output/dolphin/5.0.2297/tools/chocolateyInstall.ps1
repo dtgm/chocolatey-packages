d3376a5df69f240de9d734b7febacd37250f0896
dtgm <dtgm1594@gmail.com>
2017-02-05 09:00:02 -0600

    Updated 3 pkg(s) on 2017-02-05: dolphin,jstock,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/jstock
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.2297/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2297-x64.7z'
+$checksum = '166b7800a36f6f0433fc2eb78d8c7772534c47e06317e15972e20f53ba41ecf2'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/jstock/1.0.7.20/tools/chocolateyUninstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'jstock'
+$packageSearch = "JStock"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile = "$scriptPath\chocolateyUninstall.ahk"
+$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+Copy-Item $ahkFile $ahkRun -Force
+Start-Process 'AutoHotKey' "$ahkRun"
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like "$packageSearch*" } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($silentArgs)" `
+                                               -File "$($_.UninstallString.Replace('"',''))" `
+                                               -ValidExitCodes $validExitCodes }
+Remove-Item "$ahkRun" -Force
