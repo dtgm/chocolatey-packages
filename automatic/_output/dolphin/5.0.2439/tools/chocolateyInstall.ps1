a375ac3ff12190d3bda5d97b85f291bde3515f88
dtgm <dtgm1594@gmail.com>
2017-02-08 15:00:02 -0600

    Updated 4 pkg(s) on 2017-02-08: dolphin,firefox-nightly,freefilesync,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/freefilesync
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.2439/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2439-x64.7z'
+$checksum = 'e401f844559f4e042063327346eefaa0ba6e55b7f18e70caeff047d1436ae1a0'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
