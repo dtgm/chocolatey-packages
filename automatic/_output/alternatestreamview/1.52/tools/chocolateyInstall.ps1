bdf128e768a178e796a706ec8cc8102635509840
dtgm <dtgm1594@gmail.com>
2017-02-08 03:00:03 -0600

    Updated 4 pkg(s) on 2017-02-07: alternatestreamview,dolphin,firefox-nightly,kvrt
    https://chocolatey.org/packages/alternatestreamview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/alternatestreamview/1.52/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'alternatestreamview'
+$url = 'http://www.nirsoft.net/utils/alternatestreamview.zip'
+$checksum = 'b8ed8b6d802c1129436c6a8aba8b583aed23565f67f79ac4b3da3b46b26419fe'
+$checksumType = 'sha1'
+$url64 = 'http://www.nirsoft.net/utils/alternatestreamview-x64.zip'
+$checksum64 = 'a031600ab0b06b875baebd3315d88a1bb48d6c249b3bc130dd94370f3eb3937a'
+$checksumType64 = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/firefox-nightly/54.0.1.2017020713/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win32.installer.exe'
+$checksum = '220044023fa844623ae9b70b912ea31fbc08c8c5b8cf03f9541abea4cc162408'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win64.installer.exe'
+$checksum64 = '5ff55771a250fe6bf5e6a1bc0df981b7d869f4a899c70cae0bda54c86d90b13e'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
