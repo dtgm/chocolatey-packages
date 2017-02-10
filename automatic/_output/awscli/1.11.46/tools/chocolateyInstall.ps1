5523c784e7fc710e8d6cc72e48d52d7ab4ff76b0
dtgm <dtgm1594@gmail.com>
2017-02-10 03:00:03 -0600

    Updated 3 pkg(s) on 2017-02-09: awscli,dolphin,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/awscli/1.11.46/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.46.msi'
+$checksum = '9d2ccd82eb4a4dc7536997d0230ab380c4a3a95eaf5191354325c33db02d72e4'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.46.msi'
+$checksum64 = 'b9bc75dd14d98d5634631bb58a836571119f78c94f781e50872c7f4827ca10c8'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
