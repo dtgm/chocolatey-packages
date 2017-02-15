c1844bc93827d0108299df07f3327fe93111de23
dtgm <dtgm1594@gmail.com>
2017-02-15 12:15:58 -0600

    Updated 8 pkg(s) on 2017-02-15: cports,dolphin,firefox-nightly,kvrt,scilab,winscp,winscp.install,winscp.portable
    https://chocolatey.org/packages/cports
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/scilab
    https://chocolatey.org/packages/winscp
    https://chocolatey.org/packages/winscp.install
    https://chocolatey.org/packages/winscp.portable

---
+++ automatic/_output/cports/2.31/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'cports'
+$url = 'http://www.nirsoft.net/utils/cports.zip'
+$checksum = '282c6b3726787515e96580eb5a8c0e0f9b856f28b421499ee67b6265eced51ee'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/cports-x64.zip'
+$checksum64 = 'a5868340a49ed0b3dc8cdefc00aac8ce2a4b32d19fbfa582a1054f4f3e3c6f58'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/winscp.portable/5.9.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'winscp.portable'
+$url = 'https://sourceforge.net/projects/winscp/files/WinSCP/5.9.4/WinSCP-5.9.4-Portable.zip'
+$checksum = ''
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "WinSCP.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
