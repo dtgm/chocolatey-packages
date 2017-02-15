e4664a09e799b59f1580d86f757fb146c26c4184
dtgm <dtgm1594@gmail.com>
2017-02-14 21:00:05 -0600

    Updated 3 pkg(s) on 2017-02-14: awscli,git-annex,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/git-annex
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/git-annex/6.20170214/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'git-annex'
+$installerType = 'exe'
+$silentArgs = '/S /D=C:\Program Files\Git'
+$url = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
+$checksum = '621cafc31a6e572ee7a24794bf6c05c282c1e75d269222ec8ff416dbaa0daa81'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
