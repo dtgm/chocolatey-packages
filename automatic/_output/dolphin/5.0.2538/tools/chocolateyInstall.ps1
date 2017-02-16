939b6218c8b29eb0a333c86b1a684accc30bc333
dtgm <dtgm1594@gmail.com>
2017-02-16 09:00:02 -0600

    Updated 4 pkg(s) on 2017-02-16: dolphin,kvrt,partitionwizard,sqlitebrowser
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/partitionwizard
    https://chocolatey.org/packages/sqlitebrowser

---
+++ automatic/_output/dolphin/5.0.2538/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2538-x64.7z'
+$checksum = 'e573ff707e3e2c890c8597d23341a79c7951a0d8dc234a6562fd9a831cde584c'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/sqlitebrowser/3.9.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'sqlitebrowser'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.1/DB.Browser.for.SQLite-3.9.1-win32.exe'
+$checksum = '597939db7d2269803a068f883eb36eda'
+$checksumType = 'md5'
+$url64 = 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.1/DB.Browser.for.SQLite-3.9.1-win64.exe'
+$checksum64 = '05a0292dc2b80175b808deb9a76561c6'
+$checksumType64 = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
