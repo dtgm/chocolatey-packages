f1ff6f9c116b07771d6941d78f43f36e43de298e
dtgm <dtgm1594@gmail.com>
2017-02-16 03:00:02 -0600

    Updated 4 pkg(s) on 2017-02-15: awscli,ddu,dolphin,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/awscli/1.11.49/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.49.msi'
+$checksum = 'e2cbc13618def3526c3ab27781e9980c88fa50ad764cd2a90fd3b04df48e5f8c'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.49.msi'
+$checksum64 = '627d7ca9d0a5486633986986b923ab75fc66598342e8628668f0246f9e414beb'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/dolphin/5.0.2535/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2535-x64.7z'
+$checksum = 'bf645f5fabff7c67aef217d6d8d6f0bc15ba9399fc9401ca26d3545049515d7c'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
