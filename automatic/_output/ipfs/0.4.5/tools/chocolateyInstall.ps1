1fe2e1b111819a78eaf15fc621dc39475b060889
dtgm <dtgm1594@gmail.com>
2017-02-17 03:00:01 -0600

    Updated 3 pkg(s) on 2017-02-16: awscli,ipfs,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/ipfs
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/ipfs/0.4.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'ipfs'
+$url = 'https://dist.ipfs.io/go-ipfs/v0.4.5/go-ipfs_v0.4.5_windows-386.zip'
+$checksum = 'd29392632725adf88ad5b547d2cb07bbaf09c5d6f1319faed769e8a7489b82e5'
+$checksumType = 'sha256'
+$url64 = 'https://dist.ipfs.io/go-ipfs/v0.4.5/go-ipfs_v0.4.5_windows-amd64.zip'
+$checksum64 = '8e6cd15ed7b4c06f683ade6b74b56e000421c01a5cc9937b582bbc004196dec9'
+$checksumType64 = 'sha256'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+                             -Url $url `
+                             -UnzipLocation $toolsDir `
+                             -Url64bit $url64 `
+                             -Checksum $checksum `
+                             -ChecksumType $checksumType `
+                             -Checksum64 $checksum64 `
+                             -ChecksumType64 $checksumType64
