6aa1c9d4d476b7a75e5eeeeb5f4f17756bb640c3
dtgm <dtgm1594@gmail.com>
2017-02-20 17:49:09 -0600

    Updated 4 pkg(s) on 2017-02-20: kvrt,telegram,telegram.install,telegram.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/telegram
    https://chocolatey.org/packages/telegram.install
    https://chocolatey.org/packages/telegram.portable

---
+++ automatic/_output/kvrt/2017.02.20.2124/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '31d750407e235d86554c712c531856a1b2d7354ccae89636d9ce2ab2356e2789'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/telegram.portable/1.0.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'telegram.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://updates.tdesktop.com/tsetup/tportable.1.0.14.zip'
+$checksum = 'aa7b9a3f851415a13103d30955ae2a275e0c3ea0b9fe37308ce70a5b2d9d88a0'
+$checksumType = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
