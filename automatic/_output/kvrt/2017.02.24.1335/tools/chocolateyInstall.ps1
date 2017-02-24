0915eaf8dcb22089e28999785e24ff299e58ad99
dtgm <dtgm1594@gmail.com>
2017-02-24 15:00:02 -0600

    Updated 2 pkg(s) on 2017-02-24: exiftool,kvrt
    https://chocolatey.org/packages/exiftool
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/kvrt/2017.02.24.1335/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '4adf4f8efb4893c1f5628f94eeb45977022b58e00bcfc3a9adbac6edddd40a12'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
