5589d1fb07bb5ca26b03ab6c56136afe26b483b6
dtgm <dtgm1594@gmail.com>
2017-03-01 09:00:02 -0600

    Updated 2 pkg(s) on 2017-03-01: kvrt,scite
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/scite

---
+++ automatic/_output/kvrt/2017.03.01.0947/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '08cf63712af4d26d0bcc221e5ca243cfe32ebe3e7dd3e19b56a127d9094740bc'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
