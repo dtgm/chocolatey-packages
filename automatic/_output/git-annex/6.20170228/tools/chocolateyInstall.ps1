29b3c0f50a43916a28b93132bb14af74dff7f1be
dtgm <dtgm1594@gmail.com>
2017-02-28 21:00:03 -0600

    Updated 5 pkg(s) on 2017-02-28: dolphin,git-annex,kvrt,logrotate,nem
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/git-annex
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/logrotate
    https://chocolatey.org/packages/nem

---
+++ automatic/_output/git-annex/6.20170228/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'git-annex'
+$installerType = 'exe'
+$silentArgs = '/S /D=C:\Program Files\Git'
+$url = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
+$checksum = 'db8782745ad00c8176c514d9611aa50aaa753edfd369b9bb44d7a153a182d4c2'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
