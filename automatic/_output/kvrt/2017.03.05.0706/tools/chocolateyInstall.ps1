41651cdf7855ee6298d3ae92432fca7cb41e5362
dtgm <dtgm1594@gmail.com>
2017-03-05 09:00:02 -0600

    Updated 2 pkg(s) on 2017-03-05: kvrt,nxt
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nxt

---
+++ automatic/_output/kvrt/2017.03.05.0706/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '735671ccb3042216ea97138d0c2e326f412f5848e7ba6fc19af4f1a7a69042ea'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/nxt/1.11.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'nxt'
+$url = 'https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-1.11.4.zip'
+$checksum = '8e7d5115e1664ae70cc46ff2a9d914ed50052b4352bf8ce5bd76118d63c84817'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
