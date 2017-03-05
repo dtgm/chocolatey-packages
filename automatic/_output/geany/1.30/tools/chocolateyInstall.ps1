8df354936987c53244dba3b7f39b24f13f912f71
dtgm <dtgm1594@gmail.com>
2017-03-05 15:00:03 -0600

    Updated 4 pkg(s) on 2017-03-05: dolphin,firefox-nightly,geany,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/geany
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/geany/1.30/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'geany'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.geany.org/geany-1.30_setup.exe'
+$checksum = '5dcae3ddf4bd0022d379cf8adc09c5b11588408cc81cb6dba1d4dd359eddeb94'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2017.03.05.1510/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '467958480f4ee287f702b83b3bb897d2cd4b100c6301962b3bacefb7df5241d7'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
