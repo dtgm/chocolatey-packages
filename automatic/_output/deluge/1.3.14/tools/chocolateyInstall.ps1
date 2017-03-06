09e796a06e97544c7c7513095df4d2582102fc06
dtgm <dtgm1594@gmail.com>
2017-03-06 15:00:03 -0600

    Updated 2 pkg(s) on 2017-03-06: deluge,firefox-nightly
    https://chocolatey.org/packages/deluge
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/deluge/1.3.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'deluge'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.deluge-torrent.org/windows/deluge-1.3.14-win32-py2.7.exe'
+$checksum = 'e2dc77347449c187f6d96ccb56bc849acd1b14badb89923438a5116f46bd227b'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/firefox-nightly/54.0.1.2017030612/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win32.installer.exe'
+$checksum = '9a2428c3d50ee4df0a7916e71aa38c15a9c6fb47b7693740c569fdbc8ed2eb07'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win64.installer.exe'
+$checksum64 = 'bddca770e40037a3a5d3e60c6a7e7a9181efab3ccde1f28442dde00cffa9b7c2'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
