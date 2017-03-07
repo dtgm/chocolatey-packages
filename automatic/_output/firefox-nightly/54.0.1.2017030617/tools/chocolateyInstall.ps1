a41db9e5d02691072b460f80ba8653498bf96bb9
dtgm <dtgm1594@gmail.com>
2017-03-06 21:00:02 -0600

    Updated 2 pkg(s) on 2017-03-06: firefox-nightly,outlookattachview
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/outlookattachview

---
+++ automatic/_output/firefox-nightly/54.0.1.2017030617/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win32.installer.exe'
+$checksum = 'e9e473de977cdbd758005f0d0505788e81f9de101b9474bf4d749ad2237ea3dd'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-54.0a1.en-US.win64.installer.exe'
+$checksum64 = 'f2433c2ee34d97c356fb20e65b752f7ec0dd706da4c6ae2acd544d47f2c4e751'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/outlookattachview/3.00/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'outlookattachview'
+$url = 'http://www.nirsoft.net/utils/outlookattachview.zip'
+$checksum = '59be1e62ca1c73d59cb992cb0aa178cd2d36d3331203fc7c83c3eb42fb85aa3c'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/outlookattachview-x64.zip'
+$checksum64 = 'e18e7c569c498ddded0ec4d6fe70e78c97ad74bfcb591cfea556b52adf5dbf07'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
