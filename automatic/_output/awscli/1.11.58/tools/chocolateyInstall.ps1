52f95c66af722352ba4361868838c235e91257e5
dtgm <dtgm1594@gmail.com>
2017-03-08 03:00:02 -0600

    Updated 3 pkg(s) on 2017-03-07: awscli,shmnview,sonarr
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/shmnview
    https://chocolatey.org/packages/sonarr

---
+++ automatic/_output/awscli/1.11.58/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.58.msi'
+$checksum = '46c3a0a4af9fd89c214f4b76472ece0ed76863ebfd80fdc85d997fa360e5c8d8'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.58.msi'
+$checksum64 = '991e706406e5c0392072559ae66314ec34b5647b46a5fcc663fce7787e373dd9'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/shmnview/1.35/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'shmnview'
+$url = 'http://www.nirsoft.net/utils/shmnview.zip'
+$checksum = '3d40468be560073a5e335ecb3767f3341790e2a1bcbf32712ff999a7bce05ff5'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/shmnview-x64.zip'
+$checksum64 = '1eef9ed8c62bff8536a22d437088ae24f77ecc2814fbfaddb704dbd0ebf7c40b'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
