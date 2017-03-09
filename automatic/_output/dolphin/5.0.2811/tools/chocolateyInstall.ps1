e94e6c225241cd64b21a05ba30c694eaf6065d60
dtgm <dtgm1594@gmail.com>
2017-03-08 21:00:02 -0600

    Updated 2 pkg(s) on 2017-03-08: dolphin,electron
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/electron

---
+++ automatic/_output/dolphin/5.0.2811/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-2811-x64.7z'
+$checksum = 'b5f74b0589167668c137576a02a43882ab39c7ca49cf9ce93e11a71abc14630b'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/electron/1.6.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'electron'
+$url = 'https://github.com/electron/electron/releases/download/v1.6.2/electron-v1.6.2-win32-ia32.zip'
+$checksum = 'ff41fc4781f34354bd9ee87f027a561a'
+$checksumType = 'md5'
+$url64 = 'https://github.com/electron/electron/releases/download/v1.6.2/electron-v1.6.2-win32-x64.zip'
+$checksum64 = '6af3c88691b02abc8db4010930ade435'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
