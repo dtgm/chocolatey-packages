d6f87c59a5456cf5956de2c7598664e1730d38cf
dtgm <dtgm1594@gmail.com>
2017-03-09 03:00:02 -0600

    Updated 2 pkg(s) on 2017-03-08: awscli,freenet
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/freenet

---
+++ automatic/_output/awscli/1.11.59/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.59.msi'
+$checksum = '4ee2aeb36dbea6e1cabd9f8095fe99d510b6b55506f40173a1a3f6cbc15a2108'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.59.msi'
+$checksum64 = '7e2b9935050a1ca4259073680452865d0898821eec7ff3f88ec516336fd2fd09'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/freenet/0.7.5.1477/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'freenet'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://downloads.freenetproject.org/alpha/installer/FreenetInstaller-1477.exe'
+$checksum = 'b1097d15844706e94298aa4bd81536ea8d7e39a8c2ccd3d8980bbc326823480c'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
