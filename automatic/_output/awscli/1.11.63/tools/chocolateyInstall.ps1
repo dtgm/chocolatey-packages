554f28cd4741f3bbda0ed524ebcf53aec478c805
dtgm <dtgm1594@gmail.com>
2017-03-14 09:00:05 -0500

    Updated 2 pkg(s) on 2017-03-14: awscli,digiKam
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/digiKam

---
+++ automatic/_output/awscli/1.11.63/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.63.msi'
+$checksum = '300e40f0c1f3e207b4584951b6d2c7e2e80d79f853eafab4f54ebb3a8be034d1'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.63.msi'
+$checksum64 = '80e4ebb4c4c2820c51e11024fc88ba738f13f8b3888ccae17b6a77dcc3060e9f'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/digiKam/5.5.0.01/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'digikam'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://download.kde.org/stable/digikam/digiKam-5.5.0-01-Win32.exe'
+$checksum = '1a8e4fc6eeff6fb7684986dd4afa10f535ea6590336d6d717561905cdd56d803'
+$checksumType = 'sha256'
+$url64 = 'http://download.kde.org/stable/digikam/digiKam-5.5.0-01-Win64.exe'
+$checksum64 = 'c0c97bf790360e40464bffebbfcfa117dbc05a9f41b1421bc6d3dcca65b9797e'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
