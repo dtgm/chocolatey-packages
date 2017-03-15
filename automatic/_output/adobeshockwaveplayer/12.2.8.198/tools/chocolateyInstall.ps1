9681e91951860170f6af0a5391617d46b121ad15
dtgm <dtgm1594@gmail.com>
2017-03-14 21:00:03 -0500

    Updated 2 pkg(s) on 2017-03-14: adobeshockwaveplayer,kvrt
    https://chocolatey.org/packages/adobeshockwaveplayer
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/adobeshockwaveplayer/12.2.8.198/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = 'adobeshockwaveplayer'
+$checksum = '2f77d564cb040baefdf7705652f00b6000563b7ccad4333f6d4149dd291487ab'
+$checksumType = 'sha256'
+$installerType = 'exe'
+$silentArgs = '/S /NCRC'
+$validExitCodes = @(0)
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$ahkFile = Join-Path $toolsDir 'chocolateyInstall.ahk'
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+Import-Module (Join-Path "$toolsDir" 'Get-HtmlFromRegex.psm1')
+
+$urlAppId = 'http://filehippo.com/download_shockwave/73584/'
+$search = 'download_shockwave/download/([\w\d]+)/'
+$replace = 'http://www.filehippo.com/en/download_shockwave/download/$1/'
+$urlTemp = Get-HtmlFromRegex $urlAppId $search $replace
+$search = '/download/file/([\w\d]+)/'
+$replace = 'http://www.filehippo.com/download/file/$1/'
+$url = Get-HtmlFromRegex $urlTemp $search $replace
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2017.03.14.2005/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = 'f251c031ec240c7f4bfd6919f276d136ff03ee9a49d8425f81ffde93eb7002c2'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
