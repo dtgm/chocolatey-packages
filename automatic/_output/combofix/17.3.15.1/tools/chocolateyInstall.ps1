20560b610546ef9ac353669f499dd134db3b7fd6
dtgm <dtgm1594@gmail.com>
2017-03-15 03:00:02 -0500

    Updated 2 pkg(s) on 2017-03-14: combofix,kvrt
    https://chocolatey.org/packages/combofix
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/combofix/17.3.15.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'combofix'
+$url = 'https://download.bleepingcomputer.com/sUBs/ComboFix.exe'
+$checksum = '8920021c68f6c0cb5ff3d844c75cd5ab6a13f931046c55c9b0dcf3d1ebc9da17'
+$checksumType = 'sha256'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$installFile = Join-Path $toolsDir "ComboFix.exe"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Write-Verbose "Create an empty sidecar metadata file for shimgen.exe to designate executable as GUI."
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
