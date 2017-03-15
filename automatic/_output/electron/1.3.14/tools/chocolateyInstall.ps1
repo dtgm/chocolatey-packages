641f78a003168dc0c8014d6a5f3527b65726d6ae
dtgm <dtgm1594@gmail.com>
2017-03-15 09:00:03 -0500

    Updated 2 pkg(s) on 2017-03-15: electron,kvrt
    https://chocolatey.org/packages/electron
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/electron/1.3.14/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'electron'
+$url = 'https://github.com/electron/electron/releases/download/v1.3.14/electron-v1.3.14-win32-ia32.zip'
+$checksum = '0d968fbeb8c677838b1b0966f77e6cdc'
+$checksumType = 'md5'
+$url64 = 'https://github.com/electron/electron/releases/download/v1.3.14/electron-v1.3.14-win32-x64.zip'
+$checksum64 = '8dcd2789f8d86a8a6a06ac5dc7f4f32f'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
