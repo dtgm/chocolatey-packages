b109394bc6c7f5cce31cebc19e96ac1753c2dc31
dtgm <dtgm1594@gmail.com>
2017-03-17 15:00:02 -0500

    Updated 4 pkg(s) on 2017-03-17: ddu,firefox-nightly,kvrt,routerpassview
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/routerpassview

---
+++ automatic/_output/firefox-nightly/55.0.1.2017031711/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = '49afe6f536cc7351a856a9700d2634341e9c4c1414bed0729134549a65b601f4'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = '0911bba9297be4a57ff5a4166fc201cb9ab1843b842e175ac0cce19a322fd2e3'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/routerpassview/1.70/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$packageName = 'routerpassview'
+$url = 'http://www.nirsoft.net/toolsdownload/routerpassview.zip'
+$checksum = '75f0d4590b3bde561db1d2198ea509f7ae8554e58cd6a3d10f21d813502af803'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+$chocTempDir = Join-Path $Env:Temp "chocolatey"
+$tempDir = Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$zipFile = Join-Path $tempDir "$($packageName).zip"
+
+$referer = "http://www.nirsoft.net/utils/"
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("Referer", $referer)
+$wc.DownloadFile($url, $zipFile)
+
+Get-ChecksumValid -File "$zipFile" `
+                  -Checksum "$checksum" `
+                  -ChecksumType "$checksumType"
+
+Get-ChocolateyUnzip -FileFullPath "$zipFile" `
+                    -Destination "$toolsDir" `
+                    -PackageName "$packageName"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
