5f109e52d6a019982426f72d2202311ba7cd10e8
dtgm <dtgm1594@gmail.com>
2017-03-21 21:00:02 -0500

    Updated 2 pkg(s) on 2017-03-21: stellarium,windowsrepair
    https://chocolatey.org/packages/stellarium
    https://chocolatey.org/packages/windowsrepair

--- automatic/_output/stellarium/0.15.2/tools/chocolateyInstall.ps1
+++ automatic/_output/stellarium/0.15.2/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $checksum = '2f8a4df1d80a862e6d7822c65bbf2556cc7dc9bd811108e3abab1e1cf609e121'
 $checksumType = 'sha256'
 $url64 = $urlArray[1]
 $validExitCodes = @(0)
-$checksum64 = '{checksumx64}'
+$checksum64 = '785b7f420872d177f40b70b565e757fc26a66871f3c75c957452ae2b1b6638a0'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyPackage -PackageName "$packageName" `
