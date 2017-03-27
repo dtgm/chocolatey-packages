c685df445266c3cb2291d8d3b9292bf05c6712a3
dtgm <dtgm1594@gmail.com>
2017-03-27 15:00:03 -0500

    Updated 2 pkg(s) on 2017-03-27: kvrt,pinginfoview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/pinginfoview

---
+++ automatic/_output/kvrt/2017.03.27.1641/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = 'dd85c40f305c13ebb1125c6cb87c19359098fb88a21958fc022a974167a088b3'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/pinginfoview/1.60/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'pinginfoview'
+$url = 'http://www.nirsoft.net/utils/pinginfoview.zip'
+$checksum = '71b0757a3df79515fb5b086b79c5505ac0dbf88557748c3a7c6ba5250672a63f'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
