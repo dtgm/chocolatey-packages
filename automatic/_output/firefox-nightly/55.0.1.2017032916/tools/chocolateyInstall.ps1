234fa2fd800aa2e6350fe2cedcabc2eadabedb5a
dtgm <dtgm1594@gmail.com>
2017-03-29 15:00:03 -0500

    Updated 4 pkg(s) on 2017-03-29: adwcleaner,firefox-nightly,kvrt,winlogonview
    https://chocolatey.org/packages/adwcleaner
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/winlogonview

---
+++ automatic/_output/firefox-nightly/55.0.1.2017032916/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = '883d43c2b8a40859f3cc9b1bef3ba94f6de215e31b31af57e4b12023e726fe20'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = '3ac1f679c5cfe5bfb4553dd00d9f60438fb6cf5b1a50b00083d33656b17eac6b'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/winlogonview/1.20/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'winlogonview'
+$url = 'http://www.nirsoft.net/utils/winlogonview.zip'
+$checksum = '73006ddfd16ec71912181bac40fa3ceefabcd46e99d8663da665aa33cde836ef'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
