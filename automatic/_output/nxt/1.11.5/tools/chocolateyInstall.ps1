35a23bf751f788ca3b8c5cdf202316a287c1d55d
dtgm <dtgm1594@gmail.com>
2017-04-01 09:00:03 -0500

    Updated 2 pkg(s) on 2017-04-01: kvrt,nxt
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/nxt

---
+++ automatic/_output/nxt/1.11.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'nxt'
+$url = 'https://bitbucket.org/JeanLucPicard/nxt/downloads/nxt-client-1.11.5.zip'
+$checksum = 'facb359f2a4fae62b5e2cbb2f5a41bd8cface8090ba72d3ddcba22a904fb23d3'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
