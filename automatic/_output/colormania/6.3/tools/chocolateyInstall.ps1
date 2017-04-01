82346d7779b102672d216fcde8a6187e77ceebba
dtgm <dtgm1594@gmail.com>
2017-04-01 05:01:29 -0500

    Updated 1 pkg(s) on 2017-04-01: colormania
    https://chocolatey.org/packages/colormania

---
+++ automatic/_output/colormania/6.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'colormania'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.blacksunsoftware.com/downloads/ColorManiaSetup.exe'
+$checksum = 'f19a213b9d3bdb01fa7de606445175e116d794970769565f3e40b69925ae9c16'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
