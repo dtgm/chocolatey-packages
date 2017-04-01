5e1b5b6c9281d8604db1ae39c267df3641532a12
dtgm <dtgm1594@gmail.com>
2017-03-31 23:01:28 -0500

    Updated 2 pkg(s) on 2017-03-31: bitvise-ssh-server,dolphin
    https://chocolatey.org/packages/bitvise-ssh-server
    https://chocolatey.org/packages/dolphin

---
+++ automatic/_output/dolphin/5.0.3183/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3183-x64.7z'
+$checksum = 'ec2934c785bf34730ab9df522f85cb71cbe2746789723b67f50f51437c8f8fa6'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
