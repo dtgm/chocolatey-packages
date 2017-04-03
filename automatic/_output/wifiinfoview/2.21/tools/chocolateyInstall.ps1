d1f4b93dd53a3edb7268f73510432406dc44d520
dtgm <dtgm1594@gmail.com>
2017-04-02 21:00:03 -0500

    Updated 2 pkg(s) on 2017-04-02: kvrt,wifiinfoview
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/wifiinfoview

---
+++ automatic/_output/wifiinfoview/2.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'wifiinfoview'
+$url = 'http://www.nirsoft.net/utils/wifiinfoview.zip'
+$checksum = '3a851f2706578e05aa8c7d689db94bc62c2a2835143913ada104ce66b84031d5'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
