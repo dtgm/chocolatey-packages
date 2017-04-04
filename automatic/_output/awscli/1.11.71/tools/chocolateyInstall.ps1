5daaba44300aa3cf0f8926d19880e49ac8317d0f
dtgm <dtgm1594@gmail.com>
2017-04-04 09:00:02 -0500

    Updated 5 pkg(s) on 2017-04-04: awscli,dolphin,goodsync,izpack,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/izpack
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/awscli/1.11.71/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.71.msi'
+$checksum = '7e1300d31d09b08edd9fe6dcabef6cb32882fa235872ddd912ffaf0ccdd9b043'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.71.msi'
+$checksum64 = '8c63c6ef94bfe05c98abc4b86dd6c1186f4ecbc589fa6393af1af5478b0a4264'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/izpack/5.1.0/tools/chocolateyUninstall.ahk
@@ -0,0 +1,28 @@
+﻿; default environment
+DetectHiddenWindows, off
+SetControlDelay, 20
+SetKeyDelay, 10
+
+; modified environment
+#NoEnv
+#NoTrayIcon
+DetectHiddenText, off
+SetTitleMatchMode, 1  ;begins
+
+winTitle = IzPack - Uninstaller ahk_class SunAwtFrame
+
+WinWait, %winTitle%, , 30
+WinActivate
+Send {Space}{Tab}{Enter}
+WinMinimize, %winTitle%
+Loop, 360 {
+  Sleep 500
+  IfExist, C:\Program Files\IzPack\
+    continue
+  Else {
+    WinClose, %winTitle%
+    break
+  }
+}
+
+ExitApp
