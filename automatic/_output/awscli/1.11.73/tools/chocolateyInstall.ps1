6b25636a4487dde58f639a8ecbd770862160017b
dtgm <dtgm1594@gmail.com>
2017-04-06 03:00:03 -0500

    Updated 3 pkg(s) on 2017-04-05: awscli,freenet,kvrt
    https://chocolatey.org/packages/awscli
    https://chocolatey.org/packages/freenet
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/awscli/1.11.73/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'awscli'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.11.73.msi'
+$checksum = '4f55198f8bd7619b766b60e8da334522acd1aa20226d0bf5b463514e2510ba37'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.11.73.msi'
+$checksum64 = '7f189ef0f706feb7c42af2a79bb80f744a26a09b7dc11e6bb5bf6eef4b39e021'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/freenet/0.7.5.1478/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'freenet'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://downloads.freenetproject.org/alpha/installer/FreenetInstaller-1478.exe'
+$checksum = 'cb15513fb0c345f4113a56825ff74b72e743777a9de05436109a02b409aa6d70'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
