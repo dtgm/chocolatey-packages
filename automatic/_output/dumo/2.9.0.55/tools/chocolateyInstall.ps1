7d6a39b1b223ec1728f33314283a99c584a7e40c
dtgm <dtgm1594@gmail.com>
2017-04-05 21:00:04 -0500

    Updated 3 pkg(s) on 2017-04-05: dumo,electron,kvrt
    https://chocolatey.org/packages/dumo
    https://chocolatey.org/packages/electron
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dumo/2.9.0.55/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '834626b5542b7b89dc9bec7487a2d5b4a428fe47d5815d98bb037862cadf1da2'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/electron/1.4.16/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'electron'
+$url = 'https://github.com/electron/electron/releases/download/v1.4.16/electron-v1.4.16-win32-ia32.zip'
+$checksum = '4cfbb5336ae42d4863b01b44c26137c3'
+$checksumType = 'md5'
+$url64 = 'https://github.com/electron/electron/releases/download/v1.4.16/electron-v1.4.16-win32-x64.zip'
+$checksum64 = 'ca01cf5e2cbdb630dfe1033d620bcd9d'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
