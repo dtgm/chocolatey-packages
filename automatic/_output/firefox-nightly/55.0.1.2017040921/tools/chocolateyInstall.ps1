a931045cc2a401036d5882fd48350d96216880ad
dtgm <dtgm1594@gmail.com>
2017-04-09 21:00:07 -0500

    Updated 5 pkg(s) on 2017-04-09: dolphin,firefox-nightly,handbrake,handbrake.install,kvrt
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/handbrake
    https://chocolatey.org/packages/handbrake.install
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/firefox-nightly/55.0.1.2017040921/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = '8fce592183f514e703231c59b7bb203c9322e7caeb3346668c822fe02070dee0'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = '86633426a6d1c5a76678765fec11d8951a4c6880dfd9498ba49bb217905870d6'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/handbrake.install/1.0.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'handbrake.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://handbrake.fr/rotation.php?file=HandBrake-1.0.7-i686-Win_GUI.exe'
+$checksum = ''
+$checksumType = 'sha1'
+$url64 = 'https://handbrake.fr/rotation.php?file=HandBrake-1.0.7-x86_64-Win_GUI.exe'
+$checksum64 = '602e342cd786227812c34040574084ca95bad479'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
