161f3edabaf0167dae76b3b82fc0b0ea3ca1eb8c
dtgm <dtgm1594@gmail.com>
2017-04-12 21:00:04 -0500

    Updated 3 pkg(s) on 2017-04-12: dolphin,kvrt,soundvolumeview
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/soundvolumeview

---
+++ automatic/_output/dolphin/5.0.3352/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3352-x64.7z'
+$checksum = 'e8376466ba436cc29aa611ea5ecec81efd78085eca8f25bcf88575720bd629d0'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/soundvolumeview/1.60/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'soundvolumeview'
+$url = 'http://www.nirsoft.net/utils/soundvolumeview.zip'
+$checksum = '1d1fd18b2a7e84dd61bff6a7c79a7b88222cea39153f4d14ce4d26d9f93945e1'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/soundvolumeview-x64.zip'
+$checksum64 = 'a544d0fae1c26e2947aad52dba9e37dd34ffcf462274c234f1cc38a72b3fb558'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
