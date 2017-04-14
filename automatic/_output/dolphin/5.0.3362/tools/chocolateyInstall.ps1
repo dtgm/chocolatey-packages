9462abc8362aec73217a7e363929815527730d9a
dtgm <dtgm1594@gmail.com>
2017-04-14 15:00:13 -0500

    Updated 4 pkg(s) on 2017-04-14: ddu,dolphin,firefox-nightly,kvrt
    https://chocolatey.org/packages/ddu
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dolphin/5.0.3362/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3362-x64.7z'
+$checksum = '63448c3a913f079064ddc278fc86cea4566aea061f8c671244fdac10bfd769c9'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/firefox-nightly/55.0.1.2017041411/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = 'cb964c402023abb2614ae81eb95d69130325a8fcaedcc108a3f44e3e4e51641a'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = 'e9d816c0aa7c7df1629f5f1a25302c4de8d6df9e96fb2c40f764513d48b2eeb9'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
