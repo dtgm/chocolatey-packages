c9b35ea21fa8b16cd12775b9ca80d55551e61133
dtgm <dtgm1594@gmail.com>
2017-04-14 03:00:06 -0500

    Updated 3 pkg(s) on 2017-04-14: bitcoin-unlimited,bitcoin-unlimited.install,bitcoin-unlimited.portable
    https://chocolatey.org/packages/bitcoin-unlimited
    https://chocolatey.org/packages/bitcoin-unlimited.install
    https://chocolatey.org/packages/bitcoin-unlimited.portable

---
+++ automatic/_output/bitcoin-unlimited.install/1.0.1.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'bitcoin-unlimited.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://www.bitcoinunlimited.info/downloads/bitcoinUnlimited-1.0.1.3-win32-setup.exe'
+$checksum = 'a8db3159799309439f31e4c60faab0e2ac24819e918f7f3402cec76c0b009d68'
+$checksumType = 'sha256'
+$url64 = 'https://www.bitcoinunlimited.info/downloads/bitcoinUnlimited-1.0.1.3-win64-setup.exe'
+$checksum64 = '2fd35bdb9bf50bc78a7cba2ee4890b07475737b2d6d46d22cc68dccae85b9a6e'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/bitcoin-unlimited.portable/1.0.1.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'bitcoin-unlimited.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://www.bitcoinunlimited.info/downloads/bitcoinUnlimited-1.0.1.3-win32.zip'
+$checksum = 'e754fdd303cbceb955929e4a1b626d6251a767665c3cea9796426b0f406bdb36'
+$checksumType = 'sha256'
+$url64 = 'https://www.bitcoinunlimited.info/downloads/bitcoinUnlimited-1.0.1.3-win64.zip'
+$checksum64 = '8e837b44ee71d9c7b6693320e5909cb5cd1f12580e7b828d359828bc8a06d4ee'
+$checksumType64 = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Write-Verbose "Creating empty sidecar so shimgen.exe creates shim for GUI rather than console"
+$guiExe = Get-ChildItem -Recurse -Path $toolsDir -Include "bitcoin-qt.exe"
+if ($guiExe) {
+    Set-Content -Path $($guiExe.FullName+".gui") -Value $null
+} else {Write-Warning "Can't find bitcoin-qt.exe installation directory"}
