3433c891a603fc2e541d879ffad9304bda1c0478
dtgm <dtgm1594@gmail.com>
2017-04-21 16:04:15 -0500

    Updated 2 pkg(s) on 2017-04-21: kvrt,openra
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/openra

---
+++ automatic/_output/kvrt/2017.04.21.1951/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '50c6ad1d1d2ed7ca80b790525f58730a3587ed60bfaaf73d0e908b5fce3e2066'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
---
+++ automatic/_output/openra/2017.04.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'openra'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com//OpenRA/OpenRA/releases/download/release-20170421/OpenRA-release-20170421.exe'
+$checksum = '834f1d8b86b06f0900148b584daad20e'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
