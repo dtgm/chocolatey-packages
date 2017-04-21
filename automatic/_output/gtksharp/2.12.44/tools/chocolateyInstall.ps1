ce4f5d93b02acf3289535dd89c0c1f49c1337d67
dtgm <dtgm1594@gmail.com>
2017-04-21 10:04:24 -0500

    Updated 2 pkg(s) on 2017-04-20: gtksharp,kvrt
    https://chocolatey.org/packages/gtksharp
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/gtksharp/2.12.44/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'gtksharp'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://dl.xamarin.com/GTKforWindows/Windows/gtk-sharp-2.12.44.msi'
+$checksum = '1f7d358043a5aa56d805022d4efd0956c64e662f24b564b53e4e9d3849248d3b'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/kvrt/2017.04.20.1930/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName = 'kvrt'
+$url = 'http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe'
+$checksum = '7b521d3ff06a0feb5f88ac7a15fcb3ad7d058bd13ec95d0e1d0426497ad23ba9'
+$checksumType = 'sha256'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsPath "kvrt.exe"
+try {
+  Get-ChocolateyWebFile -PackageName "$packageName" `
+                        -FileFullPath "$installFile" `
+                        -Url "$url" `
+                        -Checksum "$checksum" `
+                        -ChecksumType "$checksumType"
+
+  # create empty sidecars so shimgen only creates one shim
+  Set-Content -Path ("$installFile.ignore") `
+              -Value $null
+
+  # create batch to start executable
+  $batchStart = Join-Path $toolsPath "kvrt.bat"
+  'start %~dp0\kvrt.exe -accepteula' | Out-File -FilePath $batchStart -Encoding ASCII
+  Install-BinFile "kvrt" "$batchStart"
+} catch {
+  throw $_.Exception
+}
