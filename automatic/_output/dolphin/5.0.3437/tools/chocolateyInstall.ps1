40ff37037b399f419fb698e33b6d7deb761bf70f
dtgm <dtgm1594@gmail.com>
2017-04-24 03:00:03 -0500

    Updated 3 pkg(s) on 2017-04-23: dolphin,kvrt,naps2.portable
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/naps2.portable

---
+++ automatic/_output/dolphin/5.0.3437/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3437-x64.7z'
+$checksum = 'fbeb535afabcee7803e64bd2adcc24e294c1d5c75871ef1010bdb7fc6c6966e7'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/naps2.portable/5.3.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'naps2.portable'
+$url = 'https://github.com/cyanfish/naps2/releases/download/v5.3.3/naps2-5.3.3-portable.zip'
+$checksum = '1df1408ef0af95057c8f3e5466cad12f2e479c1c007e8e998f55ce410bf8e690'
+$checksumType = 'sha256'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Write-Verbose "Creating GUI executable sidecar for shimgen.exe"
+$guiExe = Join-Path $toolsDir "NAPS2.Portable.exe"
+Set-Content -Path ("$guiExe.gui") `
+            -Value $null
