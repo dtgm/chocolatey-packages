33636c7bc7da5ef7da9c1802f238b26fc840b1f0
dtgm <dtgm1594@gmail.com>
2017-04-24 15:00:03 -0500

    Updated 3 pkg(s) on 2017-04-24: chromecacheview,firefox-nightly,kvrt
    https://chocolatey.org/packages/chromecacheview
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/chromecacheview/1.75/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'chromecacheview'
+$url = 'http://www.nirsoft.net/utils/chromecacheview.zip'
+$checksum = 'a386a457d37475168065ddd02cff2906d627de8a07f765d4f0a80c3c856a0857'
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/firefox-nightly/55.0.1.2017042411/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = '0b629d6bfb4149b94ce66e62e48211770fe329d165a7a461a99e8d37910bddb0'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = '4a9d2358c7b0deddefd51e30db9b7366ebf141126819d3695b43415dc16fe4b0'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
