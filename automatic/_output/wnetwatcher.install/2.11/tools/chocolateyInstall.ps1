01f0a3277532dd0c6f3ff56ee3f1fb5eb8cf079e
dtgm <dtgm1594@gmail.com>
2017-04-27 09:00:06 -0500

    Updated 4 pkg(s) on 2017-04-27: kvrt,wnetwatcher,wnetwatcher.install,wnetwatcher.portable
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/wnetwatcher
    https://chocolatey.org/packages/wnetwatcher.install
    https://chocolatey.org/packages/wnetwatcher.portable

---
+++ automatic/_output/wnetwatcher.install/2.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'wnetwatcher.install'
+$installerType = 'exe'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher_setup.exe'
+$checksum = 'bf0a9bb800c8e5bd7a38058eb3c7b5a1f1b2552a0cf785d53388ed7a72cba82b'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/wnetwatcher.portable/2.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'wnetwatcher.portable'
+$url = 'http://www.nirsoft.net/utils/wnetwatcher.zip'
+$checksum = '2f9dc34a694454ff8efbbd01042c02e99e5814433265c231cea0efffb6c446d9'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "wnetwatcher.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
