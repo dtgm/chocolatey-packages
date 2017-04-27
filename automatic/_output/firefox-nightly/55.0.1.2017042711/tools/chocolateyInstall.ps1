dcf665e98ae85375d43e654fe1df73ba0e99b1f7
dtgm <dtgm1594@gmail.com>
2017-04-27 15:00:11 -0500

    Updated 3 pkg(s) on 2017-04-27: firefox-nightly,kvrt,zimbra-desktop
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/zimbra-desktop

---
+++ automatic/_output/firefox-nightly/55.0.1.2017042711/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win32.installer.exe'
+$checksum = '6507dc03764ee76abaf9c5b3c8aa6890caf2a45d62e8b8eb71b001e8346350f4'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-55.0a1.en-US.win64.installer.exe'
+$checksum64 = 'f12d278077351101a30e339a1f453cc5145c02826cc3bef6f8329b722a9d9888'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/zimbra-desktop/7.3.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'zimbra-desktop'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://s3.amazonaws.com/files.zimbra.com/downloads/zdesktop/7.3.0/b13060/zdesktop_7_3_0_ga_b13060_20170410030639_win32.msi'
+$checksum = '3e8194fa177265360a5ee841423b3faf'
+$checksumType = 'sha256'
+$url64 = 'https://s3.amazonaws.com/files.zimbra.com/downloads/zdesktop/7.3.0/b13060/zdesktop_7_3_0_ga_b13060_20170410030639_win64.msi'
+$checksum64 = 'd5d7dfee9c5f6a7e77276d1b2535f573e45fb3131e1442540acbcdc6f974705f'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
+
