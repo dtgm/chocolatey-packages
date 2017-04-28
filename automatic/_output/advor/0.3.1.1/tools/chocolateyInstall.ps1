9e5aa0158855b79ffb3fc55b48577837b263cee1
dtgm <dtgm1594@gmail.com>
2017-04-27 21:00:13 -0500

    Updated 4 pkg(s) on 2017-04-27: advor,advtor,dolphin,kvrt
    https://chocolatey.org/packages/advor
    https://chocolatey.org/packages/advtor
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/advor/0.3.1.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'advor'
+$url = 'https://sourceforge.net/projects/advtor/files/AdvOR-0.3.1.1/AdvOR-0.3.1.1.zip'
+$checksum = '25bf86fcbd915acb613714b6845b0bb32d33a8535a2f6b851ddd41eaa37ebb61'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = Join-Path $toolsDir "AdvOR"
+$installFile = Join-Path $installDir "AdvOR.exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/dolphin/5.0.3482/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3482-x64.7z'
+$checksum = '8a74f477b874d799c9f0b1d5974a0954641ce9a575e114f199d7a0d15fa439dd'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
