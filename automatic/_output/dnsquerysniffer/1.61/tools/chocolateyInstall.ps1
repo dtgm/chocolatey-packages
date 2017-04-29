e443ac479813f5e9bd4407a4ea2e44d64142fdfd
dtgm <dtgm1594@gmail.com>
2017-04-29 15:00:02 -0500

    Updated 4 pkg(s) on 2017-04-29: dnsquerysniffer,dolphin,firefox-nightly,kvrt
    https://chocolatey.org/packages/dnsquerysniffer
    https://chocolatey.org/packages/dolphin
    https://chocolatey.org/packages/firefox-nightly
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/dnsquerysniffer/1.61/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'dnsquerysniffer'
+$url = 'http://www.nirsoft.net/utils/dnsquerysniffer.zip'
+$checksum = '3369f16518ee1c887b1b202bef21d40b7853a4afeb757ed41875eade981889f9'
+$checksumType = 'sha256'
+$url64 = 'http://www.nirsoft.net/utils/dnsquerysniffer-x64.zip'
+$checksum64 = 'da3deffa9971889952d1d98017b578d7e81e4a9fe6974a927643ca3db335860a'
+$checksumType64 = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/_output/dolphin/5.0.3506/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'dolphin'
+$url = 'http://dl.dolphin-emu.org/builds/dolphin-master-5.0-3506-x64.7z'
+$checksum = '2891fbaf307288e4c6426e9bbf243d1a5bc6d5927bb2cf7cd2f1b4ae2d9ed674'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
