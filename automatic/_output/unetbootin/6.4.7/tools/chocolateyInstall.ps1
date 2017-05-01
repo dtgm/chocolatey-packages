1cb9f3fd320008de1a3e2d3b090cf23051d13305
dtgm <dtgm1594@gmail.com>
2017-05-01 09:00:02 -0500

    Updated 3 pkg(s) on 2017-05-01: krita,kvrt,unetbootin
    https://chocolatey.org/packages/krita
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/unetbootin

---
+++ automatic/_output/unetbootin/6.4.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'unetbootin'
+$url = 'https://sourceforge.net/projects/unetbootin/files/UNetbootin/647/unetbootin-windows-647.exe'
+$checksum = '4985866361e76fea8bd4fa197a35823f883af2e96a8340fcf78626b3b96bbfc5'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+
+Write-Verbose "Create an empty sidecar metadata file for shimgen.exe to designate gui"
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
