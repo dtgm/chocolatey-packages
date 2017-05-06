ebc4d713cca8d39a622c022dad21b94856886359
dtgm <dtgm1594@gmail.com>
2017-05-06 09:00:05 -0500

    Updated 3 pkg(s) on 2017-05-06: bitcoinxt.install,kvrt,transmission
    https://chocolatey.org/packages/bitcoinxt.install
    https://chocolatey.org/packages/kvrt
    https://chocolatey.org/packages/transmission

---
+++ automatic/_output/bitcoinxt.install/0.11.0.06/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'bitcoinxt.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11F/bitcoin-xt-0.11.0-win32-setup-unsigned.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11F/bitcoin-xt-0.11.0-win64-setup-unsigned.exe'
+$checksum64 = '{checksumx64}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/transmission/2.92/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'transmission'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$url = 'https://github.com/transmission/transmission/releases/download/2.92/transmission-2.92-x86.msi'
+$checksum = 'f8bfaba603036d35f74bbd5d27ea5bc51fe58d991af48228c290323e406bef00'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/transmission/transmission/releases/download/2.92/transmission-2.92-x64.msi'
+$checksum64 = '8cee6436e79d48409d76a85bbb1bb506c8c76c8f686551b9e7e13b9a2ca2299d'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
