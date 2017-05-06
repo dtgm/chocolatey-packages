eacaf5568737d34d99308b6601e9ec0a7e4cba15
dtgm <dtgm1594@gmail.com>
2017-05-05 22:21:03 -0500

    Updated 4 pkg(s) on 2017-05-05: advor,advtor,goodsync,kvrt
    https://chocolatey.org/packages/advor
    https://chocolatey.org/packages/advtor
    https://chocolatey.org/packages/goodsync
    https://chocolatey.org/packages/kvrt

---
+++ automatic/_output/advtor/0.3.1.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'advtor'
+$url = 'https://sourceforge.net/projects/advtor/files/AdvOR-0.3.1.2/AdvOR-0.3.1.2.zip'
+$checksum = 'af80718c0debf525179861af6fac716a4437572a1c5913a31bc8a2f42113e423'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir (Join-Path "AdvOR" "AdvOR.exe")
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
