faddd2016e5b67962437ccd96aaf23f666516ffe
dtgm <dtgm1594@gmail.com>
2017-05-25 03:00:04 -0500

    Updated 2 pkg(s) on 2017-05-24: jstock,keepass-plugin-1p2kp
    https://chocolatey.org/packages/jstock
    https://chocolatey.org/packages/keepass-plugin-1p2kp

---
+++ automatic/_output/jstock/1.0.7.25/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'jstock'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/yccheok/jstock/releases/download/release_1-0-7-25/jstock-1.0.7.25-setup.exe'
+$checksum = 'c4333f5bd52b5167567183bff51c0162'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/keepass-plugin-1p2kp/0.2.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,72 @@
+﻿# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+
+$packageName = 'keepass-plugin-1p2kp'
+$typName = '1P2KeePass.plgx'
+$packageSearch = 'KeePass Password Safe'
+$url = 'https://github.com/diimdeep/1P2KeePass/releases/download/0.2.1/0.2.1.zip'
+$checksum = '2209ab4c53d1f26530425bcd672ec082'
+$checksumType = 'md5'
+
+try {
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*" `
+                           -and `
+                           $_.DisplayVersion -ge 2.0 `
+                           -and `
+                           $_.DisplayVersion -lt 3.0 } `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath
+if (! $installPath) {
+  Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "Searching $env:Path for unregistered install..."
+  $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+  if (! $installFullName) {
+    $installPath = [io.path]::GetDirectoryName($installFullName)
+  }
+}
+if (! $installPath) {
+  Write-Warning "$($packageSearch) not found."
+  throw
+}
+Write-Verbose "`t...found."
+
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+  $pluginPath = Join-Path $installPath "Plugins"
+  [System.IO.Directory]::CreateDirectory($pluginPath)
+}
+# download and extract PLGX file into Plugins dir
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$pluginPath" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+# rename PLGX file so it is clear which plugins are managed via choco
+$typPlugin = Join-Path $pluginPath $typName
+$chocoPlugin = Join-Path $pluginPath "$($packageName).plgx"
+Move-Item -Path $typPlugin -Destination $chocoPlugin -Force -ErrorAction Continue
+if ( Get-Process -Name "KeePass" `
+                 -ErrorAction SilentlyContinue ) {
+  Write-Warning "$($packageSearch) is currently running. Plugin will be available at next restart of $($packageSearch)." 
+} else {
+  Write-Host "$($packageName) will be loaded the next time KeePass is started."
+  Write-Host "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Menu>Tools"
+}} catch {
+  throw $_.Exception
+}
