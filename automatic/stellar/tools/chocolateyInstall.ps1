6a2d2428e8c07efb014abb4d9ceabd1caaa70570
dtgm <dtgm1594@gmail.com>
2017-06-07 00:33:52 -0500

    New pkgs (3): stellar, mist, ethereum

---
+++ automatic/stellar/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$url64 = '{{DownloadUrlx64}}'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Write-Verbose "Creating empty sidecar so shimgen.exe creates shim for GUI rather than console"
+$guiExe = Get-ChildItem -Recurse -Path $toolsDir -Include "StellarWallet.exe"
+if ($guiExe) {
+    Set-Content -Path $($guiExe.FullName+".gui") -Value $null
+} else {Write-Warning "Can't find StellarWallet.exe installation directory"}
