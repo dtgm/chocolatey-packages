6a1a22625303d1b85e28fe1d53e587aad7934d17
dtgm <dtgm1594@gmail.com>
2017-06-18 03:00:01 -0500

    Updated 3 pkg(s) on 2017-06-17: keepass,keepass.install,keepass.portable
    https://chocolatey.org/packages/keepass
    https://chocolatey.org/packages/keepass.install
    https://chocolatey.org/packages/keepass.portable

---
+++ automatic/_output/keepass.install/2.36/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'keepass.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.36/KeePass-2.36-Setup.exe/download'
+$checksum = '5CECFA817642EA40A84E70EF6C7822A41B7AEB77'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
