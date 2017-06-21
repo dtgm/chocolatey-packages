f46f18567abdfd4e455434876e2a520b9fb41fb6
dtgm <dtgm1594@gmail.com>
2017-06-21 15:00:02 -0500

    Updated 2 pkg(s) on 2017-06-21: 0install.portable,firefox-nightly
    https://chocolatey.org/packages/0install.portable
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/0install.portable/2.13.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+$packageName = '0install.portable'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://0install.de/files/zero-install.zip'
+$checksum = '076c715429bc58327e37d5492922cef48d3a1fedc13fab96a7748bb2a73d1aa9'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
