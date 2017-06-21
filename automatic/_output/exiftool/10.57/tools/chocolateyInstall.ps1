525f592bc38b1824c5b9c4b0ebe2b080df14b253
dtgm <dtgm1594@gmail.com>
2017-06-21 03:00:02 -0500

    Updated 2 pkg(s) on 2017-06-20: exiftool,firefox-nightly
    https://chocolatey.org/packages/exiftool
    https://chocolatey.org/packages/firefox-nightly

---
+++ automatic/_output/exiftool/10.57/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.57.zip'
+$checksum = '09abb3e53dba700b6645c59415ce3d0817e1d946'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
---
+++ automatic/_output/firefox-nightly/56.0.1.2017062011/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-56.0a1.en-US.win32.installer.exe'
+$checksum = 'dbf61ccb48b873438cbfa966f822bbc0ff1af32dd6970b6b663711d46d6127de'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-56.0a1.en-US.win64.installer.exe'
+$checksum64 = '556f83897becf74503c1df89c65338c94db3b553caddfe0e9c831764afc500a4'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
