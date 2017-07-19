8b9e29c74d2a77595a1bd2166af2d7da19ea4b46
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-07-20 02:03:27 +0900

    fix checksum error of zero.Install package

--- automatic/_output/0install.portable/2.13.1/tools/chocolateyInstall.ps1
+++ automatic/_output/0install.portable/2.13.1/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@ $packageName = '0install.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://0install.de/files/zero-install.zip'
 $checksum = '076c715429bc58327e37d5492922cef48d3a1fedc13fab96a7748bb2a73d1aa9'
-$checksumType = 'sha1'
+$checksumType = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
