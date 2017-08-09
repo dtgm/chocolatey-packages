fa7acbdac505d5b4303e4ba2496102de5e23182d
dtgm <dtgm1594@gmail.com>
2017-08-09 16:31:33 -0500

    Updated 4 pkg(s) on 2017-08-09: awscli,cyberduck.install,cyberduck,firefox-nightly
    https://chocolatey.org/packages/awscli/1.11.130
    https://chocolatey.org/packages/cyberduck.install/6.2.2.26027
    https://chocolatey.org/packages/cyberduck/6.2.2.26027
    https://chocolatey.org/packages/firefox-nightly/57.0.1.2017080912

---
+++ automatic/_output/cyberduck.install/6.2.2.26027/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-6.2.2.26027.exe'
+$checksum = '8033c5d2a07d4391c710c353ea2546f27b96141ed8de142ed34037cc7b56a3b4'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/firefox-nightly/57.0.1.2017080912/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-57.0a1.en-US.win32.installer.exe'
+$checksum = '0597bafd15b96aaf4a8a0b06ee61330208fdc5fff53b7a90d1b46f25693c04ff'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-57.0a1.en-US.win64.installer.exe'
+$checksum64 = '6a1477f14e8a8698fa6bff7fc0792f0b3c58b97e700e994b4142094362f2a6ac'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
