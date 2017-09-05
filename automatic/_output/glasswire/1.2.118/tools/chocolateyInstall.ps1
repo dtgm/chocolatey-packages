9f8a91da5c4507a3c10b6de6241f26dfd400abea
dtgm <dtgm1594@gmail.com>
2017-09-05 15:00:04 -0500

    Updated 2 pkg(s) on 2017-09-05: firefox-nightly,glasswire
    https://chocolatey.org/packages/firefox-nightly/57.0.1.2017090512
    https://chocolatey.org/packages/glasswire/1.2.118

---
+++ automatic/_output/glasswire/1.2.118/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = 'A2AC8AA269AD82E6BBA44402659883E9'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
