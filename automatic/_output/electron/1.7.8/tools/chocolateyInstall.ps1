0b03cc0bcd9a0c9f200faa4a1bdf761346756ee9
dtgm <dtgm1594@gmail.com>
2017-09-24 03:00:02 -0500

    Updated 2 pkg(s) on 2017-09-23: electron,firefox-nightly
    https://chocolatey.org/packages/electron/1.7.8
    https://chocolatey.org/packages/firefox-nightly/58.0.1.2017092401

---
+++ automatic/_output/electron/1.7.8/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'electron'
+$url = 'https://github.com//electron/electron/releases/download/v1.7.8/electron-v1.7.8-win32-ia32.zip'
+$checksum = '7574bf28f2239f2d94215137795f22aa'
+$checksumType = 'md5'
+$url64 = 'https://github.com//electron/electron/releases/download/v1.7.8/electron-v1.7.8-win32-x64.zip'
+$checksum64 = '5eedc0eaeb39852c90388e8b939789e6'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/firefox-nightly/58.0.1.2017092401/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win32.installer.exe'
+$checksum = '98eca754c91376d8d2973acf544027dc5ef466b5cc8d75c27f726e3d54a91ab4'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win64.installer.exe'
+$checksum64 = 'f99e01f303ce2203efef60277e53e2d7669b049e1ca4cc7800ad3c8116999b56'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
