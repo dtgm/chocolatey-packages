9b97d95d8ebed9c1e7b4f028dd448e3e141f8215
dtgm <dtgm1594@gmail.com>
2017-09-26 15:00:07 -0500

    Updated 2 pkg(s) on 2017-09-26: copay,firefox-nightly
    https://chocolatey.org/packages/copay/3.8.2
    https://chocolatey.org/packages/firefox-nightly/58.0.1.2017092613

---
+++ automatic/_output/copay/3.8.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'copay'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/bitpay/copay/releases/download/v3.8.2/BitPay.exe'
+$checksum = '6ea6e29b64d2d85520bbc5800070dec279f7d939968118f637c43c50f94b50e5'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
---
+++ automatic/_output/firefox-nightly/58.0.1.2017092613/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win32.installer.exe'
+$checksum = 'a72450feb277b5ea91f3258e18745002005fabf7304fc38317ded29e900df3a6'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win64.installer.exe'
+$checksum64 = '08332868339dd0a99b29e93bd82fc8bc398f79f86511a50dafaa9e83796589d7'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
