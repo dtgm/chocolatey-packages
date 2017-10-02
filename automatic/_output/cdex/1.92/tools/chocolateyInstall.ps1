f8bf7cf52fde8dbb560cb9b76a48ba4779c03a86
dtgm <dtgm1594@gmail.com>
2017-10-02 03:00:05 -0500

    Updated 2 pkg(s) on 2017-10-01: cdex,firefox-nightly
    https://chocolatey.org/packages/cdex/1.92
    https://chocolatey.org/packages/firefox-nightly/58.0.1.2017100201

---
+++ automatic/_output/cdex/1.92/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'cdex'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mirror.cdex.mu/CDex-1.92.exe'
+$checksum = '13461a26259bc976f8a40c082fa8efc44e23b9e08c3deaef8aa9de36081b50b2'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = "$scriptPath\chocolateyInstall.ahk"
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/firefox-nightly/58.0.1.2017100201/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win32.installer.exe'
+$checksum = 'eb81a3a9be6134abc3b40a8154f78bd0c781534e8f284cfd3dff7e338a6308ab'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.win64.installer.exe'
+$checksum64 = '8779339d8c796bd121f43485935f9261663c3bcb70559796abf360773e4bba01'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
