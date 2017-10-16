76429fc1df56752dbf6f1623cf6b467a48f9dbbe
dtgm <dtgm1594@gmail.com>
2017-10-16 09:00:04 -0500

    Updated 3 pkg(s) on 2017-10-16: keepass.install,keepass.portable,keepass
    https://chocolatey.org/packages/keepass.install/2.37
    https://chocolatey.org/packages/keepass.portable/2.37
    https://chocolatey.org/packages/keepass/2.37

---
+++ automatic/_output/keepass.install/2.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'keepass.install'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.37/KeePass-2.37-Setup.exe/download'
+$checksum = '4E54719C08EDBD6ECFA63EBD42A4C9270945BB55'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/_output/keepass.portable/2.37/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿# generated vars
+$packageName = 'keepass.portable'
+$url = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.37/KeePass-2.37.zip/download'
+$checksum = '81E56E80B8CA02688C0C654EC0F974F4DA17D2AD'
+
+# static vars
+$checksumType = 'sha1'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $Env:ChocolateyInstall\helpers\functions
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "KeePass.exe.gui"
+Set-Content -Path $installFile `
+            -Value $null
