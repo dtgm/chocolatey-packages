4336a6d6420466b8c360c4c56104315ff0363aa6
dtgm <dtgm1594@gmail.com>
2017-10-27 15:00:02 -0500

    Updated 2 pkg(s) on 2017-10-27: cyberduck.install,cyberduck
    https://chocolatey.org/packages/cyberduck.install/6.2.10.26754
    https://chocolatey.org/packages/cyberduck/6.2.10.26754

---
+++ automatic/_output/cyberduck.install/6.2.10.26754/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-6.2.10.26754.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
