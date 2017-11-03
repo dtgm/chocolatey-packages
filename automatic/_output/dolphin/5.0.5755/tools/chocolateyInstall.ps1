3fb43e33d803ca9c90919e8ba51a67e632612068
dtgm <dtgm1594@gmail.com>
2017-11-02 21:00:02 -0500

    Updated 2 pkg(s) on 2017-11-02: dolphin,sandboxie.install
    https://chocolatey.org/packages/dolphin/5.0.5755
    https://chocolatey.org/packages/sandboxie.install/5.20.0.20171102

---
+++ automatic/_output/dolphin/5.0.5755/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'dolphin'
+$url = 'https://dl.dolphin-emu.org/builds/dolphin-master-5.0-5755-x64.7z'
+$checksum = 'e01865b692c588077ae1cd1ab9c408a0eac0331a9202ca3adeb4bc11fcaee9ac'
+$checksumType = 'sha256'
+$url64 = $url
+$checksum64 = $checksum
+$checksumType64 = $checksumType
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
