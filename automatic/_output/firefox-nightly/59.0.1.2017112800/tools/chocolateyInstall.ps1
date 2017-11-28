453783689906692153ac08b640d0e493b56de54c
dtgm <dtgm1594@gmail.com>
2017-11-28 03:00:09 -0600

    Updated 1 pkg(s) on 2017-11-27: firefox-nightly
    https://chocolatey.org/packages/firefox-nightly/59.0.1.2017112800

---
+++ automatic/_output/firefox-nightly/59.0.1.2017112800/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+$packageName = 'firefox-nightly'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-59.0a1.en-US.win32.installer.exe'
+$checksum = '2605987a8b65c674f554d541d5450facd1d9108307dcfb55139c049efc4f8f1b'
+$checksumType = 'sha256'
+$url64 = 'https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-59.0a1.en-US.win64.installer.exe'
+$checksum64 = '5a250666091b85c681bfa144d09ff35ac7022073911254a54c2a55f4cc167233'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
