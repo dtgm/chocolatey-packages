89820bfb7fca330c3dfe95b7c30eaa02e9a2d081
dtgm <dtgm1594@gmail.com>
2017-12-11 21:00:03 -0600

    Updated 2 pkg(s) on 2017-12-11: cdex,folderchangesview
    https://chocolatey.org/packages/cdex/1.96
    https://chocolatey.org/packages/folderchangesview/2.20

---
+++ automatic/_output/cdex/1.96/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'cdex'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mirror.cdex.mu/CDex-1.96.exe'
+$checksum = 'ec92665f4092af6b6cd9fbf15e2722d0a572ac9e3e127db6a2384c8202ff7167'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = "$scriptPath\chocolateyInstall.ahk"
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
