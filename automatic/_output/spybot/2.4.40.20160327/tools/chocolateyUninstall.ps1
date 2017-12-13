17f6e493b87e4baa11f9ac2b01110ae0edfd78d2
RB <RedBaron2@users.noreply.github.com>
2017-12-13 15:23:32 -0800

    Update chocolateyUninstall.ps1

--- automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
+++ automatic/_output/spybot/2.4.40.20160327/tools/chocolateyUninstall.ps1
@@ -1,18 +1,17 @@
 $packageName = 'spybot'
 $packageSearch = "Spybot - Search and Destroy*"
 $installerType = 'exe'
--$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--$validExitCodes = @(0)
-$silentArgs = '/SILENT'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
 
 $spybotreg = Get-UninstallRegistryKey -SoftwareName $packageSearch
--Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
--				                 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
--				                 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
--                 -ErrorAction:SilentlyContinue `
--| Where-Object   {$_.DisplayName -like $packageSearch} `
--| ForEach-Object {Uninstall-ChocolateyPackage -PackageName "$packageName" `
--								                              -FileType "$installerType" `
--								                              -SilentArgs "$($silentArgs)" `
--								                              -File "$($_.UninstallString)" `
--                                              -ValidExitCodes $validExitCodes}
+Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+				                 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+				                 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   {$_.DisplayName -like $packageSearch} `
+| ForEach-Object {Uninstall-ChocolateyPackage -PackageName "$packageName" `
+								                              -FileType "$installerType" `
+								                              -SilentArgs "$($silentArgs)" `
+								                              -File "$($_.UninstallString)" `
+                                              -ValidExitCodes $validExitCodes}
