689edeb9c1b5f33a224a75a44c2dfca9441a4aee
dtgm <dtgm1594@gmail.com>
2017-12-13 21:00:02 -0600

    Updated 4 pkg(s) on 2017-12-13: autoruns,bginfo,ddu,handle
    https://chocolatey.org/packages/autoruns/13.81
    https://chocolatey.org/packages/bginfo/4.23
    https://chocolatey.org/packages/ddu/17.0.8.1
    https://chocolatey.org/packages/handle/4.11

---
+++ automatic/_output/handle/4.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+$packageName = 'handle'
+$url = 'https://download.sysinternals.com/files/Handle.zip'
+$checksum = 'a4d2ccc743c3bb4d6865d0e5f9127abc1a9c0eea37758c2f7c98c9cf774e2763'
+$checksumType = 'sha256'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+Write-Verbose "Accepting license..."
+$regRoot = 'HKCU:\Software\Sysinternals'
+$regPkg = 'Handle'
+$regPath = Join-Path $regRoot $regPkg
+if (!(Test-Path $regRoot)) {New-Item -Path "$regRoot"}
+if (!(Test-Path $regPath)) {New-Item -Path "$regRoot" -Name "$regPkg"}
+Set-ItemProperty -Path "$regPath" -Name EulaAccepted -Value 1
+if ((Get-ItemProperty -Path "$regPath").EulaAccepted -ne 1) {
+  throw "Failed setting registry value."
+}
