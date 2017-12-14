64871858f711c6e36fe8b32d8a4f87011b87d848
dtgm <dtgm1594@gmail.com>
2017-12-14 09:00:02 -0600

    Updated 1 pkg(s) on 2017-12-14: glasswire
    https://chocolatey.org/packages/glasswire/2.0.78

---
+++ automatic/_output/glasswire/2.0.78/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = '7C6C54D83343591054DAFB03B49C7166'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
