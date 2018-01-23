eb1c33b7ec66976d823c56a7aa3b1cfe626490b2
dtgm <dtgm1594@gmail.com>
2018-01-22 20:34:12 -0600

    Updated 1 pkg(s) on 2018-01-22: electron
    https://chocolatey.org/packages/electron/1.7.11

---
+++ automatic/_output/electron/1.7.11/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'electron'
+$url = 'https://github.com//electron/electron/releases/download/v1.7.11/electron-v1.7.11-win32-ia32.zip'
+$checksum = 'eb2709ef1961d19fd0dbe1f96008391b'
+$checksumType = 'md5'
+$url64 = 'https://github.com//electron/electron/releases/download/v1.7.11/electron-v1.7.11-win32-x64.zip'
+$checksum64 = '63922b1ba0820592a071231506727b8e'
+$checksumType64 = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
