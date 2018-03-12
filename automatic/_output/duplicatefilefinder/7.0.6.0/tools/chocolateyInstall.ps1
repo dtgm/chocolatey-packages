9a34554210d3f3d2044a5701f46745eef60f0813
dtgm <dtgm1594@gmail.com>
2018-03-12 09:00:03 -0500

    Updated 1 pkg(s) on 2018-03-12: duplicatefilefinder
    https://chocolatey.org/packages/duplicatefilefinder/7.0.6.0

---
+++ automatic/_output/duplicatefilefinder/7.0.6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'duplicatefilefinder'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /TASKS=desktopicon'
+$url = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
+$checksum = '73f56bef9f9ec5fed5afa10cacec88dfd4853e128c1fc0c013a5e9c8bb0e03aa'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
