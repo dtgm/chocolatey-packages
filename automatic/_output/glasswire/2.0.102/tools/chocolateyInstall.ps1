2e544658e1905b422745aec61b650711797a2abe
dtgm <dtgm1594@gmail.com>
2018-03-26 15:03:44 -0500

    Updated 3 pkg(s) on 2018-03-26: dolphin,firefox-nightly,glasswire
    https://chocolatey.org/packages/dolphin/5.0.6700
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018032615
    https://chocolatey.org/packages/glasswire/2.0.102

---
+++ automatic/_output/glasswire/2.0.102/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = '3182071124E1EA6B663721E460B23E16'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
