55ff812147c93c5e18fa558524105a42589428e1
dtgm <dtgm1594@gmail.com>
2018-03-27 15:04:53 -0500

    Updated 2 pkg(s) on 2018-03-27: exiftool,firefox-nightly
    https://chocolatey.org/packages/exiftool/10.88
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018032713

---
+++ automatic/_output/exiftool/10.88/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.88.zip'
+$checksum = '253fa50ebbcb31a065e849576fb0d4aa611f06dd'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
