64fddc538391aa4a4bfe80ecae9f2e7f24274ec3
dtgm <dtgm1594@gmail.com>
2018-03-27 03:12:06 -0500

    Updated 2 pkg(s) on 2018-03-26: cdex,firefox-nightly
    https://chocolatey.org/packages/cdex/2.00
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018032700

---
+++ automatic/_output/cdex/2.00/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'cdex'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mirror.cdex.mu/CDex-2.00.exe'
+$checksum = '9777456c028db4a7cd55fffb830f3df45c03563cf1d3a82197c4ecacce42fc08'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = "$scriptPath\chocolateyInstall.ahk"
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
