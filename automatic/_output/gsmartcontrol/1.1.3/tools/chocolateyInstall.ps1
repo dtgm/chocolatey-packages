8e517057bcb2075655bae807da48cd828b09336b
dtgm <dtgm1594@gmail.com>
2018-03-29 14:20:47 -0500

    Fix #342: update and correct auto-up for gsmartcontrol
    https://chocolatey.org/packages/gsmartcontrol/1.1.3

--- automatic/_output/gsmartcontrol/1.1.3/tools/chocolateyInstall.ps1
+++ automatic/_output/gsmartcontrol/1.1.3/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-﻿$packageName = '{{PackageName}}'
+$packageName = 'gsmartcontrol'
 $installerType = 'exe'
 $silentArgs = '/S'
-$urlArray = {{DownloadUrlx64}}
+$urlArray = @('https://sourceforge.net/projects/gsmartcontrol/files/1.1.3/gsmartcontrol-1.1.3-win32.exe/download', 'https://sourceforge.net/projects/gsmartcontrol/files/1.1.3/gsmartcontrol-1.1.3-win64.exe/download')
 $url = $urlArray[0]
-$checksum = '{{Checksum}}'
+$checksum = '29aa1b4b71677e9e01657325bd214402dcd0724cc5a65f4ff30aab487f990792'
 $checksumType = 'sha256'
 $url64 = $urlArray[1]
-$checksum64 = '{{Checksumx64}}'
+$checksum64 = 'f33064b67b7399bea4d106862afe3a6f36edf7200a7b6b1be68a39fed4e39e6d'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
