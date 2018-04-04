0b8a8dbedd056106f39c197333e407d72aa71526
dtgm <dtgm1594@gmail.com>
2018-04-04 00:09:28 -0500

    Fix #384
    https://chocolatey.org/packages/sandboxie/5.24
    https://chocolatey.org/packages/sandboxie.install/5.24

---
+++ automatic/_output/sandboxie.install/5.24/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$packageName = 'sandboxie.install'
+$packageSearch = 'sandboxie'
+$installerType = 'exe'
+$url = 'https://www.sandboxie.com/attic/SandboxieInstall32-524.exe'
+$checksum = '542acefa27cc294f267232a71091d2f8df064777'
+$checksumType = 'sha1'
+$url64 = 'https://www.sandboxie.com/attic/SandboxieInstall64-524.exe'
+$checksum64 = '7fa5497f6afcafec0f3a6271658a766fa7f1490a'
+$checksumType64 = 'sha1'
+$validExitCodes = @(0)
+$silentArgs = '/S'
+$reg = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                           -ErrorAction:SilentlyContinue `
+          | Where-Object   { $_.DisplayName -like "$packageSearch*" }
+if ($reg) { 
+  $silentArgs += ' /upgrade'
+} else {
+  $silentArgs += ' /install'
+}
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/_output/sandboxie.install/5.24/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'sandboxie.install'
+$packageSearch = 'sandboxie'
+$installerType = 'exe'
+$silentArgs = '/S /remove'
+$validExitCodes = @(0)
+
+$reg = Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                  'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                  'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                         -ErrorAction:SilentlyContinue `
+        | Where-Object   { $_.DisplayName -like "$packageSearch*" }
+$unString = $reg.UninstallString
+if ($unString | Select-String -Pattern / ) {
+  $unString = $unString | %{ $_.Split(' /')[0]; }
+}
+Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                            -FileType "$installerType" `
+                            -SilentArgs "$silentArgs" `
+                            -File "$unString" `
+                            -ValidExitCodes $validExitCodes
