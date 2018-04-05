f7b07d0a256c023ce1a6648835abe9e413492202
dtgm <dtgm1594@gmail.com>
2018-04-05 15:00:15 -0500

    Updated 1 pkg(s) on 2018-04-05: audioswitcher
    https://chocolatey.org/packages/audioswitcher/1.8.0.142

---
+++ automatic/_output/audioswitcher/1.8.0.142/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'audioswitcher'
+$url = 'https://github.com/xenolightning/AudioSwitcher_v1/releases/download/1.8.0.142/AudioSwitcher.zip'
+$checksum = '6052e39b343b6cf78881f84f1cbdd54c'
+$checksumType = 'md5'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
