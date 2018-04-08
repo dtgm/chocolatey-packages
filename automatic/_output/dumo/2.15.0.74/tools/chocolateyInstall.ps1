644441f8eecf4a1ad3cc4f8fcae56769b2ce2c07
dtgm <dtgm1594@gmail.com>
2018-04-08 15:01:54 -0500

    Updated 2 pkg(s) on 2018-04-08: dumo,firefox-nightly
    https://chocolatey.org/packages/dumo/2.15.0.74
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018040812

---
+++ automatic/_output/dumo/2.15.0.74/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'dumo'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'http://www.kcsoftwares.com/files/dumo_lite.exe'
+$checksum = '3e9f252196da75f4ace540987fe08ee8d963067ae64545c7af02b6e22f27183b'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
