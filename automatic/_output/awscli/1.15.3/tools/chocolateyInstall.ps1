a675d87255102e04a1f7fcec77793c09bced882b
dtgm <dtgm1594@gmail.com>
2018-04-10 15:08:39 -0500

    Updated 2 pkg(s) on 2018-04-10: awscli,ddu
    https://chocolatey.org/packages/awscli/1.15.3
    https://chocolatey.org/packages/ddu/17.0.8.5

--- automatic/_output/awscli/1.15.3/tools/chocolateyInstall.ps1
+++ automatic/_output/awscli/1.15.3/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$packageName = 'awscli'
+$packageName = 'awscli'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
 $url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.15.3.msi'
-$checksum = '{checksum}'
+$checksum = 'e1626efbe2dd84a476978330a6982a153d26b8ebfb1d0543f820a6c4462ac137'
 $checksumType = 'sha256'
 $url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.15.3.msi'
-$checksum64 = '{checksumx64}'
+$checksum64 = '9195f9ed37506ac97f56c34a4eb9de44005385047ea11119d575cad5ab30b157'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
