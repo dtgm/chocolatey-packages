971a283122806d786edf9cb89db32e66c2b62485
dtgm <dtgm1594@gmail.com>
2018-04-10 03:07:43 -0500

    Updated 3 pkg(s) on 2018-04-10: soundvolumeview,stellar,sumo
    https://chocolatey.org/packages/soundvolumeview/1.80
    https://chocolatey.org/packages/stellar/4.0
    https://chocolatey.org/packages/sumo/5.6.1.390

---
+++ automatic/_output/stellar/4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'stellar'
+$url = 'https://github.com/stellarchat/desktop-client/releases/download/v4.0/StellarDesktopWin32-v4.0.zip'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/stellarchat/desktop-client/releases/download/v4.0/StellarDesktopWin64-v4.0.zip'
+$checksum64 = '{checksumx64}'
+$checksumType64 = 'sha256'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+Write-Verbose "Creating empty sidecar so shimgen.exe creates shim for GUI rather than console"
+$guiExe = Get-ChildItem -Recurse -Path $toolsDir -Include "StellarWallet.exe"
+if ($guiExe) {
+    Set-Content -Path $($guiExe.FullName+".gui") -Value $null
+} else {Write-Warning "Can't find StellarWallet.exe installation directory"}
