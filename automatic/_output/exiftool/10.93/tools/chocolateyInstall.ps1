fd35012336eb3d368dfff42dfad312ecf453c633
dtgm <dtgm1594@gmail.com>
2018-04-14 03:00:08 -0500

    Updated 2 pkg(s) on 2018-04-13: dolphin,exiftool
    https://chocolatey.org/packages/dolphin/5.0.6981
    https://chocolatey.org/packages/exiftool/10.93

--- automatic/_output/exiftool/10.93/tools/chocolateyInstall.ps1
+++ automatic/_output/exiftool/10.93/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$packageName = 'dolphin'
-$url = 'https://dl.dolphin-emu.org/builds/dolphin-master-5.0-6981-x64.7z'
-$checksum = '07bbb3cb1efab3d7010e496709a753ce8bbb86328cedd49aa1afb04228a8b8ec'
-$checksumType = 'sha256'
-$url64 = $url
-$checksum64 = $checksum
-$checksumType64 = $checksumType
+﻿$packageName = 'exiftool'
+$url = 'http://owl.phy.queensu.ca/~phil/exiftool/exiftool-10.93.zip'
+$checksum = '415ec52d1e1648d6634001c77466454b39e05736'
+$checksumType = 'sha1'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
@@ -15,3 +15,5 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -ChecksumType "$checksumType" `
                              -Checksum64 "$checksum64" `
                              -ChecksumType64 "$checksumType64"
+
+Move-Item "$(Join-Path $toolsDir 'exiftool(-k).exe')" "$(Join-Path $toolsDir 'exiftool.exe')" -Force
