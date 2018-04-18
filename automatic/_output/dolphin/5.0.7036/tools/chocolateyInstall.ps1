bb48cd34a56015ec09ec3d2857e86c724c5a541c
dtgm <dtgm1594@gmail.com>
2018-04-17 21:00:03 -0500

    Updated 2 pkg(s) on 2018-04-17: dolphin,firefox-nightly
    https://chocolatey.org/packages/dolphin/5.0.7036
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018041712

--- automatic/_output/dolphin/5.0.7036/tools/chocolateyInstall.ps1
+++ automatic/_output/dolphin/5.0.7036/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = 'dolphin'
+$packageName = 'dolphin'
 $url = 'https://dl.dolphin-emu.org/builds/dolphin-master-5.0-7036-x64.7z'
-$checksum = '{checksum}'
+$checksum = '248d4eee8b85a9d902ec1390ac57c8060c140df917076052a3a001eddf1dfd62'
 $checksumType = 'sha256'
 $url64 = $url
 $checksum64 = $checksum
