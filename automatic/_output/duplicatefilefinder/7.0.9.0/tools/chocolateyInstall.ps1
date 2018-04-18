9eac2aad5380af4c5f2041b03d91018a2b1336c4
dtgm <dtgm1594@gmail.com>
2018-04-18 15:00:50 -0500

    Updated 4 pkg(s) on 2018-04-18: duplicatefilefinder,electrum.install,electrum.portable,electrum
    https://chocolatey.org/packages/duplicatefilefinder/7.0.9.0
    https://chocolatey.org/packages/electrum.install/3.1.3
    https://chocolatey.org/packages/electrum.portable/3.1.3
    https://chocolatey.org/packages/electrum/3.1.3

---
+++ automatic/_output/duplicatefilefinder/7.0.9.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'duplicatefilefinder'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /TASKS=desktopicon'
+$url = 'http://downloads.auslogics.com/en/duplicate-file-finder/duplicate-file-finder-setup.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
