63b0b26a08f3a830098b0d4aad56134d2c1f46ce
dtgm <dtgm1594@gmail.com>
2018-04-21 03:00:26 -0500

    Updated 2 pkg(s) on 2018-04-20: awscli,dolphin
    https://chocolatey.org/packages/awscli/1.15.6
    https://chocolatey.org/packages/dolphin/5.0.7058

--- automatic/_output/awscli/1.15.6/tools/chocolateyInstall.ps1
+++ automatic/_output/awscli/1.15.6/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$packageName = 'awscli'
+$packageName = 'awscli'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
 $url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.15.6.msi'
-$checksum = '{checksum}'
+$checksum = '2361e62647eef546e24b9a0ad4354a1e3b8ad09a2c680fc3d5f5025cefbeda44'
 $checksumType = 'sha256'
 $url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.15.6.msi'
-$checksum64 = '{checksumx64}'
+$checksum64 = 'bae347d4f7b192dfc131689e632ebabc104893f6c539774e204a0b572e2e7bf4'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
