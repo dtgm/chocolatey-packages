40c10cd9bc591dcbc084104c49e09bfb387971e1
dtgm <dtgm1594@gmail.com>
2018-05-03 09:00:03 -0500

    Updated 4 pkg(s) on 2018-05-03: firefox-nightly,git-annex,glasswire,goodsync
    https://chocolatey.org/packages/firefox-nightly/61.0.1.2018050312
    https://chocolatey.org/packages/git-annex/6.20180501
    https://chocolatey.org/packages/glasswire/2.0.112
    https://chocolatey.org/packages/goodsync/10.8.8

---
+++ automatic/_output/glasswire/2.0.112/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'glasswire'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://download.glasswire.com/GlassWireSetup.exe'
+$checksum = 'BA9F98D20DE6B94A9E6DB7AB4548BF78'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
