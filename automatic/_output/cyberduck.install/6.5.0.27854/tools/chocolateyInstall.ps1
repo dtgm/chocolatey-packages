a54431220f828e0888055aa8fbdee03be426d1df
dtgm <dtgm1594@gmail.com>
2018-05-03 03:00:02 -0500

    Updated 3 pkg(s) on 2018-05-03: cyberduck.install,cyberduck,dolphin
    https://chocolatey.org/packages/cyberduck.install/6.5.0.27854
    https://chocolatey.org/packages/cyberduck/6.5.0.27854
    https://chocolatey.org/packages/dolphin/5.0.7178

---
+++ automatic/_output/cyberduck.install/6.5.0.27854/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'cyberduck.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://update.cyberduck.io/windows/Cyberduck-Installer-6.5.0.27854.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
