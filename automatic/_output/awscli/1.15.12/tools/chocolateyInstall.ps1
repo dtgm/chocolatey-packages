9ccb7a9f553f6ee7e67f57536145d421ed4c34c3
dtgm <dtgm1594@gmail.com>
2018-05-03 01:44:09 -0500

    Updated 4 pkg(s) on 2018-05-02: awscli,copay,dar,dolphin
    https://chocolatey.org/packages/awscli/1.15.12
    https://chocolatey.org/packages/copay/4.2.0
    https://chocolatey.org/packages/dar/2.5.15
    https://chocolatey.org/packages/dolphin/5.0.7174

--- automatic/_output/awscli/1.15.12/tools/chocolateyInstall.ps1
+++ automatic/_output/awscli/1.15.12/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$packageName = 'awscli'
+$packageName = 'awscli'
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
 $url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.15.12.msi'
-$checksum = '{checksum}'
+$checksum = '4d72ffff23fe52a6fa852b3f9e6c0c8fee93b53dc0fceb5ac31aba3e3725b912'
 $checksumType = 'sha256'
 $url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.15.12.msi'
-$checksum64 = '{checksumx64}'
+$checksum64 = 'a7c212f691aa0baa9f5b32c50e05e68a1313ae3e561e783d3408692ccc90425f'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
---
+++ automatic/_output/dar/2.5.15/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'dar'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://sourceforge.net/projects/dar/files/dar/2.5.15/dar64-2.5.15-win64.zip/download'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
