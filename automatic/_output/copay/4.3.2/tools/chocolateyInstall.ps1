54baad14a82b94962cc06e8732a3264a631b23b6
dtgm <dtgm1594@gmail.com>
2018-05-07 20:07:36 -0500

    Updated 2 pkg(s) on 2018-05-07: copay,dolphin
    https://chocolatey.org/packages/copay/4.3.2
    https://chocolatey.org/packages/dolphin/5.0.7289

---
+++ automatic/_output/copay/4.3.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'copay'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://github.com/bitpay/copay/releases/download/v4.3.2/BitPay.exe'
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName $packageName `
+                          -FileType $installerType `
+                          -SilentArgs $silentArgs `
+                          -Url $url `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum $checksum `
+                          -ChecksumType $checksumType
