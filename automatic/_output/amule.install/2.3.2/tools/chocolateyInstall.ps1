f3df3b9d2a5e9dee363099cf679645b5455e42d4
dtgm <dtgm1594@gmail.com>
2018-05-10 15:00:04 -0500

    Updated 1 pkg(s) on 2018-05-10: amule.install
    https://chocolatey.org/packages/amule.install/2.3.2

---
+++ automatic/_output/amule.install/2.3.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'amule.install'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = ''
+$checksum = '{checksum}'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
