f715c788dcab5f83839d76adfe1cc067dd248a25
dtgm <dtgm1594@gmail.com>
2018-05-10 09:00:04 -0500

    Updated 1 pkg(s) on 2018-05-10: cdex
    https://chocolatey.org/packages/cdex/2.03

---
+++ automatic/_output/cdex/2.03/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'cdex'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'http://mirror.cdex.mu/CDex-2.03.exe'
+$checksum = 'a6e6608a14041d119d9b4f1cbc6ed1f8aa4f1727dd24281bd6cc26b3ee889cce'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$ahkFile = "$scriptPath\chocolateyInstall.ahk"
+
+Start-Process 'AutoHotKey' $ahkFile
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
