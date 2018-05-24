59d5c8fa04867af027bd95e3a20184734e1a293c
Jansohn Robin FRD FIPE7 <robin.jansohn@zf.com>
2018-05-24 17:36:55 +0200

    #235: workaround for unusable uninstallString from registry

--- automatic/bitvise-ssh-server/tools/chocolateyUninstall.ps1
+++ automatic/bitvise-ssh-server/tools/chocolateyUninstall.ps1
@@ -1,15 +1,29 @@
-﻿$packageName = '{{PackageName}}'
-$softwareName = "Bitvise SSH Server*"
-$installerType = 'exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Bitvise SSH Server*'
+  fileType      = 'exe'
+  silentArgs    = "-unat"
+  validExitCodes= @(0, 1) # unattended uninstall exits with code 1
+}
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+$uninstalled = $false
 
-$key | ForEach-Object {
-  Uninstall-ChocolateyPackage -PackageName "$packageName" `
-                              -FileType "$installerType" `
-                              -SilentArgs "$($silentArgs)" `
-                              -File "& $($_.UninstallString.Replace('"',''))" `
-                              -ValidExitCodes $validExitCodes
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.DisplayIcon)" # contains path to uninst.exe
+    # uninst.exe needs product name as an additional parameter for uninstallation
+    $packageArgs['silentArgs'] = "`"$($_.ProductName)`" $($packageArgs['silentArgs'])"
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
 }
