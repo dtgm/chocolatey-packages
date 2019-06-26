a94853cf04f82c723c951f114067e8eb2cc03d21
Arkkis <j@arkkis.net>
2019-06-27 01:00:20 +0300

    Added Spybot 2.7.64.0

---
+++ automatic/_output/spybot/2.7.64.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'spybot'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://updates2.safer-networking.org/spybot1/spybotsd-2.7.64.0.exe'
+$checksum = 'F8790425C1CEEDEAAAC4F6EE336CF1BE4180F34D'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
