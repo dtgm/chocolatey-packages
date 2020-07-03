01831ea40895155a43f01a7f6c3428ae1ccd0610
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-07-03 20:38:43 +0200

    Maintained by tunisiano

--- automatic/freeplane/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
-$unString = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName"}).UninstallString
-Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes "$validExitCodes"
