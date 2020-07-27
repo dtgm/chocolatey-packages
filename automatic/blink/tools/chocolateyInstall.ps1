65d5f368dd14a8a3a2387ee1f4471f8105050c75
Tunisiano187 <fabian@bowlman.org>
2020-07-27 18:08:44 +0200

    Now maintained by tunisiano

--- automatic/blink/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'	
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'md5'
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
