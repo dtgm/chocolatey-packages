55ad0e8240e7a3f204b910661f8f80032d8440b1
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-07-31 21:23:14 +0200

    dd Now maintained by Tunisiano (#526)
    * now maintained by tunisiano
    
    * Now maintained by tunisiano
    
    * Now maintained by tunisiano

--- automatic/dd/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -Url64bit "" -Checksum "$checksum" -ChecksumType "$checksumType"
