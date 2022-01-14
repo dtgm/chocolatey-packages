72c6985b691ecdb4d32f6aee7c88b979e505cc08
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2022-01-15 01:06:36 +0900

    Revert "Remove exiftoolgui pkg"
    This reverts commit f7c0f7dc7cb5ee93e696694c83cc5bd690643ee8.

---
+++ automatic/exiftoolgui/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$url64 = "$url"
+$checksum64 = "$checksum"
+$checksumType64 = "$checksumType"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Url64bit "$url64" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -Checksum64 "$checksum64" `
+                             -ChecksumType64 "$checksumType64"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+$installFile = Join-Path -Path $toolsDir `
+                         -ChildPath "$packageName" |
+               Join-Path -ChildPath "$($packageName).exe.gui"
+
+Set-Content -Path "$installFile" `
+            -Value $null
