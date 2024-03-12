908e8a71ae747d47acae75eabf3eb5311554064c
Tunisiano18 <tunisiano187@users.noreply.github.com>
2024-03-12 15:28:21 +0100

    Jacksum now maintained by tunisiano

--- automatic/jacksum/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$chocoBinPath = Join-Path "$Env:ChocolateyInstall" 'bin'
-$batName = "$packageName.bat"	
-Remove-Item (Join-Path "$chocoBinPath" "$batName") -Force
