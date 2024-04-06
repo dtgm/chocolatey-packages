20825577c9d711634172e5167934c8eb6d67c95a
Tunisiano18 <tunisiano187@users.noreply.github.com>
2024-04-06 19:11:00 +0000

    (duplicatecleaner) Now maintained by tunisiano

--- automatic/duplicatecleaner/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'	
-$silentArgs = '/S'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'md5'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
